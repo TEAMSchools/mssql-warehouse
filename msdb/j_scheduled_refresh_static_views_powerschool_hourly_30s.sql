0b3db85e577f5b929b51bcbc77055b5c0ad2d42f
Charlie Bini <cbini87@gmail.com>
2017-08-28 00:06:42 -0400

    upd8

--- msdb/j_scheduled_refresh_static_views_powerschool_hourly_30s.sql
+++ msdb/j_scheduled_refresh_static_views_powerschool_hourly_30s.sql
@@ -1,11 +1,11 @@
 USE [msdb]
 GO
 
-/****** Object:  Job [Scheduled | Refresh Static Views - PowerSchool (Hourly - 45's)]    Script Date: 8/25/2017 10:06:18 AM ******/
+/****** Object:  Job [Scheduled | Refresh Static Views - PowerSchool (Hourly - 30's)]    Script Date: 8/28/2017 12:05:42 AM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 8/25/2017 10:06:18 AM ******/
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 8/28/2017 12:05:42 AM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
@@ -14,7 +14,7 @@ IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 END
 
 DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Scheduled | Refresh Static Views - PowerSchool (Hourly - 45''s)', 
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Scheduled | Refresh Static Views - PowerSchool (Hourly - 30''s)', 
 		@enabled=1, 
 		@notify_level_eventlog=0, 
 		@notify_level_email=2, 
@@ -26,9 +26,54 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Scheduled | Refresh Static V
 		@owner_login_name=N'TEAMSCHOOLS\CBini', 
 		@notify_email_operator_name=N'Data Robot', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'ps_adaadm_daily_ctod';]    Script Date: 8/25/2017 10:06:19 AM ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'', ''ps_adaadm_daily_ctod'';', 
+/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool','ps_enrollment_all';]    Script Date: 8/28/2017 12:05:43 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'',''ps_enrollment_all'';', 
 		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=3, 
+		@on_fail_step_id=0, 
+		@retry_attempts=1, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'EXEC gabby.utilities.cache_view ''powerschool'',''ps_enrollment_all'';', 
+		@database_name=N'gabby', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool','ps_attendance_daily';]    Script Date: 8/28/2017 12:05:43 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'',''ps_attendance_daily'';', 
+		@step_id=2, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=3, 
+		@on_fail_step_id=0, 
+		@retry_attempts=1, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'EXEC gabby.utilities.cache_view ''powerschool'',''ps_attendance_daily'';', 
+		@database_name=N'gabby', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool','ps_membership_reg';]    Script Date: 8/28/2017 12:05:43 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'',''ps_membership_reg'';', 
+		@step_id=3, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=3, 
+		@on_fail_step_id=0, 
+		@retry_attempts=1, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'EXEC gabby.utilities.cache_view ''powerschool'',''ps_membership_reg'';', 
+		@database_name=N'gabby', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'ps_adaadm_daily_ctod';]    Script Date: 8/28/2017 12:05:43 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'', ''ps_adaadm_daily_ctod'';', 
+		@step_id=4, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -41,9 +86,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gab
 		@database_name=N'gabby', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'course_enrollments';]    Script Date: 8/25/2017 10:06:19 AM ******/
+/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'course_enrollments';]    Script Date: 8/28/2017 12:05:43 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'', ''course_enrollments'';', 
-		@step_id=2, 
+		@step_id=5, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -56,9 +101,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gab
 		@database_name=N'gabby', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'gradescaleitem_lookup';]    Script Date: 8/25/2017 10:06:19 AM ******/
+/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'gradescaleitem_lookup';]    Script Date: 8/28/2017 12:05:43 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'', ''gradescaleitem_lookup'';', 
-		@step_id=3, 
+		@step_id=6, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -71,9 +116,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gab
 		@database_name=N'gabby', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'cohort_identifiers';]    Script Date: 8/25/2017 10:06:19 AM ******/
+/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'cohort_identifiers';]    Script Date: 8/28/2017 12:05:43 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'', ''cohort_identifiers'';', 
-		@step_id=4, 
+		@step_id=7, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -86,9 +131,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gab
 		@database_name=N'gabby', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'course_section_scaffold';]    Script Date: 8/25/2017 10:06:19 AM ******/
+/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'course_section_scaffold';]    Script Date: 8/28/2017 12:05:43 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'', ''course_section_scaffold'';', 
-		@step_id=5, 
+		@step_id=8, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -101,9 +146,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gab
 		@database_name=N'gabby', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'final_grades';]    Script Date: 8/25/2017 10:06:19 AM ******/
+/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'final_grades';]    Script Date: 8/28/2017 12:05:43 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'', ''final_grades'';', 
-		@step_id=6, 
+		@step_id=9, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -116,9 +161,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gab
 		@database_name=N'gabby', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'category_grades';]    Script Date: 8/25/2017 10:06:19 AM ******/
+/****** Object:  Step [EXEC gabby.utilities.cache_view 'powerschool', 'category_grades';]    Script Date: 8/28/2017 12:05:43 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gabby.utilities.cache_view ''powerschool'', ''category_grades'';', 
-		@step_id=7, 
+		@step_id=10, 
 		@cmdexec_success_code=0, 
 		@on_success_action=1, 
 		@on_success_step_id=0, 
@@ -133,7 +178,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'EXEC gab
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Hourly - 45''s', 
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Hourly - 30''s', 
 		@enabled=1, 
 		@freq_type=4, 
 		@freq_interval=1, 
@@ -141,11 +186,11 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Hourly - 
 		@freq_subday_interval=1, 
 		@freq_relative_interval=0, 
 		@freq_recurrence_factor=0, 
-		@active_start_date=20170824, 
+		@active_start_date=20170720, 
 		@active_end_date=99991231, 
-		@active_start_time=4500, 
-		@active_end_time=234600, 
-		@schedule_uid=N'9d67b85e-f50d-4f32-bd03-a773db42daa6'
+		@active_start_time=3000, 
+		@active_end_time=2959, 
+		@schedule_uid=N'd1366ace-77eb-4acd-b06f-98fae3c7beb7'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
