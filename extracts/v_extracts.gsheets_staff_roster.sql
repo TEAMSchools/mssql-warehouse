503c106bd7e836382495c6c8d8a71c5fee08985e
Charlie Bini <cbini87@gmail.com>
2017-08-29 16:44:12 -0400

    add columns

--- extracts/v_extracts.gsheets_staff_roster.sql
+++ extracts/v_extracts.gsheets_staff_roster.sql
@@ -8,9 +8,13 @@ SELECT adp.associate_id
       ,adp.preferred_last AS preferred_last_name
       ,CONCAT(adp.preferred_last, ', ', adp.preferred_first) AS preferred_lastfirst
       ,adp.location_description AS location
+      ,adp.location_custom
       ,adp.home_department_description AS department
+      ,adp.subject_dept_custom
       ,adp.job_title_description AS job_title 
+      ,adp.job_title_custom
       ,adp.reports_to_name AS reports_to
+      ,adp.manager_custom_assoc_id
        
       ,dir.mail AS email_addr 
 FROM gabby.adp.staff_roster adp
