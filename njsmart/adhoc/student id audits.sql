47ab670fb9af87ffe78eebfa0eb2412780e7256e
Charlie Bini <cbini87@gmail.com>
2017-08-29 16:43:57 -0400

    audit for student ids on state files

---
+++ njsmart/adhoc/student id audits.sql
@@ -0,0 +1,204 @@
+WITH raw_files AS (
+  SELECT _file
+        ,_line
+        ,state_student_id
+        ,local_student_id
+        ,first_name
+        ,last_name
+        ,'gabby.njsmart.njask_archive' AS table_name
+  FROM gabby.njsmart.njask_archive
+
+  UNION ALL
+
+  SELECT _file
+        ,_line
+        ,state_student_id
+        ,local_student_id
+        ,first_name
+        ,last_name
+        ,'gabby.njsmart.hspa' AS table_name
+  FROM gabby.njsmart.hspa
+
+  UNION ALL
+
+  SELECT _file
+        ,_line
+        ,state_student_id
+        ,CONVERT(NVARCHAR,local_student_id)
+        ,first_name
+        ,last_name
+        ,'gabby.njsmart.gepa' AS table_name
+  FROM gabby.njsmart.gepa
+
+  UNION ALL
+
+  SELECT _file
+        ,_line
+        ,state_student_identifier
+        ,local_student_identifier 
+        ,first_name
+        ,COALESCE(last_name, last_or_surname) AS last_name      
+        ,'gabby.parcc.summative_record_file' AS table_name
+  FROM gabby.parcc.summative_record_file
+ )
+
+,that AS (
+  SELECT *
+        ,CASE WHEN MAX(rn_dupe) OVER(PARTITION BY _file, _line) > 1 THEN 1 ELSE 0 END AS is_dupe
+  FROM
+      (
+       SELECT r._file
+             ,r._line
+             ,r.state_student_id
+             ,r.local_student_id
+             ,r.first_name
+             ,r.last_name
+             ,r.table_name
+
+             ,sid.student_number AS sid_student_number
+             
+             ,sn.state_studentnumber AS sn_state_studentnumber
+             ,sn.lastfirst AS sn_lastfirst
+      
+             ,ROW_NUMBER() OVER(
+                PARTITION BY r._file, r._line
+                  ORDER BY sid._line DESC, sn._line DESC) AS rn_dupe
+       FROM raw_files r
+       LEFT OUTER JOIN gabby.powerschool.students sid
+         ON r.state_student_id = sid.state_studentnumber
+       LEFT OUTER JOIN gabby.powerschool.students sn
+         ON r.local_student_id = CONVERT(NVARCHAR,sn.student_number)
+      ) sub
+  WHERE rn_dupe = 1
+    AND state_student_id IS NULL
+          OR local_student_id IS NULL
+          OR sid_student_number IS NULL
+          OR sn_state_studentnumber IS NULL     
+ )
+
+/* invalid SN
+SELECT *
+      ,CASE
+        WHEN local_student_id LIKE '7325%' THEN CONCAT('UPDATE ', table_name, ' SET local_student_id = NULL WHERE _file = ''', _file, ''' AND _line = ', _line, ';') 
+        WHEN local_student_id LIKE '%.0%' THEN CONCAT('UPDATE ', table_name, ' SET local_student_id = ', REPLACE(local_student_id,'.0',''), ' WHERE _file = ''', _file, ''' AND _line = ', _line, ';') 
+       END AS update_statement
+FROM raw_files
+WHERE local_student_id LIKE '7325%'
+   OR local_student_id LIKE '%.0%'
+--*/
+
+/* missing SID
+SELECT *
+      ,CONCAT('UPDATE ', table_name, ' SET state_student_id = ', sn_state_studentnumber, ' WHERE _file = ''', _file, ''' AND _line = ', _line, ';') AS update_statement
+FROM that
+WHERE is_dupe = 0
+  AND state_student_id IS NULL
+  AND sn_state_studentnumber IS NOT NULL
+ORDER BY _file, _line
+--*/
+
+/* missing SN
+SELECT *
+      ,CONCAT('UPDATE ', table_name, ' SET local_student_id = ', sid_student_number, ' WHERE _file = ''', _file, ''' AND _line = ', _line, ';') AS update_statement
+FROM that
+WHERE is_dupe = 0
+  AND local_student_id IS NULL
+  AND sid_student_number IS NOT NULL
+--*/
+
+/* mismatch SID
+SELECT *
+      ,CONCAT('UPDATE ', table_name, ' SET state_student_id = ', sn_state_studentnumber, ' WHERE _file = ''', _file, ''' AND _line = ', _line, ';') AS update_statement
+FROM that
+WHERE state_student_id IS NOT NULL
+  AND sid_student_number IS NULL
+  AND sn_state_studentnumber IS NOT NULL
+  AND is_dupe = 0
+--*/
+
+/* mismatched SN
+SELECT *
+      ,CONCAT('UPDATE ', table_name, ' SET local_student_id = ', sid_student_number, ' WHERE _file = ''', _file, ''' AND _line = ', _line, ';') AS update_statement
+FROM that
+WHERE is_dupe = 0
+  AND local_student_id IS NOT NULL
+  AND sn_state_studentnumber IS NULL
+  AND sid_student_number IS NOT NULL
+--*/
+
+/* SN matching on name
+SELECT *
+      ,CONCAT('UPDATE ', table_name, ' SET local_student_id = ', ps_student_number, ' WHERE _file = ''', _file, ''' AND _line = ', _line, ';') AS update_statement
+FROM
+    (
+     SELECT *
+           ,MAX(rn) OVER(PARTITION BY _file, _line) AS max_rn
+     FROM
+         (
+          SELECT t.*
+      
+                ,s.student_number AS ps_student_number
+                ,s.state_studentnumber AS ps_state_studentnumber
+                ,s.lastfirst AS ps_lastfirst 
+
+                ,ROW_NUMBER() OVER(
+                   PARTITION BY t._file, t._line
+                     ORDER BY s._line DESC) AS rn
+          FROM that t
+          JOIN gabby.powerschool.students s
+            ON CHARINDEX(LTRIM(RTRIM(REPLACE(t.last_name,' ',''))), gabby.utilities.STRIP_CHARACTERS(REPLACE(s.last_name,' ',''),'^A-Z')) > 0
+           AND CHARINDEX(LTRIM(RTRIM(REPLACE(t.first_name, ' ', ''))), gabby.utilities.STRIP_CHARACTERS(REPLACE(s.first_name,' ',''),'^A-Z')) > 0
+          WHERE t.local_student_id IS NULL
+          ) sub          
+    ) sub
+--WHERE max_rn = 1
+--*/
+
+/* SID matching on name
+SELECT *
+      ,CONCAT('UPDATE ', table_name, ' SET state_student_id = ', ps_state_studentnumber, ' WHERE _file = ''', _file, ''' AND _line = ', _line, ';') AS update_statement
+FROM
+    (
+     SELECT *
+           ,MAX(rn) OVER(PARTITION BY _file, _line) AS max_rn
+     FROM
+         (
+          SELECT t.*
+      
+                ,s.student_number AS ps_student_number
+                ,s.state_studentnumber AS ps_state_studentnumber
+                ,s.lastfirst AS ps_lastfirst 
+
+                ,ROW_NUMBER() OVER(
+                   PARTITION BY t._file, t._line
+                     ORDER BY s._line DESC) AS rn
+          FROM that t
+          JOIN gabby.powerschool.students s
+            ON CHARINDEX(LTRIM(RTRIM(REPLACE(t.last_name,' ',''))), gabby.utilities.STRIP_CHARACTERS(REPLACE(s.last_name,' ',''),'^A-Z')) > 0
+           AND CHARINDEX(LTRIM(RTRIM(REPLACE(t.first_name, ' ', ''))), gabby.utilities.STRIP_CHARACTERS(REPLACE(s.first_name,' ',''),'^A-Z')) > 0
+          WHERE t.state_student_id IS NULL
+            AND s.state_studentnumber IS NOT NULL
+          ) sub          
+    ) sub
+WHERE max_rn = 1  
+--*/
+
+--SELECT *
+--      ,CONCAT('UPDATE ', table_name, ' SET local_student_id = ', sid_student_number, ' WHERE _file = ''', _file, ''' AND _line = ', _line, '; /*', first_name, ' ', last_name,'*/') AS update_statement
+--FROM that
+--WHERE local_student_id IS NULL
+--ORDER BY _file, _line
+
+--SELECT n.state_student_id
+--      ,n.local_student_id
+--      ,n.first_name
+--      ,n.last_name
+      
+--      ,s.state_studentnumber
+--      ,s.lastfirst
+      
+--      ,CONCAT('UPDATE gabby.njsmart.njask_archive SET local_student_id =  WHERE _file = ''', n._file, ''' AND _line = ', n._line, '; /*', n.first_name, ' ', n.last_name,'*/') AS update_statement
+--FROM gabby.njsmart.njask_archive n
+--LEFT OUTER JOIN gabby.powerschool.students s
+--  ON n.local_student_id = s.student_number
+--WHERE s.student_number IS NULL
