5c81317978804d3dd1e641659f028c286c9b4ffb
Charlie Bini <cbini87@gmail.com>
2017-08-31 17:22:03 -0400

    final deanslist extract

--- extracts/v_extracts.deanslist_state_test_scores.sql
+++ extracts/v_extracts.deanslist_state_test_scores.sql
@@ -1,68 +1,64 @@
-USE KIPP_NJ
+USE gabby
 GO
 
-ALTER VIEW DL$state_test_scores#extract AS 
+ALTER VIEW extracts.deanslist_state_test_scores AS
 
-WITH unioned AS (
-  SELECT co.student_number
-        ,co.year AS academic_year
-        ,'PARCC' AS test_type      
-        ,CASE WHEN parcc.subject = 'English Language Arts/Literacy' THEN 'ELA' ELSE 'Math' END AS subject
-        ,parcc.subject AS test_name
-        ,parcc.summativescalescore AS scale_score
-        ,CASE
-          WHEN parcc.summativeperformancelevel = 5 THEN 'Exceeded Expectations'
-          WHEN parcc.summativeperformancelevel = 4 THEN 'Met Expectations'
-          WHEN parcc.summativeperformancelevel = 3 THEN 'Approached Expectations'
-          WHEN parcc.summativeperformancelevel = 2 THEN 'Partially Met Expectations'
-          WHEN parcc.summativeperformancelevel = 1 THEN 'Did Not Yet Meet Expectations'
-         END AS proficiency_level
-        ,CASE
-          WHEN parcc.summativeperformancelevel >= 4 THEN 1
-          WHEN parcc.summativeperformancelevel < 4 THEN 0
-          ELSE NULL
-         END AS is_proficient    
-  FROM KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK)
-  JOIN KIPP_NJ..PARCC$district_summative_record_file parcc WITH(NOLOCK)
-    ON co.SID = parcc.statestudentidentifier
-   AND co.year = LEFT(parcc.assessmentYear,4)
-   --AND parcc.recordtype = 1 
-   --AND parcc.reportedsummativescoreflag = 'Y'
-   --AND parcc.multiplerecordflag IS NULL
-   --AND parcc.reportsuppressioncode IS NULL
-  WHERE co.year >= 2014  
-    AND co.rn = 1
-
-  UNION ALL
-
-  SELECT co.student_number      
-        ,co.year AS academic_year
-        ,CASE
-          WHEN co.schoolid = 73253 THEN 'HSPA'
-          ELSE 'NJASK' 
-         END AS test_type      
-        ,nj.subject
-        ,nj.subject AS test_name
-        ,nj.scale_score
-        ,nj.prof_level AS proficiency_level
-        ,nj.is_prof AS is_proficient
-  FROM KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK)
-  JOIN KIPP_NJ..AUTOLOAD$GDOCS_STATE_njask_hspa_scores nj WITH(NOLOCK)
-    ON co.student_number = nj.student_number
-   AND co.year = nj.academic_year 
-   AND nj.void_reason IS NULL
-  WHERE co.rn = 1
- )
-
-SELECT student_number
-      ,CONCAT(academic_year, '-', (academic_year + 1)) AS academic_year
+SELECT student_number      
       ,test_type
       ,subject
       ,test_name
       ,scale_score
       ,proficiency_level
-      ,is_proficient
+      ,is_proficient      
+      ,CONCAT(academic_year, '-', (academic_year + 1)) AS academic_year
+
       ,ROW_NUMBER() OVER(
          PARTITION BY student_number, subject
            ORDER BY academic_year) AS test_index
-FROM unioned
+FROM
+    (
+     SELECT co.student_number
+           ,co.academic_year
+           ,'PARCC' AS test_type      
+        
+           ,CASE WHEN parcc.subject = 'English Language Arts/Literacy' THEN 'ELA' ELSE 'Math' END AS subject
+           ,parcc.subject AS test_name
+           ,parcc.test_scale_score AS scale_score
+           ,CASE
+             WHEN parcc.test_performance_level = 5 THEN 'Exceeded Expectations'
+             WHEN parcc.test_performance_level = 4 THEN 'Met Expectations'
+             WHEN parcc.test_performance_level = 3 THEN 'Approached Expectations'
+             WHEN parcc.test_performance_level = 2 THEN 'Partially Met Expectations'
+             WHEN parcc.test_performance_level = 1 THEN 'Did Not Yet Meet Expectations'
+            END AS proficiency_level
+           ,CASE
+             WHEN parcc.test_performance_level >= 4 THEN 1
+             WHEN parcc.test_performance_level < 4 THEN 0
+            END AS is_proficient    
+     FROM gabby.powerschool.cohort_identifiers_static co 
+     JOIN gabby.parcc.summative_record_file_clean parcc
+       ON co.state_studentnumber = parcc.state_student_identifier
+      AND co.academic_year = LEFT(parcc.assessment_year,4)   
+     WHERE co.academic_year >= 2014  
+       AND co.rn_year = 1
+
+     UNION ALL
+
+     SELECT co.student_number      
+           ,co.academic_year
+        
+           ,nj.test_type
+           ,nj.subject
+           ,nj.subject AS test_name
+           ,nj.scaled_score AS scale_score
+           ,nj.performance_level AS proficiency_level
+           ,CASE
+             WHEN nj.scaled_score >= 200 THEN 1
+             WHEN nj.scaled_score < 200 THEN 0
+            END AS is_proficient
+     FROM gabby.powerschool.cohort_identifiers_static co
+     JOIN gabby.njsmart.all_state_assessments nj 
+       ON co.student_number = nj.local_student_id
+      AND co.academic_year = nj.academic_year 
+     WHERE co.rn_year = 1
+    ) sub
