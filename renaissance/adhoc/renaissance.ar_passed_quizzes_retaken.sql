4afbb3a0c2ebb14dcc68387c76f0c05890afbcda
Charlie Bini <cbini87@gmail.com>
2017-09-05 16:36:01 -0400

    query to find previously passed assessments

---
+++ renaissance/adhoc/renaissance.ar_passed_quizzes_retaken.sql
@@ -0,0 +1,17 @@
+SELECT ar.student_number
+      ,ar.academic_year
+      ,ar.vch_content_title
+      ,ar.i_lexile
+      ,ar.i_word_count
+      ,ar.dt_taken
+      ,ar.rn_quiz
+
+      ,s.lastfirst
+      ,s.schoolid
+      ,s.grade_level
+FROM gabby.renaissance.ar_studentpractice_identifiers_static ar
+JOIN gabby.powerschool.students s
+  ON ar.student_number = s.student_number
+WHERE ar.rn_quiz > 1
+  AND ar.ti_passed = 1
+  AND ar.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1
