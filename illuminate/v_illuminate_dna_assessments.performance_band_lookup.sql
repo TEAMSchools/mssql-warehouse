b34b02d25346969e1477a3811e90115bab818ab2
Charlie Bini <cbini87@gmail.com>
2017-09-06 17:35:53 -0400

    de-duped

--- illuminate/v_illuminate_dna_assessments.performance_band_lookup.sql
+++ illuminate/v_illuminate_dna_assessments.performance_band_lookup.sql
@@ -3,12 +3,28 @@ GO
 
 ALTER VIEW illuminate_dna_assessments.performance_band_lookup AS 
 
-SELECT pbs.performance_band_set_id
-      ,pbs.description
-      ,pb.minimum_value
-      ,LEAD(pb.minimum_value, 1, 1000) OVER(PARTITION BY pbs.performance_band_set_id ORDER BY pb.label_number) - 1 AS maximum_value
-      ,pb.label_number
-      ,pb.is_mastery
-FROM gabby.illuminate_dna_assessments.performance_band_sets pbs
-JOIN gabby.illuminate_dna_assessments.performance_bands pb
-  ON pbs.performance_band_set_id = pb.performance_band_set_id
+SELECT performance_band_set_id
+      ,description
+      ,minimum_value
+      ,maximum_value
+      ,label_number
+      ,is_mastery
+FROM
+    (
+     SELECT pbs.performance_band_set_id
+           ,pbs.description
+      
+           ,pb.minimum_value
+           ,LEAD(pb.minimum_value, 1, 1000) OVER(PARTITION BY pbs.performance_band_set_id ORDER BY pb.label_number) - 1 AS maximum_value
+           ,pb.label_number
+           ,pb.is_mastery
+
+           ,ROW_NUMBER() OVER(
+              PARTITION BY pbs.performance_band_set_id, pb.label_number
+                ORDER BY pb._fivetran_synced DESC) AS rn
+     FROM gabby.illuminate_dna_assessments.performance_band_sets pbs
+     JOIN gabby.illuminate_dna_assessments.performance_bands pb
+       ON pbs.performance_band_set_id = pb.performance_band_set_id
+     WHERE pbs.deleted_at IS NULL
+    ) sub
+WHERE rn = 1
