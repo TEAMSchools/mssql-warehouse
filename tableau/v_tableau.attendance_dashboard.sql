d048399e1798c0a4ceaff2db6d8659301cabfc54
Charlie Bini <cbini87@gmail.com>
2017-09-11 18:21:29 -0400

    resizing columns for archive table

--- tableau/v_tableau.attendance_dashboard.sql
+++ tableau/v_tableau.attendance_dashboard.sql
@@ -7,29 +7,23 @@ SELECT co.academic_year
       ,co.reporting_schoolid AS schoolid
       ,co.studentid
       ,co.student_number
-      ,co.lastfirst
+      ,CONVERT(NVARCHAR(256),co.lastfirst) AS lastfirst
       ,co.grade_level
       ,co.school_level
-      ,co.team
+      ,CONVERT(NVARCHAR(32),co.team) AS team
       ,co.enroll_status
-      ,co.iep_status
+      ,CONVERT(NVARCHAR(64),co.iep_status) AS iep_status
       ,co.gender
-      ,co.ethnicity
-      
-      ,CONVERT(NVARCHAR,dt.alt_name) AS term
+      ,CONVERT(NVARCHAR(16),co.ethnicity) AS ethnicity            
 
       ,mem.calendardate
       ,mem.membershipvalue
       ,mem.attendancevalue AS is_present
       ,ABS(mem.attendancevalue - 1) AS is_absent
-      
-      ,att.att_code
+
+      ,CONVERT(NVARCHAR(16),att.att_code) AS att_code
       ,CASE WHEN att.att_code IN ('T','T10','ET','TE') THEN 1 ELSE 0 END AS is_tardy
       ,CASE WHEN att.att_code IN ('OSS','ISS') THEN 1 ELSE 0 END AS suspension_all
-
-      ,enr.section_number
-      ,enr.teacher_name
-
       ,CASE WHEN att.att_code = 'A' THEN 1 ELSE 0 END AS n_A
       ,CASE WHEN att.att_code = 'AD' THEN 1 ELSE 0 END AS n_AD
       ,CASE WHEN att.att_code = 'AE' THEN 1 ELSE 0 END AS n_AE
@@ -56,6 +50,11 @@ SELECT co.academic_year
       ,CASE WHEN att.att_code = 'TLE' THEN 1 ELSE 0 END AS n_TLE
       ,CASE WHEN att.att_code = 'U' THEN 1 ELSE 0 END AS n_U
       ,CASE WHEN att.att_code = 'X' THEN 1 ELSE 0 END AS n_X
+
+      ,CONVERT(NVARCHAR(32),dt.alt_name) AS term
+
+      ,CONVERT(NVARCHAR(64),enr.section_number) AS section_number
+      ,CONVERT(NVARCHAR(256),enr.teacher_name) AS teacher_name
       
       ,MAX(CASE WHEN att.att_code = 'OS' THEN 1 ELSE 0 END 
             + CASE WHEN att.att_code = 'OSS' THEN 1 ELSE 0 END 
@@ -84,13 +83,14 @@ JOIN gabby.powerschool.ps_adaadm_daily_ctod_static mem
 LEFT OUTER JOIN gabby.powerschool.ps_attendance_daily_static att
   ON co.studentid = att.studentid
  AND mem.calendardate = att.att_date
-LEFT OUTER JOIN gabby.reporting.reporting_terms dt WITH(NOLOCK) 
+LEFT OUTER JOIN gabby.reporting.reporting_terms dt 
   ON co.schoolid = dt.schoolid
  AND co.academic_year = dt.academic_year
  AND mem.calendardate BETWEEN CONVERT(DATE,dt.start_date) AND CONVERT(DATE,dt.end_date)
- AND dt.identifier = 'RT'
+ AND CONVERT(NVARCHAR(16),dt.identifier) = 'RT'
 LEFT OUTER JOIN gabby.powerschool.course_enrollments_static enr
   ON co.studentid = enr.studentid
  AND co.academic_year = enr.academic_year 
- AND enr.course_number = 'HR' 
+ AND CONVERT(NVARCHAR(16),enr.course_number) = 'HR' 
  AND enr.rn_course_yr = 1
+--WHERE co.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
