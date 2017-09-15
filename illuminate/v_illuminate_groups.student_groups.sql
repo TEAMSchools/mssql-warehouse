41a240e12d6eb41fbf65949cfdabbe13a8e2175b
Charlie Bini <cbini87@gmail.com>
2017-09-15 16:34:55 -0400

    add year filter

--- illuminate/v_illuminate_groups.student_groups.sql
+++ illuminate/v_illuminate_groups.student_groups.sql
@@ -11,11 +11,10 @@ SELECT s.local_student_id
       ,aff.start_date
       ,aff.end_date
       ,aff.eligibility_start_date
-      ,aff.eligibility_end_date  
-      
-      ,gabby.utilities.DATE_TO_SY(start_date) AS academic_year           
+      ,aff.eligibility_end_date
 FROM gabby.illuminate_groups.groups g
 JOIN gabby.illuminate_groups.group_student_aff aff
   ON g.group_id = aff.group_id
 JOIN gabby.illuminate_public.students s
   ON aff.student_id = s.student_id
+WHERE start_date >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1)
