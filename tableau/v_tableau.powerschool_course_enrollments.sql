e5659ec5b50a04f2ad92903c7854341c467b1378
Charlie Bini <cbini87@gmail.com>
2017-09-15 16:34:40 -0400

    course enrollments feed for tableau filters

---
+++ tableau/v_tableau.powerschool_course_enrollments.sql
@@ -0,0 +1,15 @@
+USE gabby
+GO
+
+ALTER VIEW tableau.powerschool_course_enrollments AS
+
+SELECT student_number
+      ,schoolid      
+      ,course_name
+      ,section_number
+      ,expression
+      ,teacher_name
+FROM gabby.powerschool.course_enrollments_static
+WHERE academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
+  AND course_enroll_status = 0
+  AND section_enroll_status = 0
