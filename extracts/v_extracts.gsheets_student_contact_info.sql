232dd13678780e7ad3bd874b38a8fca9f82ae62c
Charlie Bini <cbini87@gmail.com>
2017-09-18 17:50:13 -0400

    adding more fields

--- extracts/v_extracts.gsheets_student_contact_info.sql
+++ extracts/v_extracts.gsheets_student_contact_info.sql
@@ -11,34 +11,36 @@ SELECT co.student_number
       ,co.school_name
       ,CASE WHEN co.grade_level = 0 THEN 'K' ELSE CONVERT(NVARCHAR,co.grade_level) END AS grade_level      
       ,co.team
-      ,co.advisor_name
+      ,co.advisor_name      
+      ,co.entrydate
+      ,co.boy_status
+      ,CONVERT(NVARCHAR,co.dob) AS dob
+      ,co.gender                        
+      ,co.lunchstatus
+      ,CONVERT(MONEY,co.lunch_balance) AS lunch_balance
       ,co.home_phone
       ,co.mother_cell
       ,co.father_cell
       ,co.mother
       ,co.father      
-      ,co.gender      
-      ,co.street
-      ,co.city      
-      ,co.state
-      ,CONCAT('''', co.zip) AS zip
-      ,co.guardianemail      
-      ,CONVERT(MONEY,co.lunch_balance) AS lunch_balance
-      ,CONVERT(NVARCHAR,co.dob) AS dob
-
-      ,aa.student_web_id
-      ,aa.student_web_password
-      ,aa.web_id AS family_web_id
-      ,aa.web_password AS family_web_password      
-
       ,CONCAT(co.release_1_name, ' | ', co.release_1_phone) AS release_1
       ,CONCAT(co.release_2_name, ' | ', co.release_2_phone) AS release_2
       ,CONCAT(co.release_3_name, ' | ', co.release_3_phone) AS release_3
       ,CONCAT(co.release_4_name, ' | ', co.release_4_phone) AS release_4
       ,CONCAT(co.release_5_name, ' | ', co.release_5_phone) AS release_5
+      ,co.guardianemail      
+      ,co.street
+      ,co.city      
+      ,co.state
+      ,CONCAT('''', co.zip) AS zip      
+
+      ,aa.student_web_id
+      ,aa.student_web_password
+      ,aa.web_id AS family_web_id
+      ,aa.web_password AS family_web_password      
 FROM gabby.powerschool.cohort_identifiers_static co
 LEFT OUTER JOIN gabby.extracts.powerschool_autocomm_students_accessaccounts aa
   ON co.student_number = aa.student_number
 WHERE co.enroll_status = 0
   AND co.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
-  AND co.rn_year = 1
+  AND co.rn_year = 1 
