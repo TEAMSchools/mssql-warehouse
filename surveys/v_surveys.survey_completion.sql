d9d72e7d26326fcfdac6730bbf709acbc00d83f0
Charlie Bini <cbini87@gmail.com>
2017-09-21 10:32:23 -0400

    Update v_surveys.survey_completion.sql

--- surveys/v_surveys.survey_completion.sql
+++ surveys/v_surveys.survey_completion.sql
@@ -4,15 +4,15 @@ GO
 CREATE VIEW surveys.survey_completion AS
 
 SELECT _created AS date_completed
-	   ,email AS responder
-	   ,subject_name AS feedback_recipient
-	   ,'Self & Others' AS survey
+      ,email AS responder
+      ,subject_name AS feedback_recipient
+      ,'Self & Others' AS survey
 FROM gabby.surveys.self_and_others_survey
 
 UNION ALL
 
 SELECT _created AS date_completed
-	   ,responder_name AS responder
-	   ,subject_name AS feedback_recipient
-	   ,'Manager' AS survey	
+      ,responder_name AS responder
+      ,subject_name AS feedback_recipient
+      ,'Manager' AS survey	
 FROM gabby.surveys.manager_survey
