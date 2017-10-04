9039d7a400d5433b6b6357a69512789ba44b220a
Charlie Bini <cbini87@gmail.com>
2017-10-04 13:32:56 -0400

    better fix for deleted performance bands `fivetran_deleted` is no bueno

--- illuminate/v_illuminate_dna_assessments.performance_band_lookup.sql
+++ illuminate/v_illuminate_dna_assessments.performance_band_lookup.sql
@@ -6,7 +6,7 @@ CREATE OR ALTER VIEW illuminate_dna_assessments.performance_band_lookup AS
 SELECT performance_band_set_id
       ,description
       ,minimum_value
-      ,maximum_value
+      ,LEAD(minimum_value, 1, 1001) OVER(PARTITION BY performance_band_set_id ORDER BY label_number) - 1 AS maximum_value
       ,label_number
       ,is_mastery
 FROM
@@ -14,10 +14,11 @@ FROM
      SELECT pbs.performance_band_set_id
            ,pbs.description
       
-           ,pb.minimum_value
-           ,LEAD(pb.minimum_value, 1, 1000) OVER(PARTITION BY pbs.performance_band_set_id ORDER BY pb.label_number) - 1 AS maximum_value
+           ,pb.minimum_value           
            ,pb.label_number
            ,pb.is_mastery
+           ,pb._fivetran_deleted
+           ,pb._fivetran_synced
 
            ,ROW_NUMBER() OVER(
               PARTITION BY pbs.performance_band_set_id, pb.label_number
@@ -25,6 +26,6 @@ FROM
      FROM gabby.illuminate_dna_assessments.performance_band_sets pbs
      JOIN gabby.illuminate_dna_assessments.performance_bands pb
        ON pbs.performance_band_set_id = pb.performance_band_set_id
-     WHERE pbs.deleted_at IS NULL
+     WHERE pbs.deleted_at IS NULL     
     ) sub
 WHERE rn = 1
