e936219ae519f3887b49cf4ac1f502f5b1326ffe
Charlie Bini <cbini87@gmail.com>
2017-10-05 15:46:33 -0400

    ad hoc cleanup script for TNTP data

---
+++ tntp/q_clean_school_sorter_fields.sql
@@ -0,0 +1,19 @@
+WITH remove_footnotes AS (
+  SELECT field, STUFF(field, LEN(field), 1, '') AS new_field
+  FROM gabby.tntp.teacher_survey_school_sorter
+  WHERE ISNUMERIC(RIGHT(field,1)) = 1
+    AND RIGHT(field,1) != '.'
+    AND field NOT IN ('Offer by Aug 1','Offer by July 1','Offer by June 1','Offer by May 1')
+ )
+
+,remove_ending_period AS (
+  SELECT field, STUFF(field, LEN(field), 1, '') AS new_field
+  FROM gabby.tntp.teacher_survey_school_sorter
+  WHERE RIGHT(field,1) = '.'    
+ )
+
+--UPDATE remove_footnotes
+--SET field = new_field
+
+--UPDATE remove_ending_period
+--SET field = new_field
