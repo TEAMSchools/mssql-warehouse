bdd7957549247e6905bf8d7a7404de02b3e4b5cd
Charlie Bini <cbini87@gmail.com>
2017-10-13 13:53:12 -0400

    fixed date issue, dedupe ADP

--- payroll/v_payroll.payroll_edits_combined.sql
+++ payroll/v_payroll.payroll_edits_combined.sql
@@ -3,54 +3,61 @@ GO
 
 CREATE OR ALTER VIEW payroll.payroll_edits_combined AS
 
-SELECT entity AS entity
-	     ,salesforce_position_number AS sf_position_id
+SELECT p.entity AS entity
+	     ,p.salesforce_position_number AS sf_position_id
 	     ,r.associate_id AS associate_id
 	     ,r.position_id AS position_id
 	     ,p.preferred_name AS name
-	     ,CONVERT(DATE,start_date) AS effective_date
+	     ,CONVERT(DATE,p.start_date) AS effective_date
       ,'New Hire' AS source
-      ,CONVERT(NVARCHAR,salary) AS salary
-	     ,leadership_stipend
-      ,relocation_stipend
-      ,other_stipend
-	     ,CONVERT(NVARCHAR,ISNULL(leadership_stipend,0) + ISNULL(relocation_stipend,0) + ISNULL(other_stipend,0)) AS total_stipend
+      ,CONVERT(NVARCHAR,p.salary) AS salary
+	     ,p.leadership_stipend
+      ,p.relocation_stipend
+      ,p.other_stipend
+	     ,CONVERT(NVARCHAR,ISNULL(p.leadership_stipend,0) + ISNULL(p.relocation_stipend,0) + ISNULL(p.other_stipend,0)) AS total_stipend
       ,NULL AS notes
-FROM gabby.payroll.new_hire_tracker p LEFT OUTER JOIN gabby.adp.staff_roster r
-	      ON  p.salesforce_position_number = r.salesforce_job_position_name_custom
+FROM gabby.payroll.new_hire_tracker p 
+LEFT OUTER JOIN gabby.adp.staff_roster r
+  ON p.salesforce_position_number = r.salesforce_job_position_name_custom
+ AND r.rn_curr = 1
+
 UNION ALL
 
-SELECT entity AS entity
+SELECT e.entity AS entity
 	     ,r.salesforce_job_position_name_custom AS sf_position_id
 	     ,e.associate_id AS associate_id
 	     ,e.position_id AS position_id
 	     ,employee_name AS name
-	     ,CONVERT(DATE,LEFT(payroll_date,10)) AS effective_date      
+	     ,CONVERT(DATE,e.payroll_date) AS effective_date      
       ,'Pay Edits' AS source
-      ,CONVERT(NVARCHAR,amount_of_edit) AS salary
+      ,CONVERT(NVARCHAR,e.amount_of_edit) AS salary
 	     ,NULL AS leadership_stipend
       ,NULL AS relocation_stipend
       ,NULL AS other_stipend      
       ,NULL AS total_stipend
-	     ,description AS notes
-FROM gabby.payroll.payroll_edit_tracker e LEFT OUTER JOIN gabby.adp.staff_roster r
-       ON e.associate_id = r.associate_id
+	     ,e.description AS notes
+FROM gabby.payroll.payroll_edit_tracker e 
+LEFT OUTER JOIN gabby.adp.staff_roster r
+  ON e.associate_id = r.associate_id
+ AND r.rn_curr = 1
 
 UNION ALL
 
-SELECT entity AS entity
+SELECT s.entity AS entity
 	     ,r.salesforce_job_position_name_custom AS sf_position_id
-	     ,employee_associate_id AS associate_id
-	     ,employee_position_id AS position_id
-	     ,employee_name AS name	     
-      ,CONVERT(DATE,effective_date_of_change) AS effective_date
+	     ,s.employee_associate_id AS associate_id
+	     ,s.employee_position_id AS position_id
+	     ,s.employee_name AS name	     
+      ,CONVERT(DATE,s.effective_date_of_change) AS effective_date
       ,'Status Change' AS source	   
-      ,CONVERT(NVARCHAR,_new_base_salary_) AS salary
+      ,CONVERT(NVARCHAR,s._new_base_salary_) AS salary
       ,NULL AS leadership_stipend
       ,NULL AS relocation_stipend
       ,NULL AS other_stipend      
-	     ,CONVERT(NVARCHAR,bonus_stipend_amount_) AS total_stipend
-	     ,bonus_stipend_details AS notes	     
-FROM gabby.payroll.status_change s LEFT OUTER JOIN gabby.adp.staff_roster r
-       ON s.employee_associate_id = r.associate_id
-WHERE effective_date_of_change != 'varies'
+	     ,CONVERT(NVARCHAR,s.bonus_stipend_amount_) AS total_stipend
+	     ,s.bonus_stipend_details AS notes	     
+FROM gabby.payroll.status_change s 
+LEFT OUTER JOIN gabby.adp.staff_roster r
+  ON s.employee_associate_id = r.associate_id
+ AND r.rn_curr = 1
+WHERE s.effective_date_of_change != 'varies'
