9e39f2c0b28db1916979a4b14a7cca6753ab848c
Kevin Verhoff <KVerhoff@kippnj.org>
2017-10-13 13:26:40 -0400

    Payroll_IDs
    Adding associate, Position, and salesforce ID in various parts of the view

--- payroll/v_payroll.payroll_edits_combined.sql
+++ payroll/v_payroll.payroll_edits_combined.sql
@@ -5,9 +5,9 @@ CREATE OR ALTER VIEW payroll.payroll_edits_combined AS
 
 SELECT entity AS entity
 	     ,salesforce_position_number AS sf_position_id
-	     ,NULL AS associate_id
-	     ,NULL AS position_id
-	     ,preferred_name AS name
+	     ,r.associate_id AS associate_id
+	     ,r.position_id AS position_id
+	     ,p.preferred_name AS name
 	     ,CONVERT(DATE,start_date) AS effective_date
       ,'New Hire' AS source
       ,CONVERT(NVARCHAR,salary) AS salary
@@ -16,14 +16,14 @@ SELECT entity AS entity
       ,other_stipend
 	     ,CONVERT(NVARCHAR,ISNULL(leadership_stipend,0) + ISNULL(relocation_stipend,0) + ISNULL(other_stipend,0)) AS total_stipend
       ,NULL AS notes
-FROM gabby.payroll.new_hire_tracker 
-
+FROM gabby.payroll.new_hire_tracker p LEFT OUTER JOIN gabby.adp.staff_roster r
+	      ON  p.salesforce_position_number = r.salesforce_job_position_name_custom
 UNION ALL
 
 SELECT entity AS entity
-	     ,NULL AS sf_position_id
-	     ,associate_id AS associate_id
-	     ,position_id AS position_id
+	     ,r.salesforce_job_position_name_custom AS sf_position_id
+	     ,e.associate_id AS associate_id
+	     ,e.position_id AS position_id
 	     ,employee_name AS name
 	     ,CONVERT(DATE,LEFT(payroll_date,10)) AS effective_date      
       ,'Pay Edits' AS source
@@ -33,12 +33,13 @@ SELECT entity AS entity
       ,NULL AS other_stipend      
       ,NULL AS total_stipend
 	     ,description AS notes
-FROM gabby.payroll.payroll_edit_tracker
+FROM gabby.payroll.payroll_edit_tracker e LEFT OUTER JOIN gabby.adp.staff_roster r
+       ON e.associate_id = r.associate_id
 
 UNION ALL
 
 SELECT entity AS entity
-	     ,NULL AS sf_position_id
+	     ,r.salesforce_job_position_name_custom AS sf_position_id
 	     ,employee_associate_id AS associate_id
 	     ,employee_position_id AS position_id
 	     ,employee_name AS name	     
@@ -50,5 +51,6 @@ SELECT entity AS entity
       ,NULL AS other_stipend      
 	     ,CONVERT(NVARCHAR,bonus_stipend_amount_) AS total_stipend
 	     ,bonus_stipend_details AS notes	     
-FROM gabby.payroll.status_change
+FROM gabby.payroll.status_change s LEFT OUTER JOIN gabby.adp.staff_roster r
+       ON s.employee_associate_id = r.associate_id
 WHERE effective_date_of_change != 'varies'
