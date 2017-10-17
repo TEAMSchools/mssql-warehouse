6b18a6d30ef0021f978b965bbd7b0a18d0fcfcf3
Charlie Bini <cbini87@gmail.com>
2017-10-17 17:32:37 -0400

    rearraging join

--- illuminate/v_illuminate_standards.standards_domain.sql
+++ illuminate/v_illuminate_standards.standards_domain.sql
@@ -9,8 +9,8 @@ SELECT s2.standard_id AS domain_standard_id
       ,s2.label AS domain_label
       ,s1.standard_id
       ,s1.custom_code
-FROM gabby.illuminate_standards.standards s1  
-JOIN gabby.illuminate_standards.standards s2
+FROM gabby.illuminate_standards.standards s2
+JOIN gabby.illuminate_standards.standards s1  
   ON CHARINDEX('.' + s2.custom_code + '.', '.' + s1.custom_code) > 0
- AND s2.level = 1
-WHERE s1.level > 1
+ AND s1.level > 1
+WHERE s2.level = 1 
