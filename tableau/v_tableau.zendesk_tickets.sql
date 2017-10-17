fd1a0648a4c4eca67544596c1ca26f291e6f79ae
Charlie Bini <cbini87@gmail.com>
2017-10-17 15:50:21 -0400

    name change and edits

---
+++ tableau/v_tableau.zendesk_tickets.sql
@@ -0,0 +1,58 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW tableau.zendesk_tickets AS
+
+WITH comments_count AS (
+	 SELECT c.ticket_id
+		      ,COUNT(c.ticket_id) as comments_count
+	 FROM gabby.zendesk.ticket_comments c
+	 GROUP BY c.ticket_id
+ )
+
+,closed AS (
+  SELECT ticket_id
+        ,timestamp
+        ,ROW_NUMBER() OVER(
+          PARTITION BY ticket_id
+            ORDER BY timestamp DESC) AS rn
+  FROM gabby.zendesk.ticket_history
+  WHERE property = 'status'
+    AND new_value = 'closed'
+ )
+
+SELECT t.id AS ticket_id
+	     ,CONVERT(NVARCHAR,t.subject) AS ticket_subject	     
+	     ,t.status AS ticket_status      
+  	   ,t.location	     
+	     ,t.created_at
+	     ,t.updated_at
+	     ,t.due_at      
+      ,t.category
+	     ,t.tech_tier
+      ,t.group_id
+      ,t.submitter_id
+      ,t.assignee_id            
+
+      ,s.name AS submitter_name
+
+      ,a.name AS assignee_name
+
+      ,g.name AS group_name
+
+	     ,c.comments_count	       
+      
+      ,cl.timestamp AS closed_timestamp            
+FROM gabby.zendesk.tickets t
+JOIN gabby.zendesk.users s
+  ON t.submitter_id = s.id
+LEFT OUTER JOIN gabby.zendesk.users a
+  ON t.assignee_id = a.id
+LEFT OUTER JOIN gabby.zendesk.groups g
+  ON t.group_id = g.id
+JOIN comments_count c
+  ON t.id = c.ticket_id
+LEFT OUTER JOIN closed cl
+  ON t.id = cl.ticket_id
+ AND cl.rn = 1
+WHERE t.status != 'deleted'
