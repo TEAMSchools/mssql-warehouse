82e0328407daa1f1793ea513e5adf9299075ae08
Charlie Bini <cbini87@gmail.com>
2017-10-27 13:12:39 -0400

    fixes based on KTC feedback

--- tableau/v_tableau.ktc_basic_contact_dump.sql
+++ tableau/v_tableau.ktc_basic_contact_dump.sql
@@ -114,24 +114,30 @@ WITH attending_enrollment AS (
 
 ,stipends AS (
   SELECT student_c
-        ,[F] AS stipend_status_fall
-        ,[S] AS stipend_status_spr
+        ,CASE 
+          WHEN [F] IS NOT NULL THEN 'Approved'
+          WHEN [F] IS NULL THEN 'Not Picked Up'
+        END AS stipend_status_fall
+        ,CASE 
+          WHEN [S] IS NOT NULL THEN 'Approved'
+          WHEN [S] IS NULL THEN 'Not Picked Up'
+        END AS stipend_status_spr
   FROM
       (
-       SELECT a.Student_c        
-             ,a.Status_c        
+       SELECT a.student_c        
+             ,a.date_c
              ,CASE 
-               WHEN DATEPART(MONTH, a.Date_c) BETWEEN 7 AND 12 THEN 'F'
+               WHEN DATEPART(MONTH, a.created_date) BETWEEN 7 AND 12 THEN 'F'
                ELSE 'S'
               END AS semester             
-             --,a.Amount_c
        FROM gabby.alumni.kipp_aid_c a 
        WHERE a.type_c = 'College Book Stipend Program'
+         AND a.Status_c = 'Approved'
          AND a.is_deleted = 0
-         AND a.date_c >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1)
+         AND a.created_date >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1)
       ) sub
   PIVOT(
-    MAX(status_c)
+    MAX(date_c)
     FOR semester IN ([F], [S])
    ) p
  )
@@ -145,6 +151,9 @@ WITH attending_enrollment AS (
         ,n_months_elapsed
         ,gabby.utilities.DATE_TO_SY(missing_start_date) AS missing_academic_year
         ,gabby.utilities.DATE_TO_SY(found_date) AS found_academic_year
+        ,ROW_NUMBER() OVER(
+           PARTITION BY contact_id, gabby.utilities.DATE_TO_SY(missing_start_date)
+             ORDER BY last_successful_contact_date DESC) AS rn
   FROM
       (
        SELECT Contact_c AS contact_id
@@ -159,7 +168,7 @@ WITH attending_enrollment AS (
        WHERE Status_c = 'Successful'
          AND is_deleted = 0
       ) sub
-  WHERE n_months_elapsed >= 12
+  --WHERE n_months_elapsed >= 12
  ) 
 
 ,counselor_changes AS (
@@ -219,6 +228,7 @@ SELECT c.id AS contact_id
       ,c.last_name AS LastName
       ,c.kipp_hs_class_c
       ,(gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1) - DATEPART(YEAR, c.actual_hs_graduation_date_c) AS years_out_of_HS
+      ,c.college_status_c
       ,c.currently_enrolled_school_c
       ,c.kipp_ms_graduate_c
       ,c.middle_school_attended_c
@@ -249,18 +259,22 @@ SELECT c.id AS contact_id
       ,CASE 
         --WHEN c.Post_HS_Simple_Admin_c IN ('College Grad - BA') THEN 0 /* exclude grads */
         --WHEN c.college_graduated_from_c IS NOT NULL THEN 0
-        WHEN oot.contact_id IS NOT NULL THEN 1
+        WHEN (oot.n_months_elapsed >= 12 OR oot.contact_id IS NULL) THEN 1
         ELSE 0 
        END AS is_oot_baseline
       ,CASE 
         --WHEN c.Post_HS_Simple_Admin_c IN ('College Grad - BA') THEN 0 /* exclude grads */
         --WHEN c.college_graduated_from_c IS NOT NULL THEN 0
-        WHEN oot.is_still_missing = 1 THEN 1
+        WHEN (oot.n_months_elapsed >= 12 OR oot.contact_id IS NULL)
+         AND oot.is_still_missing = 1 THEN 1
         ELSE 0 
        END AS is_out_of_touch            
       ,CASE 
         --WHEN c.Post_HS_Simple_Admin_c IN ('College Grad - BA') THEN 0 /* exclude grads */
-        WHEN oot.contact_id IS NOT NULL AND u.name = cc.new_value THEN 1 ELSE 0 
+        WHEN (oot.n_months_elapsed >= 12 OR oot.contact_id IS NULL)
+         AND u.name = cc.new_value 
+             THEN 1 
+        ELSE 0 
        END AS is_oot_assigned
       ,CASE 
         --WHEN c.Post_HS_Simple_Admin_c IN ('College Grad - BA') THEN 0 /* exclude grads */
@@ -323,6 +337,7 @@ LEFT OUTER JOIN stipends s
 LEFT OUTER JOIN oot_roster oot
   ON c.Id = oot.contact_id
  AND gabby.utilities.GLOBAL_ACADEMIC_YEAR() BETWEEN oot.missing_academic_year AND oot.found_academic_year
+ AND oot.rn = 1
 LEFT OUTER JOIN counselor_changes cc
   ON c.id = cc.contact_id
  AND cc.rn = 1
