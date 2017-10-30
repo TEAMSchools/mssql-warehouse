3d39660f375d1c47ce60dbe53405f51ba82fc15c
Charlie Bini <cbini87@gmail.com>
2017-10-30 12:03:00 -0400

    adding subject area label, filtering for only QAs

--- extracts/v_extracts.deanslist_mod_assessment.sql
+++ extracts/v_extracts.deanslist_mod_assessment.sql
@@ -11,7 +11,9 @@ SELECT asr.local_student_id AS student_number
       ,CASE
         WHEN asr.subject_area = 'Text Study' THEN 'ELA'
         WHEN asr.subject_area = 'Mathematics' THEN 'MATH'               
+        ELSE asr.subject_area
        END AS subject_area                                    
+      ,asr.subject_area AS subject_area_label
       ,CASE
         WHEN asr.performance_band_number = 5 THEN 'Above Target'
         WHEN asr.performance_band_number = 4 THEN 'Target'
@@ -21,7 +23,8 @@ SELECT asr.local_student_id AS student_number
        END AS proficiency_label
       ,SUBSTRING(asr.module_number, PATINDEX('%[0-9]%', asr.module_number), 1) AS rn_unit
 FROM gabby.illuminate_dna_assessments.agg_student_responses_all asr
-WHERE asr.scope IN ('CMA - End-of-Module','Checkpoint')
+WHERE asr.module_type = 'QA'
   AND asr.subject_area IN ('Text Study','Mathematics')
   AND asr.response_type = 'O'
   AND asr.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
+  AND asr.administered_at <= CONVERT(DATE,GETDATE())
