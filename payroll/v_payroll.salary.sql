9b5759fc443a1c708e6c9abbc2162257488ac836
kevinverhoff <kevin.verhoff@gmail.com>
2017-10-31 11:51:25 -0400

    v_salary
    Creating an ongoing view of all of the salaries with a current salary for every row for easier matching

---
+++ payroll/v_payroll.salary.sql
@@ -0,0 +1,43 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW payroll.salary AS
+
+SELECT r.position_id
+      ,a.associate_id
+      ,o.name
+      ,o.most_recent_paydate
+      ,o.most_recent_salary
+      ,r.snapshot_pay_date
+      ,r.snapshot_salary
+FROM (
+     SELECT real_time.position_id
+           ,real_time.gross_pay * 24 AS snapshot_salary
+           ,real_time.pay_date AS snapshot_pay_date
+     FROM gabby.payroll.pr_employeesummary_clean real_time
+     ) r
+LEFT OUTER JOIN (
+
+     SELECT DISTINCT 
+            c.position_id
+           ,c.name
+           ,c.gross_pay * 24 AS most_recent_salary
+           ,s.most_recent_paydate
+             
+     FROM gabby.payroll.pr_employeesummary_clean c
+          INNER JOIN (
+          SELECT p.position_id
+                ,MAX(p.pay_date) AS most_recent_paydate
+     FROM gabby.payroll.pr_employeesummary_clean p         
+     GROUP BY p.position_id ) AS s
+          ON c.position_id = s.position_id 
+          AND s.most_recent_paydate = pay_date
+      ) o
+ON r.position_id = o.position_id
+
+LEFT OUTER JOIN (
+     SELECT associate_id
+           ,position_id
+     FROM gabby.adp.staff_roster
+     ) a
+ON a.position_id = r.position_id
