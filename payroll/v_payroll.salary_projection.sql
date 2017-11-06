88a1f54df37f79befafbb6a1363f9f5d80e757b6
Charlie Bini <cbini87@gmail.com>
2017-11-06 09:54:20 -0500

    renaming, removing JOIN subqueries, refactoring most recent logic

---
+++ payroll/v_payroll.salary_projection.sql
@@ -0,0 +1,33 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW payroll.salary_projection AS
+
+WITH o AS (
+  SELECT c.position_id
+        ,c.name        
+        ,c.pay_date
+        ,c.gross_pay * 24 AS most_recent_salary
+        
+        ,ROW_NUMBER() OVER(
+           PARTITION BY c.position_id
+             ORDER BY c.pay_date DESC) AS rn_most_recent_paydate
+  FROM gabby.payroll.pr_employeesummary_clean c  
+)
+
+SELECT r.position_id
+      ,r.pay_date AS snapshot_pay_date
+      ,(r.gross_pay * 24) AS snapshot_salary      
+
+      ,o.pay_date AS most_recent_paydate
+      ,o.most_recent_salary     
+
+      ,a.associate_id           
+      ,a.preferred_first
+      ,a.preferred_last
+FROM gabby.payroll.pr_employeesummary_clean r     
+LEFT OUTER JOIN o
+  ON r.position_id = o.position_id
+ AND o.rn_most_recent_paydate = 1
+LEFT OUTER JOIN gabby.adp.staff_roster a     
+  ON a.position_id = r.position_id
