c974f87d316901b39e7087703224fd7e77866457
kevin.verhoff <kevin.verhoff@gmail.com>
2017-12-05 12:42:42 -0500

    Adding missing pay dates
    Adds a set of all possible pay dates for everyone on the staff roster so that if someone is missing, they will still show up. It will also include folks who are terminated (for up to 30 days)

--- payroll/v_payroll.pr_employeesummary_clean.sql
+++ payroll/v_payroll.pr_employeesummary_clean.sql
@@ -3,7 +3,72 @@ GO
 
 CREATE OR ALTER VIEW payroll.pr_employeesummary_clean AS
 
-SELECT sub.position_id      
+SELECT dates.position_id      
+      ,dates.pay_date
+      ,pay.company
+      ,pay.file_
+      ,COALESCE(pay.name,dates.last_name + ', ' + dates.first_name) AS Name
+      ,pay.home_department
+      ,pay.home_cost_number
+      ,pay.clock_
+      ,pay.total_reg_hours
+      ,pay.total_ot_hours
+      ,pay.total_hours_3_4
+      ,pay.gross_pay
+      ,pay.total_taxes
+      ,pay.total_deductions
+      ,pay.total_deposits
+      ,pay.net_pay
+      ,pay.total_reg_earnings
+      ,pay.total_reg_earnings_prev
+      ,pay.total_ot_earnings
+      ,pay.total_ot_earnings_prev
+      ,pay.total_earnings_3_4_5
+      ,pay.total_earnings_3_4_5_prev
+      ,pay.total_reg_earnings_prev - pay.total_reg_earnings AS total_reg_earnings_diff
+      ,CASE
+        WHEN (pay.total_reg_earnings_prev - pay.total_reg_earnings != 0 OR pay.total_reg_earnings IS NULL) THEN 1
+        ELSE 0
+       END AS total_reg_earnings_diff_flag
+      ,pay.total_ot_earnings_prev - pay.total_ot_earnings AS total_ot_earnings_diff
+      ,CASE
+        WHEN (pay.total_ot_earnings_prev - pay.total_ot_earnings != 0 OR pay.total_ot_earnings IS NULL)THEN 1
+        ELSE 0
+       END AS total_ot_earnings_diff_flag
+      ,pay.total_earnings_3_4_5_prev - pay.total_earnings_3_4_5 AS total_earnings_3_4_5_diff
+      ,CASE
+        WHEN (pay.total_earnings_3_4_5_prev - pay.total_earnings_3_4_5 != 0 OR pay.total_earnings_3_4_5 IS NULL) THEN 1
+        ELSE 0
+       END AS total_earnings_3_4_5_diff_flag
+       
+      ,pay.associate_id
+
+
+FROM (
+
+      SELECT p.associate_id
+             ,p.position_id
+             ,p.first_name
+             ,p.last_name
+             ,COALESCE(p.rehire_date, p.hire_date) AS hire_date
+             ,p.termination_date
+             ,p.position_status
+             ,e.pay_date
+
+      FROM gabby.adp.staff_roster p 
+           LEFT JOIN 
+                (SELECT DISTINCT pay_date
+                FROM gabby.payroll.pr_employeesummary_clean) e
+      ON e.pay_date BETWEEN COALESCE(p.rehire_date, p.hire_date) AND COALESCE(DATEADD(day,16,p.termination_date),DATEADD(day,16,GETDATE()))
+
+      WHERE p.termination_date IS NULL
+            OR p.termination_date >= DATEADD(day,30,GETDATE())
+            
+           ) dates
+
+LEFT OUTER JOIN
+
+(SELECT sub.position_id      
       ,sub.pay_date
       ,sub.company	
       ,sub.file_	
@@ -43,6 +108,7 @@ SELECT sub.position_id
        
       ,sr.associate_id
 FROM
+
     (
      SELECT pr.file_	
            ,pr.name	
@@ -70,4 +136,10 @@ FROM
      FROM gabby.adp.pr_employeesummary pr
     ) sub
 JOIN gabby.adp.staff_roster sr
-  ON sub.position_id = sr.position_id
+  ON sub.position_id = sr.position_id) pay
+  
+
+ON dates.position_id = pay.position_id
+   AND dates.pay_date = pay.pay_date
+
+WHERE dates.position_id IS NOT NULL
