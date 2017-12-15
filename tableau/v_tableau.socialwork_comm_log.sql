814c044c27f95d60cea21386d109824d43fe7c85
Charlie Bini <CBini@kippnj.org>
2017-12-15 14:31:08 -0500

    :lipstick:

--- tableau/v_tableau.socialwork_comm_log.sql
+++ tableau/v_tableau.socialwork_comm_log.sql
@@ -1,7 +1,7 @@
 USE gabby
 GO
 
---CREATE OR ALTER VIEW tableau.socialwork_comm_log AS
+CREATE OR ALTER VIEW tableau.socialwork_comm_log AS
 
 WITH commlog AS (
   SELECT c.student_school_id
@@ -22,64 +22,37 @@ WITH commlog AS (
   WHERE c.reason LIKE 'SW:%'
  )
 
-,promo_status AS (
-  SELECT student_number
-      ,academic_year
-	  ,att_pts_pct
-	  ,promo_status_attendance
-	  ,promo_status_grades
-	  ,GPA_Y1
-	  ,is_curterm
-	  ,term_name
-	  
-  FROM gabby.reporting.promotional_status
-
-  WHERE academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
-    AND is_curterm = 1
- )
-
-
-
 SELECT co.student_number
       ,co.lastfirst
       ,co.grade_level
       ,co.team
       ,co.region
       ,co.reporting_schoolid
-	  ,co.school_name    
-	  ,co.enroll_status
-	  ,co.is_retained_ever
-	  ,co.is_retained_year
-	  ,co.advisor_name
-	  ,co.advisor_phone
-	  ,co.mother_cell
-	  ,co.home_phone
-	  ,co.father_cell
-	  ,co.mother
-	  ,co.father
-	  ,co.guardianemail
-	  ,co.street + ', ' + co.city + ', ' + co.zip AS address
-	  ,co.specialed_classification
-	  ,co.iep_status
-	  ,co.boy_status
-	  
-	  ,promo_status.att_pts_pct
-	  ,promo_status.promo_status_attendance
-	  ,promo_status.promo_status_grades
-	  ,promo_status.GPA_Y1
-	  
-	  ,cl.commlog_staff_name
+	     ,co.school_name    
+	     ,co.enroll_status
+	     ,co.is_retained_ever
+	     ,co.is_retained_year
+	     ,co.advisor_name
+	     ,co.advisor_phone
+	     ,co.mother_cell
+	     ,co.home_phone
+	     ,co.father_cell
+	     ,co.mother
+	     ,co.father
+	     ,co.guardianemail
+	     ,co.street + ', ' + co.city + ', ' + co.zip AS address
+	     ,co.specialed_classification
+	     ,co.iep_status
+	     ,co.boy_status
+
+	     ,cl.commlog_staff_name
       ,cl.commlog_reason
       ,cl.commlog_notes
       ,cl.commlog_topic
       ,cl.followup_staff_name
       ,cl.followup_init_notes
       ,cl.followup_close_notes
-
 FROM gabby.powerschool.cohort_identifiers_static co
 JOIN commlog cl
   ON co.student_number = cl.student_school_id
-JOIN promo_status
-  ON co.student_number = promo_status.student_number
- AND co.academic_year = promo_status.academic_year
 WHERE co.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
