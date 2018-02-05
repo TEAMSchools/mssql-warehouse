9a0adba4106b2f43fbb11fbeef1112cc92bbac31
Charlie Bini <CBini@kippnj.org>
2018-02-05 13:30:20 -0500

    code review
    @kevinverhoff FYI
    
    * removed quoted identifiers and made DB friendly
    * grouped fields by table w/ functions at the bottom
    * not sure what FULL OUTER JOIN was used for
    * pulling only most recent ADP record
    * city/state/zip were all just pulling 1st 3 letters of position_id
    * DATE_TO_SY() can take care converting dates to academic years

--- surveys/v_surveys.exit_survey_detail.sql
+++ surveys/v_surveys.exit_survey_detail.sql
@@ -3,61 +3,54 @@ GO
 
 CREATE OR ALTER VIEW surveys.exit_survey_detail AS
 
-SELECT 	adp.associate_id
- ,adp.termination_date AS 'Official Termination Date'
-	,adp.termination_reason_description AS 'Official Termination Reason'
-	,adp.job_title_description AS 'Job Title'
-	,adp.birth_date AS 'Date of Birth'
-	,adp.eeo_ethnic_description AS 'Ethnicity'
-	,adp.subject_dept_custom AS 'Department'
-	,COALESCE(adp.location_custom, adp.location_description)  AS 'Location'
-	,adp.benefits_eligibility_class_description AS 'Benefits Class'
-	,COALESCE(adp.manager_name, adp.reports_to_name) AS 'Manager'
-	,adp.is_management AS 'Management'
-	,adp.gender AS 'Gender'
-	,LEFT(adp.position_id,3) AS 'City'
-	,LEFT(adp.position_id,3) AS 'State'
-	,LEFT(adp.position_id,3) AS 'ZIP'
-	,adp.hire_date AS 'Hire_date'
-	,adp.position_status AS 'Position Status'
+SELECT adp.associate_id
+      ,adp.termination_date
+	     ,adp.termination_reason_description
+	     ,adp.job_title_description
+	     ,adp.birth_date
+	     ,adp.eeo_ethnic_description
+	     ,adp.subject_dept_custom
+	     ,adp.benefits_eligibility_class_description
+	     ,adp.is_management
+	     ,adp.gender
+	     ,adp.hire_date
+	     ,adp.position_status      
+      ,adp.primary_address_city
+	     ,adp.primary_address_state_territory_code
+	     ,adp.primary_address_zip_postal_code
+      ,COALESCE(adp.location_custom, adp.location_description) AS location
+      ,COALESCE(adp.manager_name, adp.reports_to_name) AS manager
+      ,MONTH(adp.hire_date) AS hire_month
+	     ,COALESCE(YEAR(adp.termination_date), YEAR(GETDATE())) - YEAR(adp.hire_date) + 1 AS years_at_kipp
+      ,gabby.utilities.DATE_TO_SY(adp.termination_date) AS termination_academic_year
 	
-	,es.timestamp AS 'es Survey Date'
-	,es.q_1 AS 'es Survey Title'
-	,es.q_2 AS 'es Survey Location'
-	,es.q_3 AS 'NPS'
-	,es.q_4 AS 'NPS_Explanation'
-	,es.q_5	AS 'Voluntary  Termination?'
-	,es.q_6 AS 'es Survey Reason for leaving'
-	,es.q_7 AS 'Next Role'
-	,es.q_8 AS 'Next Title'
-	,es.q_9 AS 'Expectation Alignment'
-	,es.q_10 AS 'Open Comment'
-	,es.q_11 AS 'Anonymous'
-	,es.q_12 AS 'Consider KIPP in future?'
-	,es.q_13 AS 'What does KIPP do well?'
-	,es.q_14 AS 'How can KIPP improve?'
-	,es.q_15 AS 'Rating: Career Growth'
-	,es.q_16 AS 'Rating: Schedule Flexibility'
-	,es.q_17 AS 'Rating: Compensation & Benefits'
-	,es.q_18 AS 'Rating: School Culture'
-	,es.q_19 AS 'Rating: Immediate Principal/Supervisor'
-	,es.q_20 AS 'Rating: Kid Focus'
-	,es.q_21 AS 'Rating: Impact'
-	,es.q_22 AS 'Rating: Improvement'
-	,es.q_23 AS 'Rating: Freedom'
-	,es.q_24 AS 'Rating: Fun'
-	,es.q_25 AS 'Rating: TEAMwork'
-	,MONTH(adp.Hire_Date) AS 'Hire Month'
-	,CASE WHEN MONTH(adp.termination_date) > 6 
-			THEN REPLACE(STR(YEAR(adp.termination_date))+'-'+STR(YEAR(adp.termination_date)+1),' ','')
-			ELSE REPLACE(STR(YEAR(adp.termination_date)-1)+'-'+STR(YEAR(adp.termination_date)),' ','') END 
-				AS 'Termination Academic Year'
-	,CASE WHEN YEAR(adp.Termination_date) IS NULL
-			THEN YEAR(GETDATE()) - YEAR(adp.Hire_Date) + 1
-			ELSE YEAR(adp.termination_date) - YEAR(adp.Hire_Date) + 1 END
-				AS 'Years at KIPP'
-	
-
-FROM gabby.adp.staff_roster adp FULL OUTER JOIN gabby.surveys.exit_survey es
-
-	ON adp.associate_id = es.associate_id
+	     ,es.timestamp AS exit_survey_date
+	     ,es.q_1 AS exit_survey_title
+	     ,es.q_2 AS exit_survey_location
+	     ,es.q_3 AS nps
+	     ,es.q_4 AS nps_explanation
+	     ,es.q_5	AS voluntary_termination
+	     ,es.q_6 AS exit_survey_reason_for_leaving
+	     ,es.q_7 AS next_role
+	     ,es.q_8 AS next_title
+	     ,es.q_9 AS expectation_alignment
+	     ,es.q_10 AS open_comment
+	     ,es.q_11 AS anonymous
+	     ,es.q_12 AS consider_kipp_in_future
+	     ,es.q_13 AS what_does_kipp_do_well
+	     ,es.q_14 AS how_can_kipp_improve
+	     ,es.q_15 AS rating_career_growth
+	     ,es.q_16 AS rating_schedule_flexibility
+	     ,es.q_17 AS rating_compensation_benefits
+	     ,es.q_18 AS rating_school_culture
+	     ,es.q_19 AS rating_immediate_principal_supervisor
+	     ,es.q_20 AS rating_kid_focus
+	     ,es.q_21 AS rating_impact
+	     ,es.q_22 AS rating_improvement
+	     ,es.q_23 AS rating_freedom
+	     ,es.q_24 AS rating_fun
+	     ,es.q_25 AS rating_teamwork	     
+FROM gabby.adp.staff_roster adp 
+JOIN gabby.surveys.exit_survey es
+  ON adp.associate_id = es.associate_id
+WHERE adp.rn_curr = 1
