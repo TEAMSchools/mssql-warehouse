ad5c3cb83901972756887a8fb010b6e7dee3056e
Charlie Bini <cbini87@gmail.com>
2018-03-01 11:12:14 -0500

    whetstone vyooz

---
+++ whetstone/v_whetstone.assignment_tags.sql
@@ -0,0 +1,17 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW whetstone.assignment_tags AS
+
+SELECT wa._id AS assignment_id
+      ,wa.[tags] AS assignment_tags_json
+      
+      ,wt.tag_id
+      ,wt.tag_name
+FROM [gabby].[whetstone].[assignments] wa
+CROSS APPLY OPENJSON(wa.[tags], '$')
+  WITH (
+    tag_id VARCHAR(25) '$._id',
+    tag_name VARCHAR(125) '$.name'
+   ) AS wt
+WHERE wa.[tags] != '[]'
