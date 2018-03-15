40364b753151776e78c5a1185d806dd6a23453bf
kevin.verhoff <kevin.verhoff@gmail.com>
2018-03-15 12:58:23 -0400

    Done making promises, but I'm pretty sure this is it.
    ugh...making adjustments as I'm digging in further. I think this is it.

--- recruting/v_recruiting.hired_referrals.sql
+++ recruting/v_recruiting.hired_referrals.sql
@@ -4,46 +4,64 @@ USE Gabby
 GO
 
 WITH all_hires AS (
-     SELECT a.contact_id_c AS 'id'
-           ,a.name AS 'job_app'
-           ,a.hired_status_date_c AS 'hire_date'
-           ,a.stage_c AS 'selection_stage'
-           ,a.selection_status_c AS 'selection_status'
-           ,a.job_position_c AS 'job_position'
-           ,a.cultivation_owner_c AS 'recruiter'
-     FROM recruiting.job_application_c a
-
+     SELECT a.contact_id_c AS id
+           ,a.name AS job_app
+           ,a.hired_status_date_c AS hire_date
+           ,a.stage_c AS selection_stage
+           ,a.selection_status_c AS selection_status
+           ,a.job_position_c AS job_position
+           ,a.cultivation_owner_c AS recruiter
+           ,pa.name AS profile_id
+     FROM recruiting.job_application_c a 
+     LEFT JOIN recruiting.profile_application_c pa
+       ON a.profile_application_c = pa.id
      WHERE a.stage_c = 'Hired'
 )
 ,job_positions AS (
      SELECT id
-           ,position_name_c
+           ,name AS position_name
            ,status_c
            
      FROM recruiting.job_position_c
-)    
+) 
+,adp AS (
+     SELECT associate_id
+           ,salesforce_job_position_name_custom
+           ,COALESCE(rehire_date,hire_date) AS most_recent_hire_date
+           ,termination_date 
+           ,position_status
+     FROM adp.staff_roster   
+)
 
 SELECT c.created_date
-      ,c.name AS 'cultivation_number'
+      ,c.name AS cultivation_number
       ,c.contact_name_c
       ,c.cultivation_stage_c              
       ,c.referred_by_c
       ,c.regional_source_detail_c
       ,c.regional_source_c
-      ,c.cultivation_notes_c   
+      ,c.cultivation_notes_c
+      ,h.profile_id
       ,h.Job_app
       ,h.hire_date
       ,h.selection_stage
       ,h.selection_status
       ,h.Recruiter
-      ,p.position_name_c
+      ,p.position_name
       ,p.status_c AS position_status
+      ,p.position_name
+      ,adp.associate_id
+      ,adp.most_recent_hire_date
+      ,adp.termination_date 
+      ,adp.position_status
       
 FROM Recruiting.Cultivation_c c 
 LEFT OUTER JOIN all_hires h
  ON LEFT(c.CONTACT_c,LEN(c.contact_c)-3) = h.id
 LEFT OUTER JOIN job_positions p
  ON h.Job_Position = p.id
+LEFT OUTER JOIN adp
+ ON p.position_name = adp.salesforce_job_position_name_custom
 
 WHERE (c.cultivation_stage_c = 'Hired'
        OR h.selection_stage = 'Hired')
