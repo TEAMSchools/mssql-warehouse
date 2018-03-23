686370f66927107c4aad1e6cc30bae9581af20eb
Charlie Bini <CBini@kippnj.org>
2018-03-23 10:12:46 -0400

    adding GR teacher

--- tableau/v_tableau.qa_lit_test_entry.sql
+++ tableau/v_tableau.qa_lit_test_entry.sql
@@ -5,21 +5,21 @@ CREATE OR ALTER VIEW tableau.qa_lit_test_entry AS
 
 /* student identifiers */
 SELECT co.school_name
-      ,co.school_level      
+      ,co.school_level
       ,co.lastfirst AS student_name
-      ,co.grade_level      
-      ,co.team      
-      ,co.academic_year            
-      ,co.iep_status      
-      ,co.enroll_status      
+      ,co.grade_level
+      ,co.team
+      ,co.academic_year
+      ,co.iep_status
+      ,co.enroll_status
             
       ,CONVERT(VARCHAR(5),term.alt_name) AS lit_term
-      ,CASE 
-        WHEN achv.start_date >= CONVERT(DATE,GETDATE()) THEN NULL 
-        ELSE co.student_number 
-       END AS student_number      
+      ,CASE
+        WHEN achv.start_date >= CONVERT(DATE,GETDATE()) THEN NULL
+        ELSE co.student_number
+       END AS student_number
       
-      /* test identifiers */      
+      /* test identifiers */
       ,achv.read_lvl
       ,achv.lvl_num
       ,achv.dna_lvl
@@ -30,12 +30,12 @@ SELECT co.school_name
       ,achv.indep_lvl_num
       ,achv.prev_read_lvl
       ,achv.prev_lvl_num
-      ,achv.GLEQ      
+      ,achv.GLEQ
       ,achv.fp_keylever
       ,achv.is_new_test
-      ,achv.moved_levels      
+      ,achv.moved_levels
       ,SUM(CASE WHEN achv.test_round IN ('DR','BOY') THEN NULL ELSE achv.moved_levels END) OVER(
-         PARTITION BY co.student_number, co.academic_year 
+         PARTITION BY co.student_number, co.academic_year
            ORDER BY achv.start_date ASC) AS n_levels_moved_y1
 
       ,testid.academic_year AS achvtest_academic_year
@@ -49,21 +49,21 @@ SELECT co.school_name
       ,dna.academic_year AS dnatest_academic_year
       ,dna.test_round AS dnatest_test_round
       ,dna.test_date AS dna_date
-      ,CASE                      
+      ,CASE
         WHEN achv.lvl_num IS NULL THEN 3
         WHEN achv.lvl_num >= 26 THEN 1
-        WHEN achv.test_round IN ('BOY','DR') 
+        WHEN achv.test_round IN ('BOY','DR')
          AND testid.test_round IN ('EOY','Q4','T3')
-         AND achv.academic_year = testid.academic_year + 1 
+         AND achv.academic_year = testid.academic_year + 1
                THEN 1
-        WHEN achv.test_round = testid.test_round 
-         AND achv.academic_year = testid.academic_year 
+        WHEN achv.test_round = testid.test_round
+         AND achv.academic_year = testid.academic_year
                THEN 1
-        WHEN achv.test_round IN ('BOY','DR') 
-         AND dna.test_round IN ('EOY','Q4','T3') 
-         AND achv.academic_year = dna.academic_year + 1 
+        WHEN achv.test_round IN ('BOY','DR')
+         AND dna.test_round IN ('EOY','Q4','T3')
+         AND achv.academic_year = dna.academic_year + 1
                THEN 2
-        WHEN achv.test_round = dna.test_round AND achv.academic_year = dna.academic_year THEN 2        
+        WHEN achv.test_round = dna.test_round AND achv.academic_year = dna.academic_year THEN 2
         ELSE 3
        END AS test_audit
 
@@ -74,14 +74,16 @@ SELECT co.school_name
       ,achv.natl_goal_lvl
       ,achv.natl_goal_num
       ,achv.default_goal_lvl
-      ,achv.default_goal_num      
-      ,achv.lvl_num - achv.goal_num AS distance_from_goal            
+      ,achv.default_goal_num
+      ,achv.lvl_num - achv.goal_num AS distance_from_goal
       ,achv.met_goal AS met_goal
       ,achv.met_natl_goal AS met_natl_goal
-      ,achv.met_default_goal AS met_default_goal  
+      ,achv.met_default_goal AS met_default_goal
       ,achv.achv_unique_id
       ,achv.dna_unique_id
-FROM gabby.powerschool.cohort_identifiers_static co 
+
+      ,gr.gr_teacher
+FROM gabby.powerschool.cohort_identifiers_static co
 JOIN gabby.reporting.reporting_terms term
   ON co.schoolid = term.schoolid
  AND co.academic_year = term.academic_year
@@ -92,13 +94,17 @@ LEFT JOIN gabby.lit.achieved_by_round_static achv
  AND co.academic_year = achv.academic_year
  AND term.alt_name = achv.test_round
  AND achv.start_date <= CONVERT(DATE,GETDATE())
-LEFT JOIN gabby.lit.all_test_events_static testid 
+LEFT JOIN gabby.lit.all_test_events_static testid
   ON co.student_number = testid.student_number
- AND achv.achv_unique_id = testid.unique_id 
-LEFT JOIN gabby.lit.all_test_events_static dna 
+ AND achv.achv_unique_id = testid.unique_id
+LEFT JOIN gabby.lit.all_test_events_static dna
   ON co.student_number = dna.student_number
- AND achv.dna_unique_id = dna.unique_id 
+ AND achv.dna_unique_id = dna.unique_id
+LEFT JOIN gabby.lit.guided_reading_roster gr
+  ON co.student_number = gr.student_number
+ AND co.academic_year = gr.academic_year
+ AND term.alt_name = gr.test_round
 WHERE co.rn_year = 1
   AND co.grade_level != 99
   AND co.enroll_status = 0
-  AND co.academic_year >= (gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1)  
+  AND co.academic_year >= (gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1)
