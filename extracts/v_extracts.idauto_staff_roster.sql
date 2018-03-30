22f56d83c6888db8fdb420c66c09885a5be0cb70
Charlie Bini <CBini@kippnj.org>
2018-03-29 23:36:34 -0400

    updates

--- extracts/v_extracts.idauto_staff_roster.sql
+++ extracts/v_extracts.idauto_staff_roster.sql
@@ -23,14 +23,14 @@ WHERE rn_curr = 1
 
 /*
 -- Dayforce draft code
-SELECT QUOTENAME(ISNULL(df.preferred_name, ''), '"') AS ["Preferred Name"]
+SELECT QUOTENAME(ISNULL(NULL, ''), '"') AS ["Preferred Name"]
       ,QUOTENAME(ISNULL(df.payroll_company_code, ''), '"') AS ["Company Code"]
       ,QUOTENAME(ISNULL(df.primary_on_site_department, ''), '"') AS ["Home Department Description"]
       ,QUOTENAME(ISNULL(df.primary_site, ''), '"') AS ["Location Description"]
       ,QUOTENAME(ISNULL(NULL, ''), '"') AS ["Business Unit Code"]
       ,QUOTENAME(ISNULL(df.primary_on_site_department, ''), '"') AS ["Business Unit Description"]
-      ,QUOTENAME(ISNULL(df.first_name, ''), '"') AS ["First Name"]
-      ,QUOTENAME(ISNULL(df.last_name, ''), '"') AS ["Last Name"]
+      ,QUOTENAME(ISNULL(df.preferred_first_name, ''), '"') AS ["First Name"]
+      ,QUOTENAME(ISNULL(df.preferred_last_name, ''), '"') AS ["Last Name"]
       ,QUOTENAME(ISNULL(df.df_employee_number, ''), '"') AS ["Position ID"]
       ,QUOTENAME(ISNULL(df.primary_job, ''), '"') AS ["Job Title Description"]
       ,QUOTENAME(ISNULL(df.status, ''), '"') AS ["Position Status"]
@@ -39,4 +39,4 @@ SELECT QUOTENAME(ISNULL(df.preferred_name, ''), '"') AS ["Preferred Name"]
       ,QUOTENAME(ISNULL(CONVERT(VARCHAR,(CONVERT(DATE,df.birth_date)),101), ''), '"') AS ["Birth Date"]
       ,QUOTENAME(ISNULL(COALESCE(df.adp_associate_id, CONVERT(VARCHAR,df.df_employee_number)), ''), '"') AS ["Associate ID"] 
 FROM gabby.dayforce.staff_roster df
-*/
+--*/
