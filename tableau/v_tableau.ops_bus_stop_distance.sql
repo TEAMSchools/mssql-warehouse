118a6b205bcb7ddf23b65c2f8fde9fe1ac5ef245
Charlie Bini <CBini@kippnj.org>
2018-04-10 14:05:19 -0400

    porting from mandi (whaaaa???)

---
+++ tableau/v_tableau.ops_bus_stop_distance.sql
@@ -0,0 +1,15 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW tableau.ops_bus_stop_distance AS
+
+SELECT student_number AS student_id
+      ,location_name AS bus_stop_name
+      ,distance AS distance_to_bus_stop
+      ,RANK() OVER(
+        PARTITION BY student_number
+          ORDER BY distance ASC) AS distance_rank
+      ,ROW_NUMBER() OVER(
+        PARTITION BY student_number
+          ORDER BY distance ASC) AS distance_rank_unique
+FROM gabby.ops.bus_stop_distance
