b1adb54b2a95b6abb7a283a03d2909275505a516
Charlie Bini <CBini@kippnj.org>
2018-05-09 15:25:41 -0400

    updates for goal sheets

--- nwea/v_nwea.best_baseline.sql
+++ nwea/v_nwea.best_baseline.sql
@@ -3,32 +3,18 @@ GO
 
 CREATE OR ALTER VIEW nwea.best_baseline AS
 
-WITH roster AS (
-  SELECT student_number
-        ,academic_year        
-  FROM gabby.powerschool.cohort_identifiers_static
-  WHERE schoolid != 999999
-    AND rn_year = 1    
- )
- 
-,subj (measurementscale) AS (
-  SELECT 'Mathematics' UNION
-  SELECT 'Reading' UNION
+WITH subj AS (
+  SELECT 'Mathematics' AS measurementscale UNION
+  SELECT 'Reading'  UNION
   SELECT 'Language Usage' UNION
   SELECT 'Science - General Science'
  )
-
-,roster_subj_scaffold AS ( 
-  SELECT roster.student_number
-        ,roster.academic_year
-        ,subj.measurementscale
-  FROM roster
-  CROSS JOIN subj       
- )
   
-SELECT sub.student_number
-      ,sub.academic_year
-      ,sub.measurementscale
+SELECT r.student_number
+      ,r.academic_year
+      
+      ,subj.measurementscale 
+      
       ,CASE
         WHEN map_fall.test_ritscore > map_spr.test_ritscore THEN map_fall.test_id
         WHEN map_spr.test_ritscore IS NULL THEN map_fall.test_id
@@ -59,16 +45,19 @@ SELECT sub.student_number
         WHEN map_spr.test_ritscore IS NULL THEN map_fall.ritto_reading_score
         ELSE map_spr.ritto_reading_score
        END AS lexile_score
-FROM roster_subj_scaffold sub
-LEFT OUTER JOIN gabby.nwea.assessment_result_identifiers map_fall /* CURRENT YEAR FALL */
-  ON sub.student_number = map_fall.student_id
- AND sub.academic_year = map_fall.academic_year 
- AND sub.measurementscale = map_fall.measurement_scale
+FROM gabby.powerschool.cohort_identifiers_static r
+CROSS JOIN subj
+LEFT JOIN gabby.nwea.assessment_result_identifiers map_fall /* CURRENT YEAR FALL */
+  ON r.student_number = map_fall.student_id
+ AND r.academic_year = map_fall.academic_year 
+ AND subj.measurementscale = map_fall.measurement_scale
  AND map_fall.rn_term_subj = 1 
  AND map_fall.term = 'Fall'
-LEFT OUTER JOIN gabby.nwea.assessment_result_identifiers map_spr /* PREVIOUS YEAR SPRING */
-  ON sub.student_number = map_spr.student_id
- AND sub.academic_year = map_spr.test_year 
- AND sub.measurementscale = map_spr.measurement_scale
+LEFT JOIN gabby.nwea.assessment_result_identifiers map_spr /* PREVIOUS YEAR SPRING */
+  ON r.student_number = map_spr.student_id
+ AND r.academic_year = map_spr.test_year 
+ AND subj.measurementscale = map_spr.measurement_scale
  AND map_spr.rn_term_subj = 1
  AND map_spr.term = 'Spring'
+WHERE r.schoolid != 999999
+  AND r.rn_year = 1
