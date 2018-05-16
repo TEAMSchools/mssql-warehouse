7f431cfa602e16459e13cf1b26bc416148b0e26b
Charlie Bini <CBini@kippnj.org>
2018-05-16 16:47:10 -0400

    filter out miami

--- extracts/v_extracts.mailroute_new_users.sql
+++ extracts/v_extracts.mailroute_new_users.sql
@@ -7,3 +7,4 @@ SELECT REPLACE(mail,'kippnj','teamschools') AS email_addr
 FROM gabby.adsi.user_attributes
 WHERE createtimestamp >= CONVERT(DATE,DATEADD(HOUR,-144,SYSDATETIME()))
   AND mail IS NOT NULL
+  AND company != 'KIPP Miami'
