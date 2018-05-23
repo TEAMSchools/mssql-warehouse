f474ae7f7122fe79d9db6933b63af0d818633bc7
Charlie Bini <CBini@kippnj.org>
2018-05-23 09:44:56 -0400

    ensuring ugrad enrollments are valid type

--- alumni/v_alumni.enrollment_identifiers.sql
+++ alumni/v_alumni.enrollment_identifiers.sql
@@ -5,7 +5,7 @@ CREATE OR ALTER VIEW alumni.enrollment_identifiers AS
 
 WITH enrollments AS (
   SELECT student_c
-        ,MAX(CASE WHEN rn_recent_ugrad_enrollment = 1 THEN enrollment_c END) AS recent_ugrad_enrollment_c
+        ,MAX(CASE WHEN is_ecc_degree_type = 1 AND rn_recent_ugrad_enrollment = 1 THEN enrollment_c END) AS recent_ugrad_enrollment_c
         ,MAX(CASE WHEN is_ecc_degree_type = 1 AND rn_ecc_enrollment = 1 THEN enrollment_c END) AS ecc_enrollment_c      
         ,MAX(CASE WHEN is_hs_degree_type = 1 AND rn_hs_enrollment = 1 THEN enrollment_c END) AS hs_enrollment_c
   FROM
@@ -15,7 +15,7 @@ WITH enrollments AS (
              ,is_ecc_degree_type
              ,is_hs_degree_type
              ,ROW_NUMBER() OVER(
-                PARTITION BY student_c
+                PARTITION BY student_c, is_ecc_degree_type
                   ORDER BY start_date_c DESC, actual_end_date_c DESC) AS rn_recent_ugrad_enrollment
              ,ROW_NUMBER() OVER(
                 PARTITION BY student_c, is_ecc_degree_type
@@ -26,9 +26,7 @@ WITH enrollments AS (
        FROM
            (
             SELECT e.student_c
-                  ,e.id AS enrollment_c                 
-                  ,e.pursuing_degree_type_c
-                  ,e.status_c
+                  ,e.id AS enrollment_c                                   
                   ,e.start_date_c
                   ,e.actual_end_date_c
                   ,CASE WHEN e.pursuing_degree_type_c IN ('Associate''s (2 year)', 'Bachelor''s (4-year)') THEN 1 ELSE 0 END AS is_ecc_degree_type      
@@ -67,11 +65,11 @@ SELECT e.student_c
       ,hs.start_date_c AS hs_start_date
       ,hs.actual_end_date_c AS hs_actual_end_date
 FROM enrollments e
-LEFT OUTER JOIN gabby.alumni.enrollment_c ug
+LEFT JOIN gabby.alumni.enrollment_c ug
   ON e.recent_ugrad_enrollment_c = ug.id
-LEFT OUTER JOIN gabby.alumni.enrollment_c ecc
+LEFT JOIN gabby.alumni.enrollment_c ecc
   ON e.ecc_enrollment_c = ecc.id
-LEFT OUTER JOIN gabby.alumni.account ecca
+LEFT JOIN gabby.alumni.account ecca
   ON ecc.school_c = ecca.id
-LEFT OUTER JOIN gabby.alumni.enrollment_c hs
+LEFT JOIN gabby.alumni.enrollment_c hs
   ON e.hs_enrollment_c = hs.id
