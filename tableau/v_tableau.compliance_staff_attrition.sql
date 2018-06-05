e779b07b908c6693b164b1b9b7a256303ae36b55
Charlie Bini <CBini@kippnj.org>
2018-06-05 15:35:21 -0400

    update w/ dayforce tables

--- tableau/v_tableau.compliance_staff_attrition.sql
+++ tableau/v_tableau.compliance_staff_attrition.sql
@@ -63,27 +63,25 @@ WITH roster AS (
  )
 
 ,scaffold AS (
-  SELECT associate_id        
-        ,preferred_first
-        ,preferred_last
-        ,entity
-        ,job_title
+  SELECT df_employee_number        
+        ,preferred_first_name
+        ,preferred_last_name
+        ,legal_entity_name
         ,location
-        ,benefits_eligibility_class_description
+        ,job_family
         ,academic_year
         ,termination_date
-        ,termination_reason_description    
+        ,status_reason  
         ,academic_year_entrydate
         ,academic_year_exitdate
   FROM
       (
-       SELECT r.associate_id
-             ,r.entity
-             ,r.preferred_first
-             ,r.preferred_last                          
-             ,r.job_title
+       SELECT r.df_employee_number
+             ,r.legal_entity_name
+             ,r.preferred_first_name
+             ,r.preferred_last_name
              ,r.location
-             ,r.benefits_eligibility_class_description
+             ,r.job_family
              ,CASE WHEN r.end_academic_year =  y.academic_year THEN r.termination_date END AS termination_date
       
              ,y.academic_year
@@ -94,9 +92,9 @@ WITH roster AS (
                ELSE DATEFROMPARTS((y.academic_year + 1), 6, 30)
               END AS academic_year_exitdate
              ,ROW_NUMBER() OVER(
-                PARTITION BY r.associate_id, y.academic_year
+                PARTITION BY r.df_employee_number, y.academic_year
                   ORDER BY r.position_start_date DESC, COALESCE(r.termination_date,CONVERT(DATE,GETDATE())) DESC) AS rn_dupe_academic_year
-              ,termination_reason_description    
+              ,status_reason  
        FROM roster r
        JOIN years y
          ON y.academic_year BETWEEN r.start_academic_year AND COALESCE(r.end_academic_year, gabby.utilities.GLOBAL_ACADEMIC_YEAR())
@@ -104,17 +102,16 @@ WITH roster AS (
   WHERE rn_dupe_academic_year = 1
  )
 
-SELECT d.associate_id      
-      ,d.preferred_first
-      ,d.preferred_last
-      ,d.job_title
+SELECT d.df_employee_number    
+      ,d.preferred_first_name
+      ,d.preferred_last_name
       ,d.location
-      ,d.entity
-      ,d.benefits_eligibility_class_description
+      ,d.legal_entity_name
+      ,d.job_family
       ,d.academic_year      
       ,d.academic_year_entrydate      
       ,d.academic_year_exitdate
-      ,d.termination_reason_description 
+      ,d.status_reason
       ,CASE 
         WHEN d.academic_year_exitdate >= DATEFROMPARTS(d.academic_year, 9, 1) 
          AND d.academic_year_entrydate <= DATEFROMPARTS((d.academic_year + 1), 4, 30) 
@@ -131,5 +128,5 @@ SELECT d.associate_id
        END AS is_attrition
 FROM scaffold d
 LEFT OUTER JOIN scaffold n
-  ON d.associate_id = n.associate_id
+  ON d.df_employee_number = n.df_employee_number
  AND d.academic_year = (n.academic_year - 1)
