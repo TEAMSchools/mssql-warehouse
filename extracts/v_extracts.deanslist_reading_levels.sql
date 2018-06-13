665af6f2c95961320888a51663d55c98837e7dff
Charlie Bini <CBini@kippnj.org>
2018-06-13 16:39:28 -0400

    patch for displaying Q4

--- extracts/v_extracts.deanslist_reading_levels.sql
+++ extracts/v_extracts.deanslist_reading_levels.sql
@@ -7,6 +7,7 @@ SELECT student_number
       ,academic_year
       ,test_round
       ,CASE 
+        WHEN reporting_term = 'LIT4' THEN read_lvl
         WHEN end_date > CONVERT(DATE,GETDATE()) THEN NULL
         WHEN end_date <= CONVERT(DATE,GETDATE()) THEN read_lvl        
         WHEN is_new_test = 1 THEN read_lvl
@@ -14,6 +15,7 @@ SELECT student_number
        END AS read_lvl
       ,goal_lvl
       ,CASE 
+        WHEN reporting_term = 'LIT4' THEN lvl_num
         WHEN end_date > CONVERT(DATE,GETDATE()) THEN NULL
         WHEN end_date <= CONVERT(DATE,GETDATE()) THEN lvl_num
         WHEN is_new_test = 1 THEN lvl_num
@@ -21,7 +23,7 @@ SELECT student_number
        END AS read_lvl_num
       ,goal_num AS goal_lvl_num
       ,CASE
-        WHEN end_date > CONVERT(DATE,GETDATE()) THEN NULL
+        WHEN reporting_term != 'LIT4' AND end_date > CONVERT(DATE,GETDATE()) THEN NULL
         WHEN met_goal = 1 THEN 'On Track'
         WHEN met_goal = 0 THEN 'Off Track'
        END AS met_goal
