6c5dfec9ffa0108fba9f7e41345fa609cfb4d9a3
Charlie Bini <CBini@kippnj.org>
2018-07-13 14:18:19 -0400

    reviewed

--- tableau/v_tableau.compliance_staff_attrition.sql
+++ tableau/v_tableau.compliance_staff_attrition.sql
@@ -1,20 +1,20 @@
-USE gabby
+USE gabby;
 GO
 
---CREATE OR ALTER VIEW tableau.compliance_staff_attrition AS
+CREATE OR ALTER VIEW tableau.compliance_staff_attrition AS
 
 WITH roster AS (
-  SELECT adp_associate_id        
+  SELECT adp_associate_id
         ,preferred_first_name
         ,preferred_last_name
         ,legal_entity_name
         ,primary_site
-        ,COALESCE(rehire_date,original_hire_date) AS position_start_date
+        ,COALESCE(rehire_date, original_hire_date) AS position_start_date
         ,termination_date
-        ,status_reason        
+        ,status_reason
         ,job_family
         ,primary_job
-        ,gabby.utilities.DATE_TO_SY(COALESCE(rehire_date,original_hire_date)) AS start_academic_year
+        ,gabby.utilities.DATE_TO_SY(COALESCE(rehire_date, original_hire_date)) AS start_academic_year
         ,gabby.utilities.DATE_TO_SY(termination_date) AS end_academic_year
         ,df_employee_number
   FROM gabby.dayforce.staff_roster
@@ -24,10 +24,10 @@ WITH roster AS (
   SELECT n AS academic_year
   FROM gabby.utilities.row_generator
   WHERE n BETWEEN 2000 AND gabby.utilities.GLOBAL_ACADEMIC_YEAR()
- )
+)
 
 ,scaffold AS (
-  SELECT df_employee_number       
+  SELECT df_employee_number
         ,preferred_first_name
         ,preferred_last_name
         ,legal_entity_name
@@ -36,31 +36,32 @@ WITH roster AS (
         ,job_family
         ,academic_year
         ,termination_date
-        ,status_reason    
+        ,status_reason
         ,academic_year_entrydate
         ,academic_year_exitdate
-  FROM
+  FROM 
       (
        SELECT r.df_employee_number
              ,r.legal_entity_name
              ,r.preferred_first_name
-             ,r.preferred_last_name                    
+             ,r.preferred_last_name
              ,r.primary_job
              ,r.primary_site
              ,r.job_family
-             ,CASE WHEN r.end_academic_year =  y.academic_year THEN r.termination_date END AS termination_date
-      
+             ,r.status_reason
+             ,CASE WHEN r.end_academic_year = y.academic_year THEN r.termination_date END AS termination_date
+             
              ,y.academic_year
-
-             ,CASE WHEN r.start_academic_year = y.academic_year THEN r.position_start_date ELSE DATEFROMPARTS(y.academic_year, 7, 1) END AS academic_year_entrydate
-             ,CASE                 
+             ,CASE
+               WHEN r.start_academic_year = y.academic_year THEN r.position_start_date
+               ELSE DATEFROMPARTS(y.academic_year, 7, 1)
+              END AS academic_year_entrydate
+             ,CASE
                WHEN r.end_academic_year = y.academic_year THEN COALESCE(r.termination_date, DATEFROMPARTS((y.academic_year + 1), 6, 30))
                ELSE DATEFROMPARTS((y.academic_year + 1), 6, 30)
               END AS academic_year_exitdate
-             ,ROW_NUMBER() OVER(
-                PARTITION BY r.df_employee_number, y.academic_year
-                  ORDER BY r.position_start_date DESC, COALESCE(r.termination_date,CONVERT(DATE,GETDATE())) DESC) AS rn_dupe_academic_year
-              ,status_reason    
+             ,ROW_NUMBER() OVER(PARTITION BY r.df_employee_number, y.academic_year
+                                  ORDER BY r.position_start_date DESC, COALESCE(r.termination_date, CONVERT(DATE, GETDATE())) DESC) AS rn_dupe_academic_year             
        FROM roster r
        JOIN years y
          ON y.academic_year BETWEEN r.start_academic_year AND COALESCE(r.end_academic_year, gabby.utilities.GLOBAL_ACADEMIC_YEAR())
@@ -68,34 +69,32 @@ WITH roster AS (
   WHERE rn_dupe_academic_year = 1
  )
 
-SELECT d.df_employee_number    
+SELECT d.df_employee_number
       ,d.preferred_first_name
       ,d.preferred_last_name
       ,d.primary_job
       ,d.primary_site
       ,d.legal_entity_name
       ,d.job_family
-      ,d.academic_year      
-      ,d.academic_year_entrydate      
+      ,d.academic_year
+      ,d.academic_year_entrydate
       ,d.academic_year_exitdate
       ,d.status_reason
-      ,CASE 
-        WHEN d.academic_year_exitdate >= DATEFROMPARTS(d.academic_year, 9, 1) 
-         AND d.academic_year_entrydate <= DATEFROMPARTS((d.academic_year + 1), 4, 30) 
-               THEN 1         
-        ELSE 0 
-       END AS is_denominator      
-
+      ,d.termination_date
+      ,CASE
+        WHEN d.academic_year_exitdate >= DATEFROMPARTS(d.academic_year, 9, 1)
+         AND d.academic_year_entrydate <= DATEFROMPARTS((d.academic_year + 1), 4, 30) THEN 1
+        ELSE 0
+       END AS is_denominator
+      
       ,n.academic_year_exitdate AS next_academic_year_exitdate
-      ,d.termination_date     
-      ,COALESCE(n.academic_year_exitdate, d.termination_date, DATEFROMPARTS(d.academic_year + 2, 6, 30)) AS attrition_exitdate 
+      
+      ,COALESCE(n.academic_year_exitdate, d.termination_date, DATEFROMPARTS(d.academic_year + 2, 6, 30)) AS attrition_exitdate
       ,CASE
         WHEN COALESCE(n.academic_year_exitdate, d.termination_date, DATEFROMPARTS(d.academic_year + 2, 6, 30)) < DATEFROMPARTS(d.academic_year + 1, 9, 1) THEN 1
         ELSE 0
        END AS is_attrition
 FROM scaffold d
-LEFT OUTER JOIN scaffold n
+LEFT JOIN scaffold n
   ON d.df_employee_number = n.df_employee_number
  AND d.academic_year = (n.academic_year - 1)
- WHERE d.primary_job NOT IN ('Intern'
-                          ,'Security')
