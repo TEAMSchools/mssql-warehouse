bff347eb24e63a111994f95e068b8dbce5604f4a
Charlie Bini <CBini@kippnj.org>
2018-08-03 15:02:28 -0400

    updates for illuminate extracts

--- extracts/v_extracts.illuminate_mastschd.sql
+++ extracts/v_extracts.illuminate_mastschd.sql
@@ -4,29 +4,31 @@ GO
 CREATE OR ALTER VIEW extracts.illuminate_mastschd AS
 
 SELECT CONCAT(CASE
-               WHEN enr.db_name = 'kippnewark' THEN 10
-               WHEN enr.db_name = 'kippcamden' THEN 20
-               WHEN enr.db_name = 'kippmiami' THEN 30
+               WHEN sec.db_name = 'kippnewark' THEN 10
+               WHEN sec.db_name = 'kippcamden' THEN 20
+               WHEN sec.db_name = 'kippmiami' THEN 30
               END
-             ,enr.sectionid) AS [01 Section ID]
-      ,enr.schoolid AS [02 Site ID]
-      ,t.name AS [03 Term Name]
-      ,enr.course_number AS [04 Course ID]
-      ,enr.teachernumber AS [05 User ID]
-      ,enr.expression AS [06 Period]
-      ,CONCAT(academic_year, '-', (academic_year + 1)) AS [07 Academic Year]
+             ,sec.id) AS [01 Section ID]
+      ,sec.schoolid AS [02 Site ID]
+      ,tr.name AS [03 Term Name]
+      ,sec.course_number AS [04 Course ID]
+      ,t.teachernumber AS [05 User ID]
+      ,sec.expression AS [06 Period]
+      ,CONCAT((sec.yearid + 1990), '-', (sec.yearid + 1991)) AS [07 Academic Year]
       ,NULL AS [08 Room Number]
       ,NULL AS [09 Session Type ID]
       ,NULL AS [10 Local Term ID]
       ,NULL AS [11 Quarter Num]
-      ,enr.dateenrolled AS [12 User Start Date]
-      ,enr.dateleft AS [13 User End Date]
+      ,tr.firstday AS [12 User Start Date]
+      ,tr.lastday AS [13 User End Date]
       ,1 AS [14 Primary Teacher]
       ,NULL AS [15 Teacher Competency Level]
       ,NULL AS [16 Is Attendance Enabled]
-FROM gabby.powerschool.course_enrollments_static enr
-JOIN gabby.powerschool.terms t
-  ON enr.termid = t.id
- AND enr.schoolid = t.schoolid
- AND enr.db_name = t.db_name
-WHERE enr.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR();
+FROM gabby.powerschool.sections sec
+JOIN gabby.powerschool.terms tr
+  ON sec.termid = tr.id
+ AND sec.schoolid = tr.schoolid
+ AND sec.db_name = tr.db_name
+JOIN gabby.powerschool.teachers_static t
+  ON sec.teacher = t.id
+WHERE sec.yearid = (gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1990);
---
+++ extracts/v_extracts.illuminate_roles.sql
@@ -0,0 +1,22 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW extracts.illuminate_roles AS
+
+SELECT COALESCE(id.ps_teachernumber, CONVERT(VARCHAR(25),df.df_employee_number)) AS [01 Local User ID]
+      ,sch.school_number AS [02 Site ID]
+      ,'School Leadership' AS [03 Role Name]
+      ,CONCAT(gabby.utilities.GLOBAL_ACADEMIC_YEAR() , '-', (gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1))  AS [04 Academic Year]
+      ,1 AS [05 Session Type ID]
+FROM gabby.dayforce.staff_roster df
+JOIN gabby.people.id_crosswalk_powerschool id
+  ON df.df_employee_number = id.df_employee_number
+ AND id.is_master = 1
+JOIN gabby.powerschool.schools sch
+  ON CASE 
+      WHEN df.legal_entity_name = 'TEAM Academy Charter Schools' THEN 'kippnewark'
+      WHEN df.legal_entity_name = 'KIPP Cooper Norcross Academy' THEN 'kippcamden'
+      WHEN df.legal_entity_name = 'KIPP Miami' THEN 'kippmiami'
+      WHEN df.legal_entity_name = 'KIPP New Jersey' THEN sch.db_name
+     END = sch.db_name
+ AND sch.state_excludefromreporting = 0
