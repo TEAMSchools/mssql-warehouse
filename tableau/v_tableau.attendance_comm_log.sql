33b8fb38d98953d94379ba025af51615627c06a2
Charlie Bini <CBini@kippnj.org>
2018-08-06 18:32:08 -0400

    new view

--- tableau/v_tableau.attendance_comm_log.sql
+++ tableau/v_tableau.attendance_comm_log.sql
@@ -3,7 +3,32 @@ GO
 
 CREATE OR ALTER VIEW tableau.attendance_comm_log AS
 
-WITH commlog AS (
+WITH y1_ada AS (
+  SELECT studentid
+        ,schoolid
+        ,(yearid + 1990) AS academic_year
+        ,AVG(CONVERT(FLOAT,attendancevalue)) AS pct_attendance
+        ,SUM(ABS(attendancevalue - 1)) AS n_absences
+  FROM gabby.powerschool.ps_adaadm_daily_ctod
+  WHERE membershipvalue = 1
+  GROUP BY studentid
+          ,schoolid
+          ,(yearid + 1990)
+ )
+
+,weekly_ada AS (
+  SELECT studentid
+        ,schoolid
+        ,DATEADD(DAY, (1 - DATEPART(WEEKDAY, calendardate)), calendardate) AS week_start_date
+        ,SUM(ABS(attendancevalue - 1)) AS n_absences
+  FROM gabby.powerschool.ps_adaadm_daily_ctod
+  WHERE membershipvalue = 1
+  GROUP BY studentid
+          ,schoolid
+          ,DATEADD(DAY, (1 - DATEPART(WEEKDAY, calendardate)), calendardate) 
+ )
+
+,commlog AS (
   SELECT c.student_school_id
         ,c.reason AS commlog_reason
         ,c.response AS commlog_notes        
@@ -26,44 +51,40 @@ WITH commlog AS (
 
 SELECT co.student_number
       ,co.lastfirst
+      ,co.academic_year
+      ,co.region
+      ,co.school_name
       ,co.grade_level
       ,co.team
-      ,co.region
-      ,co.reporting_schoolid
+      ,co.enroll_status      
+      ,co.date AS week_start_date
+      ,DATEADD(DAY, 6, co.date) AS week_end_date
       
-      ,att.att_date
-      ,att.att_comment
-      ,CASE WHEN ac.att_code = 'true' THEN 'T' ELSE ac.att_code END AS att_code 
-      
-      ,CASE
-        WHEN att.schoolid = 73253 THEN co.advisor_name
-        ELSE cc.section_number
-       END AS homeroom
+      ,y1.pct_attendance AS y1_ada
+      ,y1.n_absences AS y1_absences
 
-      ,cl.commlog_staff_name
+      ,wk.n_absences AS wk_absences
+
       ,cl.commlog_reason
       ,cl.commlog_notes
       ,cl.commlog_topic
-      ,cl.followup_staff_name
+      ,cl.commlog_date
+      ,cl.commlog_staff_name
       ,cl.followup_init_notes
       ,cl.followup_close_notes
-
-      ,ROW_NUMBER() OVER(
-         PARTITION BY att.studentid, att.att_date
-           ORDER BY cl.student_school_id DESC) AS rn_date
-FROM gabby.powerschool.attendance_clean att
-JOIN gabby.powerschool.attendance_code ac
-  ON att.attendance_codeid = ac.id
- AND ac.att_code LIKE 'A%'
-LEFT OUTER JOIN gabby.powerschool.cc
-  ON att.studentid = cc.studentid
- AND att.att_date BETWEEN cc.dateenrolled AND cc.dateleft
- AND cc.course_number = 'HR'
-JOIN gabby.powerschool.cohort_identifiers_static co
-  ON att.studentid = co.studentid
- AND att.att_date BETWEEN co.entrydate AND co.exitdate
-LEFT OUTER JOIN commlog cl
+      ,cl.followup_staff_name
+      ,cl.outstanding      
+FROM gabby.powerschool.cohort_identifiers_scaffold co
+LEFT JOIN y1_ada y1
+  ON co.studentid = y1.studentid
+ AND co.schoolid = y1.schoolid
+ AND co.academic_year = y1.academic_year
+LEFT JOIN weekly_ada wk
+  ON co.studentid = wk.studentid
+ AND co.schoolid = wk.schoolid
+ AND co.date = wk.week_start_date
+LEFT JOIN commlog cl
   ON co.student_number = cl.student_school_id
- AND att.att_date = cl.commlog_date
-WHERE att.att_date >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 07, 01)
-  AND att.att_mode_code = 'ATT_ModeDaily'
+ AND cl.commlog_date BETWEEN co.date AND DATEADD(DAY, 6, co.date)
+WHERE DATEPART(WEEKDAY, co.date) = 1
+  AND co.academic_year >= (gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1)
