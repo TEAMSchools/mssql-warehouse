264bdf415f0820c1bd47054ddf385878d4e05d85
Charlie Bini <CBini@kippnj.org>
2018-08-07 18:16:07 -0400

    refactor to pull from franklin first

--- mcs/v_mcs.lunch_info.sql
+++ mcs/v_mcs.lunch_info.sql
@@ -3,54 +3,48 @@ GO
 
 CREATE OR ALTER VIEW mcs.lunch_info AS
 
-SELECT CONVERT(INT,c.studentnumber) AS studentnumber
-      ,c.reimbursableonlybalance
-      ,c.unallocatedbalance
-      ,c.reimbursableonlybalance + c.unallocatedbalance AS [balance]      
-
-      ,LEFT(cat.shortdesc, 1) AS mealbenefitstatus /* returns f, r, p */
-      ,CONVERT(VARCHAR(25),cat.shortdesc) AS shortdesc
-      
+SELECT CONVERT(INT,s.studentnumber) AS studentnumber
       ,s.currentapplicationid
 
       ,CONVERT(VARCHAR(125),e.description) AS description
-FROM [winsql06\han].[newton].[dbo].[customer] c
-LEFT JOIN [winsql06\han].[newton].[dbo].[student_guid_link] g
-  ON c.[customer_recid] = g.[customerid]
-LEFT JOIN [winsql06\han].[newton].[dbo].[customer_category] cat
-  ON c.[customer_categoryid] = cat.[customer_category_recid]
-LEFT JOIN [winsql06\han].[franklin].[dbo].student s
-  ON g.studentguid = s.globaluid
-LEFT JOIN [winsql06\han].[franklin].[dbo].[eligibility] e
-  ON s.[eligibilityid] = e.[eligibility_recid]
-WHERE cat.[isstudent] = 1 /* only students */
-  AND ISNUMERIC(c.[studentnumber]) = 1
-  AND c.studentnumber IN (SELECT student_number FROM kippnewark.powerschool.students)
-  AND c.inactive = 0
-  
-UNION ALL
 
-SELECT CONVERT(INT,c.studentnumber) AS studentnumber
+      ,LEFT(mbs.description, 1) AS mealbenefitstatus /* returns F/R/P */
+      ,CONVERT(VARCHAR(25),mbs.description) AS shortdesc
+
       ,c.reimbursableonlybalance
       ,c.unallocatedbalance
       ,c.reimbursableonlybalance + c.unallocatedbalance AS balance
-      
-      ,LEFT(cat.shortdesc, 1) AS mealbenefitstatus /* returns f, r, p */
-      ,CONVERT(VARCHAR(25),cat.shortdesc) AS shortdesc
-      
+FROM [winsql06\han].[franklin].[dbo].[student] s
+LEFT JOIN [winsql06\han].[franklin].[dbo].[eligibility] e 
+  ON s.[eligibilityid] = e.[eligibility_recid]
+LEFT JOIN [winsql06\han].[franklin].[dbo].[meal_benefits_status] mbs
+  ON e.meal_benefits_statusid = mbs.meal_benefits_status_recid
+LEFT JOIN [winsql06\han].[newton].[dbo].[student_guid_link] g
+  ON s.globaluid = g.studentguid
+LEFT JOIN [winsql06\han].[newton].[dbo].[customer] c 
+  ON g.[customerid] = c.[customer_recid]
+WHERE s.inactivedate IS NULL
+
+UNION ALL
+
+SELECT CONVERT(INT,s.studentnumber) AS studentnumber
       ,s.currentapplicationid
 
       ,CONVERT(VARCHAR(125),e.description) AS description
-FROM [winsql06\yoda].[newton].[dbo].[customer] c 
-LEFT JOIN [winsql06\yoda].[newton].[dbo].[student_guid_link] g 
-  ON c.[customer_recid] = g.[customerid]
-LEFT JOIN [winsql06\yoda].[newton].[dbo].[customer_category] cat 
-  ON c.[customer_categoryid] = cat.[customer_category_recid]
-LEFT JOIN [winsql06\yoda].[franklin].[dbo].student s 
-  ON g.studentguid = s.globaluid
+
+      ,LEFT(mbs.description, 1) AS mealbenefitstatus /* returns F/R/P */
+      ,CONVERT(VARCHAR(25),mbs.description) AS shortdesc
+
+      ,c.reimbursableonlybalance
+      ,c.unallocatedbalance
+      ,c.reimbursableonlybalance + c.unallocatedbalance AS balance
+FROM [winsql06\yoda].[franklin].[dbo].[student] s
 LEFT JOIN [winsql06\yoda].[franklin].[dbo].[eligibility] e 
   ON s.[eligibilityid] = e.[eligibility_recid]
-WHERE cat.[isstudent] = 1 /* only students */
-  AND ISNUMERIC(c.[studentnumber]) = 1
-  AND c.studentnumber IN (SELECT student_number FROM kippcamden.powerschool.students)
-  AND c.inactive = 0
+LEFT JOIN [winsql06\yoda].[franklin].[dbo].[meal_benefits_status] mbs
+  ON e.meal_benefits_statusid = mbs.meal_benefits_status_recid
+LEFT JOIN [winsql06\yoda].[newton].[dbo].[student_guid_link] g
+  ON s.globaluid = g.studentguid
+LEFT JOIN [winsql06\yoda].[newton].[dbo].[customer] c 
+  ON g.[customerid] = c.[customer_recid]
+WHERE s.inactivedate IS NULL
