d2929fe7372b1a51963d9f798835557f0a783b10
kevin.verhoff <kevin.verhoff@gmail.com>
2018-08-09 15:00:37 -0400

    benefits

--- extracts/v_extracts.accounting_compliance_feed.sql
+++ extracts/v_extracts.accounting_compliance_feed.sql
@@ -13,7 +13,7 @@ SELECT number
        ,base_salary AS base_salary_curr
        ,status AS status_curr
        ,LAG(base_salary,1,0) OVER(partition by number ORDER BY effective_start) AS base_salary_prev
-      ,LAG(status,1,0) OVER(partition by number ORDER BY effective_start) AS status_prev
+       ,LAG(status,1,0) OVER(partition by number ORDER BY effective_start) AS status_prev
        ,ROW_NUMBER() OVER(partition by number ORDER BY effective_start DESC) AS rn_curr
 FROM dayforce.employee_status
 ) sub
@@ -41,6 +41,31 @@ WHERE rn_curr = 1
               ,'Pension Number - TPAF')
 )
 
+,b AS
+(SELECT TOP 100 *
+      ,ROW_NUMBER() OVER
+        (PARTITION BY employee_number, category
+         ORDER BY ben_plan_option_effective_start) AS rn_curr 
+FROM 
+     (SELECT *
+             ,CASE WHEN ben_plan_name = 'Aetna HDHP' OR ben_plan_option_name = 'Waive Medical' THEN 'Medical'
+                   WHEN ben_plan_name IN ('Aetna PPO High', 'Aetna PPO Low') OR ben_plan_option_name = 'Waive Denatl' THEN 'Dental'
+                   WHEN ben_plan_name = 'Aetna Vision' OR ben_plan_option_name = 'Waive Vision' THEN 'Vision'
+                   WHEN ben_plan_name = 'Alerus 403b' OR ben_plan_option_name LIKE 'Waive 403b%' THEN '403b'
+                   WHEN ben_plan_option_name IN ('Flexible Spending Account - Dependent Care',  'Waive Flexible Spending Account - Dependent Care') THEN 'FSA-Dep'
+                   WHEN ben_plan_option_name IN ('Flexible Spending Account - Health Care', 'Waive Flexible Spending Account - Health Care') THEN 'FSA-HC'
+                   WHEN ben_plan_name = 'Health Savings Account' OR ben_plan_option_name = 'Waive Health Savings Account' THEN 'HSA'
+                   WHEN ben_plan_name = 'Imputed Income - Life Insurance' THEN 'Imputed Income - Life Insurance'
+                   WHEN ben_plan_option_name IN ('Basic Supplemental AD&D (Employer)' ,'Basic Supplemental Life (Employer)' ,'Supplemental Employee Life & AD&D Insurance' ,'Waive Supplemental Employee Life and AD&D Insurance') THEN 'Life & AD&D supplemental'
+                   WHEN ben_plan_name = 'Voluntary Accident Coverage' OR ben_plan_option_name = 'Waive Voluntary Accident Coverage' THEN 'Voluntary Accident Coverage'
+                   WHEN ben_plan_name = 'Voluntary Hospital Indemnity' OR ben_plan_option_name = 'Waive Hospital Indemnity' THEN 'Voluntary Hospital Indemnity'
+                   ELSE ben_plan_name
+                   
+                   END AS category
+      FROM dayforce.employee_benefits_options) sub
+(
+
+
 SELECT r.df_employee_number
       ,r.first_name
       ,r.last_name
@@ -67,5 +92,5 @@ FROM dayforce.staff_roster r LEFT OUTER JOIN s ON
           r.df_employee_number = s.number
      LEFT OUTER JOIN p ON
           r.df_employee_number = p.employee_reference_code
---     LEFT OUTER JOIN dayforce.employee_benefits_options b
+--     LEFT OUTER JOIN b
 --          r.df_employee_number = b.employee_number
