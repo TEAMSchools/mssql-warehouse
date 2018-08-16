fa7988fee6e6d7b0636a852dcec1f62867b4cac3
Charlie Bini <CBini@kippnj.org>
2018-08-16 09:47:55 -0400

    reverting to old version

--- tableau/v_tableau.attendance_comm_log.sql
+++ tableau/v_tableau.attendance_comm_log.sql
@@ -3,36 +3,7 @@ GO
 
 CREATE OR ALTER VIEW tableau.attendance_comm_log AS
 
-WITH y1_ada AS (
-  SELECT studentid
-        ,schoolid
-        ,db_name
-        ,(yearid + 1990) AS academic_year
-        ,AVG(CONVERT(FLOAT,attendancevalue)) AS pct_attendance
-        ,SUM(ABS(attendancevalue - 1)) AS n_absences
-  FROM gabby.powerschool.ps_adaadm_daily_ctod
-  WHERE membershipvalue = 1
-  GROUP BY studentid
-          ,schoolid
-          ,db_name
-          ,(yearid + 1990)
- )
-
-,weekly_ada AS (
-  SELECT studentid
-        ,schoolid
-        ,db_name
-        ,DATEADD(DAY, (1 - DATEPART(WEEKDAY, calendardate)), calendardate) AS week_start_date
-        ,SUM(ABS(attendancevalue - 1)) AS n_absences
-  FROM gabby.powerschool.ps_adaadm_daily_ctod
-  WHERE membershipvalue = 1
-  GROUP BY studentid
-          ,schoolid
-          ,db_name
-          ,DATEADD(DAY, (1 - DATEPART(WEEKDAY, calendardate)), calendardate) 
- )
-
-,commlog AS (
+WITH commlog AS (
   SELECT c.student_school_id
         ,c.reason AS commlog_reason
         ,c.response AS commlog_notes        
@@ -48,49 +19,54 @@ WITH y1_ada AS (
   FROM gabby.deanslist.communication c
   JOIN gabby.deanslist.users u
     ON c.dluser_id = u.dluser_id
-  LEFT OUTER JOIN gabby.deanslist.followups f
+  LEFT JOIN gabby.deanslist.followups f
     ON c.followup_id = f.followup_id
   WHERE (c.reason LIKE 'att:%' OR c.reason LIKE 'chronic%')
  )
 
 SELECT co.student_number
       ,co.lastfirst
-      ,co.academic_year
-      ,co.region
-      ,co.school_name
       ,co.grade_level
       ,co.team
-      ,co.enroll_status      
-      ,co.date AS week_start_date
-      ,DATEADD(DAY, 6, co.date) AS week_end_date
+      ,co.region
+      ,co.reporting_schoolid
       
-      ,y1.pct_attendance AS y1_ada
-      ,y1.n_absences AS y1_absences
+      ,att.att_date
+      ,att.att_comment
+      ,CASE WHEN ac.att_code = 'true' THEN 'T' ELSE ac.att_code END AS att_code 
+      
+      ,CASE
+        WHEN att.schoolid = 73253 THEN co.advisor_name
+        ELSE cc.section_number
+       END AS homeroom
 
-      ,wk.n_absences AS wk_absences
-
+      ,cl.commlog_staff_name
       ,cl.commlog_reason
       ,cl.commlog_notes
       ,cl.commlog_topic
-      ,cl.commlog_date
-      ,cl.commlog_staff_name
+      ,cl.followup_staff_name
       ,cl.followup_init_notes
       ,cl.followup_close_notes
-      ,cl.followup_staff_name
-      ,cl.outstanding      
-FROM gabby.powerschool.cohort_identifiers_scaffold co
-LEFT JOIN y1_ada y1
-  ON co.studentid = y1.studentid
- AND co.schoolid = y1.schoolid
- AND co.academic_year = y1.academic_year
- AND co.db_name = y1.db_name
-LEFT JOIN weekly_ada wk
-  ON co.studentid = wk.studentid
- AND co.schoolid = wk.schoolid
- AND co.date = wk.week_start_date
- AND co.db_name = wk.db_name
+
+      ,ROW_NUMBER() OVER(
+         PARTITION BY att.studentid, att.att_date
+           ORDER BY cl.student_school_id DESC) AS rn_date
+FROM gabby.powerschool.attendance_clean att
+JOIN gabby.powerschool.attendance_code ac
+  ON att.attendance_codeid = ac.id
+ AND att.db_name = ac.db_name
+ AND ac.att_code LIKE 'A%'
+LEFT JOIN gabby.powerschool.cc
+  ON att.studentid = cc.studentid
+ AND att.db_name = cc.db_name
+ AND att.att_date BETWEEN cc.dateenrolled AND cc.dateleft
+ AND cc.course_number = 'HR'
+JOIN gabby.powerschool.cohort_identifiers_static co
+  ON att.studentid = co.studentid
+ AND att.db_name = co.db_name
+ AND att.att_date BETWEEN co.entrydate AND co.exitdate
 LEFT JOIN commlog cl
   ON co.student_number = cl.student_school_id
- AND cl.commlog_date BETWEEN co.date AND DATEADD(DAY, 6, co.date)
-WHERE DATEPART(WEEKDAY, co.date) = 1
-  AND co.academic_year >= (gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1)
+ AND att.att_date = cl.commlog_date
+WHERE att.att_date >= DATEFROMPARTS((gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1), 07, 01)
+  AND att.att_mode_code = 'ATT_ModeDaily'
