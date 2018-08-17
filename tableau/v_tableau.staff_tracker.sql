a074207d27bebadc966ca33e7378e3d262eba45a
kevin.verhoff <kevin.verhoff@gmail.com>
2018-08-17 13:39:15 -0400

    switching_to_df
    We'll need to build out the filters, but we don't have much sample data yet.

--- tableau/v_tableau.staff_tracker.sql
+++ tableau/v_tableau.staff_tracker.sql
@@ -1,7 +1,7 @@
 USE gabby
 GO
 
-CREATE OR ALTER VIEW tableau.staff_tracker AS
+--CREATE OR ALTER VIEW tableau.staff_tracker AS
 
 WITH prof_calendar AS (
   SELECT cal.schoolid
@@ -20,40 +20,26 @@ WITH prof_calendar AS (
  )
 
 ,tracking_long AS (
-  SELECT associate_id
-        ,date
-        ,notes
-        ,field
-        ,CASE WHEN value = '' THEN NULL ELSE value END AS value
-  FROM
-      (
-       SELECT SUBSTRING(staff_name
-                       ,(CHARINDEX('[', staff_name) + 1)
-                       ,(LEN(staff_name) - CHARINDEX('[', staff_name) - 1)) AS associate_id
-             ,CONVERT(DATE,date) AS date
-             ,CONVERT(VARCHAR(250),notes_optional_) AS notes
-
-             ,ISNULL(CONVERT(VARCHAR,present),'') AS present
-             ,ISNULL(CONVERT(VARCHAR,on_time),'') AS on_time
-             ,ISNULL(CONVERT(VARCHAR,attire_optional_),'') AS attire
-             ,ISNULL(CONVERT(VARCHAR,lp_optional_),'') AS lp
-             ,ISNULL(CONVERT(VARCHAR,gr_lp_optional_),'') AS gr_lp             
-       FROM gabby.pm.teacher_tracker
-       WHERE ISDATE(date) = 1
-      ) sub
-  UNPIVOT(
-    value
-    FOR field IN (present, on_time, attire, lp, gr_lp)
-   ) u
- )
+  SELECT employee_number
+        ,pay_date as date
+        ,pay_code
+        ,pay_category
+  FROM dayforce.employee_attendance
+  )
 
 ,tracker_fields AS (
   SELECT DISTINCT
-         field
+         pay_code
   FROM tracking_long
  )
 
-SELECT COALESCE(df.adp_associate_id, CONVERT(VARCHAR,df.df_employee_number)) AS associate_id
+,directory AS (
+  SELECT CASE WHEN LEN(employeenumber) > 6 THEN RIGHT(employeenumber,LEN(employeenumber)-3) ELSE employeenumber END AS employeenumber
+        ,mail
+  FROM gabby.adsi.user_attributes_static
+ )
+ 
+SELECT df.df_employee_number
       ,df.preferred_name AS preferred_lastfirst       
       ,df.primary_site_schoolid AS schoolid
       ,df.primary_site AS location
@@ -67,19 +53,17 @@ SELECT COALESCE(df.adp_associate_id, CONVERT(VARCHAR,df.df_employee_number)) AS 
       ,cal.term
       ,cal.date_value            
        
-      ,f.field
-       
-      ,pt.notes      
-      ,pt.value
+      ,f.pay_code
+
 FROM gabby.dayforce.staff_roster df
-LEFT JOIN gabby.adsi.user_attributes_static dir
-  ON df.adp_associate_id = dir.idautopersonalternateid
+LEFT JOIN directory dir
+  ON df.df_employee_number = dir.employeenumber
 JOIN prof_calendar cal
   ON df.primary_site_schoolid = cal.schoolid
  AND df.position_effective_from_date <= cal.date_value
 CROSS JOIN tracker_fields f
 LEFT JOIN tracking_long pt
-  ON df.adp_associate_id = pt.associate_id 
+  ON df.df_employee_number = pt.employee_number
  AND cal.date_value = pt.date
- AND f.field = pt.field
+ AND f.pay_code = pt.pay_code
 WHERE df.status != 'TERMINATED'
