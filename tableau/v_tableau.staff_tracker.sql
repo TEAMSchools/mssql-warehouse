15eda98dce94064f4c079747fbfac9fd829f710f
Charlie Bini <CBini@kippnj.org>
2018-08-22 12:24:12 -0400

    code review
    * removing ctes -- not needed anymore
    * filtering out district office

--- tableau/v_tableau.staff_tracker.sql
+++ tableau/v_tableau.staff_tracker.sql
@@ -1,59 +1,9 @@
 USE gabby
 GO
 
---CREATE OR ALTER VIEW tableau.staff_tracker AS
+CREATE OR ALTER VIEW tableau.staff_tracker AS
 
-WITH prof_calendar AS (
-  SELECT cal.schoolid
-        ,cal.schoolid AS reporting_schoolid             
-        ,cal.date_value
-        ,gabby.utilities.DATE_TO_SY(cal.date_value) AS academic_year
-             
-        ,CONVERT(VARCHAR,dt.alt_name) AS term
-  FROM gabby.powerschool.calendar_day cal
-  JOIN gabby.reporting.reporting_terms dt
-    ON cal.schoolid = dt.schoolid
-   AND cal.date_value BETWEEN dt.start_date AND dt.end_date
-   AND dt.identifier = 'RT'   
-  WHERE cal.date_value BETWEEN DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1) AND GETDATE()
-    AND (cal.insession = 1 OR cal.type = 'PD')  
- )
-
-,tracking_long AS (
-  SELECT employee_number
-        ,pay_date as date
-        ,pay_code
-        ,pay_category
-  FROM dayforce.employee_attendance
-  WHERE pay_code NOT IN ('Assessment Coordinator'
-                         ,'Department Chair'
-                         ,'Discretionary Stipend'
-                         ,'Discretionary Stipend'
-                         ,'Extracurricular Coach/Leader'
-                         ,'Extracurricular Program Coordinator - Elementary'
-                         ,'Extracurricular Program Coordinator - Middle and High'
-                         ,'Grade Level Chair'
-                         ,'Instructional Coach'
-                         ,'MEAL'
-                         )
-  )
-
---@cbini, This was in the original feed as a CROSS JOIN, but I don't think we need it. Thoughts?
---,tracker_fields AS (
---  SELECT DISTINCT
---         pay_code
---        ,pay_category
---  FROM tracking_long
--- ) 
-
-,directory AS (
-  SELECT CASE WHEN LEN(employeenumber) > 6 THEN RIGHT(employeenumber,LEN(employeenumber)-3) ELSE employeenumber END AS employeenumber
-        ,mail
-  FROM gabby.adsi.user_attributes_static
- )
- 
-SELECT DISTINCT 
-       df.df_employee_number
+SELECT df.df_employee_number
       ,df.preferred_name AS preferred_lastfirst       
       ,df.primary_site_schoolid AS schoolid
       ,df.primary_site AS location
@@ -61,26 +11,30 @@ SELECT DISTINCT
       ,df.manager_name AS manager        
       ,df.status AS position_status
 
-      ,dir.mail AS email_address
-
-      ,cal.academic_year
-      ,cal.term
+      ,dir.userprincipalname AS email_address
+      
       ,cal.date_value            
-       
+      ,gabby.utilities.DATE_TO_SY(cal.date_value) AS academic_year
+
+      ,CONVERT(VARCHAR(5),dt.alt_name) AS term
+             
       ,pt.pay_code
       ,pt.pay_category
-
 FROM gabby.dayforce.staff_roster df
-LEFT JOIN directory dir
-  ON df.df_employee_number = dir.employeenumber
-JOIN prof_calendar cal
+LEFT JOIN gabby.adsi.user_attributes_static dir
+  ON CONVERT(VARCHAR(25),df.df_employee_number) = dir.employeenumber
+JOIN gabby.powerschool.calendar_day cal
   ON df.primary_site_schoolid = cal.schoolid
  AND df.position_effective_from_date <= cal.date_value
---CROSS JOIN tracker_fields f
-LEFT JOIN tracking_long pt
+ AND cal.date_value BETWEEN DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1) AND GETDATE()
+ AND (cal.insession = 1 OR cal.type = 'PD')  
+ AND cal.schoolid != 0
+JOIN gabby.reporting.reporting_terms dt
+  ON cal.schoolid = dt.schoolid
+ AND cal.date_value BETWEEN dt.start_date AND dt.end_date
+ AND dt.identifier = 'RT' 
+LEFT JOIN gabby.dayforce.employee_attendance pt
   ON df.df_employee_number = pt.employee_number
- AND cal.date_value = pt.date
- --AND f.pay_code = pt.pay_code
-
-WHERE df.termination_date BETWEEN DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1) AND GETDATE()
-      OR df.termination_date IS NULL
+ AND cal.date_value = pt.pay_date
+ AND pt.pay_code IN ('L-In') /* @KV we need to update this list to only include the absent/tardy codes */
+WHERE COALESCE(df.termination_date, GETDATE()) >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1)
