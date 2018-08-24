936edea45dde82a08313e660cb127a035154d907
Charlie Bini <CBini@kippnj.org>
2018-08-24 17:12:03 -0400

    using calculated cols for indexing

--- dayforce/v_dayforce.staff_roster.sql
+++ dayforce/v_dayforce.staff_roster.sql
@@ -5,7 +5,7 @@ CREATE OR ALTER VIEW dayforce.staff_roster AS
 
 WITH clean_people AS (
   SELECT CONVERT(INT,e.df_employee_number) AS df_employee_number 
-        ,CONVERT(VARCHAR(25),e.adp_associate_id) AS adp_associate_id 
+        ,CONVERT(VARCHAR(25),e.adp_associate_id_clean) AS adp_associate_id 
         ,CONVERT(VARCHAR(25),e.salesforce_id) AS salesforce_id 
         ,CONVERT(VARCHAR(25),e.first_name) AS first_name
         ,CONVERT(VARCHAR(25),e.last_name) AS last_name        
@@ -17,8 +17,8 @@ WITH clean_people AS (
         ,CONVERT(VARCHAR(25),e.status_reason) AS status_reason
         ,CONVERT(VARCHAR(5),e.is_manager) AS is_manager        
         ,CONVERT(VARCHAR(125),e.primary_job) AS primary_job
-        ,CONVERT(VARCHAR(125),e.primary_on_site_department) AS primary_on_site_department
-        ,CONVERT(VARCHAR(125),e.legal_entity_name) AS legal_entity_name 
+        ,CONVERT(VARCHAR(125),e.primary_on_site_department_clean) AS primary_on_site_department
+        ,CONVERT(VARCHAR(125),e.legal_entity_name_clean) AS legal_entity_name 
         ,CONVERT(VARCHAR(25),e.job_family) AS job_family         
         ,CONVERT(INT,e.employee_s_manager_s_df_emp_number_id) AS manager_df_employee_number
         ,CONVERT(VARCHAR(5),e.payclass) AS payclass 
@@ -38,15 +38,15 @@ WITH clean_people AS (
         ,CONVERT(VARCHAR(1),UPPER(e.gender)) AS gender
         ,CONVERT(VARCHAR(25),COALESCE(e.common_name, e.first_name)) AS preferred_first_name
         ,CONVERT(VARCHAR(25),COALESCE(e.preferred_last_name , e.last_name)) AS preferred_last_name
-        ,CONVERT(VARCHAR(125),REPLACE(e.primary_site, ' - Regional', '')) AS primary_site
+        ,CONVERT(VARCHAR(125),REPLACE(e.primary_site_clean, ' - Regional', '')) AS primary_site
         ,CONVERT(VARCHAR(125),RTRIM(LEFT(e.ethnicity, CHARINDEX(' (', e.ethnicity)))) AS primary_ethnicity        
         ,CONVERT(VARCHAR(25),gabby.utilities.STRIP_CHARACTERS(mobile_number, '^0-9')) AS mobile_number
         ,CASE WHEN e.ethnicity LIKE '%(Hispanic%' THEN 1 ELSE 0 END AS is_hispanic
-        ,CASE WHEN e.primary_site LIKE ' - Regional' THEN 1 ELSE 0 END AS is_regional_staff        
+        ,CASE WHEN e.primary_site_clean LIKE ' - Regional' THEN 1 ELSE 0 END AS is_regional_staff        
         ,CASE
-          WHEN e.legal_entity_name = 'TEAM Academy Charter Schools' THEN 'YHD'
-          WHEN e.legal_entity_name = 'KIPP New Jersey' THEN 'D30'
-          WHEN e.legal_entity_name = 'KIPP Cooper Norcross Academy' THEN 'D3Z'          
+          WHEN e.legal_entity_name_clean = 'TEAM Academy Charter Schools' THEN 'YHD'
+          WHEN e.legal_entity_name_clean = 'KIPP New Jersey' THEN 'D30'
+          WHEN e.legal_entity_name_clean = 'KIPP Cooper Norcross Academy' THEN 'D3Z'          
          END AS payroll_company_code
         ,CASE WHEN e.status NOT IN ('TERMINATED', 'PRESTART') THEN 0 ELSE 1 END AS is_active
 
--- extracts/v_extracts.powerschool_autocomm_teachers_accounts.sql
+++ extracts/v_extracts.powerschool_autocomm_teachers_accounts.sql
@@ -3,104 +3,98 @@ GO
 
 CREATE OR ALTER VIEW extracts.powerschool_autocomm_teachers_accounts AS
 
-SELECT sub.teachernumber
-      ,sub.first_name
-      ,sub.last_name
-      ,CASE WHEN sub.status = 1 THEN sub.loginid END AS loginid
-      ,CASE WHEN sub.status = 1 THEN sub.teacherloginid END AS teacherloginid
-      ,sub.email_addr
-      ,CONVERT(INT,COALESCE(t.schoolid, sub.homeschoolid, 0)) AS schoolid
-      ,CONVERT(INT,COALESCE(sub.homeschoolid, 0)) AS homeschoolid
-      ,sub.status      
-      ,CASE WHEN sub.status = 1 THEN 1 ELSE 0 END AS teacherldapenabled
-      ,CASE WHEN sub.status = 1 THEN 1 ELSE 0 END AS adminldapenabled      
-      ,CASE WHEN sub.status = 1 THEN 1 ELSE 0 END AS ptaccess            
-      ,sub.legal_entity_name
-FROM
-    (
-     SELECT COALESCE(psid.ps_teachernumber
-                    ,df.adp_associate_id
-                    ,CONVERT(VARCHAR(25),df.df_employee_number)) AS teachernumber
-           ,df.preferred_first_name AS first_name
-           ,df.preferred_last_name AS last_name
-           ,CASE
-             WHEN df.primary_site = '18th Ave Campus' THEN 73255
-             WHEN df.primary_site = 'KIPP Lanning Sq Campus' THEN 179901
-             ELSE df.primary_site_schoolid 
-            END AS homeschoolid
-           ,df.legal_entity_name
-           ,CASE
-             WHEN df.legal_entity_name = 'TEAM Academy Charter Schools' THEN 'kippnewark'
-             WHEN df.legal_entity_name = 'KIPP Cooper Norcross Academy' THEN 'kippcamden'
-             WHEN df.legal_entity_name = 'KIPP Miami' THEN 'kippmiami'
-            END AS db_name
+WITH users_clean AS (
+  SELECT df.preferred_first_name AS first_name
+        ,df.preferred_last_name AS last_name
+        ,df.legal_entity_name
+        ,COALESCE(psid.ps_teachernumber, df.adp_associate_id, CONVERT(VARCHAR(25),df.df_employee_number)) AS teachernumber
+        ,CASE
+          WHEN df.primary_site = '18th Ave Campus' THEN 73255
+          WHEN df.primary_site = 'KIPP Lanning Sq Campus' THEN 179901
+          ELSE df.primary_site_schoolid 
+         END AS homeschoolid        
+        ,CASE
+          WHEN df.legal_entity_name = 'TEAM Academy Charter Schools' THEN 'kippnewark'
+          WHEN df.legal_entity_name = 'KIPP Cooper Norcross Academy' THEN 'kippcamden'
+          WHEN df.legal_entity_name = 'KIPP Miami' THEN 'kippmiami'
+         END AS db_name
            
-           ,LOWER(dir.samaccountname) AS loginid
-           ,LOWER(dir.samaccountname) AS teacherloginid
-           ,LOWER(dir.mail) AS email_addr
+        ,LOWER(dir.samaccountname) AS loginid
+        ,LOWER(dir.samaccountname) AS teacherloginid
+        ,LOWER(dir.mail) AS email_addr
            
-           ,CASE
-             WHEN psid.is_master = 0 THEN 2
-             WHEN df.termination_date < GETDATE() THEN 2
-             WHEN df.status IN ('ACTIVE','INACTIVE','PRESTART') THEN 1
-             WHEN df.termination_date >= CONVERT(DATE,GETDATE()) THEN 1
-             ELSE 2
-            END AS status
-     FROM gabby.dayforce.staff_roster df
-     LEFT JOIN gabby.adsi.user_attributes_static dir
-       ON CONVERT(VARCHAR(25),df.df_employee_number) = dir.employeenumber
-     LEFT JOIN gabby.people.id_crosswalk_powerschool psid
-       ON df.adp_associate_id = psid.adp_associate_id
-      AND psid.is_master = 1
-     WHERE DATEDIFF(DAY, ISNULL(df.termination_date, CONVERT(DATE,GETDATE())), GETDATE()) <= 7 /* import terminated staff up to a week after termination date */
-       AND df.primary_on_site_department != 'Data'
-       AND df.legal_entity_name != 'KIPP New Jersey'
-    ) sub
-LEFT JOIN gabby.powerschool.teachers_static t
-  ON sub.teachernumber COLLATE Latin1_General_BIN = t.teachernumber 
- AND sub.db_name = t.db_name
+        ,CASE
+          WHEN psid.is_master = 0 THEN 2
+          WHEN df.termination_date < GETDATE() THEN 2
+          WHEN df.status IN ('ACTIVE','INACTIVE','PRESTART') THEN 1
+          WHEN df.termination_date >= CONVERT(DATE,GETDATE()) THEN 1
+          ELSE 2
+         END AS status
+  FROM gabby.dayforce.staff_roster df 
+  LEFT JOIN gabby.adsi.user_attributes_static dir
+    ON CONVERT(VARCHAR(25),df.df_employee_number) = dir.employeenumber
+  LEFT JOIN gabby.people.id_crosswalk_powerschool psid
+    ON df.df_employee_number = psid.df_employee_number
+   AND psid.is_master = 1
+  WHERE DATEDIFF(DAY, ISNULL(df.termination_date, CONVERT(DATE,GETDATE())), GETDATE()) <= 7 /* import terminated staff up to a week after termination date */
+    AND df.primary_on_site_department != 'Data'
+ )
+
+/* Home School Record */       
+SELECT df.teachernumber
+      ,df.first_name
+      ,df.last_name
+      ,CASE WHEN df.status = 1 THEN df.loginid END AS loginid
+      ,CASE WHEN df.status = 1 THEN df.teacherloginid END AS teacherloginid
+      ,df.email_addr
+      ,CONVERT(INT,COALESCE(df.homeschoolid, 0)) AS schoolid
+      ,CONVERT(INT,COALESCE(df.homeschoolid, 0)) AS homeschoolid
+      ,df.status      
+      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS teacherldapenabled
+      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS adminldapenabled      
+      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS ptaccess            
+      ,df.legal_entity_name
+FROM users_clean df
+WHERE df.legal_entity_name != 'KIPP New Jersey'
 
 UNION ALL
 
-SELECT sub.teachernumber
-      ,sub.first_name
-      ,sub.last_name
-      ,CASE WHEN sub.status = 1 THEN sub.loginid END AS loginid
-      ,CASE WHEN sub.status = 1 THEN sub.teacherloginid END AS teacherloginid
-      ,sub.email_addr
+/* KNJ Admins */
+SELECT df.teachernumber
+      ,df.first_name
+      ,df.last_name
+      ,CASE WHEN df.status = 1 THEN df.loginid END AS loginid
+      ,CASE WHEN df.status = 1 THEN df.teacherloginid END AS teacherloginid
+      ,df.email_addr
       ,0 AS schoolid
       ,0 AS homeschoolid
-      ,sub.status      
-      ,CASE WHEN sub.status = 1 THEN 1 ELSE 0 END AS teacherldapenabled
-      ,CASE WHEN sub.status = 1 THEN 1 ELSE 0 END AS adminldapenabled      
-      ,CASE WHEN sub.status = 1 THEN 1 ELSE 0 END AS ptaccess            
-      ,sub.legal_entity_name
-FROM
-    (
-     SELECT COALESCE(psid.ps_teachernumber
-                    ,df.adp_associate_id
-                    ,CONVERT(VARCHAR(25),df.df_employee_number)) AS teachernumber
-           ,df.preferred_first_name AS first_name
-           ,df.preferred_last_name AS last_name           
-           ,df.legal_entity_name           
-           
-           ,LOWER(dir.samaccountname) AS loginid
-           ,LOWER(dir.samaccountname) AS teacherloginid
-           ,LOWER(dir.mail) AS email_addr
-           
-           ,CASE
-             WHEN psid.is_master = 0 THEN 2
-             WHEN df.termination_date < GETDATE() THEN 2
-             WHEN df.status IN ('ACTIVE','INACTIVE','PRESTART') THEN 1
-             WHEN df.termination_date >= CONVERT(DATE,GETDATE()) THEN 1
-             ELSE 2
-            END AS status
-     FROM gabby.dayforce.staff_roster df
-     LEFT JOIN gabby.adsi.user_attributes_static dir
-       ON CONVERT(VARCHAR(25),df.df_employee_number) = dir.employeenumber
-     LEFT JOIN gabby.people.id_crosswalk_powerschool psid
-       ON df.adp_associate_id = psid.adp_associate_id
-      AND psid.is_master = 1
-     WHERE df.primary_on_site_department != 'Data'
-       AND df.legal_entity_name = 'KIPP New Jersey'
-    ) sub
+      ,df.status      
+      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS teacherldapenabled
+      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS adminldapenabled      
+      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS ptaccess            
+      ,df.legal_entity_name
+FROM users_clean df
+WHERE df.legal_entity_name = 'KIPP New Jersey'
+
+UNION ALL
+
+/* Additional School Records */
+SELECT df.teachernumber
+      ,df.first_name
+      ,df.last_name
+      ,CASE WHEN df.status = 1 THEN df.loginid END AS loginid
+      ,CASE WHEN df.status = 1 THEN df.teacherloginid END AS teacherloginid
+      ,df.email_addr
+      ,CONVERT(INT,COALESCE(t.schoolid, df.homeschoolid, 0)) AS schoolid
+      ,CONVERT(INT,COALESCE(df.homeschoolid, 0)) AS homeschoolid
+      ,df.status      
+      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS teacherldapenabled
+      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS adminldapenabled      
+      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS ptaccess            
+      ,df.legal_entity_name
+FROM users_clean df
+JOIN gabby.powerschool.teachers_static t
+  ON df.teachernumber = t.teachernumber COLLATE Latin1_General_BIN
+ AND df.db_name = t.db_name 
+ AND df.homeschoolid != t.schoolid
+WHERE df.legal_entity_name != 'KIPP New Jersey'
