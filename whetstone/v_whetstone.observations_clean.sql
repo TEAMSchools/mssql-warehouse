7b4c25424e345215ec6e492f4c50c143d3dd8a48
Charlie Bini <CBini@kippnj.org>
2018-09-20 14:28:10 -0400

    reviewed

--- whetstone/v_whetstone.observations_clean.sql
+++ whetstone/v_whetstone.observations_clean.sql
@@ -1,9 +1,9 @@
 USE gabby
 GO
 
---CREATE OR ALTER VIEW whetstone.observations_clean AS
+CREATE OR ALTER VIEW whetstone.observations_clean AS
 
-SELECT wo._id
+SELECT wo._id AS observation_id
       ,wo.signed
       ,wo.archived_at
       ,wo.is_published
@@ -13,55 +13,37 @@ SELECT wo._id
       ,wo.first_published
       ,wo.observed_at
       ,wo.created
-      ,wo.quick_hits
-      ,LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(wo.list_two_column_b,'[',''),']',''),'"',' '))) AS list_grows
-      ,LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(wo.list_two_column_a,'[',''),']',''),'"',' '))) AS list_glows
-      ,JSON_VALUE(wo.rubric,'$.name') AS observation_form
-      ,JSON_VALUE(wo.observation_type,'$.name') AS observation_type
-      ,wo.magic_notes --seemingly an infinite number of arrays (comments) within magic notes
-      ,wo.score AS overall_average
+      ,wo.quick_hits      
+      ,wo.score
       ,wo.score_averaged_by_strand
-      ,wo.percentage AS percentage_averaged_by_strand
+      ,wo.percentage
+      ,LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(wo.list_two_column_a,'[',''),']',''),'"',' '))) AS list_two_column_a /* list_glows */
+      ,LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(wo.list_two_column_b,'[',''),']',''),'"',' '))) AS list_two_column_b /* list_grows */
       
-      ,os.score_percentage
-      ,os.score_value
+      ,JSON_VALUE(wo.observer,'$.name') AS observer_name
+      ,JSON_VALUE(wo.observer,'$.canvasId') AS observer_canvasId
+      ,JSON_VALUE(wo.observer,'$.powerSchoolId') AS observer_powerSchoolId
+      ,JSON_VALUE(wo.observer,'$.accountingId') AS observer_accountingId
+      ,JSON_VALUE(wo.observer,'$.internalId') AS observer_internalId
+      ,JSON_VALUE(wo.observer,'$.email') AS observer_email
       
-      ,mos.name AS score_name
-            
-      ,tb.text_box_text
-      ,tb.text_box_label
+      ,JSON_VALUE(wo.teacher,'$.name') AS teacher_name
+      ,JSON_VALUE(wo.teacher,'$.canvasId') AS teacher_canvasId
+      ,JSON_VALUE(wo.teacher,'$.powerSchoolId') AS teacher_powerSchoolId
+      ,JSON_VALUE(wo.teacher,'$.accountingId') AS teacher_accountingId
+      ,JSON_VALUE(wo.teacher,'$.internalId') AS teacher_internalId
+      ,JSON_VALUE(wo.teacher,'$.email') AS teacher_email      
       
---These won't pull until the view is merged into gabby      
-      --,ta.course
-      --,ta.gradeLevel
-      --,ta.school
+      ,JSON_VALUE(wo.teaching_assignment, '$.period') AS teaching_assignment_period
+      ,JSON_VALUE(wo.teaching_assignment, '$.course') AS teaching_assignment_course
+      ,JSON_VALUE(wo.teaching_assignment, '$.gradeLevel._id') AS teaching_assignment_gradeLevel_id
+      ,JSON_VALUE(wo.teaching_assignment, '$.gradeLevel.name') AS teaching_assignment_gradeLevel_name
+      ,JSON_VALUE(wo.teaching_assignment, '$.school._id') AS teaching_assignment_school_id
+      ,JSON_VALUE(wo.teaching_assignment, '$.school.name') AS teaching_assignment_school_name
 
---These won't pull until the view is merged into gabby           
-      --,o.observed_by_name
-      --,o.observed_by_df_id
-      --,o.observed_by_email
+      ,JSON_VALUE(wo.rubric,'$._id') AS rubric_id
+      ,JSON_VALUE(wo.rubric,'$.name') AS rubric_name      
       
---These won't pull until the view is merged into gabby           
-      --,t.teacher_name
-      --,t.teacher_df_id
-      --,t.teacher_email
-      
-FROM gabby.whetstone.observations wo LEFT OUTER JOIN gabby.whetstone.observations_scores os
-     ON wo._id = os.observation_id
-  
-     LEFT OUTER JOIN gabby.whetstone.measurements mos
-     ON os.score_measurement_id = mos._id
-  
-     LEFT OUTER JOIN gabby.whetstone.observations_scores_text_boxes tb
-     ON os.score_id = tb.score_id
-
-
-
-     --LEFT OUTER JOIN gabby.whetstone.observations_teaching_assignment ta  --This join won't work until the view is merged to gabby
-     --ON wo._id = ta.observation_id
-     
-     --LEFT OUTER JOIN gabby.whetstone.observations_observer o  --This join won't work until the view is merged to gabby
-     --ON wo._id = o.observation_id
-     
-     --LEFT OUTER JOIN gabby.whetstone.observations_teacher t --This join won't work until the view is merged to gabby
-     --ON wo_id = t.observation_id
+      ,JSON_VALUE(wo.observation_type,'$._id') AS observation_type_id
+      ,JSON_VALUE(wo.observation_type,'$.name') AS observation_type_name
+FROM gabby.whetstone.observations wo
