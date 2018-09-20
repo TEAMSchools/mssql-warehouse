fa92a8aa4fbfe2f5f954078ae304d2d3f29f856d
Charlie Bini <CBini@kippnj.org>
2018-09-20 13:58:42 -0400

    save KCNA

--- mcs/v_mcs.lunch_info.sql
+++ mcs/v_mcs.lunch_info.sql
@@ -9,7 +9,7 @@ SELECT CONVERT(INT,s.studentnumber) AS studentnumber
       ,CONVERT(VARCHAR(125),e.description) AS description
 
       ,LEFT(mbs.description, 1) AS mealbenefitstatus /* returns F/R/P */
-      ,CONVERT(VARCHAR(25),mbs.description) AS shortdesc
+      ,CONVERT(VARCHAR(125),mbs.description) AS shortdesc
 
       ,c.reimbursableonlybalance
       ,c.unallocatedbalance
@@ -27,24 +27,33 @@ WHERE s.inactivedate IS NULL
 
 UNION ALL
 
-SELECT CONVERT(INT,s.studentnumber) AS studentnumber
-      ,s.currentapplicationid
-
-      ,CONVERT(VARCHAR(125),e.description) AS description
-
-      ,LEFT(mbs.description, 1) AS mealbenefitstatus /* returns F/R/P */
-      ,CONVERT(VARCHAR(25),mbs.description) AS shortdesc
+SELECT CONVERT(INT,s.StudentNumber) AS studentnumber
+      ,s.ApplicationID AS currentapplicationid      
+      ,CASE
+        WHEN s.IsDC = 1 THEN 'Direct Certification'
+        ELSE COALESCE(s.Application_Approval_Result_Description, 'No Application')
+       END AS description      
+      ,LEFT(CASE
+             WHEN s.IsDC = 1 THEN 'Free'
+             WHEN s.Application_Approval_Result_Description = 'Zero Income' THEN 'Free'
+             WHEN s.Application_Approval_Result_Description LIKE 'Free%' THEN 'Free'
+             WHEN s.Application_Approval_Result_Description LIKE 'Denied%' THEN 'Paying'
+             ELSE COALESCE(s.Application_Approval_Result_Description, 'Paying')
+            END, 1) AS mealbenefitstatus
+      ,CASE        
+        WHEN s.IsDC = 1 THEN 'Free'
+        WHEN s.Application_Approval_Result_Description = 'Zero Income' THEN 'Free'
+        WHEN s.Application_Approval_Result_Description LIKE 'Free%' THEN 'Free'
+        WHEN s.Application_Approval_Result_Description LIKE 'Denied%' THEN 'Paying'
+        ELSE COALESCE(s.Application_Approval_Result_Description, 'Paying')
+       END AS shortdesc      
 
       ,c.reimbursableonlybalance
       ,c.unallocatedbalance
       ,c.reimbursableonlybalance + c.unallocatedbalance AS balance
-FROM [winsql06\yoda].[franklin].[dbo].[student] s
-LEFT JOIN [winsql06\yoda].[franklin].[dbo].[eligibility] e 
-  ON s.[eligibilityid] = e.[eligibility_recid]
-LEFT JOIN [winsql06\yoda].[franklin].[dbo].[meal_benefits_status] mbs
-  ON e.meal_benefits_statusid = mbs.meal_benefits_status_recid
-LEFT JOIN [winsql06\yoda].[newton].[dbo].[student_guid_link] g
-  ON s.globaluid = g.studentguid
-LEFT JOIN [winsql06\yoda].[newton].[dbo].[customer] c 
+FROM [WINSQL06\YODA].[Franklin].[dbo].[VIEW_STUDENT_DATA] s
+LEFT JOIN [WINSQL06\YODA].[newton].[dbo].[student_guid_link] g
+  ON s.StudentGuid = g.studentguid
+LEFT JOIN [WINSQL06\YODA].[newton].[dbo].[customer] c 
   ON g.[customerid] = c.[customer_recid]
-WHERE s.inactivedate IS NULL
+WHERE s.Inactive = 0
