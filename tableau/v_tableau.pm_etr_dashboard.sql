09fcaf8f8febe5b5e0d6eaca47b3169332a304eb
kevin.verhoff <kevin.verhoff@gmail.com>
2018-09-21 15:54:55 -0400

    Kicking things off
    Next Steps: Replace some of the commented out fields with information from observations_clean OR from another view/parsing of JSON.

--- tableau/v_tableau.pm_etr_dashboard.sql
+++ tableau/v_tableau.pm_etr_dashboard.sql
@@ -1,9 +1,9 @@
 USE gabby
 GO
 
-CREATE OR ALTER VIEW tableau.pm_etr_dashboard AS
+--CREATE OR ALTER VIEW tableau.pm_etr_dashboard AS
 
-SELECT wo._id AS observation_id
+SELECT wo.observation_id
       ,wo.signed
       ,wo.archived_at
       ,wo.is_published
@@ -14,29 +14,29 @@ SELECT wo._id AS observation_id
       ,wo.observed_at
       ,wo.created
       ,wo.quick_hits
-      ,wo.personal_notes
+      --,wo.personal_notes
       ,wo.list_two_column_b
       ,wo.list_two_column_a
-      ,wo.classroom_visits      
-      ,wo.rubric
-      ,wo.list_three_column_b
-      ,wo.list_three_column_a
-      ,wo.teaching_assignment
-      ,wo.list_three_column_c
-      ,wo.tags
-      ,wo.observation_type
-      ,wo.magic_notes
-      ,wo.observer
-      ,wo.list_two_column_apaired
-      ,wo.teacher
-      ,wo.score_overrides
-      ,wo.video_notes
-      ,wo.files
-      ,wo.meetings
-      ,wo.event_log
-      ,wo.list_two_column_bpaired
-      ,wo.list_one_column_a
-      ,wo.last_modified
+      --,wo.classroom_visits      
+      --,wo.rubric
+      --,wo.list_three_column_b
+      --,wo.list_three_column_a
+      --,wo.teaching_assignment
+      --,wo.list_three_column_c
+      --,wo.tags
+      --,wo.observation_type
+      --,wo.magic_notes
+      --,wo.observer
+      --,wo.list_two_column_apaired
+      --,wo.teacher
+      --,wo.score_overrides
+      --,wo.video_notes
+      --,wo.files
+      --,wo.meetings
+      --,wo.event_log
+      --,wo.list_two_column_bpaired
+      --,wo.list_one_column_a
+      --,wo.last_modified
       ,wo.score
       ,wo.score_averaged_by_strand
       ,wo.percentage
@@ -57,7 +57,7 @@ SELECT wo._id AS observation_id
       ,tb.text_box_text
 FROM gabby.whetstone.observations_clean wo
 LEFT JOIN gabby.whetstone.observations_scores wos
-  ON wo._id = wos.observation_id
+  ON wo.observation_id = wos.observation_id
 LEFT JOIN gabby.whetstone.measurements wm
   ON wos.score_measurement_id = wm._id
 LEFT JOIN gabby.whetstone.observations_scores_text_boxes tb
