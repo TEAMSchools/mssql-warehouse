5497e9a4d2cf32d79fc21a5b3c38d89a4cd7a916
Kevin Verhoff <kevin.verhoff@gmail.com>
2018-11-13 12:11:30 -0500

    Update surveys/v_surveys.survey_completion.sql
    Taking it a different direction. A scaffold will be more useful than a row counter.

--- surveys/v_surveys.survey_completion.sql
+++ surveys/v_surveys.survey_completion.sql
@@ -40,7 +40,7 @@ WITH survey_feed AS (
 
   SELECT _created AS date_created
         ,CONVERT(DATETIME2,survey_timestamp) AS date_submitted
-        ,NULL AS subject_name
+        ,'R9/Engagement' AS subject_name
         ,LOWER(email) AS responder_email
         ,gabby.utilities.DATE_TO_SY(_created) AS academic_year
         ,CASE
@@ -51,24 +51,88 @@ WITH survey_feed AS (
   FROM surveys.r_9_engagement_survey
  )
 
-SELECT f.date_created
-      ,f.date_submitted
-      ,f.responder_email      
-      ,f.subject_name
-      ,f.academic_year
-      ,f.reporting_term
-      ,f.survey_type
+ , teacher_roster AS (
 
-      ,COALESCE(uupn.df_employee_number, um.df_employee_number) AS df_employee_number
-      ,COALESCE(uupn.preferred_first, um.preferred_first) AS survey_taker_first
-      ,COALESCE(uupn.preferred_last, um.preferred_last) AS survey_taker_last
-      ,COALESCE(uupn.preferred_name, um.preferred_name) AS survey_taker_name
-      ,COALESCE(uupn.location_custom, um.location_custom) AS location_custom
+   SELECT df_employee_number
+         ,preferred_first
+         ,preferred_last
+         ,preferred_name
+         ,CASE WHEN position_status IN ('INACTIVE', 'ADMIN_LEAVE')
+               THEN 'LEAVE'
+               ELSE position_status
+             END AS position_status
 
-      ,ROW_NUMBER() OVER( PARTITION BY COALESCE(uupn.df_employee_number, um.df_employee_number), f.academic_year, f.reporting_term, f.survey_type ORDER BY f.date_submitted) AS rn
+         ,subject_dept_custom
+         ,grades_taught_custom
+         ,job_title_description
+         ,legal_entity_name
+         ,location_custom
+         ,home_department_description
+         ,benefits_eligibility_class_description
+         ,is_management
+         ,manager_df_employee_number
+         ,manager_name
+         ,LOWER(userprincipalname) as email1
+         ,CASE WHEN (LOWER(REPLACE(userprincipalname,'-','')) = LOWER(userprincipalname)) 
+               THEN NULL
+               ELSE LOWER(REPLACE(userprincipalname,'-','')) 
+             END AS email2
+         ,CASE WHEN (LOWER(mail) = LOWER(userprincipalname)) 
+               THEN NULL
+               ELSE LOWER(mail)
+             END AS email3
+         ,gabby.utilities.GLOBAL_ACADEMIC_YEAR() AS academic_year
+  FROM tableau.staff_roster
+  WHERE position_status NOT IN ('TERMINATED', 'PRESTART')
+  
+  )
 
-FROM survey_feed f
-LEFT JOIN gabby.tableau.staff_roster uupn
-  ON f.responder_email = uupn.userprincipalname
-LEFT JOIN gabby.tableau.staff_roster um
-  ON f.responder_email = um.mail
+, teacher_scaffold AS (
+
+  SELECT *
+        ,'Self & Others' AS survey_type
+  FROM teacher_roster
+
+  UNION ALL
+
+  SELECT *
+        ,'R9/Engagement' AS survey_type
+  FROM teacher_roster
+
+  UNION ALL
+
+  SELECT *
+        ,'Manager' AS survey_type
+  FROM teacher_roster
+
+  )
+
+SELECT COALESCE(f1.date_created, f2.date_created, f3.date_created) AS date_created
+      ,COALESCE(f1.date_submitted, f2.date_submitted, f3.date_submitted) AS date_submitted
+      ,COALESCE(f1.responder_email, f2.responder_email, f3.responder_email, email1) AS responder_email   
+      ,COALESCE(f1.subject_name, f2.subject_name, f3.subject_name) AS subject_name
+      ,COALESCE(f1.academic_year, f2.academic_year, f3.academic_year, gabby.utilities.GLOBAL_ACADEMIC_YEAR()) AS academic_year
+      ,COALESCE(f1.reporting_term, f2.reporting_term, f3.reporting_term) AS reporting_term
+      ,COALESCE(f1.survey_type, f2.survey_type, f3.survey_type, s.survey_type) AS survey_type
+
+      ,s.df_employee_number AS df_employee_number
+      ,s.preferred_first AS survey_taker_first
+      ,s.preferred_last AS survey_taker_last
+      ,s.preferred_name AS survey_taker_name
+      ,s.location_custom AS location_custom
+      ,s.job_title_description
+      ,s.position_status
+
+FROM teacher_scaffold s
+LEFT JOIN survey_feed f1
+  ON s.email1 = f1.responder_email
+ AND s.survey_type = f1.survey_type
+ AND s.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
+LEFT JOIN survey_feed f2
+  ON s.email2 = f2.responder_email
+ AND s.survey_type = f2.survey_type
+ AND s.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
+LEFT JOIN survey_feed f3
+  ON s.email3 = f3.responder_email
+ AND s.survey_type = f3.survey_type
+ AND s.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
