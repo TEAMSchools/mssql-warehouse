10bac0c80db79177e92014e9f71d521fc7e6da00
Charlie Bini <cbini87@gmail.com>
2018-11-30 12:02:45 -0500

    reviewed

--- surveys/v_surveys.survey_completion.sql
+++ surveys/v_surveys.survey_completion.sql
@@ -1,7 +1,7 @@
 USE gabby
 GO
 
---CREATE OR ALTER VIEW surveys.survey_completion AS
+CREATE OR ALTER VIEW surveys.survey_completion AS
 
 WITH survey_feed AS (
   SELECT _created AS date_created
@@ -15,7 +15,7 @@ WITH survey_feed AS (
           WHEN MONTH(_created) IN (3, 4, 5, 6, 7) THEN 'SO3'
          END AS reporting_term
         ,'Self & Others' AS survey_type
-  FROM surveys.self_and_others_survey
+  FROM gabby.surveys.self_and_others_survey
   WHERE subject_name IS NOT NULL
     AND survey_timestamp IS NOT NULL
 
@@ -32,7 +32,7 @@ WITH survey_feed AS (
           WHEN MONTH(_created) IN (3, 4, 5, 6, 7) THEN 'MGR3'
          END AS reporting_term
         ,'Manager' AS survey_type
-  FROM surveys.manager_survey
+  FROM gabby.surveys.manager_survey
   WHERE subject_name IS NOT NULL
     AND survey_timestamp IS NOT NULL
 
@@ -48,74 +48,38 @@ WITH survey_feed AS (
           WHEN MONTH(_created) < 7 THEN 'R9S2'
          END AS reporting_term
         ,'R9/Engagement' AS survey
-  FROM surveys.r_9_engagement_survey
+  FROM gabby.surveys.r_9_engagement_survey
  )
 
- , teacher_roster AS (
+,teacher_scaffold AS (
+  SELECT sr.df_employee_number
+        ,sr.preferred_first_name AS preferred_first
+        ,sr.preferred_last_name AS preferred_last
+        ,sr.preferred_name
+        ,sr.primary_site AS location_custom
+        ,sr.primary_job AS job_title_description                  
+        ,CASE WHEN sr.status IN ('INACTIVE', 'ADMIN_LEAVE') THEN 'LEAVE' ELSE sr.status END AS position_status        
+        
+        ,LOWER(ad.userprincipalname) as email1
+        ,CASE 
+          WHEN LOWER(REPLACE(ad.userprincipalname, '-', '')) = LOWER(ad.userprincipalname) THEN NULL
+          ELSE LOWER(REPLACE(ad.userprincipalname, '-', ''))
+         END AS email2
+        ,CASE
+          WHEN LOWER(ad.mail) = LOWER(ad.userprincipalname) THEN NULL
+          ELSE LOWER(ad.mail)
+         END AS email3
 
-   SELECT df_employee_number
-         ,preferred_first
-         ,preferred_last
-         ,preferred_name
-         ,CASE WHEN position_status IN ('INACTIVE', 'ADMIN_LEAVE')
-               THEN 'LEAVE'
-               ELSE position_status
-             END AS position_status
+        ,ss.value AS survey_type
+  FROM gabby.dayforce.staff_roster sr
+  LEFT JOIN gabby.adsi.user_attributes_static ad
+    ON sr.df_employee_number = ad.employeenumber
+   AND ISNUMERIC(ad.employeenumber) = 1
+  CROSS JOIN STRING_SPLIT('Self & Others,R9/Engagement,Manager', ',') ss
+  WHERE sr.status NOT IN ('TERMINATED', 'PRESTART')
+ )
 
-         ,subject_dept_custom
-         ,grades_taught_custom
-         ,job_title_description
-         ,legal_entity_name
-         ,location_custom
-         ,home_department_description
-         ,benefits_eligibility_class_description
-         ,is_management
-         ,manager_df_employee_number
-         ,manager_name
-         ,LOWER(userprincipalname) as email1
-         ,CASE WHEN (LOWER(REPLACE(userprincipalname,'-','')) = LOWER(userprincipalname)) 
-               THEN NULL
-               ELSE LOWER(REPLACE(userprincipalname,'-','')) 
-             END AS email2
-         ,CASE WHEN (LOWER(mail) = LOWER(userprincipalname)) 
-               THEN NULL
-               ELSE LOWER(mail)
-             END AS email3
-         ,gabby.utilities.GLOBAL_ACADEMIC_YEAR() AS academic_year
-  FROM tableau.staff_roster
-  WHERE position_status NOT IN ('TERMINATED', 'PRESTART')
-  
-  )
-
-, teacher_scaffold AS (
-
-  SELECT *
-        ,'Self & Others' AS survey_type
-  FROM teacher_roster
-
-  UNION ALL
-
-  SELECT *
-        ,'R9/Engagement' AS survey_type
-  FROM teacher_roster
-
-  UNION ALL
-
-  SELECT *
-        ,'Manager' AS survey_type
-  FROM teacher_roster
-
-  )
-
-SELECT COALESCE(f1.date_created, f2.date_created, f3.date_created) AS date_created
-      ,COALESCE(f1.date_submitted, f2.date_submitted, f3.date_submitted) AS date_submitted
-      ,COALESCE(f1.responder_email, f2.responder_email, f3.responder_email, email1) AS responder_email   
-      ,COALESCE(f1.subject_name, f2.subject_name, f3.subject_name) AS subject_name
-      ,COALESCE(f1.academic_year, f2.academic_year, f3.academic_year, gabby.utilities.GLOBAL_ACADEMIC_YEAR()) AS academic_year
-      ,COALESCE(f1.reporting_term, f2.reporting_term, f3.reporting_term) AS reporting_term
-      ,COALESCE(f1.survey_type, f2.survey_type, f3.survey_type, s.survey_type) AS survey_type
-
-      ,s.df_employee_number AS df_employee_number
+SELECT s.df_employee_number AS df_employee_number
       ,s.preferred_first AS survey_taker_first
       ,s.preferred_last AS survey_taker_last
       ,s.preferred_name AS survey_taker_name
@@ -123,16 +87,20 @@ SELECT COALESCE(f1.date_created, f2.date_created, f3.date_created) AS date_creat
       ,s.job_title_description
       ,s.position_status
 
+      ,COALESCE(f1.date_created, f2.date_created, f3.date_created) AS date_created
+      ,COALESCE(f1.date_submitted, f2.date_submitted, f3.date_submitted) AS date_submitted
+      ,COALESCE(f1.responder_email, f2.responder_email, f3.responder_email, email1) AS responder_email   
+      ,COALESCE(f1.subject_name, f2.subject_name, f3.subject_name) AS subject_name
+      ,COALESCE(f1.academic_year, f2.academic_year, f3.academic_year, gabby.utilities.GLOBAL_ACADEMIC_YEAR()) AS academic_year
+      ,COALESCE(f1.reporting_term, f2.reporting_term, f3.reporting_term) AS reporting_term
+      ,COALESCE(f1.survey_type, f2.survey_type, f3.survey_type, s.survey_type) AS survey_type
 FROM teacher_scaffold s
 LEFT JOIN survey_feed f1
   ON s.email1 = f1.responder_email
  AND s.survey_type = f1.survey_type
- AND s.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
 LEFT JOIN survey_feed f2
   ON s.email2 = f2.responder_email
  AND s.survey_type = f2.survey_type
- AND s.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
 LEFT JOIN survey_feed f3
   ON s.email3 = f3.responder_email
  AND s.survey_type = f3.survey_type
- AND s.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
