69b8a2dae518d5f72245fd7adab9a72625abd1b7
Charlie Bini <cbini87@gmail.com>
2018-12-13 15:53:31 -0500

    ici view

---
+++ tntp/v_tntp.insight_index_scores.sql
@@ -0,0 +1,36 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW tntp.insight_index_scores AS
+
+SELECT sub.administration_year
+      ,sub.administration_academic_year
+      ,sub.administration_round
+      ,sub.reporting_schoolid
+      ,sub.school
+      ,SUM(sub.pct_agree) AS sum_pct_agree
+      ,ROUND((SUM(sub.pct_agree) * 3) + 1, 1) AS instructional_culture_index
+FROM
+    (
+     SELECT administration_year
+           ,administration_academic_year
+           ,administration_round
+           ,reporting_schoolid
+           ,school
+           ,variable
+           ,AVG(is_agree) AS pct_agree
+     FROM gabby.tntp.insight_survey_detail
+     WHERE survey_type = 'Teacher'
+       AND is_ici = 1
+     GROUP BY administration_year
+             ,administration_academic_year
+             ,administration_round
+             ,reporting_schoolid
+             ,school
+             ,variable
+    ) sub
+GROUP BY sub.administration_year
+        ,sub.administration_academic_year
+        ,sub.administration_round
+        ,sub.reporting_schoolid
+        ,sub.school
--- tntp/v_tntp.insight_survey_detail.sql
+++ tntp/v_tntp.insight_survey_detail.sql
@@ -6,7 +6,7 @@ CREATE OR ALTER VIEW tntp.insight_survey_detail AS
 SELECT ird.administration_year
       ,ird.administration_academic_year
       ,ird.administration_round
-      ,ird.state_cmo
+      
       ,ird.region_id
       ,ird.region
       ,ird.school_id
@@ -15,6 +15,23 @@ SELECT ird.administration_year
       ,ird.[index]
       ,ird.variable
       ,CONVERT(FLOAT,ird.value) AS value
+      ,COALESCE(ird.state_cmo, ird.region) AS state_cmo
+      ,CASE
+        WHEN school = 'KIPP BOLD Academy' THEN 73258
+        WHEN school = 'KIPP Lanning Square Middle School' THEN 179902
+        WHEN school = 'KIPP Lanning Square Primary' THEN 179901
+        WHEN school = 'KIPP Life Academy' THEN 73257
+        WHEN school = 'KIPP Miami Sunrise Academy' THEN 30200801
+        WHEN school = 'KIPP Newark Collegiate Academy' THEN 73253
+        WHEN school = 'KIPP Pathways' THEN 732574573
+        WHEN school = 'KIPP Rise Academy' THEN 73252
+        WHEN school = 'KIPP Seek Academy' THEN 73256
+        WHEN school = 'KIPP SPARK Academy' THEN 73254
+        WHEN school = 'KIPP TEAM Academy' THEN 133570965
+        WHEN school = 'KIPP THRIVE Academy' THEN 73255
+        WHEN school = 'KIPP Whittier Elementary' THEN 1799015075
+        WHEN school = 'KIPP Whittier Middle School' THEN 179903
+       END AS reporting_schoolid
 
       ,vars.label AS question_label
 
