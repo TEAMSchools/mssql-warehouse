812ebafb4b31bec517c12902c6e59207eef085b5
Charlie Bini <cbini87@gmail.com>
2019-01-10 14:45:49 -0500

    fix enroll status source

--- extracts/v_extracts.illuminate_student_portal_accounts.sql
+++ extracts/v_extracts.illuminate_student_portal_accounts.sql
@@ -9,4 +9,4 @@ SELECT student_number AS [01 Student ID]
       ,CASE WHEN enroll_status = 0 THEN 1 ELSE 0 END AS [04 Enable portal]
       ,student_web_password AS [05 Temporary password]
 FROM gabby.powerschool.student_access_accounts_static
-WHERE enroll_status = 0
+WHERE student_number IN (SELECT student_number FROM gabby.powerschool.students WHERE enroll_status = 0)
