6d32c109278cddc3a49a17c91c907bbcde025356
Charlie Bini <cbini87@gmail.com>
2019-01-31 17:27:40 -0500

    Update v_extracts.gsheets_survey_completion.sql

--- extracts/v_extracts.gsheets_survey_completion.sql
+++ extracts/v_extracts.gsheets_survey_completion.sql
@@ -6,6 +6,6 @@ CREATE OR ALTER VIEW extracts.gsheets_survey_completion AS
 SELECT sc.survey_type
       ,sc.subject_name
       ,sc.responder_email
-      ,NULL AS date_completed
+      ,sc.date_created AS date_completed
 FROM gabby.surveys.survey_completion sc
 WHERE sc.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
