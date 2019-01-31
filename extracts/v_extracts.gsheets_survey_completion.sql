4d342727aff62db05fd50f51b45d4fb4098eeb37
Charlie Bini <cbini87@gmail.com>
2019-01-31 17:02:46 -0500

    fix due to removal from underlying view
    @kevinverhoff is this an issue?

--- extracts/v_extracts.gsheets_survey_completion.sql
+++ extracts/v_extracts.gsheets_survey_completion.sql
@@ -6,6 +6,6 @@ CREATE OR ALTER VIEW extracts.gsheets_survey_completion AS
 SELECT sc.survey_type
       ,sc.subject_name
       ,sc.responder_email
-      ,CONVERT(NVARCHAR,sc.date_created) AS date_completed
+      ,NULL AS date_completed
 FROM gabby.surveys.survey_completion sc
 WHERE sc.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
