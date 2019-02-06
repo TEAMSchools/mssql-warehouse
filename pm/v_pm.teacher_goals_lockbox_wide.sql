f587eaa314fd1dbc054238e26a44f02f442d490a
Charlie Bini <cbini87@gmail.com>
2019-02-06 16:31:13 -0500

    :lipstick:

--- pm/v_pm.teacher_goals_lockbox_wide.sql
+++ pm/v_pm.teacher_goals_lockbox_wide.sql
@@ -1,25 +1,34 @@
-USE gabby
+USE gabby;
 GO
 
-CREATE OR ALTER VIEW gabby.pm.teacher_goals_lockbox_wide AS
+CREATE OR ALTER VIEW pm.teacher_goals_lockbox_wide AS
 
-SELECT *
+SELECT p.academic_year
+      ,p.df_employee_number
+      ,p.metric_bucket
+      ,p.metric_label
+      ,p.metric_name
+      ,p.pm_term
+      
+      ,p.[Metric Value]
+      ,p.Goal
+      ,p.Score      
+      ,p.[Grade-Level Weight]
+      ,p.[Bucket Weight]
+      ,p.[Bucket Score]
 FROM
-
-(SELECT academic_year
-      ,df_employee_number
-      ,metric_bucket
-      ,metric_label
-      ,metric_name
-      ,pm_term
-      ,measure_names
-      ,measure_values
-FROM pm.teacher_goals_lockbox
-) AS SourceTable PIVOT(
+    (
+     SELECT academic_year
+           ,df_employee_number
+           ,metric_bucket
+           ,metric_label
+           ,metric_name
+           ,pm_term
+           ,measure_names
+           ,measure_values
+     FROM pm.teacher_goals_lockbox
+    ) sub
+PIVOT (
   MAX(measure_values)
-  FOR measure_names IN ([Metric Value]
-                       ,[Goal]
-                       ,[Score]
-                       ,[Grade-Level Weight]
-                       ,[Bucket Score]
-                       ,[Bucket Weight])) AS PivotTable;
+  FOR measure_names IN ([Metric Value], [Goal], [Score], [Grade-Level Weight], [Bucket Score], [Bucket Weight])
+ ) AS p
