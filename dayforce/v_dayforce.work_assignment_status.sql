a403560bb969f82a8fc186c0035142b0dbcf0e83
Charlie Bini <cbini87@gmail.com>
2019-03-05 17:33:13 -0500

    fix future-dated statuses

--- dayforce/v_dayforce.work_assignment_status.sql
+++ dayforce/v_dayforce.work_assignment_status.sql
@@ -3,37 +3,63 @@ GO
 
 CREATE OR ALTER VIEW dayforce.work_assignment_status AS
 
-WITH eofy AS (
-  SELECT DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1, 6, 30) AS fy_end_date
-)
-
-,status_clean AS (
-  SELECT number AS df_employee_id
-        ,first_name
-        ,last_name
-        ,status
-        ,base_salary
-        ,CASE
-          WHEN status = 'Terminated' THEN DATEADD(DAY, 1, CONVERT(DATE,effective_start))
-          ELSE CONVERT(DATE,effective_start)
-         END AS effective_start
-        ,COALESCE(CONVERT(DATE,effective_end), (SELECT eofy.fy_end_date FROM eofy)) AS effective_end
-  FROM gabby.dayforce.employee_status
+WITH status_clean AS (
+  SELECT sub.df_employee_id
+        ,sub.first_name
+        ,sub.last_name
+        ,sub.status
+        ,sub.base_salary
+        ,sub.effective_start
+        ,COALESCE(sub.effective_end
+                 ,DATEFROMPARTS(CASE 
+                                 WHEN DATEPART(MONTH,sub.effective_start) >= 7 THEN DATEPART(YEAR,sub.effective_start) + 1
+                                 ELSE DATEPART(YEAR,sub.effective_start)
+                                END, 6, 30)) AS effective_end
+  FROM
+      (
+       SELECT number AS df_employee_id
+             ,first_name
+             ,last_name
+             ,status
+             ,base_salary
+             ,CASE
+               WHEN status = 'Terminated' THEN DATEADD(DAY, 1, CONVERT(DATE,effective_start))
+               ELSE CONVERT(DATE,effective_start)
+              END AS effective_start
+             ,CONVERT(DATE,effective_end) AS effective_end
+       FROM gabby.dayforce.employee_status
+      ) sub
  )
 
 ,work_assignment_clean AS (
-  SELECT employee_reference_code AS df_employee_id
-        ,job_family_name
-        ,legal_entity_name
-        ,physical_location_name
-        ,department_name
-        ,job_name
-        ,flsa_status_name
-        ,CONVERT(DATE,work_assignment_effective_start) AS work_assignment_effective_start
-        ,COALESCE(CONVERT(DATE,work_assignment_effective_end), (SELECT eofy.fy_end_date FROM eofy)) AS work_assignment_effective_end
-  FROM gabby.dayforce.employee_work_assignment
-  WHERE primary_work_assignment = 1
-)
+  SELECT sub.df_employee_id
+        ,sub.job_family_name
+        ,sub.legal_entity_name
+        ,sub.physical_location_name
+        ,sub.department_name
+        ,sub.job_name
+        ,sub.flsa_status_name
+        ,sub.work_assignment_effective_start
+        ,COALESCE(sub.work_assignment_effective_end
+                 ,DATEFROMPARTS(CASE 
+                                 WHEN DATEPART(MONTH,sub.work_assignment_effective_start) >= 7 THEN DATEPART(YEAR,sub.work_assignment_effective_start) + 1
+                                 ELSE DATEPART(YEAR,sub.work_assignment_effective_start)
+                                END, 6, 30)) AS work_assignment_effective_end
+  FROM
+      (
+       SELECT employee_reference_code AS df_employee_id
+             ,job_family_name
+             ,legal_entity_name
+             ,physical_location_name
+             ,department_name
+             ,job_name
+             ,flsa_status_name
+             ,CONVERT(DATE,work_assignment_effective_start) AS work_assignment_effective_start
+             ,CONVERT(DATE,work_assignment_effective_end) AS work_assignment_effective_end
+       FROM gabby.dayforce.employee_work_assignment
+       WHERE primary_work_assignment = 1
+      ) sub
+ )
 
 ,validdates AS (
   SELECT df_employee_id
@@ -50,7 +76,11 @@ WITH eofy AS (
 ,validranges AS (
   SELECT d.df_employee_id
         ,d.effective_date AS effective_start_date
-        ,DATEADD(DAY, -1, LEAD(d.effective_date, 1, (SELECT eofy.fy_end_date FROM eofy)) OVER(PARTITION BY d.df_employee_id ORDER BY d.effective_date)) AS effective_end_date
+        ,COALESCE(DATEADD(DAY, -1, LEAD(d.effective_date, 1) OVER(PARTITION BY d.df_employee_id ORDER BY d.effective_date))
+                 ,DATEFROMPARTS(CASE 
+                                 WHEN DATEPART(MONTH,d.effective_date) >= 7 THEN DATEPART(YEAR,d.effective_date) + 1
+                                 ELSE DATEPART(YEAR,d.effective_date)
+                                END, 6, 30)) AS effective_end_date
   FROM validdates d
  )
 
