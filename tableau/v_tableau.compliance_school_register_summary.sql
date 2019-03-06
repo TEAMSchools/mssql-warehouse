166792132d07d9c1217f2241e39beda759fb7107
Charlie Bini <cbini87@gmail.com>
2019-03-06 11:35:13 -0500

    refactoring, using student track to determine days open

--- tableau/v_tableau.compliance_school_register_summary.sql
+++ tableau/v_tableau.compliance_school_register_summary.sql
@@ -3,40 +3,13 @@ GO
 
 CREATE OR ALTER VIEW tableau.compliance_school_register_summary AS
 
-WITH schooldays AS (
-  SELECT sub.db_name
-        ,sub.academic_year
-        ,sub.schoolid
-        ,sub.region
-        ,sub.n_days_school
-        ,sub.n_days_remaining
-        ,MIN(n_days_school) OVER(PARTITION BY academic_year, region) n_days_region_min
-  FROM
-      (
-       SELECT db_name
-             ,yearid + 1990 AS academic_year
-             ,schoolid
-             ,CASE
-               WHEN db_name = 'kippcamden' THEN 'KCNA'
-               WHEN db_name LIKE 'kippnewark' THEN 'TEAM'
-               WHEN db_name LIKE 'kippmiami' THEN 'KMS'
-              END AS region
-             ,MAX(days_total) AS n_days_school
-             ,MAX(days_remaining) AS n_days_remaining           
-       FROM gabby.powerschool.calendar_rollup_static
-       GROUP BY yearid + 1990        
-               ,db_name
-               ,schoolid
-      ) sub
- )
-
-,att_mem AS (
-  SELECT CONVERT(INT,studentid) AS studentid
+WITH att_mem AS (
+  SELECT studentid        
+        ,yearid
         ,db_name
-        ,CONVERT(INT,yearid) + 1990 AS academic_year
-        ,CONVERT(INT,SUM(attendancevalue)) AS n_att
-        ,CONVERT(INT,SUM(membershipvalue)) AS n_mem
-        ,CONVERT(INT,SUM(CASE WHEN calendardate <= GETDATE() THEN membershipvalue END)) AS n_mem_ytd
+        ,SUM(attendancevalue) AS n_att
+        ,SUM(membershipvalue) AS n_mem
+        ,SUM(CASE WHEN calendardate <= GETDATE() THEN membershipvalue END) AS n_mem_ytd
   FROM gabby.powerschool.ps_adaadm_daily_ctod
   WHERE membershipvalue = 1
   GROUP BY studentid
@@ -45,34 +18,28 @@ WITH schooldays AS (
  )
 
 SELECT co.student_number
-      ,co.state_studentnumber
       ,co.lastfirst
       ,co.academic_year
       ,co.region      
-      ,co.schoolid
       ,co.reporting_schoolid
       ,co.grade_level
       ,co.entrydate
-      ,co.exitdate      
       ,co.ethnicity      
       ,co.lunchstatus
       ,co.iep_status
-      ,co.specialed_classification      
-      ,co.enroll_status
       ,co.is_pathways
+      ,co.track
       ,ISNULL(co.lep_status, 0) AS lep_status
-      ,COUNT(co.student_number) OVER(PARTITION BY co.schoolid, co.academic_year) AS n_students
-      
+
       ,nj.programtypecode
       
       ,iep.nj_se_placement AS special_education_placement 
       
-      ,d.n_days_school
-      ,d.n_days_region_min
-      ,d.n_days_remaining
+      ,d.days_total AS n_days_school
+      ,d.days_remaining AS n_days_remaining
       
       ,sub.n_mem
-      ,sub.n_att          
+      ,sub.n_att
       ,sub.n_mem_ytd
 FROM gabby.powerschool.cohort_identifiers_static co
 LEFT JOIN gabby.powerschool.s_nj_stu_x nj
@@ -82,12 +49,13 @@ LEFT JOIN gabby.easyiep.njsmart_powerschool_clean iep
   ON co.student_number = iep.student_number
  AND co.academic_year = iep.academic_year
  AND co.db_name = iep.db_name
-JOIN schooldays d
+JOIN gabby.powerschool.calendar_rollup_static d
   ON co.schoolid = d.schoolid
- AND co.academic_year = d.academic_year
+ AND co.yearid = d.yearid
+ AND co.track = d.track
  AND co.db_name = d.db_name
 JOIN att_mem sub
   ON co.studentid = sub.studentid
- AND co.academic_year = sub.academic_year
+ AND co.yearid = sub.yearid
  AND co.db_name = sub.db_name
 WHERE co.rn_year = 1
