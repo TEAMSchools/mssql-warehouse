1cbfed55cf5ea99af2e1b51fabb8eecf161454c7
Charlie Bini <cbini87@gmail.com>
2019-03-12 17:17:18 -0400

    cleaning up

--- tableau/v_tableau.student_attrition_over_time.sql
+++ tableau/v_tableau.student_attrition_over_time.sql
@@ -7,16 +7,16 @@ WITH enrolled_oct1 AS (
   SELECT student_number
         ,lastfirst
         ,academic_year
+        ,school_level
         ,reporting_schoolid
         ,grade_level
         ,entrydate
         ,exitdate
         ,exitcode
         ,exitcomment
-        ,enroll_status
         ,db_name
   FROM gabby.powerschool.cohort_identifiers_static
-  WHERE DATEFROMPARTS(academic_year, 10, 01) BETWEEN entrydate AND exitdate
+  WHERE is_enrolled_oct01 = 1
     AND academic_year >= (gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 2)
  )
 
@@ -32,6 +32,7 @@ WITH enrolled_oct1 AS (
 SELECT y1.student_number
       ,y1.lastfirst
       ,y1.academic_year AS year
+      ,y1.school_level
       ,y1.reporting_schoolid
       ,y1.grade_level
       ,y1.entrydate
@@ -40,21 +41,15 @@ SELECT y1.student_number
       ,s.exitdate
       ,s.exitcode
       ,s.exitcomment
-      ,s.enroll_status      
 
       ,d.date
-      
-      ,y2.entrydate AS next_entrydate
-      ,y2.exitdate AS next_exitdate
 
-      ,COALESCE(y2.exitdate, y1.exitdate) AS transferdate
       ,CASE
         WHEN y1.exitcode = 'G1' THEN 0 /* graduates != attrition */
         WHEN s.exitdate >= y1.exitdate AND s.exitdate >= d.date THEN 0 /* handles re-enrollments during the year */
         WHEN y1.exitdate <= d.date AND y2.entrydate IS NULL THEN 1 /* was not enrolled on 10/1 next year */
         ELSE 0
        END AS is_attrition
-      ,NULL AS is_graduation
 FROM enrolled_oct1 y1
 JOIN attrition_dates d
   ON y1.academic_year = d.attrition_year
@@ -66,4 +61,4 @@ LEFT JOIN gabby.powerschool.cohort_identifiers_static y2
   ON y1.student_number = y2.student_number
  AND y1.db_name = y2.db_name
  AND y1.academic_year = (y2.academic_year - 1)
- AND DATEFROMPARTS(y2.academic_year, 10, 01) BETWEEN y2.entrydate AND y2.exitdate
+ AND y2.is_enrolled_oct01 = 1
