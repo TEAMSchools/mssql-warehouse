2bee46448a8c9001543de2de0db0b67105254025
Charlie Bini <cbini87@gmail.com>
2019-04-11 09:31:04 -0500

    update dis

--- utilities/v_utilities.fivetran_audit.sql
+++ utilities/v_utilities.fivetran_audit.sql
@@ -1,31 +1,376 @@
-USE gabby
+USE gabby;
 GO
 
 CREATE OR ALTER VIEW utilities.fivetran_audit AS
 
-SELECT * FROM alumni.fivetran_audit UNION
-SELECT * FROM illuminate_groups.fivetran_audit UNION
-SELECT * FROM illuminate_standards.fivetran_audit UNION
-SELECT * FROM illuminate_dna_assessments.fivetran_audit UNION
-SELECT * FROM illuminate_public.fivetran_audit UNION
-SELECT * FROM illuminate_codes.fivetran_audit UNION
-SELECT * FROM illuminate_dna_repositories.fivetran_audit UNION
-SELECT * FROM recruiting.fivetran_audit UNION
-SELECT * FROM newarkenrolls.fivetran_audit UNION
-SELECT * FROM nwea.fivetran_audit UNION
-SELECT * FROM asana.fivetran_audit UNION
-SELECT * FROM zendesk.fivetran_audit UNION
-SELECT * FROM deanslist.fivetran_audit UNION
-SELECT * FROM naviance.fivetran_audit UNION
-SELECT * FROM steptool.fivetran_audit UNION
-SELECT * FROM powerschool.fivetran_audit UNION
-SELECT * FROM reporting.fivetran_audit UNION
-SELECT * FROM stmath.fivetran_audit UNION
-SELECT * FROM caredox.fivetran_audit UNION
-SELECT * FROM easyiep.fivetran_audit UNION
-SELECT * FROM renaissance.fivetran_audit UNION
-SELECT * FROM finance.fivetran_audit UNION
-SELECT * FROM lit.fivetran_audit UNION
-SELECT * FROM enrollment.fivetran_audit UNION
-SELECT * FROM adp.fivetran_audit UNION
-SELECT * FROM lexia.fivetran_audit
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM alumni.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM illuminate_groups.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM illuminate_standards.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM illuminate_dna_assessments.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM illuminate_public.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM illuminate_codes.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM illuminate_dna_repositories.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM recruiting.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM newarkenrolls.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM nwea.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM asana.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM zendesk.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM deanslist.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM naviance.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM steptool.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM reporting.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM stmath.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM caredox.fivetran_audit
+
+UNION
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM easyiep.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM renaissance.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM finance.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM lit.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM enrollment.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM adp.fivetran_audit
+
+UNION
+
+SELECT id
+      ,[schema]
+      ,[table]
+      ,status
+      ,message
+      ,update_id
+      ,update_started
+      ,start
+      ,done
+      ,rows_updated_or_inserted
+      ,progress
+FROM lexia.fivetran_audit;
