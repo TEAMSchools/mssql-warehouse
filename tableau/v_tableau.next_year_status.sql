6d94f39bfbcd032e1e1a78308b47a3b4034b0679
Charlie Bini <cbini87@gmail.com>
2019-04-12 08:37:58 -0500

    optimizing :zap:

--- tableau/v_tableau.next_year_status.sql
+++ tableau/v_tableau.next_year_status.sql
@@ -3,16 +3,16 @@ GO
 
 CREATE OR ALTER VIEW tableau.next_year_status AS
 
-SELECT co.academic_year
+SELECT co.student_number
+      ,co.state_studentnumber
+      ,co.lastfirst
+      ,co.academic_year
       ,co.region
       ,co.schoolid
-      ,co.state_studentnumber
       ,co.school_name 
-      ,co.student_number
-      ,co.lastfirst
       ,co.grade_level
-      ,co.cohort
       ,co.iep_status
+      ,co.cohort
       ,co.is_retained_ever
       ,co.enroll_status
 
@@ -26,8 +26,8 @@ SELECT co.academic_year
        END AS promo_status
 FROM gabby.powerschool.cohort_identifiers_static co
 LEFT JOIN gabby.powerschool.students s
-  ON s.student_number = co.student_number
- AND s.db_name = co.db_name
+  ON co.student_number = s.student_number
+ AND co.db_name = s.db_name
 WHERE co.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
   AND co.rn_year = 1
   AND co.enroll_status IN (0,-1)
