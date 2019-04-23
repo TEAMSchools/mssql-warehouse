4ce07fd4d4dec6f37ea5067d4fd7609a0d4d5080
Charlie Bini <cbini87@gmail.com>
2019-04-23 08:40:45 -0500

    fix identifier name

--- extracts/v_extracts.deanslist_missing_assignments.sql
+++ extracts/v_extracts.deanslist_missing_assignments.sql
@@ -3,9 +3,9 @@ GO
 
 CREATE OR ALTER VIEW extracts.deanslist_missing_assignments AS
 
-SELECT a.student_number      
-      ,a.grade_category            
-      ,a.assign_name      
+SELECT a.student_number
+      ,a.grade_category
+      ,a.assign_name
       ,a.assign_date
       ,a.course_name
       ,a.teacher_name
@@ -13,4 +13,4 @@ FROM gabby.tableau.gradebook_assignment_detail a
 WHERE a.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
   AND a.ismissing = 1
   AND a.finalgrade_category = 'Q'
-  AND (a.score = 0 OR a.score IS NULL)
+  AND (a.scorepoints = 0 OR a.scorepoints IS NULL)
