1dffcd6afefa8aebe4f5055ca7435530137a88f9
Kevin Verhoff <kevin.verhoff@gmail.com>
2019-04-24 11:39:52 -0400

    Update v_tableau.assessment_dashboard.sql
    Adding employee ids to assessment dashboard in order to make teacher goals linkages faster

--- tableau/v_tableau.assessment_dashboard.sql
+++ tableau/v_tableau.assessment_dashboard.sql
@@ -3,6 +3,41 @@ GO
 
 CREATE OR ALTER VIEW tableau.assessment_dashboard AS
 
+WITH teacher_crosswalk AS (
+  SELECT sr.df_employee_number
+        ,sr.preferred_name        
+        ,sr.primary_site
+        ,sr.primary_on_site_department
+        ,sr.grades_taught
+        ,sr.primary_job
+        ,sr.legal_entity_name        
+        ,sr.is_active
+        ,sr.primary_site_schoolid
+        ,sr.manager_df_employee_number
+        ,sr.manager_name
+        ,CASE
+          WHEN sr.legal_entity_name = 'TEAM Academy Charter Schools' THEN 'kippnewark'
+          WHEN sr.legal_entity_name = 'KIPP Cooper Norcross Academy' THEN 'kippcamden'
+          WHEN sr.legal_entity_name = 'KIPP Miami' THEN 'kippmiami'
+         END AS db_name
+      
+        ,COALESCE(idps.ps_teachernumber, sr.adp_associate_id, CONVERT(VARCHAR(25),sr.df_employee_number)) AS ps_teachernumber
+
+        ,ads.samaccountname AS staff_username
+
+        ,adm.samaccountname AS manager_username
+  FROM gabby.dayforce.staff_roster sr
+  LEFT JOIN gabby.people.id_crosswalk_powerschool idps
+    ON sr.df_employee_number = idps.df_employee_number
+   AND idps.is_master = 1
+  LEFT JOIN gabby.adsi.user_attributes_static ads
+    ON CONVERT(VARCHAR(25),sr.df_employee_number) = ads.employeenumber
+  LEFT JOIN gabby.adsi.user_attributes_static adm
+    ON CONVERT(VARCHAR(25),sr.manager_df_employee_number) = adm.employeenumber
+  WHERE sr.primary_job IN ('Teacher', 'Teacher Fellow', 'Teacher in Residence', 'Co-Teacher', 'Learning Specialist', 'Learning Specialist Coordinator')
+    AND ISNULL(sr.termination_date, GETDATE()) >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1)
+ )
+
 SELECT co.student_number
       ,co.lastfirst
       ,co.academic_year
@@ -52,6 +87,17 @@ SELECT co.student_number
         WHEN (co.grade_level <= 4 OR co.reporting_schoolid = 732585074) THEN hr.section_number
         ELSE enr.section_number
        END AS section_number
+      ,CASE
+        WHEN (co.grade_level <= 4 OR co.reporting_schoolid = 732585074) THEN hr.teachernumber
+        ELSE enr.teachernumber
+       END AS ps_teacher_number
+
+      ,tcw.df_employee_number
+      ,tcw.staff_username
+      ,tcw.manager_df_employee_number
+      ,tcw.manager_name
+      ,tcw.manager_username
+
 FROM gabby.powerschool.cohort_identifiers_static co 
 JOIN gabby.illuminate_dna_assessments.agg_student_responses_all asr
   ON co.student_number = asr.local_student_id
@@ -72,6 +118,8 @@ LEFT JOIN gabby.powerschool.course_enrollments_static hr
  AND hr.course_enroll_status = 0 
  AND hr.section_enroll_status = 0 
  AND hr.rn_course_yr = 1
+LEFT JOIN teacher_crosswalk tcw
+  ON tcw.ps_teachernumber = CASE WHEN (co.grade_level <= 4 OR co.reporting_schoolid = 732585074) THEN hr.teachernumber ELSE enr.teachernumber END COLLATE Latin1_General_BIN
 WHERE co.academic_year >= (gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1)
   AND co.reporting_schoolid NOT IN (5173, 999999) /* exclude OoD Placements */
   AND co.rn_year = 1
