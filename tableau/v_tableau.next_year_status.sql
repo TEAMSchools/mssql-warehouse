65e6c3d9f5b85757afc9f372281476302c4302a7
Anthony Walters <awalters@kippnj.org>
2019-05-03 17:40:15 -0400

    Create v_tableau.next_year_status.sql
    Made additions we spoke about plus a couple of other fields. Froze at academic_year = 2018

---
+++ tableau/v_tableau.next_year_status.sql
@@ -0,0 +1,37 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW tableau.next_year_status AS
+
+SELECT co.student_number
+      ,co.state_studentnumber
+      ,co.lastfirst
+      ,co.academic_year
+      ,co.region
+      ,co.schoolid
+      ,co.school_name 
+      ,co.grade_level
+      ,co.iep_status
+      ,co.cohort
+      ,co.is_retained_ever
+      ,co.enroll_status
+	  ,co.is_retained_year
+	  ,co.year_in_school
+
+      ,s.next_school
+      ,s.sched_nextyeargrade
+
+      ,co.dob
+	  ,CONCAT(co.street,', ',co.city,', ',co.state,' ',co.zip) AS student_address
+	  ,co.home_phone
+	  ,co.mother_cell
+	  ,co.father_cell
+
+FROM gabby.powerschool.cohort_identifiers_static co
+LEFT JOIN gabby.powerschool.students s
+  ON co.student_number = s.student_number
+ AND co.db_name = s.db_name
+WHERE co.academic_year = 2018
+  AND co.rn_year = 1
+  AND co.enroll_status IN (0,-1)
+  AND co.grade_level <> 99
