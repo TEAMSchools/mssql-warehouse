7a3d37bc2eb9a789b53f1959c5cc12e66ae1f040
Charlie Bini <CBini@kippnj.org>
2019-05-08 10:47:11 -0500

    :lipstick:

--- tableau/v_tableau.next_year_status.sql
+++ tableau/v_tableau.next_year_status.sql
@@ -1,4 +1,4 @@
-USE gabby
+USE gabby;
 GO
 
 CREATE OR ALTER VIEW tableau.next_year_status AS
@@ -9,39 +9,33 @@ SELECT co.student_number
       ,co.academic_year
       ,co.region
       ,co.schoolid
-      ,co.school_name 
+      ,co.school_name
       ,co.grade_level
       ,co.iep_status
       ,co.cohort
       ,co.is_retained_ever
-	  ,co.is_retained_year
-	  ,co.year_in_school
+      ,co.is_retained_year
+      ,co.year_in_school
       ,co.enroll_status
-	  ,co.gender
-	  ,co.dob
-	  ,co.home_phone
-	  ,co.mother_cell
-	  ,co.father_cell
-	  ,co.guardianemail
-	  ,CONCAT(co.street,', ',co.city,', ',co.state,' ',co.zip) AS student_address
-	  
-	  ,REPLACE(CONCAT(co.street,'+',co.city,'+',co.state,'+',co.zip),' ','+') AS gmaps_address
-
-	  ,CASE WHEN co.home_phone IS NOT NULL THEN REPLACE(CONCAT('+1',co.home_phone),'-','') 
-	        ELSE NULL END AS tel_home_phone
-	  ,CASE WHEN co.mother_cell IS NOT NULL THEN REPLACE(CONCAT('+1',co.mother_cell),'-','')
-	        ELSE NULL END AS tel_mother_cell
-	  ,CASE WHEN co.father_cell IS NOT NULL THEN REPLACE(CONCAT('+1',co.father_cell),'-','')
-	        ELSE NULL END AS tel_father_cell
-
+      ,co.gender
+      ,co.dob
+      ,co.home_phone
+      ,co.mother_cell
+      ,co.father_cell
+      ,co.guardianemail
+      ,CONCAT(co.street, ', ', co.city, ', ', co.state, ' ', co.zip) AS student_address
+      ,REPLACE(CONCAT(co.street, '+', co.city, '+', co.state, '+', co.zip), ' ', '+') AS gmaps_address
+      ,CASE WHEN co.home_phone IS NOT NULL THEN REPLACE(CONCAT('+1', co.home_phone), '-', '') END AS tel_home_phone
+      ,CASE WHEN co.mother_cell IS NOT NULL THEN REPLACE(CONCAT('+1', co.mother_cell), '-', '') END AS tel_mother_cell
+      ,CASE WHEN co.father_cell IS NOT NULL THEN REPLACE(CONCAT('+1', co.father_cell), '-', '') END AS tel_father_cell
+      
       ,s.next_school
       ,s.sched_nextyeargrade
-
 FROM gabby.powerschool.cohort_identifiers_static co
 LEFT JOIN gabby.powerschool.students s
   ON co.student_number = s.student_number
  AND co.db_name = s.db_name
 WHERE co.academic_year = 2018
   AND co.rn_year = 1
-  AND co.enroll_status IN (0,-1)
-  AND co.grade_level <> 99
+  AND co.enroll_status IN (0, -1)
+  AND co.grade_level <> 99;
