f9c66232f7d15f4a41426b78db09c6d733f6a780
Anthony Walters <awalters@kippnj.org>
2019-05-08 11:30:47 -0400

    Update v_tableau.next_year_status.sql

--- tableau/v_tableau.next_year_status.sql
+++ tableau/v_tableau.next_year_status.sql
@@ -14,21 +14,34 @@ SELECT co.student_number
       ,co.iep_status
       ,co.cohort
       ,co.is_retained_ever
+	  ,co.is_retained_year
+	  ,co.year_in_school
       ,co.enroll_status
+	  ,co.gender
+	  ,co.dob
+	  ,co.home_phone
+	  ,co.mother_cell
+	  ,co.father_cell
+	  ,co.guardianemail
+	  ,CONCAT(co.street,', ',co.city,', ',co.state,' ',co.zip) AS student_address
+	  
+	  ,REPLACE(CONCAT(co.street,'+',co.city,'+',co.state,'+',co.zip),' ','+') AS gmaps_address
+
+	  ,CASE WHEN co.home_phone IS NOT NULL THEN REPLACE(CONCAT('+1',co.home_phone),'-','') 
+	        ELSE NULL END AS tel_home_phone
+	  ,CASE WHEN co.mother_cell IS NOT NULL THEN REPLACE(CONCAT('+1',co.mother_cell),'-','')
+	        ELSE NULL END AS tel_mother_cell
+	  ,CASE WHEN co.father_cell IS NOT NULL THEN REPLACE(CONCAT('+1',co.father_cell),'-','')
+	        ELSE NULL END AS tel_father_cell
 
       ,s.next_school
       ,s.sched_nextyeargrade
 
-      ,CASE
-        WHEN co.grade_level = s.sched_nextyeargrade THEN 'Retained'
-        WHEN co.grade_level < s.sched_nextyeargrade THEN 'Promoted'
-        WHEN co.grade_level > s.sched_nextyeargrade THEN 'Demoted'
-       END AS promo_status
 FROM gabby.powerschool.cohort_identifiers_static co
 LEFT JOIN gabby.powerschool.students s
   ON co.student_number = s.student_number
  AND co.db_name = s.db_name
-WHERE co.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
+WHERE co.academic_year = 2018
   AND co.rn_year = 1
   AND co.enroll_status IN (0,-1)
   AND co.grade_level <> 99
