58d8486305c0c0e0ba51ceb1a4740b07115c9e36
Charlie Bini <CBini@kippnj.org>
2019-05-08 11:18:22 -0500

    :lipstick:

--- powerschool/v_powerschool.ps_attendance_meeting.sql
+++ powerschool/v_powerschool.ps_attendance_meeting.sql
@@ -1,42 +1,42 @@
 CREATE OR ALTER VIEW powerschool.ps_attendance_meeting AS 
 
 SELECT att.id
-		    ,att.studentid
-		    ,att.schoolid
-		    ,att.att_date
-		    ,att.attendance_codeid
+      ,att.studentid
+      ,att.schoolid
+      ,att.att_date
+      ,att.attendance_codeid
       ,att.att_mode_code
-		    ,att.att_interval
-		    ,att.calendar_dayid
-		    ,att.ccid
+      ,att.att_interval
+      ,att.calendar_dayid
+      ,att.ccid
       ,att.periodid
       ,att.programid
       ,att.total_minutes
 
-		    ,CASE WHEN ac.att_code = 'true' THEN 'T' ELSE ac.att_code END AS att_code 
+      ,CASE WHEN ac.att_code = 'true' THEN 'T' ELSE ac.att_code END AS att_code 
       ,ac.calculate_ada_yn
-		    ,ac.presence_status_cd
+      ,ac.presence_status_cd
       ,ac.course_credit_points
 
-		    ,cc.schoolid AS cc_schoolid
+      ,cc.schoolid AS cc_schoolid
       ,(CASE WHEN cc.sectionid < 0 THEN 1 ELSE 0 END) AS dropped
-      		    
-		    ,per.abbreviation AS period_abbreviation
-		    ,per.period_number
+            
+      ,per.abbreviation AS period_abbreviation
+      ,per.period_number
 
-		    ,s.id AS sectionid
-		    ,s.section_number
-		    
-		    ,cd.a
-		    ,cd.b
-		    ,cd.c
-		    ,cd.d
-		    ,cd.e
-		    ,cd.f
-		    ,cd.insession
-		    ,cd.cycle_day_id
+      ,s.id AS sectionid
+      ,s.section_number
+      
+      ,cd.a
+      ,cd.b
+      ,cd.c
+      ,cd.d
+      ,cd.e
+      ,cd.f
+      ,cd.insession
+      ,cd.cycle_day_id
 
-		    ,cy.abbreviation
+      ,cy.abbreviation
 FROM powerschool.attendance_clean att
 JOIN powerschool.cc 
   ON att.ccid = cc.id 
@@ -53,4 +53,4 @@ JOIN powerschool.period per
   ON att.periodid = per.id
 WHERE att.att_mode_code = 'ATT_ModeMeeting'
   AND att.att_date >= cc.dateenrolled 
-	 AND att.att_date < cc.dateleft
+  AND att.att_date < cc.dateleft
