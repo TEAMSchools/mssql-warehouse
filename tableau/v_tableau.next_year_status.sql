a579ff534c0319c1c492acc16669cfc468a15df6
Charlie Bini <CBini@kippnj.org>
2019-05-09 13:53:04 -0500

    refactor to pull from students and join to most recent cohort

--- tableau/v_tableau.next_year_status.sql
+++ tableau/v_tableau.next_year_status.sql
@@ -3,39 +3,39 @@ GO
 
 CREATE OR ALTER VIEW tableau.next_year_status AS
 
-SELECT co.student_number
-      ,co.state_studentnumber
-      ,co.lastfirst
+SELECT s.student_number
+      ,s.state_studentnumber
+      ,s.lastfirst
+      ,s.schoolid
+      ,s.grade_level
+      ,s.enroll_status
+      ,s.gender
+      ,s.dob
+      ,s.home_phone
+      ,s.guardianemail
+      ,s.next_school
+      ,s.sched_nextyeargrade
+      ,CONCAT(s.street, ', ', s.city, ', ', s.state, ' ', s.zip) AS student_address
+      ,REPLACE(CONCAT(s.street, '+', s.city, '+', s.state, '+', s.zip), ' ', '+') AS gmaps_address
+      ,CASE WHEN s.home_phone IS NOT NULL THEN REPLACE(CONCAT('+1', s.home_phone), '-', '') END AS tel_home_phone
+
       ,co.academic_year
       ,co.region
-      ,co.schoolid
       ,co.school_name
-      ,co.grade_level
       ,co.iep_status
       ,co.cohort
       ,co.is_retained_ever
       ,co.is_retained_year
       ,co.year_in_school
-      ,co.enroll_status
-      ,co.gender
-      ,co.dob
-      ,co.home_phone
       ,co.mother_cell
       ,co.father_cell
-      ,co.guardianemail
-      ,CONCAT(co.street, ', ', co.city, ', ', co.state, ' ', co.zip) AS student_address
-      ,REPLACE(CONCAT(co.street, '+', co.city, '+', co.state, '+', co.zip), ' ', '+') AS gmaps_address
-      ,CASE WHEN co.home_phone IS NOT NULL THEN REPLACE(CONCAT('+1', co.home_phone), '-', '') END AS tel_home_phone
+      ,co.boy_status
       ,CASE WHEN co.mother_cell IS NOT NULL THEN REPLACE(CONCAT('+1', co.mother_cell), '-', '') END AS tel_mother_cell
       ,CASE WHEN co.father_cell IS NOT NULL THEN REPLACE(CONCAT('+1', co.father_cell), '-', '') END AS tel_father_cell
-      
-      ,s.next_school
-      ,s.sched_nextyeargrade
-FROM gabby.powerschool.cohort_identifiers_static co
-LEFT JOIN gabby.powerschool.students s
-  ON co.student_number = s.student_number
- AND co.db_name = s.db_name
-WHERE co.academic_year = 2018
-  AND co.rn_year = 1
-  AND co.enroll_status IN (0, -1)
-  AND co.grade_level <> 99;
+FROM gabby.powerschool.students s
+LEFT JOIN gabby.powerschool.cohort_identifiers_static co
+  ON s.student_number = co.student_number
+ AND s.db_name = co.db_name 
+ AND co.rn_all = 1
+WHERE s.enroll_status IN (0, -1)
+  AND s.grade_level <> 99;
