5f93bc0303479b6b705c974ae16a972b929af43b
Charlie Bini <CBini@kippnj.org>
2019-05-15 10:19:07 -0500

    :lipstick:

--- renaissance/v_renaissance.ar_studentpractice_identifiers.sql
+++ renaissance/v_renaissance.ar_studentpractice_identifiers.sql
@@ -35,29 +35,28 @@ SELECT student_number
        END AS rn_quiz
 FROM
     (
-     SELECT CONVERT(VARCHAR(2),ar.ch_fiction_non_fiction) AS ch_fiction_non_fiction
+     SELECT CONVERT(INT,ar.i_student_practice_id) AS i_student_practice_id
+           ,CONVERT(INT,ar.i_user_id) AS i_user_id
+           ,CONVERT(INT,ar.i_quiz_number) AS i_quiz_number
+           ,CONVERT(VARCHAR(250),ar.vch_content_title) AS vch_content_title
+           ,CONVERT(DATE,ar.dt_taken) AS dt_taken
+           ,CONVERT(INT,ar.ti_passed) AS ti_passed
+           ,CONVERT(FLOAT,ar.i_word_count) AS i_word_count
            ,ar.d_percent_correct
-           ,ar.d_points_earned
-           ,CONVERT(DATE,ar.dt_taken) AS dt_taken           
-           ,ar.fl_lexile_calc
-           ,CONVERT(INT,ar.i_lexile) AS i_lexile
            ,CONVERT(FLOAT,ar.i_questions_correct) AS i_questions_correct
            ,CONVERT(FLOAT,ar.i_questions_presented) AS i_questions_presented
-           ,CONVERT(INT,ar.i_quiz_number) AS i_quiz_number           
-           ,CONVERT(INT,ar.i_student_practice_id) AS i_student_practice_id
-           ,CONVERT(INT,ar.i_user_id) AS i_user_id
-           ,CONVERT(FLOAT,ar.i_word_count) AS i_word_count
+           ,ar.fl_lexile_calc
+           ,CONVERT(INT,ar.i_lexile) AS i_lexile
+           ,CONVERT(VARCHAR(25),ar.vch_lexile_display) AS vch_lexile_display
+           ,ar.d_points_earned
+           ,CONVERT(VARCHAR(2),ar.ch_fiction_non_fiction) AS ch_fiction_non_fiction
            ,CONVERT(INT,ar.ti_book_rating) AS ti_book_rating
-           ,CONVERT(INT,ar.ti_passed) AS ti_passed
-           ,CONVERT(VARCHAR(250),ar.vch_content_title) AS vch_content_title
-           ,CONVERT(VARCHAR(25),ar.vch_lexile_display) AS vch_lexile_display    
 
-           ,CONVERT(INT,u.vch_previous_idnum) AS student_number                
+           ,CONVERT(INT,CASE WHEN ISNUMERIC(u.vch_previous_idnum) = 1 THEN u.vch_previous_idnum END) AS student_number
      FROM gabby.renaissance.ar_studentpractice ar
      JOIN gabby.renaissance.rl_user u
-       ON ar.i_user_id = u.i_user_id      
-      AND ISNUMERIC(u.vch_previous_idnum) = 1
+       ON ar.i_user_id = u.i_user_id
      WHERE ar.i_content_type_id = 31
+       AND ar.ti_row_status = 1
        AND ar.ch_status != 'U'
-       AND ar.ti_row_status = 1       
     ) sub
