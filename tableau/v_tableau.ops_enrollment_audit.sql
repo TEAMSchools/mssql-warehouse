a1cc8717ab499388f4ce8eb4813c53df69cc5fc4
Charlie Bini <CBini@kippnj.org>
2019-05-20 17:52:18 -0500

    refactor for pre-enrolls and multiyear

--- tableau/v_tableau.ops_enrollment_audit.sql
+++ tableau/v_tableau.ops_enrollment_audit.sql
@@ -52,105 +52,145 @@ WITH caredox_enrollment AS (
 
 ,residency_verification AS (
   SELECT nen
+        ,academic_year
         ,verification_date
   FROM
       (
        SELECT CONVERT(VARCHAR(25),subject_line) AS nen
+             ,academic_year
              ,[timestamp] AS verification_date
              ,ROW_NUMBER() OVER(
-               PARTITION BY subject_line
+               PARTITION BY subject_line, academic_year
                  ORDER BY [timestamp] DESC) AS rn_recent
        FROM gabby.enrollment.residency_verification
-       WHERE academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
       ) sub
   WHERE rn_recent = 1
  )
 
 ,all_data AS (
-  SELECT co.student_number
-        ,co.newark_enrollment_number
-        ,co.lastfirst
-        ,co.academic_year
-        ,co.region
-        ,co.reporting_schoolid
-        ,co.grade_level        
-        ,CASE
-          WHEN co.enroll_status = -1 THEN 'Pre-Registered'
-          WHEN co.year_in_network = 1 THEN 'New to KIPP NJ'
-          WHEN co.year_in_school = 1 THEN 'New to School'
-          ELSE 'Returning Student'
-         END AS entry_status           
-        ,CONVERT(VARCHAR(500),ISNULL(co.region + co.city, '')) COLLATE Latin1_General_BIN AS region_city
-        ,CONVERT(VARCHAR(500),ISNULL(co.lunch_app_status,'')) COLLATE Latin1_General_BIN AS lunch_app_status
-        ,CONVERT(VARCHAR(500),CONVERT(MONEY,ISNULL(co.lunch_balance,0))) COLLATE Latin1_General_BIN AS lunch_balance
-      
-        ,CONVERT(VARCHAR(500),ISNULL(uxs.residency_proof_1,'Missing')) COLLATE Latin1_General_BIN AS residency_proof_1
-        ,CONVERT(VARCHAR(500),CASE WHEN co.year_in_network = 1 THEN ISNULL(uxs.residency_proof_2,'Missing') END) COLLATE Latin1_General_BIN AS residency_proof_2
-        ,CONVERT(VARCHAR(500),CASE WHEN co.year_in_network = 1 THEN ISNULL(uxs.residency_proof_3,'Missing') END) COLLATE Latin1_General_BIN AS residency_proof_3        
-        ,CONVERT(VARCHAR(500),ISNULL(uxs.birth_certificate_proof,'N')) COLLATE Latin1_General_BIN AS birth_certificate_proof        
-        ,CONVERT(VARCHAR(500),ISNULL(uxs.iep_registration_followup,'')) COLLATE Latin1_General_BIN AS iep_registration_followup_required
-        ,CONVERT(VARCHAR(500),CASE 
-                               WHEN uxs.iep_registration_followup IS NULL THEN ''
-                               WHEN uxs.iep_registration_followup = 1 AND co.specialed_classification IS NOT NULL THEN 'Y'
-                               ELSE 'N'
-                              END) COLLATE Latin1_General_BIN AS iep_registration_followup_complete
-        ,CONVERT(VARCHAR(500),ISNULL(uxs.lep_registration_followup,'')) COLLATE Latin1_General_BIN AS lep_registration_followup_required
-        ,CONVERT(VARCHAR(500),CASE 
-                               WHEN uxs.lep_registration_followup IS NULL THEN ''
-                               WHEN uxs.lep_registration_followup = 1 AND co.lep_status IS NOT NULL THEN 'Y'
-                               ELSE 'N'
-                              END) COLLATE Latin1_General_BIN AS lep_registration_followup_complete
+  SELECT sub.student_number
+        ,sub.newark_enrollment_number
+        ,sub.lastfirst
+        ,sub.region
+        ,sub.reporting_schoolid
+        ,sub.grade_level
+        ,sub.academic_year
+        ,sub.entry_status
+        
+        ,CONVERT(VARCHAR(500),sub.lunch_app_status) COLLATE Latin1_General_BIN AS lunch_app_status
+        ,CONVERT(VARCHAR(500),sub.lunch_balance) COLLATE Latin1_General_BIN AS lunch_balance
+        ,CONVERT(VARCHAR(500),sub.residency_proof_1) COLLATE Latin1_General_BIN AS residency_proof_1
+        ,CONVERT(VARCHAR(500),sub.residency_proof_2) COLLATE Latin1_General_BIN AS residency_proof_2
+        ,CONVERT(VARCHAR(500),sub.residency_proof_3) COLLATE Latin1_General_BIN AS residency_proof_3
+        ,CONVERT(VARCHAR(500),sub.birth_certificate_proof) COLLATE Latin1_General_BIN AS birth_certificate_proof
+        ,CONVERT(VARCHAR(500),sub.iep_registration_followup) COLLATE Latin1_General_BIN AS iep_registration_followup_required
+        ,CONVERT(VARCHAR(500),sub.lep_registration_followup) COLLATE Latin1_General_BIN AS lep_registration_followup_required
+        ,CONVERT(VARCHAR(500),sub.caredox_enrollment_status) COLLATE Latin1_General_BIN AS caredox_enrollment_status
+        ,CONVERT(VARCHAR(500),sub.caredox_immunization_status) COLLATE Latin1_General_BIN AS caredox_immunization_status
+        ,CONVERT(VARCHAR(500),sub.caredox_screenings_status) COLLATE Latin1_General_BIN AS caredox_screenings_status
+        ,CONVERT(VARCHAR(500),sub.caredox_medication_status) COLLATE Latin1_General_BIN AS caredox_medication_status
+        ,CONVERT(VARCHAR(500),sub.residency_verification_scanned) COLLATE Latin1_General_BIN AS residency_verification_scanned
+        
+        ,CONVERT(VARCHAR(500),sub.region + sub.city) COLLATE Latin1_General_BIN AS region_city
+        
         ,CONVERT(VARCHAR(500),CASE
-                               WHEN co.year_in_network = 1 
-                                AND CONCAT(ISNULL(uxs.residency_proof_1,'Missing')
-                                          ,ISNULL(uxs.residency_proof_2,'Missing')
-                                          ,ISNULL(uxs.residency_proof_3,'Missing')) NOT LIKE '%Missing%' 
-                                      THEN 'Y'
-                               WHEN co.year_in_network > 1 
-                                AND ISNULL(uxs.residency_proof_1,'Missing') != 'N' 
-                                AND rv.verification_date IS NOT NULL
-                                      THEN 'Y'
+                               WHEN sub.iep_registration_followup = '1'
+                                AND sub.specialed_classification != ''
+                                     THEN 'Y'
+                               WHEN sub.iep_registration_followup = '1'
+                                AND sub.specialed_classification = ''
+                                     THEN 'N'
+                               ELSE ''
+                              END) COLLATE Latin1_General_BIN AS iep_registration_followup_complete
+        ,CONVERT(VARCHAR(500),CASE
+                               WHEN sub.lep_registration_followup = '1'
+                                AND sub.lep_status != ''
+                                     THEN 'Y'
+                               WHEN sub.lep_registration_followup = '1'
+                                AND sub.lep_status = ''
+                                     THEN 'N'
+                               ELSE ''
+                              END) COLLATE Latin1_General_BIN AS lep_registration_followup_complete
+        ,CONVERT(VARCHAR(500),CASE 
+                               WHEN CONCAT(sub.residency_proof_1, sub.residency_proof_2, sub.residency_proof_3) NOT LIKE '%Missing%' THEN 'Y' 
                                ELSE 'N'
                               END) COLLATE Latin1_General_BIN AS residency_proof_all
+  FROM
+      (
+       SELECT s.student_number
+             ,s.lastfirst
+             ,s.grade_level
+             ,s.enroll_status
+             ,s.city
+             ,CASE
+               WHEN s.db_name = 'kippcamden' THEN 'KCNA' 
+               WHEN s.db_name = 'kippnewark' THEN 'TEAM' 
+               WHEN s.db_name = 'kippmiami' THEN 'KMS' 
+              END AS region
 
-        ,CONVERT(VARCHAR(500),ISNULL(CASE WHEN rv.nen IS NOT NULL THEN 'Y' END,'N')) COLLATE Latin1_General_BIN AS residency_verification_scanned
-        --,CONVERT(VARCHAR(500),CASE WHEN co.year_in_network > 1 THEN ISNULL(rv.verification_date,'1900-07-01') END) AS reverification_date
+             ,COALESCE(co.reporting_schoolid, s.schoolid) AS reporting_schoolid
+             ,ISNULL(co.specialed_classification, '') AS specialed_classification
+             ,ISNULL(co.lep_status, '') AS lep_status
+             ,ISNULL(co.lunch_app_status, '') AS lunch_app_status
+             ,CONVERT(MONEY, ISNULL(co.lunch_balance, 0)) AS lunch_balance
+             ,CASE
+               WHEN s.enroll_status = -1 THEN 'Pre-Registered'
+               WHEN COALESCE(co.year_in_network, 1) = 1 THEN 'New to KIPP NJ'
+               WHEN COALESCE(co.year_in_school, 1) = 1 THEN 'New to School'
+               ELSE 'Returning Student'
+              END AS entry_status
 
-        ,CONVERT(VARCHAR(500),ISNULL(cde.status,'')) COLLATE Latin1_General_BIN AS caredox_enrollment_status
-        
-        ,CONVERT(VARCHAR(500),ISNULL(cdi.status,'')) COLLATE Latin1_General_BIN AS caredox_immunization_status
-        
-        ,CONVERT(VARCHAR(500),ISNULL(cds.status,'')) COLLATE Latin1_General_BIN AS caredox_screenings_status
-        
-        ,CONVERT(VARCHAR(500),ISNULL(cdm.medication,'')) COLLATE Latin1_General_BIN AS caredox_medication_status
-  FROM gabby.powerschool.cohort_identifiers_static co  
-  LEFT JOIN gabby.powerschool.u_def_ext_students uxs
-    ON co.students_dcid = uxs.studentsdcid
-   AND co.db_name = uxs.db_name
-  LEFT JOIN residency_verification rv
-    ON co.newark_enrollment_number = rv.nen  
-  LEFT JOIN caredox_enrollment cde
-    ON co.student_number = cde.student_id
-   AND cde.rn_last_updated = 1
-  LEFT JOIN caredox_immunization cdi
-    ON co.student_number = cdi.student_id
-   AND cdi.rn_last_updated = 1
-  LEFT JOIN caredox_screenings cds
-    ON co.student_number = cds.student_id
-   AND cds.rn_last_updated = 1
-  LEFT JOIN caredox_medications cdm
-    ON co.student_number = cdm.student_id
-  WHERE co.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()    
-    AND co.rn_year = 1
-    AND co.enroll_status IN (-1, 0)
-    AND co.schoolid != 999999    
+             ,suf.newark_enrollment_number
+
+             ,ISNULL(uxs.residency_proof_1, 'Missing') AS residency_proof_1
+             ,ISNULL(uxs.residency_proof_2, 'Missing') AS residency_proof_2
+             ,ISNULL(uxs.residency_proof_3, 'Missing') AS residency_proof_3
+             ,ISNULL(uxs.birth_certificate_proof, 'N') AS birth_certificate_proof
+             ,ISNULL(uxs.iep_registration_followup, '') AS iep_registration_followup
+             ,ISNULL(uxs.lep_registration_followup, '') AS lep_registration_followup
+
+             ,CASE WHEN rv.nen IS NOT NULL THEN 'Y' ELSE 'N' END AS residency_verification_scanned
+
+             ,ISNULL(cde.status, '') AS caredox_enrollment_status
+             ,ISNULL(cdi.status, '') AS caredox_immunization_status
+             ,ISNULL(cds.status, '') AS caredox_screenings_status
+             ,ISNULL(cdm.medication, '') AS caredox_medication_status
+
+             ,gabby.utilities.GLOBAL_ACADEMIC_YEAR() AS academic_year
+       FROM gabby.powerschool.students s
+       LEFT JOIN gabby.powerschool.cohort_identifiers_static co
+         ON s.student_number = co.student_number
+        AND s.db_name = co.db_name
+        AND co.rn_undergrad = 1
+       LEFT JOIN gabby.powerschool.u_studentsuserfields suf
+         ON s.dcid = suf.studentsdcid
+        AND s.db_name = suf.db_name
+       LEFT JOIN gabby.powerschool.u_def_ext_students uxs
+         ON s.dcid = uxs.studentsdcid
+        AND s.db_name = uxs.db_name
+       LEFT JOIN residency_verification rv
+         ON suf.newark_enrollment_number = rv.nen
+        AND rv.academic_year = COALESCE(co.academic_year, gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1)
+       LEFT JOIN caredox_enrollment cde
+         ON s.student_number = cde.student_id
+        AND cde.rn_last_updated = 1
+       LEFT JOIN caredox_immunization cdi
+         ON s.student_number = cdi.student_id
+        AND cdi.rn_last_updated = 1
+       LEFT JOIN caredox_screenings cds
+         ON s.student_number = cds.student_id
+        AND cds.rn_last_updated = 1
+       LEFT JOIN caredox_medications cdm
+         ON s.student_number = cdm.student_id
+       WHERE s.enroll_status IN (-1, 0)
+      ) sub
  )
 
 ,unpivoted AS (
-  SELECT student_number        
-        ,academic_year        
+  SELECT student_number
+        ,academic_year
         ,field
-        ,value      
+        ,value
   FROM all_data
   UNPIVOT(
     value
@@ -164,8 +204,7 @@ WITH caredox_enrollment AS (
                  ,residency_proof_1
                  ,residency_proof_2
                  ,residency_proof_3
-                 --,reverification_date
-                 ,birth_certificate_proof               
+                 ,birth_certificate_proof
                  ,caredox_enrollment_status
                  ,caredox_immunization_status
                  ,caredox_screenings_status
@@ -174,7 +213,6 @@ WITH caredox_enrollment AS (
                  ,residency_verification_scanned)
    ) u
  )
-
 
 SELECT a.student_number
       ,a.newark_enrollment_number
@@ -190,7 +228,7 @@ SELECT a.student_number
       ,a.residency_proof_2
       ,a.residency_proof_3
       ,a.residency_proof_all
-      ,NULL AS reverification_date      
+      ,NULL AS reverification_date
       ,a.birth_certificate_proof
       ,a.residency_verification_scanned
       ,a.iep_registration_followup_required
@@ -238,13 +276,11 @@ SELECT a.student_number
         WHEN u.field = 'residency_proof_3' AND u.value NOT IN ('','Missing') THEN 1
         WHEN u.field = 'residency_proof_3' AND u.value IN ('','Missing') THEN -1
         WHEN u.field = 'residency_proof_all' AND u.value = 'Y' THEN 1
-        WHEN u.field = 'residency_proof_all' AND u.value = 'N' THEN -1        
-        --WHEN u.field = 'reverification_date' AND CONVERT(DATE,u.value) >= DATEFROMPARTS(2017, 5, 17) THEN 1 /* UPDATE ANNUALLY WITH VERIFICATION DATE CUTOFF */
-        --WHEN u.field = 'reverification_date' AND CONVERT(DATE,u.value) < DATEFROMPARTS(2017, 5, 17) THEN -1 /* UPDATE ANNUALLY WITH VERIFICATION DATE CUTOFF */
+        WHEN u.field = 'residency_proof_all' AND u.value = 'N' THEN -1
         WHEN u.field = 'residency_verification_scanned' AND u.value = 'Y' THEN 1
         WHEN u.field = 'residency_verification_scanned' AND u.value IN ('','N') THEN -1
        END AS audit_status
 FROM all_data a
-INNER JOIN unpivoted u
-   ON a.student_number = u.student_number
-  AND a.academic_year = u.academic_year
+JOIN unpivoted u
+  ON a.student_number = u.student_number
+ AND a.academic_year = u.academic_year
