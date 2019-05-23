61cbd5b6bbd07c07bc564dcff46bd4803dd33dea
Charlie Bini <CBini@kippnj.org>
2019-05-23 16:08:29 -0500

    add calcs for miami enrollment, need to be added to main query

--- powerschool/v_powerschool.cohort_identifiers.sql
+++ powerschool/v_powerschool.cohort_identifiers.sql
@@ -7,6 +7,8 @@ WITH enr AS (
         ,MAX(sub.is_enrolled_oct01) AS is_enrolled_oct01
         ,MAX(sub.is_enrolled_oct15) AS is_enrolled_oct15
         ,MAX(sub.is_enrolled_recent) AS is_enrolled_recent
+        ,MAX(sub.is_enrolled_oct15_week) AS is_enrolled_oct15_week
+        ,MAX(sub.is_enrolled_jan15_week) AS is_enrolled_jan15_week
   FROM
       (
        SELECT co.student_number
@@ -16,6 +18,26 @@ WITH enr AS (
              ,CASE WHEN DATEFROMPARTS(co.academic_year, 10, 1) BETWEEN co.entrydate AND co.exitdate THEN 1 END AS is_enrolled_oct01
              ,CASE WHEN DATEFROMPARTS(co.academic_year, 10, 15) BETWEEN co.entrydate AND co.exitdate THEN 1 END AS is_enrolled_oct15
              ,CASE WHEN co.exitdate >= c.max_calendardate THEN 1 END AS is_enrolled_recent
+              /* enrolled week of 10/15 */
+             ,CASE
+               WHEN co.entrydate <= DATEADD(DAY
+                                           ,7 - (DATEPART(WEEKDAY, DATEFROMPARTS(co.academic_year, 10, 15)))
+                                           ,DATEFROMPARTS(co.academic_year, 10, 15)) /* entered before 10/15 week end */
+                AND co.exitdate  >= DATEADD(DAY
+                                           ,0 - (DATEPART(WEEKDAY, DATEFROMPARTS(co.academic_year, 10, 15)) - 1)
+                                           ,DATEFROMPARTS(co.academic_year, 10, 15)) /* exited after 10/15 week start */
+                      THEN 1
+              END AS is_enrolled_oct15_week
+              /* enrolled week of 01/15 */
+             ,CASE
+               WHEN co.entrydate <= DATEADD(DAY
+                                           ,7 - (DATEPART(WEEKDAY, DATEFROMPARTS(co.academic_year + 1, 1, 15)))
+                                           ,DATEFROMPARTS(co.academic_year + 1, 1, 15)) /* entered before 01/15 week end */
+                AND co.exitdate  >= DATEADD(DAY
+                                           ,0 - (DATEPART(WEEKDAY, DATEFROMPARTS(co.academic_year + 1, 1, 15)) - 1)
+                                           ,DATEFROMPARTS(co.academic_year + 1, 1, 15)) /* exited after 01/15 week start */
+                      THEN 1
+              END AS is_enrolled_jan15_week
        FROM powerschool.cohort_static co
        LEFT JOIN powerschool.calendar_rollup_static c
          ON co.schoolid = c.schoolid
