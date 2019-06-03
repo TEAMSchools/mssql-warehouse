71c98fe2a6f6d829601833386c1a1991607b5b87
Charlie Bini <CBini@kippnj.org>
2019-06-03 16:19:41 -0500

    rename, refactor pivot to sub, :lipstick:

---
+++ pm/v_pm.teacher_goals_overall_scores.sql
@@ -0,0 +1,73 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW pm.teacher_goals_overall_scores AS
+
+SELECT sub.df_employee_number
+      ,sub.preferred_name
+      ,sub.legal_entity_name
+      ,sub.hire_date
+      ,sub.primary_job
+      ,sub.primary_site
+      ,sub.academic_year
+      ,sub.pm_term
+      ,sub.etr_score
+      ,sub.so_score
+      ,sub.overall_score
+      ,CASE
+        WHEN sub.etr_score >= 3.5 THEN 4
+        WHEN sub.etr_score >= 2.75 THEN 3
+        WHEN sub.etr_score >= 1.75 THEN 2
+        WHEN sub.etr_score < 1.75 THEN 1
+       END AS etr_tier
+      ,CASE
+        WHEN sub.so_score >= 3.5 THEN 4
+        WHEN sub.so_score >= 3.0 THEN 3
+        WHEN sub.so_score >= 2.0 THEN 2
+        WHEN sub.so_score < 2.0 THEN 1
+       END AS so_tier
+      ,CASE
+        WHEN sub.overall_score >= 3.5 THEN 4
+        WHEN sub.overall_score >= 2.75 THEN 3
+        WHEN sub.overall_score >= 1.75 THEN 2
+        WHEN sub.overall_score < 1.75 THEN 1
+       END AS overall_tier
+FROM
+    (
+     SELECT p.df_employee_number
+           ,p.preferred_name
+           ,p.legal_entity_name
+           ,p.hire_date
+           ,p.primary_job
+           ,p.primary_site
+           ,p.academic_year
+           ,p.pm_term
+           ,p.[Excellent Teaching Rubric] AS etr_score
+           ,p.[Self & Others] AS so_score
+           ,COALESCE((p.[Excellent Teaching Rubric] * 0.8) + (p.[Self & Others] * 0.2)
+                    ,p.[Self & Others]
+                    ,p.[Excellent Teaching Rubric]) AS overall_score
+     FROM
+         (
+          SELECT tg.df_employee_number
+                ,tg.preferred_name
+                ,tg.primary_job
+                ,tg.primary_site
+                ,tg.academic_year
+                ,tg.pm_term
+                ,tg.metric_label
+                ,tg.metric_value_stored
+
+                ,r.original_hire_date AS hire_date
+                ,r.legal_entity_name
+          FROM gabby.tableau.pm_teacher_goals tg
+          LEFT JOIN gabby.dayforce.staff_roster r
+            ON tg.df_employee_number = r.df_employee_number
+          WHERE tg.metric_label IN ('Excellent Teaching Rubric', 'Self & Others')
+            AND tg.metric_value_stored IS NOT NULL
+         ) sub
+     PIVOT (
+       MAX(metric_value_stored)
+       FOR metric_label IN ([Excellent Teaching Rubric], [Self & Others])
+      ) p
+    ) sub
