731bb2146b93e220f4cbc71434c423272cb8ce27
Charlie Bini <CBini@kippnj.org>
2019-06-11 13:40:41 -0500

    use staff crosswalk

--- tableau/v_tableau.dayforce_tafw_requests.sql
+++ tableau/v_tableau.dayforce_tafw_requests.sql
@@ -11,13 +11,13 @@ SELECT t.reference_code AS df_employee_number
 
       ,r.preferred_name AS employee_name
       ,r.userprincipalname AS employee_email
-      ,r.location_description AS location
-      ,r.home_department_description AS department
-      ,r.job_title_description AS job_title
-      ,r.position_status
+      ,r.primary_site AS location
+      ,r.primary_on_site_department AS department
+      ,r.primary_job AS job_title
+      ,r.status AS position_status
       ,r.manager_df_employee_number
       ,r.manager_name
-      ,r.manager_mail
+      ,r.manager_userprincipalname AS manager_mail
 FROM gabby.dayforce.tafw_requests t
-JOIN gabby.tableau.staff_roster r
-  ON t.reference_code = r.df_employee_number 
+JOIN gabby.people.staff_crosswalk_static r
+  ON t.reference_code = r.df_employee_number
