98bf1299ee7a1efd81cdf045bfafc7b8cb96c73a
Charlie Bini <CBini@kippnj.org>
2019-07-09 10:43:36 -0400

    fixing dupes

---
+++ powerschool/v_powerschool.u_clg_et_stu_alt_clean.sql
@@ -0,0 +1,18 @@
+CREATE OR ALTER VIEW powerschool.u_clg_et_stu_alt_clean AS
+
+SELECT sub.id
+      ,sub.studentsdcid
+      ,sub.exit_date
+      ,sub.exit_code
+FROM
+    (
+     SELECT CONVERT(INT, id) AS id
+           ,CONVERT(INT, studentsdcid) AS studentsdcid
+           ,exit_date
+           ,CONVERT(VARCHAR(5), exit_code) AS exit_code
+           ,ROW_NUMBER() OVER(
+              PARTITION BY studentsdcid, exit_date
+                ORDER BY id DESC) AS rn
+     FROM powerschool.u_clg_et_stu_alt
+    ) sub
+WHERE rn = 1
