232123c0a728714a66d4bc5e50b29ad598ce8b4d
Charlie Bini <CBini@kippnj.org>
2019-07-16 16:17:02 -0500

    refactor, filter for only matching homeschoolid

--- extracts/v_extracts.powerschool_autocomm_teachers_accounts.sql
+++ extracts/v_extracts.powerschool_autocomm_teachers_accounts.sql
@@ -6,96 +6,44 @@ CREATE OR ALTER VIEW extracts.powerschool_autocomm_teachers_accounts AS
 WITH users_clean AS (
   SELECT df.preferred_first_name AS first_name
         ,df.preferred_last_name AS last_name
-        ,df.legal_entity_name
-        ,COALESCE(psid.ps_teachernumber, df.adp_associate_id, CONVERT(VARCHAR(25),df.df_employee_number)) AS teachernumber
+        ,sc.region AS legal_entity_name
+        ,df.ps_teachernumber AS teachernumber
+        ,df.primary_site_schoolid AS homeschoolid
+        ,LOWER(df.samaccountname) AS loginid
+        ,LOWER(df.samaccountname) AS teacherloginid
+        ,LOWER(df.mail) AS email_addr
         ,CASE
-          WHEN df.primary_site = '18th Ave Campus' THEN 73255
-          WHEN df.primary_site = 'KIPP Lanning Sq Campus' THEN 179901
-          ELSE df.primary_site_schoolid 
-         END AS homeschoolid        
-        ,CASE
-          WHEN df.legal_entity_name = 'TEAM Academy Charter Schools' THEN 'kippnewark'
-          WHEN df.legal_entity_name = 'KIPP Cooper Norcross Academy' THEN 'kippcamden'
-          WHEN df.legal_entity_name = 'KIPP Miami' THEN 'kippmiami'
-         END AS db_name
-           
-        ,LOWER(dir.samaccountname) AS loginid
-        ,LOWER(dir.samaccountname) AS teacherloginid
-        ,LOWER(dir.mail) AS email_addr
-           
-        ,CASE
-          WHEN psid.is_master = 0 THEN 2
-          WHEN df.termination_date < GETDATE() THEN 2
-          WHEN df.status IN ('ACTIVE','INACTIVE','PRESTART', 'PLOA', 'ADMIN_LEAVE') THEN 1
-          WHEN df.termination_date >= CONVERT(DATE,GETDATE()) THEN 1
+          WHEN DATEDIFF(DAY, ISNULL(df.termination_date, CONVERT(DATE, GETDATE())), GETDATE()) <= 7 THEN 1
+          WHEN df.[status] IN ('ACTIVE','INACTIVE','PRESTART', 'PLOA', 'ADMIN_LEAVE') THEN 1
+          WHEN df.termination_date >= CONVERT(DATE, GETDATE()) THEN 1
           ELSE 2
-         END AS status
-  FROM gabby.dayforce.staff_roster df 
-  LEFT JOIN gabby.adsi.user_attributes_static dir
-    ON CONVERT(VARCHAR(25),df.df_employee_number) = dir.employeenumber
-  LEFT JOIN gabby.people.id_crosswalk_powerschool psid
-    ON df.df_employee_number = psid.df_employee_number
-   AND psid.is_master = 1
-  WHERE DATEDIFF(DAY, ISNULL(df.termination_date, CONVERT(DATE,GETDATE())), GETDATE()) <= 7 /* import terminated staff up to a week after termination date */
+         END AS [status]
+  FROM gabby.people.staff_crosswalk_static df
+  JOIN gabby.people.school_crosswalk sc
+    ON df.primary_site = sc.site_name
+  JOIN gabby.powerschool.users u
+    ON df.ps_teachernumber = u.teachernumber COLLATE Latin1_General_BIN
+   AND df.primary_site_schoolid = u.homeschoolid
+   AND CASE 
+        WHEN sc.region = 'TEAM Academy Charter Schools' THEN 'kippnewark'
+        WHEN sc.region = 'KIPP Cooper Norcross Academy' THEN 'kippcamden'
+        WHEN sc.region = 'KIPP Miami' THEN 'kippmiami'
+       END = u.[db_name]
+  WHERE DATEDIFF(DAY, ISNULL(df.termination_date, CONVERT(DATE, GETDATE())), GETDATE()) <= 14 /* import terminated staff up to a week after termination date */
     AND df.primary_on_site_department != 'Data'
  )
 
-/* Home School Record */       
 SELECT df.teachernumber
       ,df.first_name
       ,df.last_name
-      ,CASE WHEN df.status = 1 THEN df.loginid END AS loginid
-      ,CASE WHEN df.status = 1 THEN df.teacherloginid END AS teacherloginid
+      ,CASE WHEN df.[status] = 1 THEN df.loginid END AS loginid
+      ,CASE WHEN df.[status] = 1 THEN df.teacherloginid END AS teacherloginid
       ,df.email_addr
-      ,CONVERT(INT,COALESCE(df.homeschoolid, 0)) AS schoolid
-      ,CONVERT(INT,COALESCE(df.homeschoolid, 0)) AS homeschoolid
-      ,df.status      
-      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS teacherldapenabled
-      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS adminldapenabled      
-      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS ptaccess            
+      ,CONVERT(INT, COALESCE(df.homeschoolid, 0)) AS schoolid
+      ,CONVERT(INT, COALESCE(df.homeschoolid, 0)) AS homeschoolid
+      ,df.[status]
+      ,CASE WHEN df.[status] = 1 THEN 1 ELSE 0 END AS teacherldapenabled
+      ,CASE WHEN df.[status] = 1 THEN 1 ELSE 0 END AS adminldapenabled
+      ,CASE WHEN df.[status] = 1 THEN 1 ELSE 0 END AS ptaccess
       ,df.legal_entity_name
 FROM users_clean df
-WHERE df.legal_entity_name != 'KIPP New Jersey'
-
-UNION ALL
-
-/* KNJ Admins */
-SELECT df.teachernumber
-      ,df.first_name
-      ,df.last_name
-      ,CASE WHEN df.status = 1 THEN df.loginid END AS loginid
-      ,CASE WHEN df.status = 1 THEN df.teacherloginid END AS teacherloginid
-      ,df.email_addr
-      ,0 AS schoolid
-      ,0 AS homeschoolid
-      ,df.status      
-      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS teacherldapenabled
-      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS adminldapenabled      
-      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS ptaccess            
-      ,df.legal_entity_name
-FROM users_clean df
-WHERE df.legal_entity_name = 'KIPP New Jersey'
-
-UNION ALL
-
-/* Additional School Records */
-SELECT df.teachernumber
-      ,df.first_name
-      ,df.last_name
-      ,CASE WHEN df.status = 1 THEN df.loginid END AS loginid
-      ,CASE WHEN df.status = 1 THEN df.teacherloginid END AS teacherloginid
-      ,df.email_addr
-      ,CONVERT(INT,COALESCE(t.schoolid, df.homeschoolid, 0)) AS schoolid
-      ,CONVERT(INT,COALESCE(df.homeschoolid, 0)) AS homeschoolid
-      ,df.status      
-      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS teacherldapenabled
-      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS adminldapenabled      
-      ,CASE WHEN df.status = 1 THEN 1 ELSE 0 END AS ptaccess            
-      ,df.legal_entity_name
-FROM users_clean df
-JOIN gabby.powerschool.teachers_static t
-  ON df.teachernumber = t.teachernumber COLLATE Latin1_General_BIN
- AND df.db_name = t.db_name 
- AND df.homeschoolid != t.schoolid
- AND t.schoolid != 0
-WHERE df.legal_entity_name != 'KIPP New Jersey'
