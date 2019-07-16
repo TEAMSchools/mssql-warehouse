c02cdcb4fd20a91564c36912e2231bc343ebec12
Charlie Bini <CBini@kippnj.org>
2019-07-16 16:16:20 -0500

    integrate staff and campus crosswalk, limit CMO access

--- extracts/v_extracts.illuminate_roles.sql
+++ extracts/v_extracts.illuminate_roles.sql
@@ -4,80 +4,44 @@ GO
 CREATE OR ALTER VIEW extracts.illuminate_roles AS
 
 /* KNJ specific departments = all CMO schools */
-SELECT COALESCE(id.ps_teachernumber, CONVERT(VARCHAR(25),df.df_employee_number)) AS [01 Local User ID]
+SELECT df.ps_teachernumber AS [01 Local User ID]
       ,sch.school_number AS [02 Site ID]
       ,'School Leadership' AS [03 Role Name]
       ,CONCAT(gabby.utilities.GLOBAL_ACADEMIC_YEAR() , '-', (gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1))  AS [04 Academic Year]
       ,1 AS [05 Session Type ID]
-FROM gabby.dayforce.staff_roster df
-LEFT JOIN gabby.people.id_crosswalk_powerschool id
-  ON df.df_employee_number = id.df_employee_number
- AND id.is_master = 1
+FROM gabby.people.staff_crosswalk_static df
 JOIN gabby.powerschool.schools sch
   ON sch.state_excludefromreporting = 0
 WHERE df.is_active = 1
-  AND df.primary_on_site_department IN ('Teaching and Learning', 'Data', 'Executive', 'School Support')
+  AND df.primary_on_site_department IN ('Teaching and Learning', 'Data', 'Executive')
   AND df.legal_entity_name = 'KIPP New Jersey'
 
 UNION ALL
 
-/* R9/R10 regional = all schools in region */
-SELECT COALESCE(id.ps_teachernumber, CONVERT(VARCHAR(25),df.df_employee_number)) AS [01 Local User ID]
-      ,sch.school_number AS [02 Site ID]
-      ,'School Leadership' AS [03 Role Name]
-      ,CONCAT(gabby.utilities.GLOBAL_ACADEMIC_YEAR() , '-', (gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1))  AS [04 Academic Year]
-      ,1 AS [05 Session Type ID]
-FROM gabby.dayforce.staff_roster df
-LEFT JOIN gabby.people.id_crosswalk_powerschool id
-  ON df.df_employee_number = id.df_employee_number
- AND id.is_master = 1
-JOIN gabby.powerschool.schools sch
-  ON CASE 
-      WHEN df.legal_entity_name = 'TEAM Academy Charter Schools' THEN 'kippnewark'
-      WHEN df.legal_entity_name = 'KIPP Cooper Norcross Academy' THEN 'kippcamden'
-      WHEN df.legal_entity_name = 'KIPP Miami' THEN 'kippmiami'
-      WHEN df.legal_entity_name = 'KIPP New Jersey' THEN sch.db_name
-     END = sch.db_name
- AND sch.state_excludefromreporting = 0
-WHERE df.is_active = 1
-  AND primary_site IN ('Room 9 - 60 Park Pl','Room 10 - 740 Chestnut St')
-  AND legal_entity_name != 'KIPP New Jersey'
-
-UNION ALL
-
 /* Campus-based staff = all schools at campus */
-SELECT COALESCE(id.ps_teachernumber, CONVERT(VARCHAR(25),df.df_employee_number)) AS [01 Local User ID]
-      ,sch.school_number AS [02 Site ID]
+SELECT df.ps_teachernumber AS [01 Local User ID]
+      ,cc.ps_school_id AS [02 Site ID]
       ,'School Leadership' AS [03 Role Name]
       ,CONCAT(gabby.utilities.GLOBAL_ACADEMIC_YEAR() , '-', (gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1))  AS [04 Academic Year]
       ,1 AS [05 Session Type ID]
-FROM gabby.dayforce.staff_roster df
-LEFT JOIN gabby.people.id_crosswalk_powerschool id
-  ON df.df_employee_number = id.df_employee_number
- AND id.is_master = 1
-CROSS APPLY STRING_SPLIT(CASE 
-                          WHEN df.primary_site = 'KIPP Lanning Sq Campus' THEN '179901,179902'
-                          WHEN df.primary_site = '18th Ave Campus' THEN '73255,73258'
-                         END, ',') ss
-JOIN gabby.powerschool.schools sch
-  ON ss.value = sch.school_number
+FROM gabby.people.staff_crosswalk_static df
+JOIN gabby.people.campus_crosswalk cc
+  ON df.primary_site = cc.campus_name
+ AND cc.is_pathways = 0
+ AND cc._fivetran_deleted = 0
 WHERE df.is_active = 1
-  AND df.primary_site IN ('18th Ave Campus', 'KIPP Lanning Sq Campus')
+  AND df.primary_on_site_department NOT IN ('Teaching and Learning', 'Data', 'Executive')
+  AND df.is_campus_staff = 1
 
 UNION ALL
 
 /* School-based staff = only respective school */
-SELECT COALESCE(id.ps_teachernumber, CONVERT(VARCHAR(25),df.df_employee_number)) AS [01 Local User ID]
-      ,sch.school_number AS [02 Site ID]
+SELECT df.ps_teachernumber AS [01 Local User ID]
+      ,df.primary_site_schoolid AS [02 Site ID]
       ,'School Leadership' AS [03 Role Name]
       ,CONCAT(gabby.utilities.GLOBAL_ACADEMIC_YEAR() , '-', (gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1))  AS [04 Academic Year]
       ,1 AS [05 Session Type ID]
-FROM gabby.dayforce.staff_roster df
-LEFT JOIN gabby.people.id_crosswalk_powerschool id
-  ON df.df_employee_number = id.df_employee_number
- AND id.is_master = 1
-JOIN gabby.powerschool.schools sch
-  ON df.primary_site_schoolid = sch.school_number
+FROM gabby.people.staff_crosswalk_static df
 WHERE df.is_active = 1
-  AND primary_site NOT IN ('Room 9 - 60 Park Pl','Room 10 - 740 Chestnut St','18th Ave Campus','KIPP Lanning Sq Campus')
-  AND legal_entity_name != 'KIPP New Jersey'
+  AND df.primary_on_site_department NOT IN ('Teaching and Learning', 'Data', 'Executive')
+  AND df.is_campus_staff = 0
