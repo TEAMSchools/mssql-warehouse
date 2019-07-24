5c3da8587d235a47a9b621677ee207e808b82d20
Charlie Bini <CBini@kippnj.org>
2019-07-24 14:37:03 -0500

    existing staff must match on schoolid, new users must not exist

--- extracts/v_extracts.powerschool_autocomm_teachers_accounts.sql
+++ extracts/v_extracts.powerschool_autocomm_teachers_accounts.sql
@@ -4,6 +4,7 @@ GO
 CREATE OR ALTER VIEW extracts.powerschool_autocomm_teachers_accounts AS
 
 WITH users_clean AS (
+  /* existing users, DF schoolid matches PS homeschoolid */
   SELECT df.preferred_first_name AS first_name
         ,df.preferred_last_name AS last_name
         ,sc.region AS legal_entity_name
@@ -33,6 +34,39 @@ WITH users_clean AS (
        END = u.[db_name]
   WHERE DATEDIFF(DAY, ISNULL(df.termination_date, CONVERT(DATE, GETDATE())), GETDATE()) <= 14 /* import terminated staff up to a week after termination date */
     AND df.primary_on_site_department != 'Data'
+
+  UNION ALL
+
+  /* new users, teachernumber does not exist */
+  SELECT df.preferred_first_name AS first_name
+        ,df.preferred_last_name AS last_name
+        ,sc.region AS legal_entity_name
+        ,df.ps_teachernumber AS teachernumber
+        ,df.primary_site_schoolid AS homeschoolid
+        ,df.birth_date
+        ,LOWER(df.samaccountname) AS loginid
+        ,LOWER(df.samaccountname) AS teacherloginid
+        ,LOWER(df.mail) AS email_addr
+        ,CASE
+          WHEN DATEDIFF(DAY, ISNULL(df.termination_date, CONVERT(DATE, GETDATE())), GETDATE()) <= 7 THEN 1
+          WHEN df.[status] IN ('ACTIVE','INACTIVE','PRESTART', 'PLOA', 'ADMIN_LEAVE') THEN 1
+          WHEN df.termination_date >= CONVERT(DATE, GETDATE()) THEN 1
+          ELSE 2
+         END AS [status]
+  FROM gabby.people.staff_crosswalk_static df
+  JOIN gabby.people.school_crosswalk sc
+    ON df.primary_site = sc.site_name
+   AND sc._fivetran_deleted = 0
+  LEFT JOIN gabby.powerschool.users u
+    ON df.ps_teachernumber = u.teachernumber COLLATE Latin1_General_BIN
+   AND CASE 
+        WHEN sc.region = 'TEAM Academy Charter Schools' THEN 'kippnewark'
+        WHEN sc.region = 'KIPP Cooper Norcross Academy' THEN 'kippcamden'
+        WHEN sc.region = 'KIPP Miami' THEN 'kippmiami'
+       END = u.[db_name]
+  WHERE DATEDIFF(DAY, ISNULL(df.termination_date, CONVERT(DATE, GETDATE())), GETDATE()) <= 14 /* import terminated staff up to a week after termination date */
+    AND df.primary_on_site_department != 'Data'
+    AND u.dcid IS NULL
  )
 
 SELECT df.teachernumber
