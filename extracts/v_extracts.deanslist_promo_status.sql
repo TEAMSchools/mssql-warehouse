49ab44afa1d0b6c1500e03a6f802f5c3acc8a007
Charlie Bini <CBini@kippnj.org>
2019-07-30 08:57:21 -0500

    new promo status

--- extracts/v_extracts.deanslist_promo_status.sql
+++ extracts/v_extracts.deanslist_promo_status.sql
@@ -5,22 +5,22 @@ CREATE OR ALTER VIEW extracts.deanslist_promo_status AS
 
 SELECT student_number
       ,academic_year
-      ,term_name AS term
+      ,reporting_term_name AS term
       ,promo_status_overall
       ,promo_status_attendance
       ,promo_status_lit
-      ,promo_status_grades      
-      ,promo_status_credits     
-      ,att_pts_pct
-      ,GPA_term
-      ,GPA_Y1
-      ,GPA_cum
-      ,GPA_term_status
-      ,GPA_Y1_status
-      ,GPA_cum_status      
-      ,projected_credits_earned_cum AS projected_credits_earned
-      ,credits_enrolled_cum AS credits_enrolled
-      ,HWQ_Y1
-      ,lvls_grown_yr AS reading_lvl_growth_y1
+      ,promo_status_grades
+      --,promo_status_credits     
+      --,att_pts_pct
+      --,GPA_term
+      --,GPA_Y1
+      --,GPA_cum
+      --,GPA_term_status
+      --,GPA_Y1_status
+      --,GPA_cum_status
+      --,grades_y1_credits_projected AS projected_credits_earned
+      --,credits_enrolled_cum AS credits_enrolled
+      --,HWQ_Y1
+      --,lvls_grown_yr AS reading_lvl_growth_y1
 FROM gabby.reporting.promotional_status
 WHERE academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
