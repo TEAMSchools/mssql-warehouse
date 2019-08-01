9d071b6da37e0148e771347ffd9f63c0a11f73b3
Charlie Bini <CBini@kippnj.org>
2019-08-01 18:28:38 -0500

    fp extract

---
+++ extracts/v_extracts.fpodms_students.sql
@@ -0,0 +1,20 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW extracts.fpodms_students AS
+
+SELECT co.student_number AS [studentIdentifier]
+      ,co.first_name AS [firstName]
+      ,co.last_name AS [lastName]
+      ,co.grade_level + 1 AS [gradeId]
+      ,CONVERT(VARCHAR,CONVERT(DATETIME2,co.entrydate), 126) AS [classStudentStartDate]
+      ,CONVERT(VARCHAR,CONVERT(DATETIME2,co.exitdate), 126) AS [classStudentEndDate]
+      
+      ,sch.name AS [schoolName]
+FROM gabby.powerschool.cohort_identifiers_static co
+JOIN gabby.powerschool.schools sch
+  ON co.schoolid = sch.school_number
+ AND co.db_name = sch.db_name
+WHERE co.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
+  AND co.school_level IN ('ES', 'MS')
+  AND co.is_enrolled_recent = 1
