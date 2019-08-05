c00710a6eed3e9b03830a0c2cca9acbe4a2ccbfa
Anthony Walters <awalters@kippnj.org>
2019-08-05 16:53:52 -0400

    Update v_tableau.attendance_comm_log.sql
    Updates for additional chron abs tracking + additional successful call features

--- tableau/v_tableau.attendance_comm_log.sql
+++ tableau/v_tableau.attendance_comm_log.sql
@@ -24,6 +24,45 @@ WITH commlog AS (
   WHERE (c.reason LIKE 'att:%' OR c.reason LIKE 'chronic%')
  )
 
+,ada AS (
+  SELECT psa.studentid
+        ,psa.db_name
+        ,psa.yearid + 1990 AS academic_year
+        ,ROUND(AVG(CAST(psa.attendancevalue AS FLOAT)), 2) AS ada
+  FROM gabby.powerschool.ps_adaadm_daily_ctod psa   
+  WHERE psa.membershipvalue = 1
+    AND psa.calendardate <= CAST(SYSDATETIME() AS DATE)
+  GROUP BY psa.studentid
+          ,psa.yearid
+          ,psa.db_name
+ )
+ 
+,gpa_y1 AS (
+  SELECT db_name
+        ,student_number
+        ,academic_year
+        ,schoolid
+        ,gpa_y1
+        ,n_failing_y1
+
+  FROM powerschool.gpa_detail
+
+  WHERE is_curterm = 1
+ )
+
+,reading AS (
+  SELECT student_number
+      ,schoolid
+      ,academic_year
+      ,read_lvl
+      ,goal_status
+      ,gleq
+
+  FROM lit.achieved_by_round_static
+
+  WHERE is_curterm = 1
+ )
+
 SELECT co.student_number
       ,co.lastfirst
       ,co.academic_year
@@ -31,6 +70,9 @@ SELECT co.student_number
       ,co.reporting_schoolid
       ,co.grade_level
       ,co.team
+      ,co.iep_status
+      ,co.gender
+      ,co.is_retained_year
 
       ,att.att_date
       ,att.att_comment
@@ -48,6 +90,17 @@ SELECT co.student_number
       ,cl.followup_staff_name
       ,cl.followup_init_notes
       ,cl.followup_close_notes
+
+      ,rt.alt_name AS term
+
+      ,ada.ada
+
+      ,CASE WHEN co.school_level = 'ES' THEN CAST(r.read_lvl AS varchar)
+            WHEN co.school_level IN ('HS','MS') THEN CAST(gpa.gpa_y1 AS varchar)
+            ELSE NULL END AS indicator_1
+      ,CASE WHEN co.school_level = 'ES' THEN CAST(r.goal_status AS varchar)
+            WHEN co.school_level IN ('HS','MS') THEN CAST(gpa.n_failing_y1 AS varchar)
+            ELSE NULL END AS indicator_2
 
       ,ROW_NUMBER() OVER(
          PARTITION BY att.studentid, att.att_date
@@ -69,5 +122,24 @@ JOIN gabby.powerschool.cohort_identifiers_static co
 LEFT JOIN commlog cl
   ON co.student_number = cl.student_school_id
  AND att.att_date = cl.commlog_date
+JOIN ada
+  ON ada.studentid = co.studentid
+ AND ada.db_name = co.db_name
+ AND ada.academic_year = co.academic_year
+LEFT JOIN gpa_y1 gpa
+  ON gpa.academic_year = co.academic_year
+ AND gpa.db_name = co.db_name
+ AND gpa.schoolid = co.schoolid
+ AND gpa.student_number = co.student_number
+LEFT JOIN reading r
+  ON r.student_number = co.student_number
+ AND r.academic_year = co.academic_year
+ AND r.schoolid = co.schoolid
+LEFT JOIN gabby.reporting.reporting_terms rt 
+ ON co.schoolid = rt.schoolid
+AND co.yearid = rt.yearid
+AND att.att_date BETWEEN rt.[start_date] AND rt.end_date
+AND rt.identifier = 'RT'
+
 WHERE att.att_date >= DATEFROMPARTS((gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1), 07, 01)
   AND att.att_mode_code = 'ATT_ModeDaily'
