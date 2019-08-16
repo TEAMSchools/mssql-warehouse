fedda1a229d66132d30711d291fe45f839498626
Charlie Bini <CBini@kippnj.org>
2019-08-16 13:38:04 -0500

    switch to static tables

--- surveygizmo/v_surveygizmo.survey_detail.sql
+++ surveygizmo/v_surveygizmo.survey_detail.sql
@@ -25,17 +25,17 @@ FROM gabby.surveygizmo.survey_clean s
 JOIN gabby.surveygizmo.survey_response_clean sr
   ON s.survey_id = sr.survey_id
  AND sr.[status] = 'Complete'
-LEFT JOIN gabby.surveygizmo.survey_campaign_clean sc
-  ON sr.survey_id = sc.survey_id
- AND sr.date_started BETWEEN sc.link_open_date AND sc.link_close_date
 JOIN gabby.surveygizmo.survey_question_clean sq
   ON s.survey_id = sq.survey_id
  AND sq.base_type = 'Question'
-LEFT JOIN gabby.surveygizmo.survey_response_data srd
+LEFT JOIN gabby.surveygizmo.survey_campaign_clean sc
+  ON sr.survey_id = sc.survey_id
+ AND sr.date_started BETWEEN sc.link_open_date AND sc.link_close_date
+LEFT JOIN gabby.surveygizmo.survey_response_data_static srd
   ON sr.survey_id = srd.survey_id
  AND sr.survey_response_id = srd.survey_response_id
  AND sq.survey_question_id = srd.question_id
-LEFT JOIN gabby.surveygizmo.survey_question_options qo
+LEFT JOIN gabby.surveygizmo.survey_question_options_static qo
   ON sq.survey_id = qo.survey_id
  AND sq.survey_question_id = qo.question_id
  AND srd.answer_id = qo.option_id
