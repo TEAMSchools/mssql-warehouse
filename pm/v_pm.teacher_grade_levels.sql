a825c97a06a685275d77c1835d50bc3e8d33cc88
Kevin Verhoff <kevin.verhoff@gmail.com>
2019-08-20 14:35:37 -0400

    Update v_pm.teacher_grade_levels.sql

--- pm/v_pm.teacher_grade_levels.sql
+++ pm/v_pm.teacher_grade_levels.sql
@@ -25,9 +25,9 @@ WITH ps_section_teacher AS (
 ,gl_students AS (
 
   SELECT sr.df_employee_number
-        ,enr.academic_year
+        ,co.academic_year
         ,co.grade_level AS student_grade_level
-        ,COUNT(DISTINCT enr.student_number) AS n_gl_students
+        ,COUNT(enr.student_number) AS n_gl_students
   FROM gabby.people.staff_crosswalk_static sr 
   JOIN ps_section_teacher st
     ON sr.ps_teachernumber = st.teachernumber COLLATE Latin1_General_BIN
@@ -37,44 +37,27 @@ WITH ps_section_teacher AS (
    AND st.db_name = enr.db_name
   JOIN gabby.powerschool.cohort_identifiers_static co
     ON enr.student_number = co.student_number
-  GROUP BY sr.df_employee_number, enr.academic_year, co.grade_level
-
-)
-
-, total_students AS (
-  SELECT sr.df_employee_number
-        ,enr.academic_year
-        ,COUNT(DISTINCT enr.student_number) AS n_total_students
-  FROM gabby.people.staff_crosswalk_static sr 
-  JOIN ps_section_teacher st
-    ON sr.ps_teachernumber = st.teachernumber COLLATE Latin1_General_BIN
-   AND sr.db_name = st.db_name
-  JOIN gabby.powerschool.course_enrollments_static enr
-    ON st.sectionid = enr.abs_sectionid
-   AND st.db_name = enr.db_name
-
-  GROUP BY sr.df_employee_number, enr.academic_year
+   AND enr.academic_year = co.academic_year
+  GROUP BY sr.df_employee_number, co.academic_year, co.grade_level
 )
 
 ,percentages AS (
 
-SELECT t.df_employee_number
-      ,t.academic_year
-      ,t.n_total_students
-      ,g.student_grade_level
-      ,g.n_gl_students
-      ,CONVERT(FLOAT,g.n_gl_students) / (CASE WHEN t.n_total_students = 0 THEN 1 ELSE CONVERT(FLOAT,t.n_total_students) END) AS percent_gl
-      ,MAX(CONVERT(FLOAT,g.n_gl_students) / (CASE WHEN t.n_total_students = 0 THEN 1 ELSE CONVERT(FLOAT,t.n_total_students) END)) AS max_percentage
-FROM gl_students g LEFT JOIN total_students t
-  ON g.df_employee_number = t.df_employee_number
- AND g.academic_year = t.academic_year
-WHERE n_total_students > 0
-GROUP BY t.df_employee_number, t.academic_year, t.n_total_students, g.student_grade_level, g.n_gl_students
+  SELECT g.df_employee_number
+        ,g.academic_year
+        ,g.student_grade_level
+        ,g.n_gl_students
+        ,SUM(g.n_gl_students) OVER( PARTITION BY g.df_employee_number, g.academic_year) AS n_total_students
+        ,CONVERT(FLOAT,n_gl_students)/CONVERT(FLOAT,SUM(g.n_gl_students) OVER( PARTITION BY g.df_employee_number, g.academic_year)) AS percent_gl
+  FROM gl_students g
 
 )
 
 SELECT df_employee_number
       ,academic_year
-      ,student_grade_level AS primary_grade_level
-FROM percentages 
-WHERE percent_gl = max_percentage
+      ,student_grade_level
+      ,n_gl_students
+      ,n_total_students
+      ,percent_gl
+      ,CASE WHEN MAX(percent_gl) OVER( PARTITION BY df_employee_number, academic_year) = percent_gl THEN 1 ELSE 0 END AS primary_gl
+FROM percentages
