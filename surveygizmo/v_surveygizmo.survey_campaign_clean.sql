9a7ccb0e2026d25bb8cc9ceca958fe3cec9fb966
Charlie Bini <CBini@kippnj.org>
2019-08-27 15:40:23 -0500

    clean rt

--- surveygizmo/v_surveygizmo.survey_campaign_clean.sql
+++ surveygizmo/v_surveygizmo.survey_campaign_clean.sql
@@ -21,6 +21,7 @@ SELECT id AS survey_campaign_id
       ,[ssl]
       ,limit_responses
       ,token_variables
+      ,LTRIM(RTRIM(RIGHT([name], CHARINDEX(' ', REVERSE([name]))))) AS reporting_term_code
 
       ,gabby.utilities.DATE_TO_SY(CONVERT(DATETIME2,CASE WHEN link_open_date = '0000-00-00 00:00:00' THEN NULL ELSE link_open_date END)) AS academic_year
 FROM gabby.surveygizmo.survey_campaign
