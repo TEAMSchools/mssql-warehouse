90d31c03a312fcaa7c6b8617dc7af861c17a61a7
Charlie Bini <CBini@kippnj.org>
2019-08-26 23:38:32 -0500

    point-in-time mgr, rename some cols, fix for preferred name match

--- surveygizmo/v_surveygizmo.survey_response_identifiers.sql
+++ surveygizmo/v_surveygizmo.survey_response_identifiers.sql
@@ -7,14 +7,14 @@ WITH response_pivot AS (
   SELECT p.survey_response_id
         ,p.survey_id
         ,p.date_started
-        ,CONVERT(VARCHAR(25), p.adp_associate_id) AS respondent_adp_associate_id
-        ,CONVERT(VARCHAR(125),LOWER(p.userprincipalname)) AS respondent_userprincipalname
+        ,CONVERT(VARCHAR(25), p.respondent_adp_associate_id) AS respondent_adp_associate_id
+        ,CONVERT(VARCHAR(125),LOWER(p.respondent_userprincipalname)) AS respondent_userprincipalname
         ,CONVERT(INT, CASE
-                       WHEN ISNUMERIC(p.df_employee_number) = 1 THEN p.df_employee_number
-                       WHEN CHARINDEX('[', p.df_employee_number) = 0 THEN NULL
-                       ELSE SUBSTRING(p.df_employee_number
-                                     ,CHARINDEX('[', p.df_employee_number) + 1
-                                     ,CHARINDEX(']', p.df_employee_number) - CHARINDEX('[', p.df_employee_number) - 1)
+                       WHEN ISNUMERIC(p.respondent_df_employee_number) = 1 THEN p.respondent_df_employee_number
+                       WHEN CHARINDEX('[', p.respondent_df_employee_number) = 0 THEN NULL
+                       ELSE SUBSTRING(p.respondent_df_employee_number
+                                     ,CHARINDEX('[', p.respondent_df_employee_number) + 1
+                                     ,CHARINDEX(']', p.respondent_df_employee_number) - CHARINDEX('[', p.respondent_df_employee_number) - 1)
                       END) AS respondent_df_employee_number
         ,CONVERT(INT, CASE
                        WHEN CHARINDEX('[', p.subject_df_employee_number) = 0 THEN NULL
@@ -22,6 +22,7 @@ WITH response_pivot AS (
                                      ,CHARINDEX('[', p.subject_df_employee_number) + 1
                                      ,CHARINDEX(']', p.subject_df_employee_number) - CHARINDEX('[', p.subject_df_employee_number) - 1)
                       END) AS subject_df_employee_number
+        ,CASE WHEN CHARINDEX('[', p.subject_df_employee_number) = 0 THEN p.subject_df_employee_number END AS subject_preferred_name
   FROM
       (
        SELECT srd.survey_response_id
@@ -38,10 +39,10 @@ WITH response_pivot AS (
       ) sub
   PIVOT(
     MAX(answer)
-    FOR field_mapping IN (df_employee_number
-                         ,subject_df_employee_number
-                         ,adp_associate_id
-                         ,userprincipalname)
+    FOR field_mapping IN (respondent_df_employee_number
+                         ,respondent_userprincipalname
+                         ,respondent_adp_associate_id
+                         ,subject_df_employee_number)
    ) p
  )
 
@@ -49,7 +50,7 @@ WITH response_pivot AS (
   SELECT rp.survey_response_id
         ,rp.survey_id
         ,rp.date_started
-        ,rp.subject_df_employee_number
+        ,COALESCE(rp.subject_df_employee_number, subj.df_employee_number) AS subject_df_employee_number
         ,COALESCE(rp.respondent_df_employee_number
                  ,upn.df_employee_number
                  ,adp.df_employee_number
@@ -66,6 +67,8 @@ WITH response_pivot AS (
     ON rp.survey_id = ab.survey_id
    AND rp.survey_response_id = ab.survey_response_id
    AND ab._fivetran_deleted = 0
+  LEFT JOIN gabby.people.staff_crosswalk_static subj
+    ON rp.subject_preferred_name = subj.preferred_name
  )
 
 ,work_assignment AS (
@@ -90,6 +93,24 @@ WITH response_pivot AS (
   WHERE ewa.primary_work_assignment = 1
 )
 
+,manager AS (
+  SELECT em.employee_reference_code
+        ,em.manager_employee_number
+        ,CONVERT(DATE, em.manager_effective_start) AS manager_effective_start
+        ,CONVERT(DATE, COALESCE(CASE WHEN em.manager_effective_end != '' THEN em.manager_effective_end END
+                               ,GETDATE())) AS manager_effective_end
+
+        ,mgr.manager_df_employee_number
+        ,mgr.manager_name
+        ,mgr.manager_mail
+        ,mgr.manager_userprincipalname
+        ,mgr.manager_samaccountname
+  FROM gabby.dayforce.employee_manager em
+  JOIN gabby.people.staff_crosswalk_static mgr
+    ON em.employee_reference_code = mgr.df_employee_number
+  WHERE em.manager_derived_method = 'Direct Report'
+ )
+
 SELECT rc.survey_response_id
       ,rc.survey_id
       ,rc.date_started
@@ -101,11 +122,6 @@ SELECT rc.survey_response_id
       ,resp.userprincipalname AS respondent_userprincipalname
       ,resp.mail AS respondent_mail
       ,resp.samaccountname AS respondent_samaccountname
-      ,resp.manager_df_employee_number AS respondent_manager_df_employee_number
-      ,resp.manager_name AS respondent_manager_name
-      ,resp.manager_mail AS respondent_manager_mail
-      ,resp.manager_userprincipalname AS respondent_manager_userprincipalname
-      ,resp.manager_samaccountname AS respondent_manager_samaccountname
 
       ,rwa.legal_entity_name AS respondent_legal_entity_name
       ,rwa.primary_site AS respondent_primary_site
@@ -114,16 +130,17 @@ SELECT rc.survey_response_id
       ,rwa.primary_site_schoolid AS respondent_primary_site_schoolid
       ,rwa.primary_site_school_level AS respondent_primary_site_school_level
 
+      ,rmgr.manager_df_employee_number AS respondent_manager_df_employee_number
+      ,rmgr.manager_name AS respondent_manager_name
+      ,rmgr.manager_mail AS respondent_manager_mail
+      ,rmgr.manager_userprincipalname AS respondent_manager_userprincipalname
+      ,rmgr.manager_samaccountname AS respondent_manager_samaccountname
+
       ,subj.preferred_name AS subject_preferred_name
       ,subj.adp_associate_id AS subject_adp_associate_id
       ,subj.userprincipalname AS subject_userprincipalname
       ,subj.mail AS subject_mail
       ,subj.samaccountname AS subject_samaccountname
-      ,subj.manager_df_employee_number AS subject_manager_df_employee_number
-      ,subj.manager_name AS subject_manager_name
-      ,subj.manager_mail AS subject_manager_mail
-      ,subj.manager_userprincipalname AS subject_manager_userprincipalname
-      ,subj.manager_samaccountname AS subject_manager_samaccountname
 
       ,swa.legal_entity_name AS subject_legal_entity_name
       ,swa.primary_site AS subject_primary_site
@@ -131,16 +148,28 @@ SELECT rc.survey_response_id
       ,swa.primary_job AS subject_primary_job
       ,swa.primary_site_schoolid AS subject_primary_site_schoolid
       ,swa.primary_site_school_level AS subject_primary_site_school_level
+
+      ,smgr.manager_df_employee_number AS subject_manager_df_employee_number
+      ,smgr.manager_name AS subject_manager_name
+      ,smgr.manager_mail AS subject_manager_mail
+      ,smgr.manager_userprincipalname AS subject_manager_userprincipalname
+      ,smgr.manager_samaccountname AS subject_manager_samaccountname
 FROM response_clean rc
 LEFT JOIN gabby.people.staff_crosswalk_static resp
   ON rc.respondent_df_employee_number = resp.df_employee_number
-LEFT JOIN gabby.people.staff_crosswalk_static subj
-  ON rc.subject_df_employee_number = subj.df_employee_number
 LEFT JOIN work_assignment rwa
-  ON resp.df_employee_number = rwa.employee_reference_code
+  ON rc.respondent_df_employee_number = rwa.employee_reference_code
  AND rc.date_started BETWEEN rwa.work_assignment_effective_start AND rwa.work_assignment_effective_end
  AND rwa.rn = 1
+LEFT JOIN manager rmgr
+  ON rc.respondent_df_employee_number = rmgr.employee_reference_code
+ AND rc.date_started BETWEEN rmgr.manager_effective_start AND rmgr.manager_effective_end
+LEFT JOIN gabby.people.staff_crosswalk_static subj
+  ON rc.subject_df_employee_number = subj.df_employee_number
 LEFT JOIN work_assignment swa
-  ON subj.df_employee_number = swa.employee_reference_code
+  ON rc.subject_df_employee_number = swa.employee_reference_code
  AND rc.date_started BETWEEN swa.work_assignment_effective_start AND swa.work_assignment_effective_end
- AND swa.rn = 1;
+ AND swa.rn = 1
+LEFT JOIN manager smgr
+  ON rc.subject_df_employee_number = smgr.employee_reference_code
+ AND rc.date_started BETWEEN smgr.manager_effective_start AND smgr.manager_effective_end
