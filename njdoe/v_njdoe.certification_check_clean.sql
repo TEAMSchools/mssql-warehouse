461a3f19cfba3107de5da98d83e2280c244bcf4c
Charlie Bini <CBini@kippnj.org>
2019-09-11 11:03:30 -0500

    rn

--- njdoe/v_njdoe.certification_check_clean.sql
+++ njdoe/v_njdoe.certification_check_clean.sql
@@ -9,4 +9,7 @@ SELECT df_employee_number
       ,JSON_VALUE(applicant, '$.phone_number') AS applicant_phone_number      
       ,application_history
       ,certificate_history
+      ,ROW_NUMBER() OVER(
+         PARTITION BY df_employee_number
+           ORDER BY _modified DESC) AS rn_employee_current
 FROM gabby.njdoe.certification_check
