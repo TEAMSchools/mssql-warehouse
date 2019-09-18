19896f8e633ac6d906eae3cc0ac8a6ec08a96249
Charlie Bini <CBini@kippnj.org>
2019-09-18 13:10:11 -0500

    use staff crosswalk

--- powerschool/v_powerschool.advisory.sql
+++ powerschool/v_powerschool.advisory.sql
@@ -1,8 +1,8 @@
 CREATE OR ALTER VIEW powerschool.advisory AS
 
 SELECT studentid
-      ,student_number                      
-      ,academic_year           
+      ,student_number
+      ,academic_year
       ,teachernumber
       ,advisor_name
       ,dateenrolled
@@ -13,29 +13,22 @@ SELECT studentid
 FROM
     (
      SELECT enr.studentid
-           ,enr.student_number                      
-           ,enr.academic_year           
+           ,enr.student_number
+           ,enr.academic_year
            ,enr.teachernumber
            ,enr.teacher_name AS advisor_name
            ,enr.dateenrolled
-           ,enr.dateleft           
+           ,enr.dateleft
 
-           ,df.mobile_number COLLATE Latin1_General_BIN AS advisor_phone
-
-           ,dir.mail COLLATE Latin1_General_BIN AS advisor_email
+           ,scw.mobile_number AS advisor_phone
+           ,scw.mail AS advisor_email
 
            ,CONVERT(INT,ROW_NUMBER() OVER(
                           PARTITION BY enr.student_number, enr.academic_year
                             ORDER BY enr.dateleft DESC, enr.dateenrolled DESC)) AS rn_year
-     FROM powerschool.course_enrollments_static enr           
-     LEFT JOIN gabby.people.id_crosswalk_powerschool psid
-       ON enr.teachernumber = psid.ps_teachernumber COLLATE Latin1_General_BIN
-     LEFT JOIN gabby.dayforce.staff_roster df
-       ON COALESCE(psid.df_employee_number, enr.teachernumber) = df.df_employee_number
-     LEFT JOIN gabby.adsi.user_attributes_static dir
-       ON df.df_employee_number = dir.employeenumber
-      AND dir.is_active = 1
-      AND ISNUMERIC(dir.employeenumber) = 1
+     FROM powerschool.course_enrollments_static enr
+     LEFT JOIN gabby.people.staff_crosswalk_static scw
+       ON enr.teachernumber = scw.ps_teachernumber COLLATE Latin1_General_BIN
      WHERE enr.course_number = 'HR'
        AND enr.sectionid > 0
     ) sub
