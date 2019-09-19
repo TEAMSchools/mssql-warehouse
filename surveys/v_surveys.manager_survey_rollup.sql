79f31480db5cbbc44106d2de36e807ee1e201322
Charlie Bini <CBini@kippnj.org>
2019-09-19 15:59:22 -0500

    fix manager survey bindings

--- surveys/v_surveys.manager_survey_rollup.sql
+++ surveys/v_surveys.manager_survey_rollup.sql
@@ -3,38 +3,38 @@ GO
 
 CREATE OR ALTER VIEW surveys.manager_survey_rollup AS
 
-SELECT survey_type
-      ,academic_year
-      ,reporting_term
-      ,term_name            
-      ,subject_name
-      ,subject_location
-      ,region AS subject_legal_entity_name
-      ,reporting_schoolid AS subject_primary_site_schoolid
-      ,school_level AS subject_primary_site_school_level
-      ,subject_manager_id
-      ,subject_username
+SELECT survey_title AS survey_type
+      ,campaign_academic_year AS academic_year
+      ,campaign_reporting_term AS reporting_term
+      ,campaign_name AS term_name
+      ,subject_preferred_name AS subject_name
+      ,subject_primary_site AS subject_location
+      ,subject_legal_entity_name AS subject_legal_entity_name
+      ,subject_primary_site_schoolid AS subject_primary_site_schoolid
+      ,subject_primary_site_school_level AS subject_primary_site_school_level
+      ,subject_manager_df_employee_number AS subject_manager_id
+      ,subject_samaccountname AS subject_username
       ,subject_manager_name
-      ,subject_manager_username
-      ,question_code
-      ,question_text     
+      ,subject_manager_samaccountname AS subject_manager_username
+      ,question_shortname AS question_code
+      ,question_title AS question_text
       
-      ,AVG(response_value) AS avg_response_value
-      ,MAX(avg_response_value_location) AS avg_response_value_location
+      ,AVG(answer_value) AS avg_response_value
+      ,NULL AS avg_response_value_location
 FROM gabby.surveys.manager_survey_detail
-WHERE open_ended = 'N'
-GROUP BY survey_type
-        ,academic_year
-        ,reporting_term
-        ,term_name            
-        ,subject_name
-        ,subject_location
-        ,region
-        ,reporting_schoolid
-        ,school_level
-        ,subject_manager_id
-        ,subject_username
+WHERE is_open_ended = 'N'
+GROUP BY survey_title
+        ,campaign_academic_year
+        ,campaign_reporting_term
+        ,campaign_name
+        ,subject_preferred_name
+        ,subject_primary_site
+        ,subject_legal_entity_name
+        ,subject_primary_site_schoolid
+        ,subject_primary_site_school_level
+        ,subject_manager_df_employee_number
+        ,subject_samaccountname
         ,subject_manager_name
-        ,subject_manager_username
-        ,question_code
-        ,question_text
+        ,subject_manager_samaccountname
+        ,question_shortname
+        ,question_title
