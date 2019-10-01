187f2c6583a98b7dd0cad914ebd2be1e374aa5be
Charlie Bini <CBini@kippnj.org>
2019-10-01 15:31:31 -0500

    fixins

--- powerschool/v_powerschool.gradebook_setup.sql
+++ powerschool/v_powerschool.gradebook_setup.sql
@@ -1,94 +1,100 @@
 CREATE OR ALTER VIEW powerschool.gradebook_setup AS
 
-WITH default_gfs AS (
-  SELECT gfs.gradeformulasetid	        
-        ,gfs.yearid  
-        ,gfs.name
+SELECT sub.sectionsdcid
+      ,sub.sectionsdcid AS psm_sectionid
+      ,sub.term_abbreviation
+      ,sub.term_start_date
+      ,sub.term_end_date
+      ,sub.storecode
+      ,sub.gradeformulasetid
+      ,sub.grade_formula_set_name
+      ,sub.gradecalculationtypeid
+      ,sub.grade_calculation_type
+      ,sub.gradecalcformulaweightid
+      ,sub.grade_calc_formula_weight_type
+      ,sub.[weight]
 
-        ,gct.abbreviation
-        ,gct.storecode
-        ,gct.gradecalculationtypeid
-        ,gct.type
-        
-        ,sch.school_number
-  FROM powerschool.gradeformulaset gfs 
-  JOIN powerschool.gradecalculationtype gct 
-    ON gfs.gradeformulasetid = gct.gradeformulasetid
-  JOIN powerschool.gradecalcschoolassoc gcsa 
-    ON gct.gradecalculationtypeid = gcsa.gradecalculationtypeid
-  JOIN powerschool.schools sch 
-    ON gcsa.schoolsdcid = sch.dcid
-  WHERE gfs.sectionsdcid IS NULL
-    AND ((gfs.name IN ('KNJ Middle Schools', 'Middle school') AND sch.high_grade = 8)
-         OR (gfs.name IN ('KNJ High Schools') AND sch.high_grade = 12))
- )
-    
-SELECT CONVERT(INT,sectionsdcid) AS sectionsdcid
-      ,CONVERT(INT,sectionsdcid) AS psm_sectionid                
-      ,CONVERT(INT,ISNULL(gradeformulasetid, 0)) AS finalgradesetupid
-      ,CONVERT(VARCHAR(25),gct_type) AS finalgradesetuptype        
-      ,CONVERT(INT,gradecalculationtypeid) AS fg_reportingtermid
-      ,CONVERT(VARCHAR(5),storecode) AS reportingterm_name
-      ,date_1 AS startdate
-      ,date_2 AS enddate
-      ,CONVERT(INT,ISNULL(gradecalcformulaweightid, gradecalculationtypeid)) AS gradingformulaid
-      ,CONVERT(VARCHAR(25),ISNULL(gcfw_type, gct_type)) AS gradingformulaweightingtype
-      ,weight AS weighting
-                
-      ,CONVERT(INT,COALESCE(districtteachercategoryid, teachercategoryid, gradecalculationtypeid)) AS assignmentcategoryid
-      ,CONVERT(VARCHAR(125),COALESCE(dtc_name, tc_name, gct_type)) AS category_name
-      ,CONVERT(VARCHAR(125),COALESCE(dtc_name, tc_name, gct_type)) AS category_abbreviation
-      ,CONVERT(INT,COALESCE(dtc_defaultscoretype, tc_defaultscoretype)) AS defaultscoretype
-      ,CONVERT(INT,COALESCE(dtc_isinfinalgrades, tc_isinfinalgrades, 1)) AS includeinfinalgrades
+      ,sub.gradeformulasetid AS finalgradesetupid
+      ,sub.grade_calculation_type AS finalgradesetuptype
+      ,sub.gradecalculationtypeid AS fg_reportingtermid
+      
+      ,COALESCE(sub.gradecalculationtypeid, sub.gradecalcformulaweightid, -1) AS gradingformulaid
+      ,COALESCE(sub.grade_calculation_type, sub.grade_calc_formula_weight_type) AS gradingformulaweightingtype
+      ,COALESCE(sub.teachercategoryid, sub.districtteachercategoryid, sub.gradecalculationtypeid, -1) AS assignmentcategoryid
+      ,COALESCE(sub.tc_name, sub.dtc_name, sub.grade_calculation_type) AS category_name
+      ,COALESCE(sub.tc_name, sub.dtc_name, sub.grade_calculation_type) AS category_abbreviation
+      ,COALESCE(sub.tc_defaultscoretype, sub.dtc_defaultscoretype) AS defaultscoretype
+      ,COALESCE(sub.tc_isinfinalgrades, sub.dtc_isinfinalgrades, 0) AS includeinfinalgrades
 FROM
     (
-     SELECT sec.dcid AS sectionsdcid        
-           ,sec.schoolid
+     SELECT sub.sectionsdcid
+           ,sub.gradeformulasetid
 
-           ,tb.storecode
-           ,tb.date_1
-           ,tb.date_2
+           ,CONVERT(VARCHAR(125), gfs.[name]) AS grade_formula_set_name
 
-           ,COALESCE(gfs.gradeformulasetid, d.gradeformulasetid) AS gradeformulasetid
-           ,COALESCE(gct.gradecalculationtypeid, d.gradecalculationtypeid) AS gradecalculationtypeid
-           ,COALESCE(gct.type, d.type) AS gct_type       
+           ,CONVERT(VARCHAR(5), t.abbreviation) AS term_abbreviation
 
-           ,gcfw.gradecalcformulaweightid
-           ,gcfw.teachercategoryid
-           ,gcfw.districtteachercategoryid
-           ,gcfw.weight
-           ,gcfw.type AS gcfw_type        
-        
-           ,tc.teachermodified
-           ,tc.name AS tc_name
-           ,tc.defaultscoretype AS tc_defaultscoretype
-           ,tc.isinfinalgrades AS tc_isinfinalgrades
+           ,CONVERT(VARCHAR(5), tb.storecode) AS storecode
+           ,tb.date_1 AS term_start_date
+           ,tb.date_2 AS term_end_date
 
-           ,dtc.name AS dtc_name
-           ,dtc.defaultscoretype AS dtc_defaultscoretype
-           ,dtc.isinfinalgrades AS dtc_isinfinalgrades                
-     FROM powerschool.sections sec 
-     JOIN powerschool.terms rt 
-       ON sec.termid = rt.id
-      AND sec.schoolid = rt.schoolid
-     JOIN powerschool.termbins tb 
-       ON rt.schoolid = tb.schoolid
-      AND rt.id = tb.termid
-     JOIN default_gfs d
-       ON sec.schoolid = d.school_number
-      AND sec.yearid = d.yearid
-      AND rt.abbreviation = d.abbreviation
-      AND tb.storecode = d.storecode      
+           ,CONVERT(INT, gct.gradecalculationtypeid) AS gradecalculationtypeid
+           ,CONVERT(VARCHAR(25), gct.[type]) AS grade_calculation_type
+
+           ,CONVERT(INT, gcfw.gradecalcformulaweightid) AS gradecalcformulaweightid
+           ,CONVERT(VARCHAR(25), gcfw.[type]) AS grade_calc_formula_weight_type
+           ,CONVERT(INT, gcfw.teachercategoryid) AS teachercategoryid
+           ,CONVERT(INT, gcfw.districtteachercategoryid) AS districtteachercategoryid
+           ,gcfw.[weight]
+
+           ,CONVERT(VARCHAR(125), tc.[name]) AS tc_name
+           ,CONVERT(INT, tc.defaultscoretype) AS tc_defaultscoretype
+           ,CONVERT(INT, tc.isinfinalgrades) AS tc_isinfinalgrades
+
+           ,CONVERT(VARCHAR(125), dtc.[name]) AS dtc_name
+           ,CONVERT(INT, dtc.defaultscoretype) AS dtc_defaultscoretype
+           ,CONVERT(INT, dtc.isinfinalgrades) AS dtc_isinfinalgrades
+     FROM 
+         (
+          SELECT CONVERT(INT, sec.dcid) AS sectionsdcid
+                ,CONVERT(INT, sec.schoolid) AS schoolid
+                ,CONVERT(INT, sec.termid) AS termid
+
+                ,gsec.gradeformulasetid AS section_gradeformulasetid
+
+                ,gsfa.gradeformulasetid AS school_gradeformulasetid
+
+                ,CONVERT(INT, COALESCE(gsec.gradeformulasetid, gsfa.gradeformulasetid, 0)) AS gradeformulasetid
+          FROM powerschool.sections sec
+          INNER JOIN powerschool.schools sch
+             ON sec.schoolid = sch.school_number
+          LEFT JOIN powerschool.gradesectionconfig gsec
+            ON sec.dcid = gsec.sectionsdcid
+           AND gsec.[type] = 'Admin'
+          LEFT JOIN powerschool.gradeschoolconfig gsch
+            ON sch.dcid = gsch.schoolsdcid
+           AND sec.yearid = gsch.yearid
+          LEFT JOIN powerschool.gradeschoolformulaassoc gsfa
+            ON gsch.gradeschoolconfigid = gsfa.gradeschoolconfigid
+           AND gsfa.isdefaultformulaset = 1
+          WHERE sec.gradebooktype = 2 /* PTP */
+         ) sub
      LEFT JOIN powerschool.gradeformulaset gfs 
-       ON sec.dcid = gfs.sectionsdcid         
+       ON sub.gradeformulasetid = gfs.gradeformulasetid
+     LEFT JOIN powerschool.terms t
+       ON sub.termid = t.id
+      AND sub.schoolid = t.schoolid
+     JOIN powerschool.termbins tb
+       ON t.schoolid = tb.schoolid
+      AND t.id = tb.termid
      LEFT JOIN powerschool.gradecalculationtype gct 
-       ON gfs.gradeformulasetid = gct.gradeformulasetid    
-      AND tb.storecode = gct.storecode 
+       ON gfs.gradeformulasetid = gct.gradeformulasetid
+      AND t.abbreviation = gct.abbreviation
+      AND tb.storecode = gct.storecode
      LEFT JOIN powerschool.gradecalcformulaweight gcfw 
-       ON COALESCE(gct.gradecalculationtypeid, d.gradecalculationtypeid) = gcfw.gradecalculationtypeid
+       ON gct.gradecalculationtypeid = gcfw.gradecalculationtypeid
      LEFT JOIN powerschool.teachercategory tc 
        ON gcfw.teachercategoryid = tc.teachercategoryid 
      LEFT JOIN powerschool.districtteachercategory dtc 
        ON gcfw.districtteachercategoryid = dtc.districtteachercategoryid
-     WHERE sec.gradebooktype = 2
     ) sub
