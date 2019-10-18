b4b86e5569d3a361c8d2df54c12476dbf230c77b
Charlie Bini <CBini@kippnj.org>
2019-10-18 09:31:52 -0500

    fix gradescale translation for empty strings, add gpa to clever extract

--- utilities/fn/fn_utilities.PS_UNWEIGHTED_GRADESCALE_NAME.sql
+++ utilities/fn/fn_utilities.PS_UNWEIGHTED_GRADESCALE_NAME.sql
@@ -21,8 +21,8 @@ BEGIN
      WHEN @academic_year < 2016 AND @gradescale_name = 'NCA Honors' THEN 'NCA 2011' /* unweighted pre-2016 */
      WHEN @academic_year >= 2016 AND @gradescale_name = 'NCA Honors'  THEN 'KIPP NJ 2016 (5-12)' /* unweighted 2016-2018 */
      WHEN @academic_year >= 0 AND @gradescale_name = 'KIPP NJ 2019 (5-12) Weighted' THEN 'KIPP NJ 2019 (5-12) Unweighted' /* unweighted 2019+ */
-     WHEN @academic_year < 2016 AND @gradescale_name IS NULL THEN 'NCA 2011' /* MISSING GRADESCALE - default pre-2016 */
-     WHEN @academic_year >= 2016 AND @gradescale_name IS NULL THEN 'KIPP NJ 2016 (5-12)' /* MISSING GRADESCALE - default 2016+ */
+     WHEN @academic_year < 2016 AND ISNULL(@gradescale_name, '') = '' THEN 'NCA 2011' /* MISSING GRADESCALE - default pre-2016 */
+     WHEN @academic_year >= 2016 AND ISNULL(@gradescale_name, '') = '' THEN 'KIPP NJ 2016 (5-12)' /* MISSING GRADESCALE - default 2016+ */
      WHEN @academic_year < 2016 AND @gradescale_name = 'NULL' THEN 'NCA 2011' /* MISSING GRADESCALE - default pre-2016 */
      WHEN @academic_year >= 2016 AND @gradescale_name = 'NULL' THEN 'KIPP NJ 2016 (5-12)' /* MISSING GRADESCALE - default 2016+ */
      ELSE @gradescale_name
