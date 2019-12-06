3bee342827e0efa9c62017d7525754e9894bb86e
Anthony Walters <awalters@kippnj.org>
2019-12-06 17:28:45 -0500

    Create v_tableau.consequence_dashboard.sql

---
+++ tableau/v_tableau.consequence_dashboard.sql
@@ -0,0 +1,107 @@
+WITH custom_fields AS (
+  SELECT p.incident_id
+        ,p.[Restraint Used]
+        ,p.[Perceived Motivation]
+        ,p.[Parent Contacted?]
+        ,p.[Others Involved]
+        ,p.[NJ State Reporting]
+        ,p.[Behavior Category]
+        ,p.[SSDS Incident ID]
+  FROM
+      (
+       SELECT incident_id
+             ,field_name
+             ,value
+       FROM gabby.deanslist.incidents_custom_fields
+       WHERE field_name IN ('Behavior Category'
+                           ,'NJ State Reporting'
+                           ,'Others Involved'
+                           ,'Parent Contacted?'
+                           ,'Perceived Motivation'
+                           ,'Restraint Used'
+                           ,'SSDS Incident ID')
+      ) sub
+  PIVOT(
+    MAX(value)
+    FOR field_name IN ([Behavior Category]
+                      ,[NJ State Reporting]
+                      ,[Others Involved]
+                      ,[Parent Contacted?]
+                      ,[Perceived Motivation]
+                      ,[Restraint Used]
+                      ,[SSDS Incident ID])
+   ) p
+ )
+
+SELECT co.student_number
+      ,co.state_studentnumber
+      ,co.lastfirst
+      ,co.academic_year
+      ,co.reporting_schoolid
+      ,co.schoolid
+      ,co.school_name
+      ,co.grade_level
+      ,co.team
+      ,co.advisor_name
+      ,co.iep_status
+      ,co.c_504_status
+      ,co.lep_status
+      ,co.lunchstatus
+      ,co.is_retained_year
+      ,co.is_retained_ever
+      ,co.gender
+      ,co.ethnicity
+      ,s.fedethnicity
+      ,co.region
+
+      ,dli.student_id AS dl_student_id
+      ,dli.incident_id
+      ,dli.reporting_incident_id
+      ,dli.[status]
+      ,dli.[location]
+      ,dli.reported_details
+      ,dli.admin_summary
+      ,dli.context
+      ,CONCAT(dli.create_last,', ',dli.create_first) AS created_staff
+      ,CONCAT(dli.update_last,', ',dli.update_first) AS last_update_staff
+      ,dli.update_ts AS dl_timestamp
+      ,dli.infraction AS incident_type
+      ,dli.is_referral
+      ,dli.category AS referral_category
+
+      ,CONVERT(VARCHAR(5),d.alt_name) AS term
+
+      ,dlp.penaltyname
+      ,dlp.startdate
+      ,dlp.enddate
+      ,dlp.numdays
+      ,dlp.issuspension
+
+      ,cf.[Behavior Category]
+      ,cf.[NJ State Reporting]
+      ,cf.[Others Involved]
+      ,cf.[Parent Contacted?]
+      ,cf.[Perceived Motivation]
+      ,cf.[Restraint Used]
+      ,cf.[SSDS Incident ID]
+
+
+FROM powerschool.cohort_identifiers_static co
+LEFT JOIN powerschool.students s
+  ON co.student_number = s.student_number
+LEFT JOIN deanslist.incidents_clean_static dli
+  ON co.student_number = dli.student_school_id
+ AND co.academic_year = dli.create_academic_year
+LEFT JOIN gabby.reporting.reporting_terms d
+  ON co.schoolid = d.schoolid
+ AND CONVERT(DATE,dli.create_ts) BETWEEN d.start_date AND d.end_date
+ AND d.identifier = 'RT'
+ AND d._fivetran_deleted = 0
+LEFT JOIN deanslist.incidents_penalties_static dlp
+  ON dli.incident_id = dlp.incident_id
+LEFT JOIN custom_fields cf
+  ON dli.incident_id = cf.incident_id
+
+WHERE co.academic_year IN (gabby.utilities.GLOBAL_ACADEMIC_YEAR(), gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1)
+  AND co.rn_year = 1
+  AND co.grade_level <> 99
