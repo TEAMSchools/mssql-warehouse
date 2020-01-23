4f07bc478e3d2fc5009acb80c4f8b2ca3ac6792e
cbini <cbini87@gmail.com>
2020-01-23 09:28:01 -0600

    refactor view for differing parcc tables

--- parcc/v_parcc.summative_record_file_clean.sql
+++ parcc/v_parcc.summative_record_file_clean.sql
@@ -1,12 +1,15 @@
+USE kippnewark;
+GO
+
 CREATE OR ALTER VIEW parcc.summative_record_file_clean AS
 
 SELECT state_abbreviation
-      ,COALESCE(CONVERT(VARCHAR(25),testing_district_code), eoy_testing_district_identifier) AS testing_district_code
+      ,COALESCE(CONVERT(VARCHAR(25), testing_district_code), eoy_testing_district_identifier) AS testing_district_code
       ,COALESCE(testing_school_code, eoy_testing_school_institution_identifier) AS testing_school_code
       ,COALESCE(responsible_accountable_district_code, responsible_district_code, responsible_district_identifier) AS responsible_district_code
       ,COALESCE(responsible_accountable_school_code, responsible_school_code, responsible_school_institution_identifier) AS responsible_school_code
       ,state_student_identifier
-      ,CONVERT(INT,local_student_identifier) AS local_student_identifier
+      ,CONVERT(INT, local_student_identifier) AS local_student_identifier
       ,COALESCE(parccstudent_identifier, parcc_student_identifier) AS parccstudent_identifier
       ,COALESCE(last_or_surname, last_name) AS last_or_surname
       ,first_name
@@ -39,8 +42,8 @@ SELECT state_abbreviation
       ,state_field_9
       ,state_field_10
       ,class_name
-      ,CONVERT(VARCHAR(25),test_administrator) AS test_administrator
-      ,CONVERT(VARCHAR(25),staff_member_identifier) AS staff_member_identifier
+      ,CONVERT(VARCHAR(25), test_administrator) AS test_administrator
+      ,CONVERT(VARCHAR(25), staff_member_identifier) AS staff_member_identifier
       ,test_code
       ,retest
       ,elaccommodation
@@ -56,15 +59,15 @@ SELECT state_abbreviation
       ,COALESCE(student_reads_assessment_aloudto_self, student_reads_assessment_aloud_to_themselves) AS student_reads_assessment_aloud_to_self
       ,human_signer_for_test_directions
       ,calculation_device_and_mathematics_tools
-      ,COALESCE(CONVERT(VARCHAR,elalconstructed_response), elal_constructed_response) AS elalconstructed_response
-      ,COALESCE(CONVERT(VARCHAR,elalselected_response_or_technology_enhanced_items), elal_selected_response_or_technology_enhanced_items) AS elalselected_response_or_technology_enhanced_items
+      ,COALESCE(CONVERT(VARCHAR, elalconstructed_response), elal_constructed_response) AS elalconstructed_response
+      ,COALESCE(CONVERT(VARCHAR, elalselected_response_or_technology_enhanced_items), elal_selected_response_or_technology_enhanced_items) AS elalselected_response_or_technology_enhanced_items
       ,mathematics_response
       ,monitor_test_response
       ,word_prediction
       ,COALESCE(administration_directions_clarifiedin_students_native_language, administration_directions_clarified_in_students_native_language) AS administration_directions_clarifiedin_students_native_language
       ,administration_directions_read_aloudin_students_native_language
-      ,mathematics_response_el
-      ,wordto_word_dictionary_english_native_language
+      ,NULL AS mathematics_response_el
+      ,NULL AS wordto_word_dictionary_english_native_language
       ,emergency_accommodation
       ,extended_time
       ,COALESCE(student_test_uuid, summative_score_record_uuid) AS student_test_uuid
@@ -118,9 +121,9 @@ SELECT state_abbreviation
       ,unit_4_online_test_end_date_time
       ,assessment_year
       ,assessment_grade
-      ,CONVERT(VARCHAR(125),subject) AS subject
+      ,CONVERT(VARCHAR(125), [subject]) AS [subject]
       ,federal_race_ethnicity
-      ,period
+      ,[period]
       ,testing_organizational_type
       ,COALESCE(testing_district_name, eoy_testing_district_name) AS testing_district_name
       ,COALESCE(testing_school_name, eoy_testing_school_institution_name) AS testing_school_name
@@ -128,9 +131,9 @@ SELECT state_abbreviation
       ,responsible_organizational_type
       ,responsible_district_name
       ,COALESCE(responsible_school_name, responsible_school_institution_name) AS responsible_school_name
-      ,CONVERT(INT,COALESCE(test_scale_score, summative_scale_score)) AS test_scale_score
+      ,CONVERT(INT, COALESCE(test_scale_score, summative_scale_score)) AS test_scale_score
       ,COALESCE(test_csemprobable_range, summative_csem) AS test_csemprobable_range
-      ,CONVERT(INT,COALESCE(test_performance_level, summative_performance_level)) AS test_performance_level
+      ,CONVERT(INT, COALESCE(test_performance_level, summative_performance_level)) AS test_performance_level
       ,COALESCE(test_reading_scale_score, summative_reading_scale_score) AS test_reading_scale_score
       ,COALESCE(test_reading_csem, summative_reading_csem) AS test_reading_csem
       ,COALESCE(test_writing_scale_score, summative_writing_scale_score) AS test_writing_scale_score
@@ -141,7 +144,7 @@ SELECT state_abbreviation
       ,subclaim_4_category
       ,subclaim_5_category
       ,test_score_complete
-      ,word_prediction_for_elal
+      ,NULL AS word_prediction_for_elal
       ,record_type
       ,assessment_accommodation_english_learner
       ,assessment_accommodation_504
@@ -168,7 +171,188 @@ SELECT state_abbreviation
       ,pba_unit_2_total_number_of_items
       ,pba_unit_3_number_of_attempted_items
       ,pba_unit_3_total_number_of_items
-      ,CONVERT(INT,LEFT(assessment_year, 4)) AS academic_year
+      ,CONVERT(INT, LEFT(assessment_year, 4)) AS academic_year
 FROM parcc.summative_record_file
 WHERE (assessment_year = '2014-2015' AND record_type = 1 AND reported_summative_score_flag = 'Y')
-   OR (assessment_year != '2014-2015' AND test_status = 'Attempt' AND summative_flag = 'Y')
+   OR (assessment_year != '2014-2015' AND test_status = 'Attempt' AND summative_flag = 'Y');
+
+GO
+
+USE kippcamden;
+GO
+
+CREATE OR ALTER VIEW parcc.summative_record_file_clean AS
+
+SELECT state_abbreviation
+      ,CONVERT(VARCHAR(25),testing_district_code) AS testing_district_code
+      ,testing_school_code
+      ,COALESCE(responsible_accountable_district_code, responsible_district_code) AS responsible_district_code
+      ,COALESCE(responsible_accountable_school_code, responsible_school_code) AS responsible_school_code
+      ,state_student_identifier
+      ,CONVERT(INT,local_student_identifier) AS local_student_identifier
+      ,parccstudent_identifier
+      ,last_or_surname AS last_or_surname
+      ,first_name
+      ,middle_name
+      ,birthdate
+      ,sex
+      ,grade_level_when_assessed
+      ,hispanic_or_latino_ethnicity
+      ,american_indian_or_alaska_native
+      ,asian
+      ,black_or_african_american
+      ,native_hawaiian_or_other_pacific_islander
+      ,white
+      ,two_or_more_races
+      ,english_learner_el
+      ,title_iiilimited_english_proficient_participation_status
+      ,giftedand_talented
+      ,migrant_status
+      ,economic_disadvantage_status
+      ,student_with_disabilities
+      ,primary_disability_type
+      ,state_field_1
+      ,state_field_2
+      ,state_field_3
+      ,state_field_4
+      ,state_field_5
+      ,state_field_6
+      ,state_field_7
+      ,state_field_8
+      ,state_field_9
+      ,NULL AS state_field_10
+      ,class_name
+      ,CONVERT(VARCHAR(25), test_administrator) AS test_administrator
+      ,CONVERT(VARCHAR(25), staff_member_identifier) AS staff_member_identifier
+      ,test_code
+      ,retest
+      ,elaccommodation
+      ,frequent_breaks
+      ,separate_alternate_location
+      ,small_testing_group
+      ,specialized_equipment_or_furniture
+      ,specified_area_or_setting
+      ,time_of_day
+      ,answer_masking
+      ,NULL AS assistive_technology_screen_reader
+      ,NULL AS closed_captioning_for_elal
+      ,student_reads_assessment_aloudto_self
+      ,human_signer_for_test_directions
+      ,calculation_device_and_mathematics_tools
+      ,elalconstructed_response
+      ,elalselected_response_or_technology_enhanced_items
+      ,NULL AS mathematics_response
+      ,NULL AS monitor_test_response
+      ,word_prediction
+      ,administration_directions_clarifiedin_students_native_language
+      ,administration_directions_read_aloudin_students_native_language
+      ,mathematics_response_el
+      ,wordto_word_dictionary_english_native_language
+      ,emergency_accommodation
+      ,extended_time
+      ,student_test_uuid
+      ,NULL AS paper_form_id
+      ,online_form_id
+      ,test_status
+      ,total_test_items
+      ,test_attemptedness_flag
+      ,total_test_items_attempted
+      ,NULL AS paper_attempt_create_date
+      ,NULL AS paper_section_1_total_test_items
+      ,NULL AS paper_section_1_numberof_attempted_items
+      ,NULL AS paper_section_2_total_test_items
+      ,NULL AS paper_section_2_numberof_attempted_items
+      ,NULL AS paper_section_3_total_test_items
+      ,NULL AS paper_section_3_numberof_attempted_items
+      ,NULL AS paper_section_4_total_test_items
+      ,NULL AS paper_section_4_numberof_attempted_items
+      ,student_unit_1_test_uuid
+      ,unit_1_form_id
+      ,unit_1_total_test_items
+      ,unit_1_numberof_attempted_items
+      ,student_unit_2_test_uuid
+      ,unit_2_form_id
+      ,unit_2_total_test_items
+      ,unit_2_number_of_attempted_items
+      ,student_unit_3_test_uuid
+      ,unit_3_form_id
+      ,unit_3_total_test_items
+      ,unit_3_number_of_attempted_items
+      ,student_unit_4_test_uuid
+      ,unit_4_form_id
+      ,unit_4_total_test_items
+      ,unit_4_numberof_attempted_items
+      ,not_tested_code
+      ,not_tested_reason
+      ,void_score_code
+      ,void_score_reason
+      ,ship_report_district_code
+      ,ship_report_school_code
+      ,summative_flag
+      ,multiple_test_registration
+      ,attempt_create_date
+      ,unit_1_online_test_start_date_time
+      ,unit_1_online_test_end_date_time
+      ,unit_2_online_test_start_date_time
+      ,unit_2_online_test_end_date_time
+      ,unit_3_online_test_start_date_time
+      ,unit_3_online_test_end_date_time
+      ,unit_4_online_test_start_date_time
+      ,unit_4_online_test_end_date_time
+      ,assessment_year
+      ,assessment_grade
+      ,CONVERT(VARCHAR(125), [subject]) AS [subject]
+      ,federal_race_ethnicity
+      ,[period]
+      ,testing_organizational_type
+      ,testing_district_name
+      ,testing_school_name
+      ,responsible_organization_code_type
+      ,responsible_organizational_type
+      ,responsible_district_name
+      ,responsible_school_name
+      ,CONVERT(INT, test_scale_score) AS test_scale_score
+      ,test_csemprobable_range
+      ,CONVERT(INT, test_performance_level) AS test_performance_level
+      ,test_reading_scale_score
+      ,test_reading_csem
+      ,test_writing_scale_score
+      ,test_writing_csem
+      ,subclaim_1_category
+      ,subclaim_2_category
+      ,subclaim_3_category
+      ,subclaim_4_category
+      ,subclaim_5_category
+      ,test_score_complete
+      ,word_prediction_for_elal
+      ,NULL AS record_type
+      ,NULL AS assessment_accommodation_english_learner
+      ,NULL AS assessment_accommodation_504
+      ,NULL AS assessment_accommodation_individualized_educational_plan
+      ,text_to_speech
+      ,NULL AS text_to_speech_for_mathematics
+      ,NULL AS text_to_speech_for_elal
+      ,human_reader_or_human_signer
+      ,NULL AS human_reader_or_human_signer_for_mathematics
+      ,NULL AS human_reader_or_human_signer_for_elal
+      ,NULL AS pba_form_id
+      ,NULL AS pba_not_tested_reason
+      ,NULL AS pba_student_test_uuid
+      ,NULL AS pba_test_attemptedness_flag
+      ,NULL AS pba_testing_district_identifier
+      ,NULL AS pba_testing_district_name
+      ,NULL AS pba_testing_school_institution_identifier
+      ,NULL AS pba_testing_school_institution_name
+      ,NULL AS pba_total_test_items
+      ,NULL AS pba_total_test_items_attempted
+      ,NULL AS pba_unit_1_number_of_attempted_items
+      ,NULL AS pba_unit_1_total_number_of_items
+      ,NULL AS pba_unit_2_number_of_attempted_items
+      ,NULL AS pba_unit_2_total_number_of_items
+      ,NULL AS pba_unit_3_number_of_attempted_items
+      ,NULL AS pba_unit_3_total_number_of_items
+      ,CONVERT(INT, LEFT(assessment_year, 4)) AS academic_year
+FROM parcc.summative_record_file
+WHERE test_status = 'Attempt' AND summative_flag = 'Y';
+
+GO
