2775dff08b046a2f787dde6e1da67ec0b3f6dfb0
cbini <cbini87@gmail.com>
2020-03-03 13:21:00 -0600

    more specific rules for column type mismatch

--- utilities/adhoc/q_generate_gabby_unions.sql
+++ utilities/adhoc/q_generate_gabby_unions.sql
@@ -1,55 +1,79 @@
 WITH all_tables_columns_pivot AS (
-  SELECT p.schema_name
-        ,p.table_name
-        ,p.column_name
-        ,p.kippmiami AS kippmiami_column_type
-        ,p.kippcamden AS kippcamden_column_type
-        ,p.kippnewark AS kippnewark_column_type
-        ,CASE
-          WHEN CONCAT(p.kippcamden,p.kippnewark) != '' AND CHARINDEX(p.kippmiami, CONCAT(p.kippcamden,p.kippnewark)) = 0
-            OR CONCAT(p.kippmiami,p.kippnewark) != '' AND CHARINDEX(p.kippcamden, CONCAT(p.kippmiami,p.kippnewark)) = 0
-            OR CONCAT(p.kippmiami,p.kippcamden) != '' AND CHARINDEX(p.kippnewark, CONCAT(p.kippmiami,p.kippcamden)) = 0
-               THEN 1
-          ELSE 0
-         END AS column_type_mismatch        
+  SELECT sub.schema_name
+        ,sub.table_name
+        ,sub.column_name
+        ,sub.kippmiami_column_type
+        ,sub.kippcamden_column_type
+        ,sub.kippnewark_column_type
+        ,sub.column_type_mismatch
         ,CASE 
-          WHEN p.kippnewark IS NULL THEN 'NULL AS [' + p.column_name + ']'
-          ELSE '[' + p.column_name + ']' 
+          WHEN sub.kippnewark_column_type IS NULL THEN 'NULL AS [' + sub.column_name + ']'
+          WHEN sub.column_type_mismatch = 1 AND sub.column_type_concat LIKE '%varchar%' 
+               THEN 'CONVERT(NVARCHAR, [' + sub.column_name + ']) AS [' + sub.column_name + ']' 
+          WHEN sub.column_type_mismatch = 1 AND sub.column_type_concat LIKE '%float%' 
+               THEN 'CONVERT(FLOAT, [' + sub.column_name + ']) AS [' + sub.column_name + ']' 
+          WHEN sub.column_type_mismatch = 1 THEN 'CONVERT(INT, [' + sub.column_name + ']) AS [' + sub.column_name + ']' 
+          ELSE '[' + sub.column_name + ']' 
          END AS kippnewark
         ,CASE 
-          WHEN p.kippmiami IS NULL THEN 'NULL AS [' + p.column_name + ']'
-          WHEN p.kippmiami != p.kippnewark THEN 'CONVERT(' + p.kippnewark + ',[' + p.column_name + ']) AS [' + p.column_name + ']' 
-          ELSE '[' + p.column_name + ']' 
+          WHEN sub.kippmiami_column_type IS NULL THEN 'NULL AS [' + sub.column_name + ']'
+          WHEN sub.column_type_mismatch = 1 AND sub.column_type_concat LIKE '%varchar%' 
+               THEN 'CONVERT(NVARCHAR, [' + sub.column_name + ']) AS [' + sub.column_name + ']' 
+          WHEN sub.column_type_mismatch = 1 AND sub.column_type_concat LIKE '%float%' 
+               THEN 'CONVERT(FLOAT, [' + sub.column_name + ']) AS [' + sub.column_name + ']' 
+          WHEN sub.column_type_mismatch = 1 THEN 'CONVERT(INT, [' + sub.column_name + ']) AS [' + sub.column_name + ']' 
+          ELSE '[' + sub.column_name + ']' 
          END AS kippmiami        
         ,CASE 
-          WHEN p.kippcamden IS NULL THEN 'NULL AS [' + p.column_name + ']'
-          WHEN p.kippcamden != p.kippnewark THEN 'CONVERT(' + p.kippnewark + ',[' + p.column_name + ']) AS [' + p.column_name + ']' 
-          ELSE '[' + p.column_name + ']' 
+          WHEN sub.kippcamden_column_type IS NULL THEN 'NULL AS [' + sub.column_name + ']'
+          WHEN sub.column_type_mismatch = 1 AND sub.column_type_concat LIKE '%varchar%' 
+               THEN 'CONVERT(NVARCHAR, [' + sub.column_name + ']) AS [' + sub.column_name + ']' 
+          WHEN sub.column_type_mismatch = 1 AND sub.column_type_concat LIKE '%float%' 
+               THEN 'CONVERT(FLOAT, [' + sub.column_name + ']) AS [' + sub.column_name + ']' 
+          WHEN sub.column_type_mismatch = 1 THEN 'CONVERT(INT, [' + sub.column_name + ']) AS [' + sub.column_name + ']' 
+          ELSE '[' + sub.column_name + ']'
          END AS kippcamden
-  FROM 
+  FROM
       (
-       SELECT db_name
-             ,schema_name
-             ,table_name
-             ,column_name
-             ,column_type
-       FROM gabby.utilities.all_tables_columns
-       WHERE column_id > -1
-         AND db_name != 'gabby'
-      ) sub
-  PIVOT(
-    MAX(column_type)
-    FOR db_name IN ([kippnewark], [kippcamden], [kippmiami])
-   ) p
+       SELECT p.[schema_name]
+             ,p.table_name
+             ,p.column_name
+             ,p.kippmiami AS kippmiami_column_type
+             ,p.kippcamden AS kippcamden_column_type
+             ,p.kippnewark AS kippnewark_column_type
+             ,CONCAT(p.kippnewark, p.kippcamden, p.kippmiami) AS column_type_concat
+             ,CASE
+               WHEN (CONCAT(p.kippcamden,p.kippnewark) != '' AND CHARINDEX(p.kippmiami, CONCAT(p.kippcamden,p.kippnewark)) = 0)
+                 OR (CONCAT(p.kippmiami,p.kippnewark) != '' AND CHARINDEX(p.kippcamden, CONCAT(p.kippmiami,p.kippnewark)) = 0)
+                 OR (CONCAT(p.kippmiami,p.kippcamden) != '' AND CHARINDEX(p.kippnewark, CONCAT(p.kippmiami,p.kippcamden)) = 0)
+                    THEN 1
+               ELSE 0
+              END AS column_type_mismatch
+       FROM 
+           (
+            SELECT [db_name]
+                  ,[schema_name]
+                  ,table_name
+                  ,column_name
+                  ,column_type
+            FROM gabby.utilities.all_tables_columns
+            WHERE column_id > -1
+              AND [db_name] != 'gabby'
+           ) sub
+       PIVOT(
+         MAX(column_type)
+         FOR [db_name] IN ([kippnewark], [kippcamden], [kippmiami])
+        ) p
+    ) sub
  )
 
-SELECT sub.schema_name
+SELECT sub.[schema_name]
       ,sub.table_name
       ,sub.column_type_mismatch
       ,CASE 
         WHEN t.n = 0 THEN 'USE gabby'
         WHEN t.n = 1 THEN 'GO'
-        WHEN t.n = 2 THEN 'CREATE OR ALTER VIEW ' + sub.schema_name + '.' + sub.table_name + ' AS ' 
+        WHEN t.n = 2 THEN 'CREATE OR ALTER VIEW ' + sub.[schema_name] + '.' + sub.table_name + ' AS ' 
                             + CASE WHEN sub.kippnewark_count > 0 THEN sub.kippnewark ELSE '' END
                             + CASE WHEN sub.kippcamden_count > 0 THEN ' UNION ALL ' + sub.kippcamden ELSE '' END
                             + CASE WHEN sub.kippmiami_count > 0 THEN ' UNION ALL ' + sub.kippmiami ELSE '' END
@@ -58,11 +82,11 @@ SELECT sub.schema_name
        END AS query
 FROM
     (
-     SELECT atc.schema_name
+     SELECT atc.[schema_name]
            ,atc.table_name                 
-           ,'SELECT ''kippnewark'' AS [db_name] ' + gabby.dbo.GROUP_CONCAT_D(',' + atc.kippnewark, '') + ' FROM kippnewark.' + atc.schema_name + '.' + atc.table_name AS kippnewark
-           ,'SELECT ''kippcamden'' AS [db_name] ' + gabby.dbo.GROUP_CONCAT_D(',' + atc.kippcamden, '') + ' FROM kippcamden.' + atc.schema_name + '.' + atc.table_name AS kippcamden
-           ,'SELECT ''kippmiami'' AS [db_name] ' + gabby.dbo.GROUP_CONCAT_D(',' + atc.kippmiami, '') + ' FROM kippmiami.' + atc.schema_name + '.' + atc.table_name AS kippmiami
+           ,'SELECT ''kippnewark'' AS [db_name] ' + gabby.dbo.GROUP_CONCAT_D(',' + atc.kippnewark, '') + ' FROM kippnewark.' + atc.[schema_name] + '.' + atc.table_name AS kippnewark
+           ,'SELECT ''kippcamden'' AS [db_name] ' + gabby.dbo.GROUP_CONCAT_D(',' + atc.kippcamden, '') + ' FROM kippcamden.' + atc.[schema_name] + '.' + atc.table_name AS kippcamden
+           ,'SELECT ''kippmiami'' AS [db_name] ' + gabby.dbo.GROUP_CONCAT_D(',' + atc.kippmiami, '') + ' FROM kippmiami.' + atc.[schema_name] + '.' + atc.table_name AS kippmiami
            
            ,MAX(atc.column_type_mismatch) AS column_type_mismatch
            ,COUNT(CASE WHEN atc.kippnewark NOT LIKE '%NULL%' THEN atc.kippnewark END) AS kippnewark_count
@@ -71,7 +95,7 @@ FROM
      FROM all_tables_columns_pivot atc
      WHERE atc.table_name NOT LIKE 'fivetran%'
      GROUP BY atc.table_name
-             ,atc.schema_name
+             ,atc.[schema_name]
     ) sub
 CROSS JOIN (SELECT n FROM gabby.utilities.row_generator WHERE n <= 3) t
-ORDER BY sub.schema_name, sub.table_name, t.n
+ORDER BY sub.[schema_name], sub.table_name, t.n
--- utilities/v_utilities.all_tables_columns.sql
+++ utilities/v_utilities.all_tables_columns.sql
@@ -3,128 +3,136 @@ GO
 
 CREATE OR ALTER VIEW utilities.all_tables_columns AS
 
-SELECT 'gabby' AS db_name
-      ,s.name COLLATE Latin1_General_BIN AS schema_name
-      ,t.name COLLATE Latin1_General_BIN AS table_name
-      ,t.type
+SELECT 'gabby' AS [db_name]
+      ,s.[name] COLLATE Latin1_General_BIN AS [schema_name]
+      ,t.[name] COLLATE Latin1_General_BIN AS table_name
+      ,t.[type]
       ,-1 AS column_id
       ,NULL AS column_name
+      ,NULL AS column_max_length
       ,NULL AS column_type
 FROM gabby.sys.schemas AS s
 INNER JOIN gabby.sys.objects AS t
    ON s.[schema_id] = t.[schema_id]
-  AND t.type IN ('U', 'V')
-WHERE s.schema_id BETWEEN 5 AND 16383
+  AND t.[type] IN ('U', 'V')
+WHERE s.[schema_id] BETWEEN 5 AND 16383
 UNION ALL
-SELECT 'gabby' AS db_name
-      ,s.name COLLATE Latin1_General_BIN AS schema_name
-      ,t.name COLLATE Latin1_General_BIN AS table_name
-      ,t.type
+SELECT 'gabby' AS [db_name]
+      ,s.[name] COLLATE Latin1_General_BIN AS [schema_name]
+      ,t.[name] COLLATE Latin1_General_BIN AS table_name
+      ,t.[type]
       ,c.column_id
-      ,c.name COLLATE Latin1_General_BIN AS column_name
-      ,y.name COLLATE Latin1_General_BIN AS column_type
+      ,c.[name] COLLATE Latin1_General_BIN AS column_name
+      ,c.max_length AS column_max_length
+      ,y.[name] COLLATE Latin1_General_BIN AS column_type
 FROM gabby.sys.schemas AS s
 INNER JOIN gabby.sys.objects AS t
    ON s.[schema_id] = t.[schema_id]
-  AND t.type IN ('U', 'V')
+  AND t.[type] IN ('U', 'V')
 INNER JOIN gabby.sys.columns c
-   ON t.object_id = c.object_id
+   ON t.[object_id] = c.[object_id]
 INNER JOIN gabby.sys.types y
    ON c.user_type_id = y.user_type_id
-WHERE s.schema_id BETWEEN 5 AND 16383
+WHERE s.[schema_id] BETWEEN 5 AND 16383
 
 UNION ALL
 
-SELECT 'kippnewark' AS db_name
-      ,s.name AS schema_name
-      ,t.name AS table_name
-      ,t.type
+SELECT 'kippnewark' AS [db_name]
+      ,s.[name] AS [schema_name]
+      ,t.[name] AS table_name
+      ,t.[type]
       ,-1 AS column_id
       ,NULL AS column_name
+      ,NULL AS column_max_length
       ,NULL AS column_type
 FROM [kippnewark].sys.schemas AS s
 INNER JOIN [kippnewark].sys.objects AS t
    ON s.[schema_id] = t.[schema_id]
-  AND t.type IN ('U', 'V')
-WHERE s.schema_id BETWEEN 5 AND 16383
+  AND t.[type] IN ('U', 'V')
+WHERE s.[schema_id] BETWEEN 5 AND 16383
 UNION ALL
-SELECT 'kippnewark' AS db_name
-      ,s.name AS schema_name
-      ,t.name AS table_name
-      ,t.type
+SELECT 'kippnewark' AS [db_name]
+      ,s.[name] AS [schema_name]
+      ,t.[name] AS table_name
+      ,t.[type]
       ,c.column_id
-      ,c.name AS column_name
-      ,y.name AS column_type
+      ,c.[name] AS column_name
+      ,c.max_length AS column_max_length
+      ,y.[name] AS column_type
 FROM [kippnewark].sys.schemas AS s
 INNER JOIN [kippnewark].sys.objects AS t
    ON s.[schema_id] = t.[schema_id]
-  AND t.type IN ('U', 'V')
+  AND t.[type] IN ('U', 'V')
 INNER JOIN [kippnewark].sys.columns c
-   ON t.object_id = c.object_id
+   ON t.[object_id] = c.[object_id]
 INNER JOIN kippnewark.sys.types y
    ON c.user_type_id = y.user_type_id
-WHERE s.schema_id BETWEEN 5 AND 16383
+WHERE s.[schema_id] BETWEEN 5 AND 16383
 
 UNION ALL
 
-SELECT 'kippcamden' AS db_name
-      ,s.name AS schema_name
-      ,t.name AS table_name
-      ,t.type
+SELECT 'kippcamden' AS [db_name]
+      ,s.[name] AS [schema_name]
+      ,t.[name] AS table_name
+      ,t.[type]
       ,-1 AS column_id
       ,NULL AS column_name
+      ,NULL AS column_max_length
       ,NULL AS column_type
 FROM [kippcamden].sys.schemas AS s
 INNER JOIN [kippcamden].sys.objects AS t
    ON s.[schema_id] = t.[schema_id]
-  AND t.type IN ('U', 'V')
-WHERE s.schema_id BETWEEN 5 AND 16383
+  AND t.[type] IN ('U', 'V')
+WHERE s.[schema_id] BETWEEN 5 AND 16383
 UNION ALL
-SELECT 'kippcamden' AS db_name
-      ,s.name AS schema_name
-      ,t.name AS table_name
-      ,t.type
+SELECT 'kippcamden' AS [db_name]
+      ,s.[name] AS [schema_name]
+      ,t.[name] AS table_name
+      ,t.[type]
       ,c.column_id
-      ,c.name AS column_name
-      ,y.name AS column_type
+      ,c.[name] AS column_name
+      ,c.max_length AS column_max_length
+      ,y.[name] AS column_type
 FROM [kippcamden].sys.schemas AS s
 INNER JOIN [kippcamden].sys.objects AS t
    ON s.[schema_id] = t.[schema_id]
-  AND t.type IN ('U', 'V')
+  AND t.[type] IN ('U', 'V')
 INNER JOIN [kippcamden].sys.columns c
-   ON t.object_id = c.object_id
+   ON t.[object_id] = c.[object_id]
 INNER JOIN kippcamden.sys.types y
    ON c.user_type_id = y.user_type_id
-WHERE s.schema_id BETWEEN 5 AND 16383
+WHERE s.[schema_id] BETWEEN 5 AND 16383
 
 UNION ALL
 
-SELECT 'kippmiami' AS db_name
-      ,s.name AS schema_name
-      ,t.name AS table_name
-      ,t.type
+SELECT 'kippmiami' AS [db_name]
+      ,s.[name] AS [schema_name]
+      ,t.[name] AS table_name
+      ,t.[type]
       ,-1 AS column_id
       ,NULL AS column_name
+      ,NULL AS column_max_length
       ,NULL AS column_type
 FROM [kippmiami].sys.schemas AS s
 INNER JOIN [kippmiami].sys.objects AS t
    ON s.[schema_id] = t.[schema_id]
-  AND t.type IN ('U', 'V')
-WHERE s.schema_id BETWEEN 5 AND 16383
+  AND t.[type] IN ('U', 'V')
+WHERE s.[schema_id] BETWEEN 5 AND 16383
 UNION ALL
-SELECT 'kippmiami' AS db_name
-      ,s.name AS schema_name
-      ,t.name AS table_name
-      ,t.type
+SELECT 'kippmiami' AS [db_name]
+      ,s.[name] AS [schema_name]
+      ,t.[name] AS table_name
+      ,t.[type]
       ,c.column_id
-      ,c.name AS column_name
-      ,y.name AS column_type
+      ,c.[name] AS column_name
+      ,c.max_length AS column_max_length
+      ,y.[name] AS column_type
 FROM [kippmiami].sys.schemas AS s
 INNER JOIN [kippmiami].sys.objects AS t
    ON s.[schema_id] = t.[schema_id]
-  AND t.type IN ('U', 'V')
+  AND t.[type] IN ('U', 'V')
 INNER JOIN [kippmiami].sys.columns c
-   ON t.object_id = c.object_id
+   ON t.[object_id] = c.[object_id]
 INNER JOIN kippmiami.sys.types y
    ON c.user_type_id = y.user_type_id
-WHERE s.schema_id BETWEEN 5 AND 16383
+WHERE s.[schema_id] BETWEEN 5 AND 16383
