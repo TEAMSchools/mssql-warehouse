fccefe20362b2ea2ce36e34ad21798d0f8e207fd
cbini <cbini87@gmail.com>
2020-03-05 15:52:50 -0600

    update to new source table

--- renaissance/v_renaissance.ar_studentpractice_identifiers.sql
+++ renaissance/v_renaissance.ar_studentpractice_identifiers.sql
@@ -3,60 +3,56 @@ GO
 
 CREATE OR ALTER VIEW renaissance.ar_studentpractice_identifiers AS
 
-SELECT student_number
-      ,ch_fiction_non_fiction
-      ,d_percent_correct
-      ,d_points_earned
-      ,dt_taken
-      ,fl_lexile_calc
-      ,i_lexile 
-      ,i_questions_correct
-      ,i_questions_presented
-      ,i_quiz_number           
-      ,i_student_practice_id
-      ,i_user_id
-      ,i_word_count      
-      ,ti_book_rating
-      ,ti_passed
-      ,vch_content_title
-      ,vch_lexile_display      
-
-      ,gabby.utilities.DATE_TO_SY(dt_taken) AS academic_year
+SELECT sub.student_identifier AS student_number
+      ,sub.fiction_non_fiction AS ch_fiction_non_fiction
+      ,sub.percent_correct AS d_percent_correct
+      ,sub.points_earned AS d_points_earned
+      ,sub.date_quiz_completed_local AS dt_taken
+      ,CONVERT(FLOAT, sub.lexile_level) AS fl_lexile_calc
+      ,sub.lexile_level AS i_lexile 
+      ,sub.questions_correct AS i_questions_correct
+      ,sub.questions_presented AS i_questions_presented
+      ,sub.quiz_number AS i_quiz_number
+      ,NULL i_student_practice_id
+      ,NULL AS i_user_id
+      ,sub.word_count AS i_word_count
+      ,NULL AS ti_book_rating
+      ,sub.passed AS ti_passed
+      ,sub.content_title AS vch_content_title
+      ,sub.lexile_measure AS vch_lexile_display
+      ,sub.academic_year
 
       ,CASE
         /* for failing attempts, valid row should be most recent */
-        WHEN ti_passed = 0 THEN ROW_NUMBER() OVER(
-                                  PARTITION BY student_number, i_quiz_number, ti_passed
-                                    ORDER BY dt_taken DESC) 
+        WHEN sub.passed = 0 THEN ROW_NUMBER() OVER(
+                                   PARTITION BY sub.student_identifier, sub.quiz_number, sub.passed
+                                     ORDER BY sub.date_quiz_completed_local DESC) 
         /* for passing attempts, valid row should be first */
-        WHEN ti_passed = 1 THEN ROW_NUMBER() OVER(
-                                  PARTITION BY student_number, i_quiz_number, ti_passed
-                                    ORDER BY dt_taken ASC) 
+        WHEN sub.passed = 1 THEN ROW_NUMBER() OVER(
+                                   PARTITION BY sub.student_identifier, sub.quiz_number, sub.passed
+                                     ORDER BY sub.date_quiz_completed_local ASC)
        END AS rn_quiz
 FROM
     (
-     SELECT CONVERT(INT,ar.i_student_practice_id) AS i_student_practice_id
-           ,CONVERT(INT,ar.i_user_id) AS i_user_id
-           ,CONVERT(INT,ar.i_quiz_number) AS i_quiz_number
-           ,CONVERT(VARCHAR(250),ar.vch_content_title) AS vch_content_title
-           ,CONVERT(DATE,ar.dt_taken) AS dt_taken
-           ,CONVERT(INT,ar.ti_passed) AS ti_passed
-           ,CONVERT(FLOAT,ar.i_word_count) AS i_word_count
-           ,ar.d_percent_correct
-           ,CONVERT(FLOAT,ar.i_questions_correct) AS i_questions_correct
-           ,CONVERT(FLOAT,ar.i_questions_presented) AS i_questions_presented
-           ,ar.fl_lexile_calc
-           ,CONVERT(INT,ar.i_lexile) AS i_lexile
-           ,CONVERT(VARCHAR(25),ar.vch_lexile_display) AS vch_lexile_display
-           ,ar.d_points_earned
-           ,CONVERT(VARCHAR(2),ar.ch_fiction_non_fiction) AS ch_fiction_non_fiction
-           ,CONVERT(INT,ar.ti_book_rating) AS ti_book_rating
-
-           ,CONVERT(INT,CASE WHEN ISNUMERIC(u.vch_previous_idnum) = 1 THEN u.vch_previous_idnum END) AS student_number
-     FROM gabby.renaissance.ar_studentpractice ar
-     JOIN gabby.renaissance.rl_user u
-       ON ar.i_user_id = u.i_user_id
-     WHERE ar.i_content_type_id = 31
-       AND ar.ti_row_status = 1
-       AND ar.ch_status != 'U'
+     SELECT CONVERT(INT, CASE WHEN ISNUMERIC(student_identifier) = 1 THEN student_identifier END) AS student_identifier
+           ,quiz_number
+           ,content_title
+           ,CONVERT(DATE, date_quiz_completed_local) AS date_quiz_completed_local
+           ,passed
+           ,CONVERT(FLOAT, word_count) AS word_count
+           ,points_earned
+           ,CONVERT(FLOAT, questions_correct) AS questions_correct
+           ,CONVERT(FLOAT, questions_presented) AS questions_presented
+           ,percent_correct
+           ,lexile_level
+           ,lexile_measure
+           ,CASE
+             WHEN fiction_non_fiction = 'Fiction' THEN 'F'
+             WHEN fiction_non_fiction = 'NonFiction' THEN 'NF'
+            END AS fiction_non_fiction
+           ,CONVERT(INT, LEFT(school_year, 4)) AS academic_year
+     FROM gabby.renaissance.generic_ar_extract
+     WHERE quiz_deleted = 0
+       AND quiz_type = 'Reading Practice Quiz'
     ) sub
+WHERE sub.student_identifier IS NOT NULL
