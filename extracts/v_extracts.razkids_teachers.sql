0f3371d2d881f340184a7fd9f0dda0094d1a7767
cbini <cbini87@gmail.com>
2020-04-13 14:51:55 -0500

    remove role logic

--- extracts/v_extracts.razkids_teachers.sql
+++ extracts/v_extracts.razkids_teachers.sql
@@ -3,20 +3,6 @@ GO
 
 CREATE OR ALTER VIEW extracts.razkids_teachers AS
 
---WITH hr_teachers AS (
---  SELECT DISTINCT 
---         t.teachernumber COLLATE Latin1_General_BIN AS teachernumber
---        ,t.[db_name]
---  FROM gabby.powerschool.sections sec
---  JOIN gabby.powerschool.teachers_static t
---    ON sec.teacher = t.id
---   AND sec.schoolid = t.schoolid
---   AND sec.[db_name] = t.[db_name]
---  WHERE sec.course_number_clean = 'HR'
---    AND sec.yearid = (gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1990)
---    AND sec.no_of_students > 0
--- )
-
 SELECT CASE
         WHEN scw.legal_entity_name = 'KIPP Miami' THEN LOWER(LEFT(scw.userprincipalname, CHARINDEX('@', scw.userprincipalname))) + 'kippmiami.org'
         ELSE LOWER(LEFT(scw.userprincipalname, CHARINDEX('@', scw.userprincipalname))) + 'apps.teamschools.org' 
@@ -28,14 +14,6 @@ SELECT CASE
         ELSE LOWER(LEFT(scw.userprincipalname, CHARINDEX('@', scw.userprincipalname))) + 'apps.teamschools.org' 
        END AS email
       ,scw.primary_site AS school_organization
-
-      --,CASE 
-      --  WHEN hr.teachernumber IS NOT NULL THEN 'teacher'
-      --  ELSE 'school_admin'
-      -- END AS [role]
 FROM gabby.people.staff_crosswalk_static scw
---LEFT JOIN hr_teachers hr
---  ON scw.ps_teachernumber = hr.teachernumber
--- AND scw.[db_name] = hr.[db_name]
 WHERE (scw.primary_site_school_level = 'ES' OR scw.primary_site = 'KIPP Whittier Middle')
   AND scw.[status] NOT IN ('TERMINATED', 'PRESTART')
