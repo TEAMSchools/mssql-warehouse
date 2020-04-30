3989b9824149e13341c7a43d21ff3a541f8d8047
cbini <cbini87@gmail.com>
2020-04-30 15:59:15 -0500

    :lipstick:

--- tableau/v_tableau.es_remote_learning_grades.sql
+++ tableau/v_tableau.es_remote_learning_grades.sql
@@ -1,14 +1,20 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW tableau.es_remote_learning_grades AS 
+
 SELECT r.roster_id
       ,r.roster_name
+
       ,ra.school_name
       ,ra.student_school_id
       ,CONCAT(ra.last_name,', ',ra.first_name) AS student_name
-      ,b.behavior_date AS assignment_date
-      ,CASE WHEN b.staff_last_name IS NULL THEN NULL
-            ELSE CONCAT(b.staff_last_name,', ',b.staff_first_name) END AS staff_name
+
       ,b.assignment AS assignment_name
-      ,CAST(b.behavior AS int) AS score
+      ,b.behavior_date AS assignment_date
       ,b.notes
+      ,CAST(b.behavior AS INT) AS score
+      ,b.staff_last_name + ', ' + b.staff_first_name AS staff_name
 FROM gabby.deanslist.rosters_all r
 JOIN gabby.deanslist.roster_assignments ra
   ON r.roster_id = ra.dlroster_id
