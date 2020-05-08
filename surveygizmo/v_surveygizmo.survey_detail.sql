0e243df8ef878cbcce9c55eaf235d8556809304c
Kevin Verhoff <kevin.verhoff@gmail.com>
2020-05-08 11:59:11 -0400

    gender, race, ethnicity, and textboxes
    Adding TEXTBOX responses to survey_detail to pull in textbox responses; this should not affect other views, but I can confirm/adjust filters if needed
    
    Also adding gender and ethnicity to the covid_survey view

--- surveygizmo/v_surveygizmo.survey_detail.sql
+++ surveygizmo/v_surveygizmo.survey_detail.sql
@@ -64,7 +64,8 @@ JOIN gabby.surveygizmo.survey_question_clean_static sq
   ON s.survey_id = sq.survey_id
  AND sq.base_type = 'Question'
  AND sq.is_identifier_question = 0
- AND sq.[type] IN ('RADIO', 'ESSAY')
+ AND sq.[type] IN ('RADIO', 'ESSAY', 'TEXTBOX')
+ AND sq.shortname IS NOT NULL
 JOIN gabby.surveygizmo.survey_response_identifiers_static sri
   ON s.survey_id = sri.survey_id
  AND sri.[status] = 'Complete'
