e6f6fc8c0fc9e9d7323982250d2015ca56c9b051
cbini <cbini87@gmail.com>
2020-05-20 13:24:06 -0500

    :lipstick: and refactoring filters

--- surveys/v_surveys.covid_familysurvey_detail.sql
+++ surveys/v_surveys.covid_familysurvey_detail.sql
@@ -6,29 +6,28 @@ CREATE OR ALTER VIEW surveys.covid_familysurvey_detail AS
 WITH response_ids AS (
   SELECT survey_id
         ,survey_response_id
-        ,date_started
         ,grade_level
-        ,location AS school_site
-  FROM (
+        ,[location] AS school_site
+  FROM
+      (
        SELECT sq.survey_id
              ,sq.shortname
 
              ,srd.survey_response_id
-             ,srd.date_started
              ,srd.answer
        FROM gabby.surveygizmo.survey_question_clean_static sq
        JOIN gabby.surveygizmo.survey_response_data_static srd
          ON sq.survey_id = srd.survey_id
         AND sq.survey_question_id = srd.question_id
         AND srd.answer IS NOT NULL
-       WHERE sq.survey_id = 5593585
+       WHERE sq.shortname IN ('grade_level', 'location')
       ) sub
   PIVOT(
     MAX(answer)
-    FOR shortname IN (grade_level
-                     ,[location])
-       ) p
-       ) 
+    FOR shortname IN (grade_level, [location])
+   ) p
+ )
+
 SELECT s.survey_id
       ,s.title AS survey_title
 
@@ -48,8 +47,7 @@ SELECT s.survey_id
 
       ,sc.academic_year
       ,sc.reporting_term_code
-      ,sc.name AS campaign_name
-
+      ,sc.[name] AS campaign_name
 FROM surveygizmo.survey_clean s
 JOIN gabby.surveygizmo.survey_question_clean_static sq
   ON s.survey_id = sq.survey_id
@@ -62,6 +60,7 @@ JOIN gabby.surveygizmo.survey_response_data_static srd
 JOIN gabby.surveygizmo.survey_response_clean_static sr
   ON sq.survey_id = sr.survey_id
  AND srd.survey_response_id = sr.survey_response_id
+ AND sr.[status] = 'Complete'
 JOIN response_ids r
   ON s.survey_id = r.survey_id
  AND srd.survey_response_id = r.survey_response_id
@@ -72,4 +71,4 @@ LEFT JOIN gabby.surveygizmo.survey_question_options_static qo
 LEFT JOIN gabby.surveygizmo.survey_campaign_clean_static sc
   ON s.survey_id = sc.survey_id
  AND srd.date_started BETWEEN sc.link_open_date AND sc.link_close_date
-WHERE sr.[status] = 'Complete'
+WHERE s.survey_id = 5593585
