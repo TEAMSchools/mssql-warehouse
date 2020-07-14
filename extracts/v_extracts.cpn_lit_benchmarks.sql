c76d25fbc36b215d5546d353ca2c1449a1a979a4
cbini <cbini87@gmail.com>
2020-07-14 14:20:32 -0500

    Update v_extracts.cpn_lit_benchmarks.sql

--- extracts/v_extracts.cpn_lit_benchmarks.sql
+++ extracts/v_extracts.cpn_lit_benchmarks.sql
@@ -3,24 +3,29 @@ GO
 
 CREATE OR ALTER VIEW extracts.cpn_lit_benchmarks AS
 
-SELECT student_number
-      ,academic_year
-      ,reporting_term
-      ,lit_term_start_date
-      ,lit_term_end_date
-      ,unique_id
-      ,is_fp
-      ,test_date      
-      ,read_lvl
-      ,lvl_num
-      ,gleq      
-      ,goal_lvl
-      ,goal_num
-      ,met_goal            
-      ,color
-      ,genre
-      ,fp_keylever            
-FROM gabby.tableau.lit_tracker
-WHERE rn_test = 1
-  AND region = 'KCNA'
-  AND unique_id IS NOT NULL
+SELECT achv.student_number
+      ,achv.academic_year
+      ,achv.reporting_term
+      ,achv.[start_date] AS lit_term_start_date
+      ,achv.end_date AS lit_term_end_date
+      ,achv.achv_unique_id AS unique_id
+      ,1 AS is_fp
+      ,ate.test_date
+      ,achv.read_lvl
+      ,achv.lvl_num
+      ,achv.gleq
+      ,achv.goal_lvl
+      ,achv.goal_num
+      ,achv.met_goal
+      ,ate.color
+      ,ate.genre
+      ,ate.fp_keylever
+FROM gabby.lit.achieved_by_round_static achv
+JOIN gabby.powerschool.cohort_identifiers_static co
+  ON achv.student_number = co.student_number
+ AND achv.academic_year = co.academic_year
+ AND co.rn_year = 1
+ AND co.region = 'KCNA'
+JOIN gabby.lit.all_test_events_static ate
+  ON achv.achv_unique_id = ate.unique_id
+WHERE achv.achv_unique_id LIKE 'FPBAS%'
