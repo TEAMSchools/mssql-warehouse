07fa8d86ef233cbc6eb49fceba84e998ade4ffc4
Charlie Bini <cbini87@gmail.com>
2020-08-28 14:00:01 -0500

    :lipstick:

--- extracts/v_extracts.fpodms_students.sql
+++ extracts/v_extracts.fpodms_students.sql
@@ -7,14 +7,14 @@ SELECT co.student_number AS [studentIdentifier]
       ,co.first_name AS [firstName]
       ,co.last_name AS [lastName]
       ,co.grade_level + 1 AS [gradeId]
-      ,CONVERT(VARCHAR,CONVERT(DATETIME2,co.entrydate), 126) AS [classStudentStartDate]
-      ,CONVERT(VARCHAR,CONVERT(DATETIME2,co.exitdate), 126) AS [classStudentEndDate]
-      
-      ,sch.name AS [schoolName]
+      ,CONVERT(VARCHAR, CONVERT(DATETIME2,co.entrydate), 126) AS [classStudentStartDate]
+      ,CONVERT(VARCHAR, CONVERT(DATETIME2,co.exitdate), 126) AS [classStudentEndDate]
+
+      ,sch.[name] AS [schoolName]
 FROM gabby.powerschool.cohort_identifiers_static co
 JOIN gabby.powerschool.schools sch
   ON co.schoolid = sch.school_number
- AND co.db_name = sch.db_name
+ AND co.[db_name] = sch.[db_name]
 WHERE co.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
   AND co.school_level IN ('ES', 'MS')
   AND co.is_enrolled_recent = 1
