82de22eb9639a6b7ba4a81e3166764ad1b6f1ae8
Charlie Bini <cbini87@gmail.com>
2020-09-03 11:17:54 -0500

    Update v_tableau.ops_enrollment_audit.sql

--- tableau/v_tableau.ops_enrollment_audit.sql
+++ tableau/v_tableau.ops_enrollment_audit.sql
@@ -3,70 +3,23 @@ GO
 
 CREATE OR ALTER VIEW tableau.ops_enrollment_audit AS
 
---WITH caredox_enrollment AS (
---  SELECT student_id_clean AS student_id
---        ,status_clean AS status
---        ,ROW_NUMBER() OVER(
---           PARTITION BY student_id_clean
---             ORDER BY CONVERT(DATETIME2,last_updated_at) DESC) AS rn_last_updated
---  FROM gabby.caredox.enrollment
---  WHERE status_clean != 'new'
--- )
-
---,caredox_immunization AS (
---  SELECT student_id_clean AS student_id
---        ,status_clean AS status
---        ,ROW_NUMBER() OVER(
---           PARTITION BY student_id_clean
---             ORDER BY CONVERT(DATETIME2,last_updated_at) DESC) AS rn_last_updated  
---  FROM gabby.caredox.immunization
---  WHERE status_clean IN ('Valid', 'N/A')
--- )
-
---,caredox_screenings AS (
---  SELECT student_id_clean AS student_id
---        ,status_clean AS status
---        ,ROW_NUMBER() OVER(
---           PARTITION BY student_id_clean
---             ORDER BY CONVERT(DATETIME2,last_updated_at) DESC) AS rn_last_updated
---  FROM gabby.caredox.screenings
---  WHERE status_clean = 'compliant'
--- )
-
---,caredox_medications AS (
---  SELECT student_id
---        ,gabby.dbo.GROUP_CONCAT_D(medication, ' | ') AS medication
---  FROM
---      (
---       SELECT student_id_clean AS student_id
---             ,CONCAT(inventory_id_and_date_created, ' - ', medication_name) AS medication
---             ,ROW_NUMBER() OVER(
---                PARTITION BY student_id_clean, medication_name
---                  ORDER BY CONVERT(DATETIME2,last_updated_at) DESC) AS rn_last_updated
---       FROM gabby.caredox.medication_inventory
---       WHERE event = 'create'
---      ) sub
---  WHERE rn_last_updated = 1
---  GROUP BY student_id
--- )
-
 WITH residency_verification AS (
-  SELECT nen
-        ,academic_year
-        ,verification_date
-        ,approved
-  FROM
-      (
-       SELECT CONVERT(VARCHAR(25),subject_line) AS nen
-             ,academic_year
-             ,[timestamp] AS verification_date
-             ,approved
-             ,ROW_NUMBER() OVER(
-               PARTITION BY subject_line, academic_year
-                 ORDER BY [timestamp] DESC) AS rn_recent
-       FROM gabby.enrollment.residency_verification
-      ) sub
-  WHERE rn_recent = 1
+  SELECT s.student_number
+        ,COALESCE(rv.doc_1, x.residency_proof_1) COLLATE Latin1_General_BIN AS residency_proof_1
+        ,COALESCE(rv.doc_2, x.residency_proof_2) COLLATE Latin1_General_BIN AS residency_proof_2
+        ,COALESCE(rv.doc_3, x.residency_proof_3) COLLATE Latin1_General_BIN AS residency_proof_3
+        ,ROW_NUMBER() OVER(
+           PARTITION BY s.student_number
+             ORDER BY rv.[timestamp] DESC) AS rn
+  FROM gabby.powerschool.u_def_ext_students x
+  JOIN gabby.powerschool.students s
+    ON x.studentsdcid = s.dcid
+   AND x.[db_name] = s.[db_name]
+   AND s.enroll_status = 0
+  LEFT JOIN gabby.enrollment.residency_verification rv
+    ON s.student_number = rv.id
+   AND rv.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
+   AND rv._fivetran_deleted = 0
  )
 
 ,all_data AS (
@@ -79,46 +32,44 @@ WITH residency_verification AS (
         ,sub.academic_year
         ,sub.is_pathways
         ,sub.entry_status
+        ,sub.registration_status
         
-        ,CONVERT(VARCHAR(500),sub.lunch_app_status) COLLATE Latin1_General_BIN AS lunch_app_status
-        ,CONVERT(VARCHAR(500),sub.lunch_balance) COLLATE Latin1_General_BIN AS lunch_balance
-        ,CONVERT(VARCHAR(500),sub.iep_registration_followup) COLLATE Latin1_General_BIN AS iep_registration_followup_required
-        ,CONVERT(VARCHAR(500),sub.lep_registration_followup) COLLATE Latin1_General_BIN AS lep_registration_followup_required
-        --,CONVERT(VARCHAR(500),sub.caredox_enrollment_status) COLLATE Latin1_General_BIN AS caredox_enrollment_status
-        --,CONVERT(VARCHAR(500),sub.caredox_immunization_status) COLLATE Latin1_General_BIN AS caredox_immunization_status
-        --,CONVERT(VARCHAR(500),sub.caredox_screenings_status) COLLATE Latin1_General_BIN AS caredox_screenings_status
-        --,CONVERT(VARCHAR(500),sub.caredox_medication_status) COLLATE Latin1_General_BIN AS caredox_medication_status
-        ,CONVERT(VARCHAR(500),sub.birth_certificate_proof) COLLATE Latin1_General_BIN AS birth_certificate_proof
-        ,CONVERT(VARCHAR(500),sub.residency_proof_1) COLLATE Latin1_General_BIN AS residency_proof_1
-        ,CONVERT(VARCHAR(500),sub.residency_proof_2) COLLATE Latin1_General_BIN AS residency_proof_2
-        ,CONVERT(VARCHAR(500),sub.residency_proof_3) COLLATE Latin1_General_BIN AS residency_proof_3
-        ,CONVERT(VARCHAR(500),sub.residency_verification_scanned) COLLATE Latin1_General_BIN AS residency_verification_scanned
-        ,CONVERT(VARCHAR(500),sub.residency_verification_approved) COLLATE Latin1_General_BIN AS residency_verification_approved
+        ,CONVERT(VARCHAR(500), sub.lunch_app_status) COLLATE Latin1_General_BIN AS lunch_app_status
+        ,CONVERT(VARCHAR(500), sub.lunch_balance) COLLATE Latin1_General_BIN AS lunch_balance
+        ,CONVERT(VARCHAR(500), sub.iep_registration_followup) COLLATE Latin1_General_BIN AS iep_registration_followup_required
+        ,CONVERT(VARCHAR(500), sub.lep_registration_followup) COLLATE Latin1_General_BIN AS lep_registration_followup_required
+        ,CONVERT(VARCHAR(500), sub.birth_certificate_proof) COLLATE Latin1_General_BIN AS birth_certificate_proof
+        ,CONVERT(VARCHAR(500), sub.residency_proof_1) COLLATE Latin1_General_BIN AS residency_proof_1
+        ,CONVERT(VARCHAR(500), sub.residency_proof_2) COLLATE Latin1_General_BIN AS residency_proof_2
+        ,CONVERT(VARCHAR(500), sub.residency_proof_3) COLLATE Latin1_General_BIN AS residency_proof_3
         
-        ,CONVERT(VARCHAR(500),sub.region + sub.city) COLLATE Latin1_General_BIN AS region_city
+        ,CONVERT(VARCHAR(500), sub.region + sub.city) COLLATE Latin1_General_BIN AS region_city
         
-        ,CONVERT(VARCHAR(500),CASE
-                               WHEN sub.iep_registration_followup = '1'
-                                AND sub.specialed_classification != ''
-                                     THEN 'Y'
-                               WHEN sub.iep_registration_followup = '1'
-                                AND sub.specialed_classification = ''
-                                     THEN 'N'
-                               ELSE ''
-                              END) COLLATE Latin1_General_BIN AS iep_registration_followup_complete
-        ,CONVERT(VARCHAR(500),CASE
-                               WHEN sub.lep_registration_followup = '1'
-                                AND sub.lep_status != ''
-                                     THEN 'Y'
-                               WHEN sub.lep_registration_followup = '1'
-                                AND sub.lep_status = ''
-                                     THEN 'N'
-                               ELSE ''
-                              END) COLLATE Latin1_General_BIN AS lep_registration_followup_complete
-        ,CONVERT(VARCHAR(500),CASE 
-                               WHEN CONCAT(sub.residency_proof_1, sub.residency_proof_2, sub.residency_proof_3) NOT LIKE '%Missing%' THEN 'Y' 
-                               ELSE 'N'
-                              END) COLLATE Latin1_General_BIN AS residency_proof_all
+        ,CONVERT(VARCHAR(500), 
+           CASE
+            WHEN sub.iep_registration_followup = '1'
+             AND sub.specialed_classification != ''
+                  THEN 'Y'
+            WHEN sub.iep_registration_followup = '1'
+             AND sub.specialed_classification = ''
+                  THEN 'N'
+            ELSE ''
+           END) COLLATE Latin1_General_BIN AS iep_registration_followup_complete
+        ,CONVERT(VARCHAR(500), 
+           CASE
+            WHEN sub.lep_registration_followup = '1'
+             AND sub.lep_status != ''
+                  THEN 'Y'
+            WHEN sub.lep_registration_followup = '1'
+             AND sub.lep_status = ''
+                  THEN 'N'
+            ELSE ''
+           END) COLLATE Latin1_General_BIN AS lep_registration_followup_complete
+        ,CONVERT(VARCHAR(500), 
+           CASE 
+            WHEN CONCAT(sub.residency_proof_1, sub.residency_proof_2, sub.residency_proof_3) NOT LIKE '%Missing%' THEN 'Y' 
+            ELSE 'N'
+           END) COLLATE Latin1_General_BIN AS residency_proof_all
   FROM
       (
        SELECT s.student_number
@@ -127,13 +78,12 @@ WITH residency_verification AS (
              ,s.enroll_status
              ,s.city
              ,CASE
-               WHEN s.db_name = 'kippcamden' THEN 'KCNA' 
-               WHEN s.db_name = 'kippnewark' THEN 'TEAM' 
-               WHEN s.db_name = 'kippmiami' THEN 'KMS' 
+               WHEN s.[db_name] = 'kippcamden' THEN 'KCNA' 
+               WHEN s.[db_name] = 'kippnewark' THEN 'TEAM' 
+               WHEN s.[db_name] = 'kippmiami' THEN 'KMS' 
               END AS region
 
              ,COALESCE(co.reporting_schoolid, s.next_school) AS reporting_schoolid
-             --,s.next_school AS reporting_schoolid
              ,COALESCE(co.is_pathways, 0) AS is_pathways
              ,ISNULL(co.specialed_classification, '') AS specialed_classification
              ,ISNULL(co.lep_status, '') AS lep_status
@@ -147,52 +97,35 @@ WITH residency_verification AS (
               END AS entry_status
 
              ,suf.newark_enrollment_number
+             ,suf.registration_status
 
-             ,ISNULL(uxs.residency_proof_1, 'Missing') AS residency_proof_1
-             ,CASE
-               WHEN (s.enroll_status = -1 OR COALESCE(co.year_in_network, 1) = 1) THEN ISNULL(uxs.residency_proof_2, 'Missing') 
-              END AS residency_proof_2
-             ,CASE
-               WHEN (s.enroll_status = -1 OR COALESCE(co.year_in_network, 1) = 1) THEN ISNULL(uxs.residency_proof_3, 'Missing') 
-              END AS residency_proof_3
              ,ISNULL(uxs.birth_certificate_proof, 'N') AS birth_certificate_proof
              ,ISNULL(uxs.iep_registration_followup, '') AS iep_registration_followup
              ,ISNULL(uxs.lep_registration_followup, '') AS lep_registration_followup
 
-             ,CASE WHEN rv.nen IS NOT NULL THEN 'Y' ELSE 'N' END AS residency_verification_scanned
-             ,ISNULL(rv.approved, '') AS residency_verification_approved
-
-             --,ISNULL(cde.status, '') AS caredox_enrollment_status
-             --,ISNULL(cdi.status, '') AS caredox_immunization_status
-             --,ISNULL(cds.status, '') AS caredox_screenings_status
-             --,ISNULL(cdm.medication, '') AS caredox_medication_status
+             ,ISNULL(rv.residency_proof_1, 'Missing') AS residency_proof_1
+             ,CASE
+               WHEN (s.enroll_status = -1 OR COALESCE(co.year_in_network, 1) = 1) THEN ISNULL(rv.residency_proof_2, 'Missing') 
+              END AS residency_proof_2
+             ,CASE
+               WHEN (s.enroll_status = -1 OR COALESCE(co.year_in_network, 1) = 1) THEN ISNULL(rv.residency_proof_3, 'Missing') 
+              END AS residency_proof_3
 
              ,gabby.utilities.GLOBAL_ACADEMIC_YEAR() AS academic_year
        FROM gabby.powerschool.students s
        LEFT JOIN gabby.powerschool.cohort_identifiers_static co
          ON s.student_number = co.student_number
-        AND s.db_name = co.db_name
+        AND s.[db_name] = co.[db_name]
         AND co.rn_undergrad = 1
        LEFT JOIN gabby.powerschool.u_studentsuserfields suf
          ON s.dcid = suf.studentsdcid
-        AND s.db_name = suf.db_name
+        AND s.[db_name] = suf.[db_name]
        LEFT JOIN gabby.powerschool.u_def_ext_students uxs
          ON s.dcid = uxs.studentsdcid
-        AND s.db_name = uxs.db_name
+        AND s.[db_name] = uxs.[db_name]
        LEFT JOIN residency_verification rv
-         ON suf.newark_enrollment_number = rv.nen
-        AND rv.academic_year = 2019 /* update manually */
-       --LEFT JOIN caredox_enrollment cde
-       --  ON s.student_number = cde.student_id
-       -- AND cde.rn_last_updated = 1
-       --LEFT JOIN caredox_immunization cdi
-       --  ON s.student_number = cdi.student_id
-       -- AND cdi.rn_last_updated = 1
-       --LEFT JOIN caredox_screenings cds
-       --  ON s.student_number = cds.student_id
-       -- AND cds.rn_last_updated = 1
-       --LEFT JOIN caredox_medications cdm
-       --  ON s.student_number = cdm.student_id
+         ON s.student_number = rv.student_number
+        AND rv.rn = 1
        WHERE s.enroll_status IN (-1, 0)
       ) sub
  )
@@ -201,10 +134,10 @@ WITH residency_verification AS (
   SELECT student_number
         ,academic_year
         ,field
-        ,value
+        ,[value]
   FROM all_data
   UNPIVOT(
-    value
+    [value]
     FOR field IN (region_city
                  ,iep_registration_followup_required
                  ,iep_registration_followup_complete
@@ -216,13 +149,7 @@ WITH residency_verification AS (
                  ,residency_proof_2
                  ,residency_proof_3
                  ,birth_certificate_proof
-                 --,caredox_enrollment_status
-                 --,caredox_immunization_status
-                 --,caredox_screenings_status
-                 --,caredox_medication_status
-                 ,residency_proof_all
-                 ,residency_verification_scanned
-                 ,residency_verification_approved)
+                 ,residency_proof_all)
    ) u
  )
 
@@ -243,60 +170,42 @@ SELECT a.student_number
       ,a.residency_proof_all
       ,NULL AS reverification_date
       ,a.birth_certificate_proof
-      ,a.residency_verification_scanned
-      ,a.residency_verification_approved
       ,a.iep_registration_followup_required
       ,a.iep_registration_followup_complete
       ,a.lep_registration_followup_required
       ,a.lep_registration_followup_complete
-      --,a.caredox_enrollment_status
-      --,a.caredox_immunization_status
-      --,a.caredox_screenings_status
-      --,a.caredox_medication_status
+      ,a.registration_status
       ,CASE WHEN a.region_city IN ('TEAMNewark', 'KCNACamden', 'KMSMiami') THEN 'Resident' ELSE 'Non-Resident' END AS residency_status
 
       ,u.field AS audit_field
-      ,u.value AS audit_value
+      ,u.[value] AS audit_value
       ,CASE
         /* 0 = FLAG || -1 = BAD || 1 = OK */        
-        WHEN u.field = 'region_city' AND u.value NOT IN ('TEAMNewark', 'KCNACamden', 'KMSMiami') THEN 0
-        WHEN u.field = 'region_city' AND u.value IN ('TEAMNewark', 'KCNACamden', 'KMSMiami') THEN 1
-        --WHEN u.field = 'caredox_enrollment_status' AND u.value = 'approved' THEN 1
-        --WHEN u.field = 'caredox_enrollment_status' AND u.value IN ('review_pending','started') THEN 0
-        --WHEN u.field = 'caredox_enrollment_status' AND u.value IN ('rejected','') THEN -1
-        --WHEN u.field = 'caredox_immunization_status' AND u.value IN ('Valid', 'N/A') THEN 1        
-        --WHEN u.field = 'caredox_immunization_status' AND (u.value LIKE 'Not Valid%' OR u.value = '') THEN -1        
-        --WHEN u.field = 'caredox_medication_status' AND u.value != '' THEN 0
-        --WHEN u.field = 'caredox_screenings_status' AND u.value = 'compliant' THEN 1
-        --WHEN u.field = 'caredox_screenings_status' AND u.value = '' THEN -1
+        WHEN u.field = 'region_city' AND u.[value] NOT IN ('TEAMNewark', 'KCNACamden', 'KMSMiami') THEN 0
+        WHEN u.field = 'region_city' AND u.[value] IN ('TEAMNewark', 'KCNACamden', 'KMSMiami') THEN 1
         WHEN u.field = 'iep_registration_followup_required' AND a.iep_registration_followup_complete = 'Y' THEN 1
-        WHEN u.field = 'iep_registration_followup_required' AND u.value = '1' THEN 0
-        WHEN u.field = 'iep_registration_followup_complete' AND u.value = 'Y' THEN 1
-        WHEN u.field = 'iep_registration_followup_complete' AND u.value = 'N' THEN -1
+        WHEN u.field = 'iep_registration_followup_required' AND u.[value] = '1' THEN 0
+        WHEN u.field = 'iep_registration_followup_complete' AND u.[value] = 'Y' THEN 1
+        WHEN u.field = 'iep_registration_followup_complete' AND u.[value] = 'N' THEN -1
         WHEN u.field = 'lep_registration_followup_required' AND a.lep_registration_followup_complete = 'Y' THEN 1
-        WHEN u.field = 'lep_registration_followup_required' AND u.value = '1' THEN 0
-        WHEN u.field = 'lep_registration_followup_complete' AND u.value = 'Y' THEN 1
-        WHEN u.field = 'lep_registration_followup_complete' AND u.value = 'N' THEN -1
-        WHEN u.field = 'lunch_app_status' AND u.value NOT IN ('No Application', '') THEN 1
-        WHEN u.field = 'lunch_app_status' AND u.value IN ('No Application', '') THEN -1
-        WHEN u.field = 'lunch_balance' AND CONVERT(MONEY,u.value) > 0 THEN 1
-        WHEN u.field = 'lunch_balance' AND CONVERT(MONEY,u.value) = 0 THEN 0
-        WHEN u.field = 'lunch_balance' AND CONVERT(MONEY,u.value) < 0 THEN -1
-        WHEN u.field = 'birth_certificate_proof' AND u.value NOT IN ('','N') THEN 1
-        WHEN u.field = 'birth_certificate_proof' AND u.value IN ('','N') THEN -1
-        WHEN u.field = 'residency_proof_1' AND u.value NOT IN ('','Missing') THEN 1
-        WHEN u.field = 'residency_proof_1' AND u.value IN ('','Missing') THEN -1
-        WHEN u.field = 'residency_proof_2' AND u.value NOT IN ('','Missing') THEN 1
-        WHEN u.field = 'residency_proof_2' AND u.value IN ('','Missing') THEN -1
-        WHEN u.field = 'residency_proof_3' AND u.value NOT IN ('','Missing') THEN 1
-        WHEN u.field = 'residency_proof_3' AND u.value IN ('','Missing') THEN -1
-        WHEN u.field = 'residency_proof_all' AND u.value = 'Y' THEN 1
-        WHEN u.field = 'residency_proof_all' AND u.value = 'N' THEN -1
-        WHEN u.field = 'residency_verification_scanned' AND u.value = 'Y' THEN 1
-        WHEN u.field = 'residency_verification_scanned' AND u.value IN ('','N') THEN -1
-        WHEN u.field = 'residency_verification_approved' AND u.value = 'Yes' THEN 1
-        WHEN u.field = 'residency_verification_approved' AND u.value = 'No' THEN 0
-        WHEN u.field = 'residency_verification_approved' AND u.value = '' THEN -1
+        WHEN u.field = 'lep_registration_followup_required' AND u.[value] = '1' THEN 0
+        WHEN u.field = 'lep_registration_followup_complete' AND u.[value] = 'Y' THEN 1
+        WHEN u.field = 'lep_registration_followup_complete' AND u.[value] = 'N' THEN -1
+        WHEN u.field = 'lunch_app_status' AND u.[value] NOT IN ('No Application', '') THEN 1
+        WHEN u.field = 'lunch_app_status' AND u.[value] IN ('No Application', '') THEN -1
+        WHEN u.field = 'lunch_balance' AND CONVERT(MONEY, u.[value]) > 0 THEN 1
+        WHEN u.field = 'lunch_balance' AND CONVERT(MONEY, u.[value]) = 0 THEN 0
+        WHEN u.field = 'lunch_balance' AND CONVERT(MONEY, u.[value]) < 0 THEN -1
+        WHEN u.field = 'birth_certificate_proof' AND u.[value] NOT IN ('','N') THEN 1
+        WHEN u.field = 'birth_certificate_proof' AND u.[value] IN ('','N') THEN -1
+        WHEN u.field = 'residency_proof_1' AND u.[value] NOT IN ('','Missing') THEN 1
+        WHEN u.field = 'residency_proof_1' AND u.[value] IN ('','Missing') THEN -1
+        WHEN u.field = 'residency_proof_2' AND u.[value] NOT IN ('','Missing') THEN 1
+        WHEN u.field = 'residency_proof_2' AND u.[value] IN ('','Missing') THEN -1
+        WHEN u.field = 'residency_proof_3' AND u.[value] NOT IN ('','Missing') THEN 1
+        WHEN u.field = 'residency_proof_3' AND u.[value] IN ('','Missing') THEN -1
+        WHEN u.field = 'residency_proof_all' AND u.[value] = 'Y' THEN 1
+        WHEN u.field = 'residency_proof_all' AND u.[value] = 'N' THEN -1
        END AS audit_status
 FROM all_data a
 JOIN unpivoted u
