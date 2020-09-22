a1eea78267534fef96e0bf8ebed1907d61f6bc63
Charlie Bini <cbini87@gmail.com>
2020-09-22 15:11:34 -0500

    expand users included, renlearn cribs off of clever

--- extracts/v_extracts.clever_staff.sql
+++ extracts/v_extracts.clever_staff.sql
@@ -24,11 +24,11 @@ LEFT JOIN gabby.people.campus_crosswalk ccw
  AND ccw.is_pathways = 0
 WHERE df.[status] NOT IN ('TERMINATED', 'PRESTART')
   AND df.primary_on_site_department NOT IN ('Data', 'Teaching and Learning')
-  AND COALESCE(ccw.ps_school_id, df.primary_site_schoolid) != 0
+  AND COALESCE(ccw.ps_school_id, df.primary_site_schoolid) <> 0
 
 UNION ALL
 
-/* T&L/Data to all schools under CMO */
+/* T&L/EDs/Data to all schools under CMO */
 SELECT CONVERT(VARCHAR(25), sch.school_number) AS [School_id]
       ,df.ps_teachernumber AS [Staff_id]
       ,df.userprincipalname AS [Staff_email]
@@ -44,38 +44,8 @@ JOIN gabby.powerschool.schools sch
   ON sch.state_excludefromreporting = 0
 WHERE df.[status] NOT IN ('TERMINATED', 'PRESTART')
   AND (df.primary_on_site_department IN ('Data', 'Teaching and Learning') 
-        OR df.ps_teachernumber IN ('CMMCYBMTL', '91TKT1L73')) /* special exception for FW/SA */
-
-UNION ALL
-
-/* ad hoc drivel */
-SELECT '73253' AS [School_id]
-      ,'data_test' AS [Staff_id]
-      ,'data_test@kippnj.org' AS [Staff_email]
-      ,'Demo_Test' AS [First_name]
-      ,'Data_Test' AS [Last_name]
-      ,NULL AS [Department]
-      ,'School Admin' AS [Title]
-      ,'data_test' AS [Username]
-      ,NULL AS [Password]
-      ,'School Tech Lead' AS [Role]
-
-UNION ALL
-
-/* all Newark MS */
-SELECT s.school_number AS School_id
-      ,'100107' AS Staff_id
-      ,'lcooke@kippnj.org' AS Staff_email
-      ,'Lauren' AS First_name
-      ,'Cooke' AS Last_name
-      ,'Math' AS Department
-      ,'School Admin' AS Title
-      ,'lcooke' AS Username
-      ,NULL AS [Password]
-      ,NULL AS [Role]
-FROM kippnewark.powerschool.schools s
-WHERE s.high_grade = 8
-  AND s.school_number != 732510 /* already assigned by default */
+        OR df.primary_job = 'Executive Director'
+        OR df.ps_teachernumber = 'CMMCYBMTL') /* special exception for FW */
 
 UNION ALL
 
@@ -91,7 +61,37 @@ SELECT CONVERT(VARCHAR(25), sch.school_number) AS [School_id]
       ,NULL AS [Password]
       ,CASE WHEN df.primary_on_site_department = 'Operations' THEN 'School Tech Lead' END AS [Role]
 FROM gabby.people.staff_crosswalk_static df
-JOIN kippnewark.powerschool.schools sch
-  ON sch.state_excludefromreporting = 0
+JOIN gabby.powerschool.schools sch
+  ON df.[db_name] = sch.[db_name]
+ AND sch.state_excludefromreporting = 0
 WHERE df.[status] NOT IN ('TERMINATED', 'PRESTART')
-  AND df.ps_teachernumber IN ('50001', 'DMCYRU7B2', '445')
+  AND (df.primary_job IN ('Assistant Superintendent', 'Head of Schools')
+         OR df.ps_teachernumber = '445') /* exception for AR */
+
+UNION ALL
+
+/* ad hoc drivel */
+SELECT s.school_number AS School_id
+      ,'100107' AS Staff_id
+      ,'lcooke@kippnj.org' AS Staff_email
+      ,'Lauren' AS First_name
+      ,'Cooke' AS Last_name
+      ,'Math' AS Department
+      ,'School Admin' AS Title
+      ,'lcooke' AS Username
+      ,NULL AS [Password]
+      ,NULL AS [Role]
+FROM kippnewark.powerschool.schools s
+WHERE s.high_grade = 8
+  AND s.school_number <> 732510 /* already assigned by default */
+UNION ALL
+SELECT '73253' AS [School_id]
+      ,'data_test' AS [Staff_id]
+      ,'data_test@kippnj.org' AS [Staff_email]
+      ,'Demo_Test' AS [First_name]
+      ,'Data_Test' AS [Last_name]
+      ,NULL AS [Department]
+      ,'School Admin' AS [Title]
+      ,'data_test' AS [Username]
+      ,NULL AS [Password]
+      ,'School Tech Lead' AS [Role]
--- extracts/v_extracts.renlearn_teachers.sql
+++ extracts/v_extracts.renlearn_teachers.sql
@@ -3,18 +3,12 @@ GO
 
 CREATE OR ALTER VIEW extracts.renlearn_teachers AS
 
-SELECT scw.ps_teachernumber AS id
-      ,scw.ps_teachernumber AS teachernumber
-      ,COALESCE(ccw.ps_school_id, scw.primary_site_schoolid) AS schoolid
-      ,scw.preferred_last_name AS last_name
-      ,scw.preferred_first_name AS first_name
+SELECT Staff_id AS id
+      ,Staff_id AS teachernumber
+      ,School_id AS schoolid
+      ,Last_name AS last_name
+      ,First_name AS first_name
       ,NULL AS middle_name
-      ,scw.samaccountname AS teacherloginid
-      ,scw.userprincipalname AS staff_email
-FROM gabby.people.staff_crosswalk_static scw
-LEFT JOIN gabby.people.campus_crosswalk ccw
-  ON scw.primary_site = ccw.campus_name
- AND ccw.is_pathways = 0
- AND ccw._fivetran_deleted = 0
-WHERE scw.[status] != 'TERMINATED'
-  AND COALESCE(ccw.ps_school_id, scw.primary_site_schoolid) != 0
+      ,Username AS teacherloginid
+      ,Staff_email AS staff_email
+FROM gabby.extracts.clever_staff
