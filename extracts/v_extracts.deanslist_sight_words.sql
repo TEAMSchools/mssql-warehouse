53bd7ab04febc9c7b205171cf66230ebb600d5c2
Charlie Bini <cbini87@gmail.com>
2020-11-12 08:52:36 -0600

    static swq

--- extracts/v_extracts.deanslist_sight_words.sql
+++ extracts/v_extracts.deanslist_sight_words.sql
@@ -5,7 +5,7 @@ CREATE OR ALTER VIEW extracts.deanslist_sight_words AS
 
 SELECT swd.local_student_id AS student_number
       ,swd.date_administered
-      ,swd.label AS word
+      ,swd.[label] AS word
       ,CASE 
         WHEN swd.[value] = 'yes' THEN 'Mastered'
         WHEN swd.[value] = 'no' THEN 'Not Mastered'
@@ -18,9 +18,10 @@ SELECT swd.local_student_id AS student_number
        END AS is_mastery
 
       ,rt.alt_name AS term_name
-FROM gabby.illuminate_dna_repositories.sight_words_data swd
+FROM gabby.illuminate_dna_repositories.sight_words_data_static swd
 JOIN gabby.reporting.reporting_terms rt
   ON swd.date_administered BETWEEN rt.[start_date] AND rt.end_date
  AND rt.identifier = 'RT'
  AND rt.schoolid = 0
  AND rt._fivetran_deleted = 0
+ AND rt.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
