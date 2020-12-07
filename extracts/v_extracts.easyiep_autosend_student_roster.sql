d61abb7622a630f515aafec6c6999e487f5dbd88
Charlie Bini <cbini87@gmail.com>
2020-12-07 15:29:36 -0600

    use specprog_name

--- extracts/v_extracts.easyiep_autosend_student_roster.sql
+++ extracts/v_extracts.easyiep_autosend_student_roster.sql
@@ -21,18 +21,18 @@ SELECT s.state_studentnumber
       ,CASE
         WHEN sp.programid = 5173 THEN nj.schoolcodeattending
         ELSE CONCAT(s.schoolid, sp.programid) 
-       END AS schoolid      
+       END AS schoolid
       ,nj.referral_date
 FROM gabby.powerschool.students s
 LEFT JOIN gabby.powerschool.studentcorefields scf
   ON s.dcid = scf.studentsdcid
- AND s.db_name = scf.db_name
-LEFT JOIN gabby.powerschool.spenrollments_gen sp
+ AND s.[db_name] = scf.[db_name]
+LEFT JOIN gabby.powerschool.spenrollments_gen_static sp
   ON s.id = sp.studentid
  AND s.entrydate BETWEEN sp.enter_date AND sp.exit_date
- AND s.db_name = sp.db_name
- AND sp.programid IN (4573, 5074, 5075, 5173)  
+ AND s.[db_name] = sp.[db_name]
+ AND sp.specprog_name IN ('Out of District', 'Pathways ES', 'Pathways MS', 'Whittier ES')
 LEFT JOIN gabby.powerschool.s_nj_stu_x nj
   ON s.dcid = nj.studentsdcid
- AND s.db_name = nj.db_name
+ AND s.[db_name] = nj.[db_name]
 WHERE s.enroll_status = 0
