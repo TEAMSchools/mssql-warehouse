6f610307347beb866b38b1fa01230086d6af6936
Charlie Bini <cbini87@gmail.com>
2020-12-07 15:30:21 -0600

    :lipstick:

--- tableau/v_tableau.attendance_chronic_absenteeism_log.sql
+++ tableau/v_tableau.attendance_chronic_absenteeism_log.sql
@@ -4,22 +4,22 @@ GO
 CREATE OR ALTER VIEW tableau.attendance_chronic_absenteeism_log AS
 
 WITH commlog AS (
-  SELECT CONVERT(INT,c.student_school_id) AS student_school_id
+  SELECT c.student_school_id
         ,c.reason AS commlog_reason
         ,c.response AS commlog_notes        
         ,c.call_topic AS commlog_topic
-        ,CONVERT(DATE,c.call_date_time) AS commlog_date
-        
+        ,CONVERT(DATE, c.call_date_time) AS commlog_date
+
         ,CONCAT(u.first_name, ' ', u.last_name) AS commlog_staff_name
-        
+
         ,f.init_notes AS followup_init_notes
         ,f.followup_notes AS followup_close_notes
         ,f.outstanding AS followup_outstanding
-        ,CONCAT(f.c_first, ' ', f.c_last) AS followup_staff_name      
+        ,CONCAT(f.c_first, ' ', f.c_last) AS followup_staff_name
   FROM gabby.deanslist.communication c
   JOIN gabby.deanslist.users u
     ON c.dluser_id = u.dluser_id
-  LEFT OUTER JOIN gabby.deanslist.followups f
+  LEFT JOIN gabby.deanslist.followups f
     ON c.followup_id = f.followup_id
   WHERE c.reason LIKE 'chronic%'
  )
@@ -48,41 +48,40 @@ FROM
            ,co.grade_level
            ,co.team
            ,co.region
-           ,co.reporting_schoolid      
-           
+           ,co.reporting_schoolid
+
            ,CASE
-             WHEN att.schoolid = 73253 THEN co.advisor_name
+             WHEN co.schoolid = 73253 THEN co.advisor_name
              ELSE cc.section_number
             END AS homeroom
 
            ,COUNT(att.id) AS n_absences
-     FROM gabby.powerschool.attendance_clean att
+     FROM gabby.powerschool.attendance_clean_current_static att
      JOIN gabby.powerschool.attendance_code ac
        ON att.attendance_codeid = ac.id
-      AND att.db_name = ac.db_name
+      AND att.[db_name] = ac.[db_name]
       AND ac.att_code IN ('A','AD')
-     LEFT OUTER JOIN gabby.powerschool.cc
+     LEFT JOIN gabby.powerschool.cc
        ON att.studentid = cc.studentid
-      AND att.db_name = cc.db_name
-      AND CONVERT(DATE,GETDATE()) BETWEEN cc.dateenrolled AND cc.dateleft
+      AND att.[db_name] = cc.[db_name]
+      AND CONVERT(DATE, GETDATE()) BETWEEN cc.dateenrolled AND cc.dateleft
       AND cc.course_number = 'HR'
      JOIN gabby.powerschool.cohort_identifiers_static co
        ON att.studentid = co.studentid
-      AND att.db_name = co.db_name
+      AND att.[db_name] = co.[db_name]
       AND att.att_date BETWEEN co.entrydate AND co.exitdate
       AND co.enroll_status = 0
-     WHERE att.att_date >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 07, 01)
-       AND att.att_mode_code = 'ATT_ModeDaily'
+     WHERE att.att_mode_code = 'ATT_ModeDaily'
      GROUP BY co.student_number
              ,co.lastfirst
              ,co.grade_level
              ,co.team
              ,co.region
-             ,co.reporting_schoolid                 
+             ,co.reporting_schoolid
              ,CASE
-               WHEN att.schoolid = 73253 THEN co.advisor_name
+               WHEN co.schoolid = 73253 THEN co.advisor_name
                ELSE cc.section_number
               END
     ) sub
-LEFT OUTER JOIN commlog cl
+LEFT JOIN commlog cl
   ON sub.student_number = cl.student_school_id
