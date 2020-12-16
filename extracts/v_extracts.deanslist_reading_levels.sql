beffefdeb2c343cf5725e7728e6ad8535f0c189b
Charlie Bini <cbini87@gmail.com>
2020-12-16 11:19:57 -0600

    add ytd lvl growth

--- extracts/v_extracts.deanslist_reading_levels.sql
+++ extracts/v_extracts.deanslist_reading_levels.sql
@@ -11,6 +11,7 @@ SELECT student_number
       ,lvl_num AS read_lvl_num
       ,goal_num AS goal_lvl_num
       ,goal_status AS met_goal
+      ,ISNULL(lvl_num - MAX(CASE WHEN rn_round_asc = 1 THEN lvl_num END) OVER(PARTITION BY student_number, academic_year), 0) AS lvl_growth_ytd
 FROM gabby.lit.achieved_by_round_static
 WHERE academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()  
   AND achv_unique_id LIKE 'FPBAS%'
