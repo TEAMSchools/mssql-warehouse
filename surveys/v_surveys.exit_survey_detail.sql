8d8860d5067d5bbbad97de6e66b346315121a644
Charlie Bini <cbini87@gmail.com>
2020-12-22 12:40:12 -0600

    :lipstick:

--- surveys/v_surveys.exit_survey_detail.sql
+++ surveys/v_surveys.exit_survey_detail.sql
@@ -15,29 +15,29 @@ SELECT df.df_employee_number
       ,df.is_manager AS is_management
       ,df.gender
       ,df.original_hire_date AS hire_date
-      ,df.status AS position_status      
-      ,df.address
+      ,df.[status] AS position_status
+      ,df.[address]
       ,df.city AS primary_address_city
-      ,df.state AS primary_address_state_territory_code
+      ,df.[state] AS primary_address_state_territory_code
       ,df.postal_code AS primary_address_zip_postal_code
-      ,df.primary_site AS location
+      ,df.primary_site AS [location]
       ,df.manager_name AS manager
       ,MONTH(df.original_hire_date) AS hire_month
       ,COALESCE(YEAR(df.termination_date), YEAR(GETDATE())) - YEAR(df.original_hire_date) + 1 AS years_at_kipp
       ,gabby.utilities.DATE_TO_SY(df.termination_date) AS termination_academic_year
-	
-      ,es.timestamp AS exit_survey_date
+
+      ,es.[timestamp] AS exit_survey_date
       ,es.q_1 AS exit_survey_title
       ,es.q_2 AS exit_survey_location
       ,es.q_3 AS nps
       ,es.q_4 AS nps_explanation
-      ,es.q_5	AS voluntary_termination
+      ,es.q_5 AS voluntary_termination
       ,es.q_6 AS exit_survey_reason_for_leaving
       ,es.q_7 AS next_role
       ,es.q_8 AS next_title
       ,es.q_9 AS expectation_alignment
       ,es.q_10 AS open_comment
-      ,es.q_11 AS anonymous
+      ,es.q_11 AS [anonymous]
       ,es.q_12 AS consider_kipp_in_future
       ,es.q_13 AS what_does_kipp_do_well
       ,es.q_14 AS how_can_kipp_improve
@@ -51,7 +51,7 @@ SELECT df.df_employee_number
       ,es.q_22 AS rating_improvement
       ,es.q_23 AS rating_freedom
       ,es.q_24 AS rating_fun
-      ,es.q_25 AS rating_teamwork        
+      ,es.q_25 AS rating_teamwork
 FROM gabby.surveys.exit_survey es
 LEFT JOIN gabby.people.staff_crosswalk_static df
   ON es.df_id = df.df_employee_number
