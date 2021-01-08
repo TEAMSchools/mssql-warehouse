b10fce2bc4083bb57b0768bbf05b1d37f1110a66
Charlie Bini <cbini87@gmail.com>
2021-01-08 09:57:24 -0600

    add temps

--- extracts/v_extracts.parentsquare_staff.sql
+++ extracts/v_extracts.parentsquare_staff.sql
@@ -21,3 +21,27 @@ SELECT df.ps_teachernumber AS [staff_id]
 FROM gabby.people.staff_crosswalk_static df
 WHERE df.[status] NOT IN ('TERMINATED', 'PRESTART')
   AND df.mail NOT LIKE '%kippmiami.org'
+
+UNION
+
+SELECT ad.employeeid AS [staff_id]
+      ,ad.cn AS [first_name]
+      ,ad.givenname AS [last_name]
+      ,ad.userprincipalname AS [login]
+      ,ad.mail AS [email]
+      ,ad.mobile AS [mobile]
+      ,ad.title AS [title]
+      ,CASE 
+        WHEN ad.physicaldeliveryofficename = 'Room 9 - 60 Park Pl' THEN 1000
+        WHEN ad.physicaldeliveryofficename IN ('Room 10 - 121 Market St', 'Room 10 - 740 Chestnut St') THEN 1001
+        WHEN ad.physicaldeliveryofficename = '18th Ave Campus' THEN 1002
+        WHEN ad.physicaldeliveryofficename = 'KIPP Lanning Sq Campus' THEN 1003
+        WHEN ad.physicaldeliveryofficename = 'Norfolk St Campus' THEN 1004
+        ELSE scw.ps_school_id
+       END AS [school_id]
+FROM gabby.adsi.user_attributes_static ad
+JOIN gabby.people.school_crosswalk scw
+  ON ad.physicaldeliveryofficename = scw.site_name
+WHERE ad.is_active = 1
+  AND ad.mail NOT LIKE '%kippmiami.org'
+  AND ad.employeeid LIKE 'TMP%'
