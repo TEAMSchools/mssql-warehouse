7f7fd5ff5ccde0389536ba71b73379b089726616
Charlie Bini <cbini87@gmail.com>
2021-01-25 10:45:42 -0600

    fix data type

--- extracts/v_extracts.whetstone_users.sql
+++ extracts/v_extracts.whetstone_users.sql
@@ -17,19 +17,19 @@ SELECT CONVERT(VARCHAR(25), scw.df_employee_number) AS user_internal_id
       ,CASE WHEN scw.[status] = 'TERMINATED' THEN 1 ELSE 0 END AS inactive
       ,scw.google_email AS user_email
       ,CASE
-        WHEN scw.grades_taught = 'Grade 10' THEN '10th grade'
-        WHEN scw.grades_taught = 'Grade 11' THEN '11th grade'
-        WHEN scw.grades_taught = 'Grade 12' THEN '12th grade'
-        WHEN scw.grades_taught = 'Grade 1' THEN '1st grade'
-        WHEN scw.grades_taught = 'Grade 2' THEN '2nd grade'
-        WHEN scw.grades_taught = 'Grade 3' THEN '3rd grade'
-        WHEN scw.grades_taught = 'Grade 4' THEN '4th grade'
-        WHEN scw.grades_taught = 'Grade 5' THEN '5th grade'
-        WHEN scw.grades_taught = 'Grade 6' THEN '6th grade'
-        WHEN scw.grades_taught = 'Grade 7' THEN '7th grade'
-        WHEN scw.grades_taught = 'Grade 8' THEN '8th grade'
-        WHEN scw.grades_taught = 'Grade 9' THEN '9th grade'
-        WHEN scw.grades_taught = 'Kindergarten' THEN 'Kindergarten'
+        WHEN scw.grades_taught = 10 THEN '10th grade'
+        WHEN scw.grades_taught = 11 THEN '11th grade'
+        WHEN scw.grades_taught = 12 THEN '12th grade'
+        WHEN scw.grades_taught = 1 THEN '1st grade'
+        WHEN scw.grades_taught = 2 THEN '2nd grade'
+        WHEN scw.grades_taught = 3 THEN '3rd grade'
+        WHEN scw.grades_taught = 4 THEN '4th grade'
+        WHEN scw.grades_taught = 5 THEN '5th grade'
+        WHEN scw.grades_taught = 6 THEN '6th grade'
+        WHEN scw.grades_taught = 7 THEN '7th grade'
+        WHEN scw.grades_taught = 8 THEN '8th grade'
+        WHEN scw.grades_taught = 9 THEN '9th grade'
+        WHEN scw.grades_taught = 0 THEN 'Kindergarten'
        END AS grade_name
       ,CASE
         WHEN m.manager_df_employee_number IS NOT NULL THEN 'observers'
