d2d4f04c959cfd46f63d073c4b0f627b38540af4
Charlie Bini <cbini87@gmail.com>
2021-02-05 16:12:04 -0600

    cleaned up and fixing any overlaps

--- people/v_people.manager_history_clean.sql
+++ people/v_people.manager_history_clean.sql
@@ -3,24 +3,6 @@ GO
 
 CREATE OR ALTER VIEW people.manager_history_clean AS
 
-WITH adp_manager_history_clean AS (
-
-     SELECT mh.associate_id
-           ,mh.position_id
-           ,mh.reports_to_associate_id
-           ,CASE 
-             WHEN mh.reports_to_effective_date < CONVERT(DATE, '2021-01-01') THEN CONVERT(DATE, '2021-01-01') ELSE CONVERT(DATE,mh.reports_to_effective_date)
-            END AS reports_to_effective_date           
-           
-           ,CONVERT(DATE, mh.reports_to_effective_end_date) AS reports_to_effective_end_date
-
-           ,sr.file_number AS employee_number
-     FROM gabby.adp.manager_history mh
-     JOIN gabby.adp.employees_all sr
-       ON mh.associate_id = sr.associate_id
-     WHERE mh.reports_to_associate_id IS NOT NULL
-  )
-
 SELECT sub.employee_number
       ,sub.associate_id
       ,sub.position_id
@@ -39,26 +21,48 @@ SELECT sub.employee_number
                                ELSE gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1
                               END, 6, 30)) AS reports_to_effective_end_date_eoy
 FROM
-    (SELECT *
-     FROM adp_manager_history_clean
-     WHERE CONVERT(DATE, reports_to_effective_date) < CONVERT(DATE, reports_to_effective_end_date)
-       OR CONVERT(DATE, reports_to_effective_end_date) IS NULL
+    (
+     SELECT mh.associate_id
+           ,mh.position_id
+           ,mh.reports_to_associate_id
+           ,CASE 
+             WHEN mh.reports_to_effective_date > '2021-01-01' THEN CONVERT(DATE, mh.reports_to_effective_date)
+             ELSE '2021-01-01'
+            END AS reports_to_effective_date
+           ,CONVERT(DATE, mh.reports_to_effective_end_date) AS reports_to_effective_end_date
+
+           ,sr.file_number AS employee_number
+     FROM gabby.adp.manager_history mh
+     JOIN gabby.adp.employees_all sr
+       ON mh.associate_id = sr.associate_id
+     WHERE '2021-01-01' BETWEEN CONVERT(DATE, mh.reports_to_effective_date) AND COALESCE(CONVERT(DATE, mh.reports_to_effective_end_date), GETDATE())
 
      UNION ALL
 
-     SELECT sre.associate_id AS associate_id
+     SELECT sub.associate_id
            ,NULL AS position_id
-           ,srm.associate_id AS reports_to_associate_id
-           ,CONVERT(DATE, dm.manager_effective_start) AS reports_to_effective_date
-           ,CASE 
-             WHEN CONVERT(DATE,dm.manager_effective_end) > CONVERT(DATE,'2020-12-31') THEN CONVERT(DATE,'2020-12-31') ELSE COALESCE(CONVERT(DATE,dm.manager_effective_end),CONVERT(DATE,'2020-12-31'))
-            END AS reports_to_effective_end_date
-           ,dm.employee_reference_code AS employee_number
-     FROM gabby.dayforce.employee_manager dm
-     JOIN gabby.adp.employees_all sre
-       ON dm.employee_reference_code = sre.file_number
-     LEFT JOIN gabby.adp.employees_all srm
-       ON dm.manager_employee_number = srm.file_number
-     WHERE (CONVERT(DATE, dm.manager_effective_start) < CONVERT(DATE, dm.manager_effective_end)
-              OR dm.manager_effective_end IS NULL)
+           ,sub.reports_to_associate_id
+           ,sub.reports_to_effective_date
+           ,sub.reports_to_effective_end_date
+           ,sub.employee_number
+     FROM
+         (
+          SELECT sre.associate_id AS associate_id
+                ,srm.associate_id AS reports_to_associate_id
+                ,CONVERT(DATE, dm.manager_effective_start) AS reports_to_effective_date
+                ,CASE 
+                  WHEN CONVERT(DATE,dm.manager_effective_end) > '2020-12-31' THEN '2020-12-31'
+                  ELSE COALESCE(CONVERT(DATE,dm.manager_effective_end), '2020-12-31')
+                 END AS reports_to_effective_end_date
+                ,dm.employee_reference_code AS employee_number
+                ,ROW_NUMBER() OVER(PARTITION BY dm.employee_reference_code, dm.manager_effective_start ORDER BY COALESCE(CONVERT(DATE,dm.manager_effective_end), '2020-12-31') DESC) AS rn
+          FROM gabby.dayforce.employee_manager dm
+          JOIN gabby.adp.employees_all sre
+            ON dm.employee_reference_code = sre.file_number
+          LEFT JOIN gabby.adp.employees_all srm
+            ON dm.manager_employee_number = srm.file_number
+          WHERE dm.manager_derived_method = 'Direct Report'
+            AND CONVERT(DATE, dm.manager_effective_start) <= '2020-12-31'
+         ) sub
+     WHERE sub.rn = 1
     ) sub
--- people/v_people.salary_history_clean.sql
+++ people/v_people.salary_history_clean.sql
@@ -3,24 +3,6 @@ GO
 
 CREATE OR ALTER VIEW people.salary_history_clean AS
 
-WITH adp_salary_history_clean AS (
-     SELECT sh.associate_id
-           ,sh.position_id
-           ,CASE 
-             WHEN sh.regular_pay_effective_date < CONVERT(DATE, '2021-01-01') THEN CONVERT(DATE, '2021-01-01') ELSE CONVERT(DATE,sh.regular_pay_effective_date)
-            END AS regular_pay_effective_date
-            ,CONVERT(DATE, sh.regular_pay_effective_end_date) AS regular_pay_effective_end_date
-           ,CONVERT(MONEY, sh.annual_salary) AS annual_salary
-           ,CONVERT(MONEY, sh.regular_pay_rate_amount) AS regular_pay_rate_amount
-           ,sh.compensation_change_reason_description
-
-           ,sr.file_number AS employee_number
-     FROM gabby.adp.salary_history sh
-     JOIN gabby.adp.employees_all sr
-       ON sh.associate_id = sr.associate_id
-     WHERE (CONVERT(DATE, sh.regular_pay_effective_date) < CONVERT(DATE, sh.regular_pay_effective_end_date) OR sh.regular_pay_effective_end_date IS NULL)
-  )
-
 SELECT sub.employee_number
       ,sub.associate_id
       ,sub.position_id
@@ -41,19 +23,35 @@ SELECT sub.employee_number
                                ELSE gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1
                               END, 6, 30)) AS regular_pay_effective_end_date_eoy
 FROM
-    (SELECT *
-     FROM adp_salary_history_clean
-     WHERE CONVERT(DATE, regular_pay_effective_date) < CONVERT(DATE, regular_pay_effective_end_date)
-        OR CONVERT(DATE, regular_pay_effective_end_date) IS NULL
-     
+    (
+     SELECT sh.associate_id
+           ,sh.position_id
+           ,CASE 
+             WHEN sh.regular_pay_effective_date > '2021-01-01' THEN CONVERT(DATE, sh.regular_pay_effective_date)
+             ELSE '2021-01-01'
+            END AS regular_pay_effective_date
+           ,CONVERT(DATE, sh.regular_pay_effective_end_date) AS regular_pay_effective_end_date
+           ,CONVERT(MONEY, sh.annual_salary) AS annual_salary
+           ,CONVERT(MONEY, sh.regular_pay_rate_amount) AS regular_pay_rate_amount
+           ,sh.compensation_change_reason_description
+
+           ,sr.file_number AS employee_number
+     FROM gabby.adp.salary_history sh
+     JOIN gabby.adp.employees_all sr
+       ON sh.associate_id = sr.associate_id
+     WHERE (CONVERT(DATE, sh.regular_pay_effective_date) < CONVERT(DATE, sh.regular_pay_effective_end_date) OR sh.regular_pay_effective_end_date IS NULL)
+       AND '2021-01-01' BETWEEN CONVERT(DATE, sh.regular_pay_effective_date) AND COALESCE(CONVERT(DATE, sh.regular_pay_effective_end_date), GETDATE())
+
      UNION ALL
-     
+
      SELECT sr.associate_id
            ,NULL AS position_id
-           ,CONVERT(DATE,ds.effective_start) AS regular_pay_effective_date
-           ,CASE 
-             WHEN CONVERT(DATE,ds.effective_end) > CONVERT(DATE,'2020-12-31') THEN CONVERT(DATE,'2020-12-31') ELSE COALESCE(CONVERT(DATE, ds.effective_end),CONVERT(DATE,'2020-12-31'))
-            END AS regular_pay_effective_end_date
+           ,CONVERT(DATE, ds.effective_start) AS regular_pay_effective_date
+           ,COALESCE(CASE 
+                      WHEN CONVERT(DATE, ds.effective_end) < '2020-12-31' THEN CONVERT(DATE, ds.effective_end)
+                      ELSE '2020-12-31'
+                     END
+                    ,'2020-12-31') AS regular_pay_effective_end_date
            ,CONVERT(MONEY, ds.base_salary) AS annual_salary
            ,NULL AS regular_pay_rate_amount
            ,ds.status_reason_description AS compensation_change_reason_description
@@ -62,8 +60,6 @@ FROM
      FROM gabby.dayforce.employee_status ds
      JOIN gabby.adp.employees_all sr
        ON ds.number = sr.file_number
-     WHERE (CONVERT(DATE, ds.effective_start) < CONVERT(DATE, ds.effective_end)
-              OR ds.effective_end IS NULL)
-
+     WHERE CONVERT(DATE, ds.effective_start) <= '2020-12-31'
     ) sub
 WHERE sub.annual_salary > 0
