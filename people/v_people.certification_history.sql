13bf079c51f003557c3437158f67f453462bf910
Kevin Verhoff <kevin.verhoff@gmail.com>
2021-02-17 13:42:02 -0500

    Create v_people.certification_history.sql
    Creating a view for certifications that eventually we'll be able to feed Florida (and possibly other states') info into

---
+++ people/v_people.certification_history.sql
@@ -0,0 +1,68 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW people.certification_history AS
+
+WITH certs AS (
+  SELECT c.df_employee_number AS employee_number
+        ,c.seq_number
+        ,c.certificate_id
+        ,c.certificate_type
+        ,c.is_charter_school_only
+        ,c.endorsement AS endorsement_or_rank
+        ,c.county_code
+        ,c.basis_code
+        ,c.district_code
+        ,c.month_year_issued
+        ,c.issued_date
+        ,c.month_year_expiration
+        ,c.expiration_date
+        ,NULL AS cert_status
+        ,CASE 
+          WHEN GETDATE() BETWEEN c.issued_date AND COALESCE(expiration_date,DATEADD(day,1,GETDATE())) THEN 1
+          ELSE 0
+         END AS valid_cert
+        ,'NJ' AS cert_state
+  FROM njdoe.certification_certificate_history c
+  )
+
+SELECT s.df_employee_number
+      ,s.adp_associate_id
+      ,s.preferred_name
+      ,s.original_hire_date
+      ,s.primary_job
+      ,s.legal_entity_name
+      ,s.[status]
+      ,s.primary_site
+      ,s.userprincipalname
+
+      ,c.seq_number
+      ,c.certificate_id
+      ,c.certificate_type
+      ,c.is_charter_school_only
+      ,c.endorsement_or_rank
+      ,c.county_code
+      ,c.basis_code
+      ,c.district_code
+      ,c.month_year_issued
+      ,c.issued_date
+      ,c.month_year_expiration
+      ,c.expiration_date
+      ,cert_status
+      ,c.valid_cert
+      ,c.cert_state
+
+      ,pss.schoolstate
+
+      ,CASE 
+        WHEN pss.schoolstate = c.cert_state
+        THEN MAX(c.valid_cert) OVER(PARTITION BY s.df_employee_number)
+        ELSE NULL
+       END AS is_certified
+
+FROM people.staff_crosswalk_static s
+
+LEFT JOIN powerschool.schools pss
+  ON pss.school_number = s.primary_site_schoolid
+LEFT JOIN certs c
+  ON s.df_employee_number = c.employee_number
