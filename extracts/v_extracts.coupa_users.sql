9543a8a90ca9fc7c44cf41a5c61e0924ef1af588
Charlie Bini <cbini87@gmail.com>
2021-03-02 13:20:34 -0600

    add school lookup + some cleanup

--- extracts/v_extracts.coupa_users.sql
+++ extracts/v_extracts.coupa_users.sql
@@ -3,64 +3,102 @@ GO
 
 CREATE OR ALTER VIEW extracts.coupa_users AS
 
-SELECT LOWER(scw.samaccountname) AS [Login]
-      ,NULL AS ID -- NULL for new users
-      ,'active' AS [Status]
-      ,'No' AS [Purchasing User]
-      ,'Yes' AS [Expense User]
-      ,'SAML' AS [Authentication Method]
-      ,LOWER(scw.userprincipalname) AS [Sso Identifier]
-      ,'No' AS [Generate Password And Notify User]
-      ,LOWER(scw.mail) AS [Email]
-      ,scw.preferred_first_name AS [First Name]
-      ,scw.preferred_last_name AS [Last Name]
-      ,scw.df_employee_number AS [Employee Number]
-      ,'Expense User' AS [User Role Names]
+SELECT LOWER(sub.samaccountname) AS [Login]
       ,CASE
-        WHEN scw.legal_entity_name = 'KIPP New Jersey' THEN 'KIPP NJ'
-        WHEN scw.legal_entity_name = 'TEAM Academy Charter Schools' THEN 'TEAM'
-        WHEN scw.legal_entity_name = 'KIPP Cooper Norcross Academy' THEN 'KCNA'
-        WHEN scw.legal_entity_name = 'KIPP Miami' THEN 'MIA'
-       END AS [Content Groups]
-      ,CONCAT(scw.preferred_first_name, scw.preferred_last_name ) AS [Mention Name]
-      ,scw.primary_site AS [School Name] -- match on lookup table (content group/department)
-FROM gabby.people.staff_crosswalk_static scw
-LEFT JOIN gabby.coupa.users cu
-  ON scw.df_employee_number = cu.employee_number
-WHERE scw.[status] NOT IN ('Prestart', 'Terminated')
-  AND scw.primary_on_site_department NOT IN ('Interns') -- exclude interns
-  AND cu.employee_number IS NULL
-  AND scw.legal_entity_name IN ('KIPP New Jersey', 'KIPP Cooper Norcross Academy') -- only TEAM/KCNA temporarily
-
-UNION ALL
-
-SELECT LOWER(scw.samaccountname) AS [Login]
-      ,cu.ID
-      ,CASE
-        WHEN scw.[status] = 'Terminated' THEN 'inactive'
+        WHEN sub.[status] = 'Terminated' THEN 'inactive'
         ELSE 'active'
        END AS [Status]
-      ,COALESCE(cu.purchasing_user, 'No') AS [Purchasing User] -- preserve what's in Coupa, otherwise No
+      ,COALESCE(sub.purchasing_license, 'No') AS [Purchasing User] -- preserve Coupa, otherwise No
       ,'Yes' AS [Expense User]
       ,'SAML' AS [Authentication Method]
-      ,LOWER(scw.userprincipalname) AS [Sso Identifier]
+      ,LOWER(sub.userprincipalname) AS [Sso Identifier]
       ,'No' AS [Generate Password And Notify User]
-      ,LOWER(scw.mail) AS [Email]
-      ,scw.preferred_first_name AS [First Name]
-      ,scw.preferred_last_name AS [Last Name]
-      ,scw.df_employee_number AS [Employee Number]
-      ,CONCAT('Expense User, ', cu.user_role_names) AS [User Role Names]
-      ,COALESCE(cu.content_groups
+      ,LOWER(sub.mail) AS [Email]
+      ,sub.preferred_first_name AS [First Name]
+      ,sub.preferred_last_name AS [Last Name]
+      ,sub.employee_number AS [Employee Number]
+      ,CONCAT('Expense User, ', sub.roles) AS [User Role Names]
+      ,COALESCE(sub.content_groups
                ,CASE
-                 WHEN scw.legal_entity_name = 'KIPP New Jersey' THEN 'KIPP NJ'
-                 WHEN scw.legal_entity_name = 'TEAM Academy Charter Schools' THEN 'TEAM'
-                 WHEN scw.legal_entity_name = 'KIPP Cooper Norcross Academy' THEN 'KCNA'
-                 WHEN scw.legal_entity_name = 'KIPP Miami' THEN 'MIA'
-                END) AS [Content Groups] -- preserve what's in Coupa, otherwise use HRIS
-      ,COALESCE(cu.mention_name, CONCAT(scw.preferred_first_name, scw.preferred_last_name )) AS [Mention Name] -- preserve what's in Coupa, otherwise use HRIS
-      ,COALESCE(cu.school_name, scw.primary_site) AS [School Name] -- preserve what's in Coupa, otherwise use match on lookup table (content group/department)
-FROM gabby.people.staff_crosswalk_static scw
-INNER JOIN gabby.coupa.users cu
-  ON scw.df_employee_number = cu.employee_number
-WHERE scw.[status] <> 'Prestart'
-  AND scw.primary_on_site_department NOT IN ('Interns') -- exclude interns
+                 WHEN sub.legal_entity_abbreviation = 'KIPP New Jersey' THEN 'KIPP NJ'
+                 WHEN sub.legal_entity_abbreviation = 'TEAM Academy Charter Schools' THEN 'TEAM'
+                 WHEN sub.legal_entity_abbreviation = 'KIPP Cooper Norcross Academy' THEN 'KCNA'
+                 WHEN sub.legal_entity_abbreviation = 'KIPP Miami' THEN 'MIA'
+                END) AS [Content Groups] -- preserve Coupa, otherwise use HRIS
+      ,COALESCE(sub.mention_name, CONCAT(sub.preferred_first_name, sub.preferred_last_name )) AS [Mention Name] -- preserve Coupa, otherwise use HRIS
+      ,COALESCE(x.coupa_school_name
+               ,CASE
+                 WHEN sn.coupa_school_name = '<Use PhysicalDeliveryOfficeName>' 
+                  AND sub.physicaldeliveryofficename IN ('KIPP Cooper Norcross High (KCNA)', 'KIPP Cooper Norcross High School') 
+                      THEN 'KCNHS'
+                 WHEN sn.coupa_school_name = '<Use PhysicalDeliveryOfficeName>' AND sub.physicaldeliveryofficename = 'KIPP Lanning Square Middle' THEN 'Lanning Square Middle'
+                 WHEN sn.coupa_school_name = '<Use PhysicalDeliveryOfficeName>' AND sub.physicaldeliveryofficename = 'KIPP Lanning Square Primary' THEN 'Lanning Square Primary'
+                 WHEN sn.coupa_school_name = '<Use PhysicalDeliveryOfficeName>' AND sub.physicaldeliveryofficename = 'KIPP Whittier Middle' THEN 'Whittier Middle'
+                 WHEN sn.coupa_school_name = '<Use PhysicalDeliveryOfficeName>' THEN sub.physicaldeliveryofficename
+                 ELSE sn.coupa_school_name
+                END) AS [School Name] -- preserve Coupa, otherwise use match on lookup table (content group/department)
+FROM
+    (
+     /* existing users */
+     SELECT sr.employee_number
+           ,sr.preferred_first_name
+           ,sr.preferred_last_name
+           ,sr.legal_entity_abbreviation
+           ,sr.home_department
+           ,sr.[status]
+
+           ,ad.samaccountname
+           ,ad.userprincipalname
+           ,ad.mail
+           ,ad.physicaldeliveryofficename
+
+           ,cu.content_groups
+           ,cu.school_name
+           ,cu.mention_name
+           ,cu.purchasing_license
+           ,cu.roles
+     FROM gabby.people.staff_roster sr
+     INNER JOIN gabby.adsi.user_attributes_static ad
+       ON sr.employee_number = ad.employeenumber
+      AND ISNUMERIC(ad.employeenumber) = 1
+     INNER JOIN gabby.coupa.users cu
+       ON sr.employee_number = cu.employee_number
+     WHERE sr.[status] <> 'Prestart'
+       AND sr.home_department NOT IN ('Interns')
+
+     UNION ALL
+
+     /* new users */
+     SELECT sr.employee_number
+           ,sr.preferred_first_name
+           ,sr.preferred_last_name
+           ,sr.legal_entity_abbreviation
+           ,sr.home_department
+           ,sr.[status]
+
+           ,ad.samaccountname
+           ,ad.userprincipalname
+           ,ad.mail
+           ,ad.physicaldeliveryofficename
+
+           ,NULL AS content_groups
+           ,NULL AS school_name
+           ,NULL AS mention_name
+           ,'No' AS purchasing_license
+           ,'Expense User' AS roles
+     FROM gabby.people.staff_roster sr
+     INNER JOIN gabby.adsi.user_attributes_static ad
+       ON sr.employee_number = ad.employeenumber
+      AND ISNUMERIC(ad.employeenumber) = 1
+     LEFT JOIN gabby.coupa.users cu
+       ON sr.employee_number = cu.employee_number
+     WHERE sr.[status] NOT IN ('Prestart', 'Terminated')
+       AND sr.home_department NOT IN ('Interns')
+       AND cu.employee_number IS NULL
+       AND sr.legal_entity_name IN ('KIPP New Jersey', 'KIPP Cooper Norcross Academy') -- only TEAM/KCNA temporarily
+    ) sub
+LEFT JOIN gabby.coupa.school_name_lookup sn
+  ON sub.legal_entity_abbreviation = sn.entity_abbv
+ AND sub.home_department = sn.home_department
+LEFT JOIN gabby.coupa.user_exceptions x
+  ON sub.employee_number = x.employee_number
