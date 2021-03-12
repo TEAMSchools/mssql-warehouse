04fc4950feeda191de72517e9de01e2141065116
Charlie Bini <cbini87@gmail.com>
2021-03-12 14:34:20 -0600

    :lipstick:

--- njdoe/v_njdoe.background_check_approval_history.sql
+++ njdoe/v_njdoe.background_check_approval_history.sql
@@ -4,7 +4,6 @@ GO
 CREATE OR ALTER VIEW njdoe.background_check_approval_history AS
 
 SELECT bg.df_employee_number
-      ,bg.approval_history AS approval_history_json
 
       ,ah.approvaldate
       ,ah.countycode
@@ -22,18 +21,18 @@ SELECT bg.df_employee_number
       ,s.[status]
       ,s.preferred_name
       ,s.userprincipalname
-FROM gabby.njdoe.background_check_clean bg
+FROM gabby.njdoe.background_check bg
 LEFT JOIN gabby.people.staff_crosswalk_static s
   ON bg.df_employee_number = s.df_employee_number
 CROSS APPLY OPENJSON(bg.approval_history, '$')
   WITH (
-    approvaldate DATETIME2,
-    countycode VARCHAR(5),
-    districtcode VARCHAR(5),
-    schoolcode VARCHAR(5),
-    contractorcode VARCHAR(5),
-    jobposition VARCHAR(125),
-    pcn VARCHAR(25),
-    transferind DATETIME2
+    approvaldate DATE,
+    countycode NVARCHAR(256),
+    districtcode NVARCHAR(256),
+    schoolcode NVARCHAR(256),
+    contractorcode NVARCHAR(256),
+    jobposition NVARCHAR(256),
+    pcn NVARCHAR(256),
+    transferind DATE
    ) AS ah
 WHERE bg.approval_history <> '[]'
--- njdoe/v_njdoe.background_check_clean.sql
+++ njdoe/v_njdoe.background_check_clean.sql
@@ -3,22 +3,8 @@ GO
 
 CREATE OR ALTER VIEW njdoe.background_check_clean AS
 
-SELECT sub.df_employee_number
-      ,sub.approval_history
-      ,sub.document_id
-      ,sub.number_of_approvals
-      ,sub.rn_employee_current
-FROM
-    (
-     SELECT bc.df_employee_number
-           ,bc.approval_history
-      
-           ,JSON_VALUE(bc.applicant, '$.document_id') AS document_id
-           ,JSON_VALUE(bc.applicant, '$.number_of_approvals') AS number_of_approvals
-      
-           ,ROW_NUMBER() OVER(
-              PARTITION BY bc.df_employee_number
-                ORDER BY bc._modified DESC) AS rn_employee_current
-     FROM gabby.njdoe.background_check bc
-    ) sub
-WHERE rn_employee_current = 1
+SELECT bc.df_employee_number
+
+      ,JSON_VALUE(bc.applicant, '$.document_id') AS document_id
+      ,JSON_VALUE(bc.applicant, '$.number_of_approvals') AS number_of_approvals
+FROM gabby.njdoe.background_check bc
