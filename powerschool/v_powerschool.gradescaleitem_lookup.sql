65a74ae85d3b90230b8dc4bdd62bf4a34abee72a
Charlie Bini <cbini87@gmail.com>
2021-03-15 11:20:34 -0500

    :lipstick:

--- powerschool/v_powerschool.gradescaleitem_lookup.sql
+++ powerschool/v_powerschool.gradescaleitem_lookup.sql
@@ -1,12 +1,14 @@
 CREATE OR ALTER VIEW powerschool.gradescaleitem_lookup AS
 
-SELECT CONVERT(INT,parent.id) AS gradescaleid
-      ,CONVERT(VARCHAR(125),parent.name) AS gradescale_name      
-      
-      ,CONVERT(VARCHAR(125),items.name) AS letter_grade
+SELECT CONVERT(INT, parent.id) AS gradescaleid
+      ,CONVERT(VARCHAR(125), parent.[name]) AS gradescale_name
+
+      ,CONVERT(VARCHAR(125), items.[name]) AS letter_grade
       ,items.grade_points
-      ,items.cutoffpercentage AS min_cutoffpercentage       
-      ,LEAD(items.cutoffpercentage, 1, 1000) OVER(PARTITION BY parent.id ORDER BY items.cutoffpercentage) - 1 AS max_cutoffpercentage 
+      ,items.cutoffpercentage AS min_cutoffpercentage
+      ,LEAD(items.cutoffpercentage, 1, 1000) OVER(
+         PARTITION BY parent.id 
+           ORDER BY items.cutoffpercentage) - 1 AS max_cutoffpercentage
 FROM powerschool.gradescaleitem parent
 JOIN powerschool.gradescaleitem items
   ON parent.id = items.gradescaleid
