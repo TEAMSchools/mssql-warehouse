4228a4b3ad3df32a797d7fa9ade3e52a4c614ca4
Charlie Bini <cbini87@gmail.com>
2021-03-19 12:37:30 -0500

    add race/eth/gender fields

--- people/v_people.staff_roster.sql
+++ people/v_people.staff_roster.sql
@@ -74,11 +74,6 @@ WITH all_staff AS (
         ,sub.address_city
         ,sub.address_state
         ,sub.address_zip
-        ,sub.race
-        ,sub.ethnicity
-        ,sub.is_hispanic
-        ,sub.gender
-        ,sub.preferred_gender
         ,sub.birth_date
         ,sub.personal_email
         ,sub.personal_mobile
@@ -89,15 +84,41 @@ WITH all_staff AS (
         ,sub.legal_entity_name
         ,sub.legal_entity_abbreviation
         ,sub.primary_site_clean
+        ,sub.sex
+        ,sub.preferred_gender
+        ,sub.gender_reporting
+        ,sub.preferred_race_ethnicity
+        ,sub.ethnicity
+        ,sub.race
+        ,sub.race_reporting
+        ,sub.is_race_asian
+        ,sub.is_race_black
+        ,sub.is_race_decline
+        ,sub.is_race_mideast
+        ,sub.is_race_multi
+        ,sub.is_race_nhpi
+        ,sub.is_race_other
+        ,sub.is_race_white
         ,REPLACE(sub.primary_site_clean, ' - Regional', '') AS primary_site
         ,COALESCE(sub.preferred_first_name, sub.first_name) AS preferred_first_name
         ,COALESCE(sub.preferred_last_name , sub.last_name) AS preferred_last_name
         ,CASE WHEN sub.primary_site_clean LIKE '% - Regional%' THEN 1 ELSE 0 END AS is_regional_staff
         ,CASE
-          WHEN sub.race = 'Hispanic or Latino' THEN 'Hispanic or Latino'
-          WHEN sub.race = 'Decline to Answer' THEN NULL
-          ELSE CONVERT(VARCHAR(125), RTRIM(LEFT(sub.race, CHARINDEX(' (', sub.race))))
-         END AS primary_ethnicity
+          WHEN sub.ethnicity = 'Hispanic or Latino' THEN 1
+          WHEN sub.ethnicity = 'Not Hispanic or Latino' THEN 0
+         END AS is_hispanic
+        ,CASE
+          WHEN sub.race_reporting IS NULL AND sub.ethnicity = 'Hispanic or Latino' THEN 'Hispanic or Latino'
+          WHEN sub.race_reporting = 'I decline to state my preferred racial/ethnic identity' THEN 'Decline to state'
+          WHEN sub.race_reporting = 'My racial/ethnic identity is not listed' THEN 'Not Listed'
+          WHEN sub.race_reporting = 'Latinx/Hispanic/Chicana(o)' THEN 'Hispanic or Latino'
+          WHEN sub.race_reporting = 'Black or African American' THEN 'Black/African American'
+          WHEN sub.race_reporting = 'Two or more races (Not Hispanic or Latino)' THEN 'Bi/Multiracial'
+          ELSE sub.race_reporting + (CASE 
+                                      WHEN sub.ethnicity = 'Hispanic or Latino' THEN ' - ' + sub.ethnicity
+                                      ELSE '' 
+                                     END)
+         END AS race_ethnicity_reporting
         ,CASE
           WHEN sub.position_status = 'Leave' THEN 'INACTIVE'
           ELSE UPPER(sub.position_status)
@@ -159,22 +180,72 @@ WITH all_staff AS (
              ,ea.primary_address_city AS address_city
              ,ea.primary_address_state_territory_code AS address_state
              ,ea.primary_address_zip_postal_code AS address_zip
-             ,ea.race_description AS race
              ,ea.personal_contact_personal_email AS personal_email
-             ,ea.ethnicity
              ,ea.preferred_gender
+             ,ea.race_description AS race
+             ,ea.preferred_race_ethnicity
              /* transformations */
              ,CONVERT(DATE, ea.birth_date) AS birth_date
-             ,LEFT(UPPER(ea.gender), 1) AS gender
+             ,LEFT(UPPER(ea.gender), 1) AS sex
              ,CASE 
                WHEN ea.primary_address_address_line_1 IS NOT NULL 
                     THEN CONCAT(ea.primary_address_address_line_1, ', ' + ea.primary_address_address_line_2)
               END AS address_street
              ,CONVERT(NVARCHAR(256), gabby.utilities.STRIP_CHARACTERS(ea.personal_contact_personal_mobile, '^0-9')) AS personal_mobile
+             ,COALESCE(ea.preferred_gender
+                      ,CASE
+                        WHEN ea.gender = 'Male' THEN 'Man'
+                        WHEN ea.gender = 'Female' THEN 'Woman'
+                       END) AS gender_reporting
              ,CASE
-               WHEN ea.ethnicity = 'Hispanic or Latino' THEN 1
-               WHEN ea.ethnicity = 'Not Hispanic or Latino' THEN 0
-              END AS is_hispanic
+               WHEN ea.ethnicity IS NULL AND ea.preferred_race_ethnicity IS NULL THEN NULL
+               WHEN CHARINDEX('Decline to state', ea.preferred_race_ethnicity) > 0 THEN NULL
+               WHEN CHARINDEX('Latinx/Hispanic/Chicana(o)', ea.preferred_race_ethnicity) > 0 THEN 'Hispanic or Latino'
+               WHEN ea.ethnicity = 'Hispanic or Latino' THEN 'Hispanic or Latino'
+               ELSE 'Not Hispanic or Latino'
+              END AS ethnicity
+             ,CASE 
+               WHEN ea.race_description = 'Black or African American' THEN 1
+               WHEN CHARINDEX('Black/African American', ea.preferred_race_ethnicity) > 0 THEN 1
+               ELSE 0
+              END AS is_race_black
+             ,CASE 
+               WHEN ea.race_description = 'Asian' THEN 1
+               WHEN CHARINDEX('Asian', ea.preferred_race_ethnicity) > 0 THEN 1
+               ELSE 0
+              END AS is_race_asian
+             ,CASE 
+               WHEN ea.race_description = 'Native Hawaiian or Other Pacific Islander' THEN 1
+               WHEN CHARINDEX('Pacific Islander', ea.preferred_race_ethnicity) > 0 THEN 1
+               ELSE 0
+              END AS is_race_nhpi
+             ,CASE 
+               WHEN CHARINDEX('Middle Eastern', ea.preferred_race_ethnicity) > 0 THEN 1
+               ELSE 0
+              END AS is_race_mideast
+             ,CASE 
+               WHEN ea.race_description = 'White' THEN 1
+               WHEN CHARINDEX('White', ea.preferred_race_ethnicity) > 0 THEN 1
+               ELSE 0
+              END AS is_race_white
+             ,CASE
+               WHEN ea.race_description = 'Two or more races (Not Hispanic or Latino)' THEN 1
+               WHEN CHARINDEX('Bi/Multiracial', ea.preferred_race_ethnicity) > 0 THEN 1
+               WHEN CHARINDEX(';', ea.preferred_race_ethnicity) > 0 THEN 1
+               ELSE 0
+              END AS is_race_multi
+             ,CASE
+               WHEN CHARINDEX('My racial/ethnic identity is not listed', ea.preferred_race_ethnicity) > 0 THEN 1
+               ELSE 0
+              END AS is_race_other
+             ,CASE
+               WHEN CHARINDEX('Decline to state', ea.preferred_race_ethnicity) > 0 THEN 1
+               ELSE 0
+              END AS is_race_decline
+             ,CASE
+               WHEN CHARINDEX(';', ea.preferred_race_ethnicity) > 0 THEN 'Bi/Multiracial'
+               ELSE COALESCE(ea.preferred_race_ethnicity, ea.race_description)
+              END AS race_reporting
 
              ,p.wfmgr_pay_rule
              ,p.worker_category_description AS worker_category
@@ -224,11 +295,6 @@ SELECT c.employee_number
       ,c.address_city
       ,c.address_state
       ,c.address_zip
-      ,c.race
-      ,c.ethnicity
-      ,c.is_hispanic
-      ,c.gender
-      ,c.preferred_gender
       ,c.birth_date
       ,c.personal_email
       ,c.wfmgr_pay_rule
@@ -242,11 +308,27 @@ SELECT c.employee_number
       ,c.primary_site_clean
       ,c.primary_site
       ,c.is_regional_staff
-      ,c.primary_ethnicity
       ,c.[status]
       ,c.position_title
       ,c.primary_on_site_department_entity
       ,c.primary_site_entity
+      ,c.sex
+      ,c.preferred_gender
+      ,c.gender_reporting
+      ,c.race
+      ,c.preferred_race_ethnicity
+      ,c.ethnicity
+      ,c.is_hispanic
+      ,c.race_reporting
+      ,c.is_race_asian
+      ,c.is_race_black
+      ,c.is_race_decline
+      ,c.is_race_mideast
+      ,c.is_race_multi
+      ,c.is_race_nhpi
+      ,c.is_race_other
+      ,c.is_race_white
+      ,c.race_ethnicity_reporting
       ,c.preferred_last_name + ', ' + c.preferred_first_name AS preferred_name
       ,SUBSTRING(c.personal_mobile, 1, 3) + '-'
          + SUBSTRING(c.personal_mobile, 4, 3) + '-'
