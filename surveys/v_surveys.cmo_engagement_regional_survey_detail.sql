e075c2411f533958b2dc7ededed9da2a1b908aac
Kevin Verhoff <kevin.verhoff@gmail.com>
2021-05-14 11:10:57 -0400

    Update v_surveys.cmo_engagement_regional_survey_detail.sql
    updating to the new reporting fields

--- surveys/v_surveys.cmo_engagement_regional_survey_detail.sql
+++ surveys/v_surveys.cmo_engagement_regional_survey_detail.sql
@@ -25,15 +25,15 @@ SELECT d.survey_id
       ,d.respondent_manager_name
       ,w.job_name AS respondent_primary_job
       ,w.physical_location_name AS respondent_primary_site
-      ,s.primary_ethnicity
-      ,s.gender
+      ,s.race_ethnicity_reporting AS primary_ethnicity
+      ,s.gender_reporting AS gender
 FROM gabby.surveygizmo.survey_detail d
 LEFT JOIN gabby.dayforce.work_assignment_status w
   ON d.respondent_df_employee_number = w.df_employee_id
  AND d.date_submitted BETWEEN w.effective_start_date 
                           AND COALESCE(w.effective_end_date, DATEFROMPARTS((d.campaign_academic_year + 1), 7, 1))
-LEFT JOIN gabby.people.staff_crosswalk_static s
-  ON d.respondent_df_employee_number = s.df_employee_number
+LEFT JOIN gabby.people.staff_roster s
+  ON d.respondent_df_employee_number = s.employee_number
 WHERE d.survey_id = 5300913
   AND d.rn_respondent_subject = 1
   AND d.campaign_academic_year >= (gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1)
