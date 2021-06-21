25148ad2bd809c1c38bf1ec6a0ea443ae2fc0a4a
Kevin Verhoff <kevin.verhoff@gmail.com>
2021-06-21 15:29:34 -0400

    Update v_surveys.survey_assignments_and_tracking_scaffold.sql
    Switching over CTE to new unpivoted view + trying to sort out how to manage surveys that are completed (but weren't assigned to be completed)

--- surveys/v_surveys.survey_assignments_and_tracking_scaffold.sql
+++ surveys/v_surveys.survey_assignments_and_tracking_scaffold.sql
@@ -17,67 +17,15 @@ WITH survey_terms AS (
           ELSE NULL
          END AS current_survey_term
   FROM gabby.surveygizmo.survey_campaign_clean_static c
-  LEFT JOIN gabby.surveygizmo.survey s
+  JOIN gabby.surveygizmo.survey s
     ON c.survey_id = s.id
   WHERE c.link_type = 'email'
   )
 
-,surveys_unpivoted AS ( --replace this shit with surveys.so_assignments_long once it's pulled into gabby
-  SELECT u.employee_number AS survey_taker_id
-        ,u.survey_taker AS survey_round_status
-        ,u.assignment
-        ,CASE WHEN CHARINDEX('[',u.assignment) = 0 THEN NULL
-              ELSE SUBSTRING(u.assignment,CHARINDEX('[',u.assignment) + 1,6)
-         END AS assingment_employee_id
-  FROM gabby.surveys.so_assignments
-  UNPIVOT(
-    assignment
-    FOR number IN (so_assignment_1
-                  ,so_assignment_2
-                  ,so_assignment_3
-                  ,so_assignment_4
-                  ,so_assignment_5
-                  ,so_assignment_6
-                  ,so_assignment_7
-                  ,so_assignment_8
-                  ,so_assignment_9
-                  ,so_assignment_10)
-   ) u
- )
-
-,so_survey_on_pr AS (
-  SELECT u.survey_taker_id
-        ,u.survey_round_status
-        ,u.assignment
-        ,u.assingment_employee_id
-        ,COALESCE(r.preferred_name,assignment) AS assignment_preferred_name
-        ,r.location AS assignment_location
-        ,r.position_status AS assignment_adp_status
-        ,'Self & Others - Peer Feedback' AS assignment_type
-  FROM surveys_unpivoted u
-  LEFT JOIN gabby.people.staff_roster r
-    ON u.assingment_employee_id = r.employee_number
-
-  UNION ALL
-
-  SELECT manager_employee_number  AS survey_taker_id
-        ,'Yes' AS survey_round_status
-        ,preferred_name + '[' + CONVERT(varchar(6),employee_number) + ']' AS assignment
-        ,employee_number AS assignment_employee_id
-        ,preferred_name AS assignment_preferred_name
-        ,location AS assignment_location
-        ,position_status AS assignment_adp_status
-        ,'Self & Others - Manager Feedback' AS assignment_type
-  FROM gabby.people.staff_roster
-  WHERE position_status != 'Terminated'
-    AND COALESCE(rehire_date,original_hire_date) < DATEADD(DAY,-30,GETDATE())
-  )
-
-
-SELECT r.employee_number AS survey_taker_id
-      ,r.preferred_name AS survey_taker_name
-      ,r.location AS survey_taker_location
-      ,r.position_status AS survey_taker_adp_status
+SELECT COALESCE(r.employee_number,c.df_employee_number) AS survey_taker_id
+      ,COALESCE(r.preferred_name,c.survey_taker_name) AS survey_taker_name
+      ,COALESCE(r.[location],c.location_custom) AS survey_taker_location
+      ,COALESCE(r.position_status,c.position_status) AS survey_taker_adp_status
 
       ,s.survey_round_status
       ,s.assignment
@@ -87,8 +35,8 @@ SELECT r.employee_number AS survey_taker_id
       ,s.assignment_adp_status
       ,s.assignment_type
 
-      ,st.academic_year
-      ,st.reporting_term_code AS reporting_term
+      ,COALESCE(st.academic_year,c.academic_year) AS academic_year
+      ,COALESCE(st.reporting_term_code,c.reporting_term) AS reporting_term
       ,st.survey_round_open_minus_fifteen
       ,st.survey_round_open
       ,st.survey_round_close
@@ -98,26 +46,26 @@ SELECT r.employee_number AS survey_taker_id
       ,c.date_submitted AS survey_completion_date
       ,c.is_manager
 
-FROM so_survey_on_pr s
+FROM gabby.surveys.so_assignments_long s
 JOIN gabby.people.staff_roster r
   ON s.survey_taker_id = r.employee_number
- AND r.position_status != 'Terminated'
 JOIN survey_terms st
   ON st.current_survey_term = 1
  AND st.survey_id = 4561325 --S&O Survey Code
-LEFT JOIN gabby.surveys.survey_completion c
+FULL JOIN gabby.surveys.survey_completion c
   ON CONVERT(nvarchar,assingment_employee_id) = SUBSTRING(c.subject_name,CHARINDEX('[',c.subject_name) + 1,6)
  AND s.survey_taker_id = c.df_employee_number
  AND st.academic_year = c.academic_year
  AND st.reporting_term_code = c.reporting_term
- AND survey_type = 'Self & Others'
+WHERE c.survey_type = 'Self & Others'
+  AND c.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
 
 UNION ALL
 
-SELECT r.employee_number AS survey_taker_id
-      ,r.preferred_name AS survey_taker_name
-      ,r.location AS survey_taker_location
-      ,r.position_status AS survey_taker_adp_status
+SELECT COALESCE(r.employee_number,c.df_employee_number) AS survey_taker_id
+      ,COALESCE(r.preferred_name,c.survey_taker_name) AS survey_taker_name
+      ,COALESCE(r.[location],c.location_custom) AS survey_taker_location
+      ,COALESCE(r.position_status,c.position_status) AS survey_taker_adp_status
 
       ,'Yes' AS survey_round_status
       ,'Your Manager' AS assignment
@@ -127,8 +75,8 @@ SELECT r.employee_number AS survey_taker_id
       ,NULL AS assignment_adp_status
       ,'Manager Survey' AS assignment_type
 
-      ,st.academic_year
-      ,st.reporting_term_code AS reporting_term
+      ,COALESCE(st.academic_year,c.academic_year) AS academic_year
+      ,COALESCE(st.reporting_term_code,c.reporting_term) AS reporting_term
       ,st.survey_round_open_minus_fifteen
       ,st.survey_round_open
       ,st.survey_round_close
@@ -142,12 +90,14 @@ FROM gabby.people.staff_roster r
 JOIN survey_terms st
   ON st.current_survey_term = 1
  AND st.survey_id = 4561288 --MGR Survey Code
-LEFT JOIN gabby.surveys.survey_completion c
+FULL JOIN gabby.surveys.survey_completion c
   ON r.employee_number = c.df_employee_number
  AND st.academic_year = c.academic_year
  AND st.reporting_term_code = c.reporting_term
  AND survey_type = 'Manager'
 WHERE r.position_status != 'Terminated'
+  AND c.survey_type = 'Manager'
+  AND c.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
 
 UNION ALL
 
@@ -259,6 +209,3 @@ LEFT JOIN gabby.surveys.survey_completion c
  AND st.reporting_term_code = c.reporting_term
  AND survey_type = 'Staff Update'
 WHERE r.position_status != 'Terminated'
-
-
---One thing that's kind of missing - how do we address surveys that folks complete that aren't necessarily assigned (eg: i give s&o feedback to someone who I'm not assigned to)
