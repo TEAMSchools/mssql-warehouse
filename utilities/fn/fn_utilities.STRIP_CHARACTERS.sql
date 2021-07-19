292a3122aa4383c2a0639d042ac8ee32f468c7e1
Charlie Bini <cbini87@gmail.com>
2021-07-19 13:49:04 -0500

    :lipstick:

--- utilities/fn/fn_utilities.STRIP_CHARACTERS.sql
+++ utilities/fn/fn_utilities.STRIP_CHARACTERS.sql
@@ -2,7 +2,7 @@ USE gabby;
 GO
 
 CREATE OR ALTER FUNCTION utilities.STRIP_CHARACTERS (
-    @string           NVARCHAR(MAX)
+    @string NVARCHAR(MAX)
    ,@match_expression VARCHAR(255)
 )
   RETURNS NVARCHAR(MAX)
@@ -14,7 +14,7 @@ BEGIN
   SET @match_expression = '%[' + @match_expression + ']%';
 
   WHILE PATINDEX(@match_expression, @string) > 0
-  SET @string = STUFF(@string, PATINDEX(@match_expression, @String), 1, '');
+    SET @string = STUFF(@string, PATINDEX(@match_expression, @string), 1, '');
 
   RETURN @string;
 
