a777c74af7a5092fb5c8bdf0a92b9650a805030a
Charlie Bini <cbini87@gmail.com>
2021-07-20 16:46:50 -0500

    read180 extracts

---
+++ extracts/v_extracts.read180_teachers.sql
@@ -0,0 +1,36 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW extracts.read180_teachers AS
+
+SELECT scw.df_employee_number AS DISTRICT_USER_ID
+      --,NULL AS SPS_ID
+      --,NULL AS PREFIX
+      ,scw.preferred_first_name AS FIRST_NAME
+      ,scw.preferred_last_name AS LAST_NAME
+      --,NULL AS TITLE
+      --,NULL AS SUFFIX
+      ,scw.mail AS EMAIL
+      ,scw.samaccountname AS [USER_NAME]
+      ,NULL AS [PASSWORD]
+      ,sch.[name] AS SCHOOL_NAME
+      ,cou.course_name AS CLASS_NAME
+      ,scw.google_email AS EXTERNAL_ID
+FROM gabby.powerschool.sections sec
+JOIN gabby.powerschool.sectionteacher st
+  ON sec.id = st.sectionid
+ AND sec.[db_name] = st.[db_name]
+JOIN gabby.powerschool.teachers_static t
+  ON st.teacherid = t.id
+ AND st.[db_name] = t.[db_name]
+JOIN gabby.people.staff_crosswalk_static scw
+  ON t.teachernumber = scw.ps_teachernumber COLLATE Latin1_General_BIN
+JOIN gabby.powerschool.courses cou
+  ON sec.course_number = cou.course_number
+ AND sec.[db_name] = cou.[db_name]
+JOIN gabby.powerschool.schools sch
+  ON sec.schoolid = sch.school_number
+ AND sec.[db_name] = sch.[db_name]
+WHERE sec.yearid = (gabby.utilities.GLOBAL_ACADEMIC_YEAR() - 1990)
+  AND sec.course_number IN ('MAT02999G1', 'ELA01068G1')
+  AND sec.[db_name] IN ('kippnewark', 'kippcamden')
