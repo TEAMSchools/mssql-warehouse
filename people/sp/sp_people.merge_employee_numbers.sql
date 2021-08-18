f99a1be5441741f68dfae37aaaaf8c38638bf07a
Charlie Bini <cbini87@gmail.com>
2021-08-18 11:35:46 -0500

    formatting

--- people/sp/sp_people.merge_employee_numbers.sql
+++ people/sp/sp_people.merge_employee_numbers.sql
@@ -4,7 +4,11 @@ GO
 CREATE OR ALTER PROCEDURE people.merge_employee_numbers AS
 
 MERGE INTO [gabby].[people].[employee_numbers] AS [Target]
-USING (SELECT [associate_id] FROM [gabby].[adp].[employees_all] WHERE [file_number] IS NOT NULL) AS [Source]
+USING (
+       SELECT [associate_id] 
+       FROM [gabby].[adp].[employees_all] 
+       WHERE [file_number] IS NOT NULL
+      ) AS [Source]
    ON [Target].[associate_id] = [Source].[associate_id]
 WHEN NOT MATCHED BY TARGET THEN
  INSERT([associate_id])
