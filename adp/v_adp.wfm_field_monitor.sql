4e7c77fd693ecfbf2875ccb7ebd53b2368a4db36
Charlie Bini <cbini87@gmail.com>
2021-09-24 15:48:21 -0500

    filter term staff

--- adp/v_adp.wfm_field_monitor.sql
+++ adp/v_adp.wfm_field_monitor.sql
@@ -27,6 +27,7 @@ WITH unpivoted AS (
              ,CONVERT(NVARCHAR(MAX), wfmgr_ee_type) AS wfmgr_ee_type
        FROM gabby.adp.employees_archive
        WHERE position_id IS NOT NULL
+         AND position_status <> 'Terminated'
          AND CONVERT(DATE, _modified) BETWEEN DATEADD(DAY, -3, CONVERT(DATE, GETDATE()))
                                           AND CONVERT(DATE, GETDATE())
       ) sub
