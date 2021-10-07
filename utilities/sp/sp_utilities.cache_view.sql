f4acb51315c3079407527642d19a6bf8a229ad82
Charlie Bini <5003326+cbini@users.noreply.github.com>
2021-10-07 12:25:49 -0500

    break up try/catch

--- utilities/sp/sp_utilities.cache_view.sql
+++ utilities/sp/sp_utilities.cache_view.sql
@@ -16,8 +16,9 @@ AS
 BEGIN
   SET XACT_ABORT ON;
 
-  DECLARE @sql                    NVARCHAR(MAX)
-         ,@sql_drop               NVARCHAR(MAX)
+  DECLARE @sql_create             NVARCHAR(MAX)
+         ,@sql_drop1              NVARCHAR(MAX)
+         ,@sql_drop2              NVARCHAR(MAX)
          ,@sql_selectinto         NVARCHAR(MAX)
          ,@sql_truncateinsert     NVARCHAR(MAX)
          ,@rowcount               INT
@@ -34,86 +35,134 @@ BEGIN
   /* if source view does not exist, exit */
   IF OBJECT_ID(@source_view, 'V') IS NULL
     BEGIN
-      PRINT ('View does not exist');
+      RAISERROR('View does not exist', 0, 1) WITH NOWAIT;
       RETURN;
     END;
 
-  BEGIN TRY
-    /* if destination table does not exist, create and exit */
-    IF OBJECT_ID(@destination_table_name) IS NULL
-      BEGIN
-        SET @sql = N'
-          SELECT *
-          INTO ' + @destination_table_name + N'
-          FROM ' + @source_view + N';
-        ';
+  /* if destination table does not exist, create and exit */
+  IF OBJECT_ID(@destination_table_name) IS NULL
+    BEGIN
+      SET @sql_create = N'
+        SELECT *
+        INTO ' + @destination_table_name + N'
+        FROM ' + @source_view + N';
+      ';
+      BEGIN TRY
         BEGIN TRANSACTION
-          PRINT (@sql);
-          EXEC (@sql);
+          RAISERROR(@sql_create, 0, 1) WITH NOWAIT;
+          EXEC (@sql_create);
         COMMIT
-      END;
-    ELSE
-      BEGIN
-        /* drop temp table, if exists... */
-        SET @sql_drop = N'
-          IF OBJECT_ID(N''' + @temp_table_name + N''') IS NOT NULL
-            BEGIN
-              DROP TABLE ' + @temp_table_name + N';
-            END
-        '
+      END TRY
+      BEGIN CATCH
+        ROLLBACK;
+        SET @email_body = ERROR_MESSAGE();
+        SET @email_subject = N'CREATE - ' + @destination_table_name + N' failed';
+        EXEC msdb.dbo.sp_send_dbmail @profile_name = 'datarobot'
+                                    ,@recipients = 'u7c1r1b1c5n4p0q0@kippnj.slack.com'
+                                    ,@subject = @email_subject
+                                    ,@body = @email_body;
+        THROW;
+      END CATCH;
+    END;
+  ELSE
+    BEGIN
+      /* drop temp table, if exists... */
+      SET @sql_drop1 = N'
+        IF OBJECT_ID(N''' + @temp_table_name + N''') IS NOT NULL
+          BEGIN
+            DROP TABLE ' + @temp_table_name + N';
+          END
+      '
+      BEGIN TRY
         BEGIN TRANSACTION
-          PRINT(@sql_drop);
-          EXEC (@sql_drop);
+          RAISERROR(@sql_drop1, 0, 1) WITH NOWAIT;
+          EXEC (@sql_drop1);
         COMMIT
+      END TRY
+      BEGIN CATCH
+        ROLLBACK;
+        SET @email_body = ERROR_MESSAGE();
+        SET @email_subject = N'DROP TEMP - ' + @destination_table_name + N' failed';
+        EXEC msdb.dbo.sp_send_dbmail @profile_name = 'datarobot'
+                                    ,@recipients = 'u7c1r1b1c5n4p0q0@kippnj.slack.com'
+                                    ,@subject = @email_subject
+                                    ,@body = @email_body;
+        THROW;
+      END CATCH;
 
-        /* load data from view into temp table... */
-        SET @sql_selectinto = N'
-          SELECT *
-          INTO ' + @temp_table_name + N'
-          FROM ' + @source_view + N';
-        '
+      /* load data from view into temp table... */
+      SET @sql_selectinto = N'
+        SELECT *
+        INTO ' + @temp_table_name + N'
+        FROM ' + @source_view + N';
+      '
+      BEGIN TRY
         BEGIN TRANSACTION
-          PRINT(@sql_selectinto);
+          RAISERROR(@sql_selectinto, 0, 1) WITH NOWAIT;
           EXEC (@sql_selectinto);
         COMMIT
+      END TRY
+      BEGIN CATCH
+        ROLLBACK;
+        SET @email_body = ERROR_MESSAGE();
+        SET @email_subject = N'INSERT TEMP - ' + @destination_table_name + N' failed';
+        EXEC msdb.dbo.sp_send_dbmail @profile_name = 'datarobot'
+                                    ,@recipients = 'u7c1r1b1c5n4p0q0@kippnj.slack.com'
+                                    ,@subject = @email_subject
+                                    ,@body = @email_body;
+        THROW;
+      END CATCH;
 
-        /* truncate destination table... */
-        /* insert into destination table */
-        SET @sql_truncateinsert = N'
-          TRUNCATE TABLE ' + @destination_table_name + N';
-          INSERT INTO ' + @destination_table_name + N' WITH(TABLOCKX)
-          SELECT * 
-          FROM ' + @temp_table_name + N';
-        ';
-        IF @@ROWCOUNT > 0
-          BEGIN
+      /* truncate destination table... */
+      /* insert into destination table */
+      SET @sql_truncateinsert = N'
+        TRUNCATE TABLE ' + @destination_table_name + N';
+        INSERT INTO ' + @destination_table_name + N' WITH(TABLOCKX)
+        SELECT * 
+        FROM ' + @temp_table_name + N';
+      ';
+      IF @@ROWCOUNT > 0
+        BEGIN
+          BEGIN TRY
             BEGIN TRANSACTION
-              PRINT(@sql_truncateinsert);
+              RAISERROR(@sql_truncateinsert, 0, 1) WITH NOWAIT;
               EXEC (@sql_truncateinsert);
             COMMIT
-          END
+          END TRY
+          BEGIN CATCH
+            ROLLBACK;
+            SET @email_body = ERROR_MESSAGE();
+            SET @email_subject = N'TRUNCATE/INSERT DEST - ' + @destination_table_name + N' failed';
+            EXEC msdb.dbo.sp_send_dbmail @profile_name = 'datarobot'
+                                        ,@recipients = 'u7c1r1b1c5n4p0q0@kippnj.slack.com'
+                                        ,@subject = @email_subject
+                                        ,@body = @email_body;
+            THROW;
+          END CATCH;
+        END
 
-        /* drop temp table, if exists... */
-        SET @sql_drop = N'
-          IF OBJECT_ID(N''' + @temp_table_name + N''') IS NOT NULL
-            BEGIN
-              DROP TABLE ' + @temp_table_name + N';
-            END
-        '
+      /* drop temp table, if exists... */
+      SET @sql_drop2 = N'
+        IF OBJECT_ID(N''' + @temp_table_name + N''') IS NOT NULL
+          BEGIN
+            DROP TABLE ' + @temp_table_name + N';
+          END
+      '
+      BEGIN TRY
         BEGIN TRANSACTION
-          PRINT(@sql_drop);
-          EXEC (@sql_drop);
+          RAISERROR(@sql_drop2, 0, 1) WITH NOWAIT;
+          EXEC (@sql_drop2);
         COMMIT
-      END
-  END TRY
-  BEGIN CATCH
-    ROLLBACK;
-    SET @email_body = ERROR_MESSAGE();
-    SET @email_subject = @destination_table_name + N' refresh failed';
-    EXEC msdb.dbo.sp_send_dbmail @profile_name = 'datarobot'
-                                ,@recipients = 'u7c1r1b1c5n4p0q0@kippnj.slack.com'
-                                ,@subject = @email_subject
-                                ,@body = @email_body;
-    THROW;
-  END CATCH;
+      END TRY
+      BEGIN CATCH
+        ROLLBACK;
+        SET @email_body = ERROR_MESSAGE();
+        SET @email_subject = N'DROP TEMP - ' + @destination_table_name + N' failed';
+        EXEC msdb.dbo.sp_send_dbmail @profile_name = 'datarobot'
+                                    ,@recipients = 'u7c1r1b1c5n4p0q0@kippnj.slack.com'
+                                    ,@subject = @email_subject
+                                    ,@body = @email_body;
+        THROW;
+      END CATCH;
+    END
 END;
