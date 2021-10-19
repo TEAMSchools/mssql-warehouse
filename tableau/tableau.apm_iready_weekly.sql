6a0c4c22e1cff196e67f2e2dccd11f6ceb944b0e
Anthony Walters <awalters@kippteamandfamily.org>
2021-10-19 15:10:58 -0400

    Update tableau.apm_iready_weekly.sql

--- tableau/tableau.apm_iready_weekly.sql
+++ tableau/tableau.apm_iready_weekly.sql
@@ -3,25 +3,30 @@ GO
 
 CREATE OR ALTER VIEW tableau.apm_iready_weekly AS
 
-WITH iready_lessons AS (
+SELECT cis.student_number
+      ,cis.[date]
+      ,cis.lastfirst
+      ,cis.grade_level
+      ,cis.region
+      ,cis.school_name
+      ,cis.advisor_name
+      ,cis.team
+      ,cis.lep_status
+      ,cis.iep_status
+      ,cis.term
+      ,cis.school_level
+      ,cis.gender
+      ,cis.ethnicity
+      ,cis.schoolid
 
-SELECT pil.student_id
+      ,subjects.[value] AS [subject]
+
       ,pil.lesson_id
       ,pil.passed_or_not_passed
       ,pil.[subject]
       ,pil.total_time_on_lesson_min_
       ,pil.completion_date
 
-FROM gabby.iready.personalized_instruction_by_lesson pil
-
-WHERE LEFT(pil.academic_year,4) = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
-
-)
-
-,iready_diagnostic_recent AS (
-
-SELECT di.student_id
-      ,di.[subject]
       ,di.diagnostic_completion_date_most_recent_
       ,di.diagnostic_overall_scale_score_most_recent_
       ,di.diagnostic_overall_placement_most_recent_
@@ -31,93 +36,16 @@ SELECT di.student_id
       ,di.annual_typical_growth_measure
       ,di.diagnostic_gain_note_negative_gains_zero_
 
-FROM gabby.iready.diagnostic_and_instruction di
-
-WHERE LEFT(di.academic_year,4) = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
-
-)
-
-SELECT cis.student_number
-      ,cis.[date]
-      ,cis.lastfirst
-      ,cis.grade_level
-      ,cis.region
-      ,cis.school_name
-      ,cis.advisor_name
-      ,cis.team
-      ,cis.lep_status
-      ,cis.iep_status
-      ,cis.term
-      ,cis.school_level
-      ,cis.gender
-      ,cis.ethnicity
-      ,cis.schoolid
-      ,il.lesson_id
-      ,il.passed_or_not_passed
-      ,'Reading' AS [subject]
-      ,il.total_time_on_lesson_min_
-      ,dr.diagnostic_completion_date_most_recent_
-      ,dr.diagnostic_overall_scale_score_most_recent_
-      ,dr.diagnostic_overall_placement_most_recent_
-      ,dr.diagnostic_overall_relative_placement_most_recent_
-      ,dr.diagnostic_percentile_most_recent_
-      ,dr.annual_stretch_growth_measure
-      ,dr.annual_typical_growth_measure
-      ,dr.diagnostic_gain_note_negative_gains_zero_
-
-
 FROM gabby.powerschool.cohort_identifiers_scaffold_current_static cis
-LEFT JOIN iready_lessons il
-  ON cis.student_number = il.student_id
- AND cis.[date] = CAST(il.completion_date AS date)
- AND il.[subject] = 'Reading'
-LEFT JOIN iready_diagnostic_recent dr
-  ON cis.student_number = dr.student_id
- AND dr.[subject] = 'Reading' 
+CROSS JOIN STRING_SPLIT ('Reading,Math',',') subjects
+LEFT JOIN gabby.iready.personalized_instruction_by_lesson pil
+  ON cis.student_number = pil.student_id
+ AND cis.[date] = pil.completion_date
+ AND subjects.[value] = pil.[subject]
+LEFT JOIN gabby.iready.diagnostic_and_instruction di
+  ON cis.student_number = di.student_id
+ AND subjects.[value] = di.[subject]
 
-WHERE cis.grade_level BETWEEN 5 AND 8
-  AND cis.is_enrolled = 1
-  AND cis.[date] <= GETDATE()
-
-UNION ALL
-
-SELECT cis.student_number
-      ,cis.[date]
-      ,cis.lastfirst
-      ,cis.grade_level
-      ,cis.region
-      ,cis.school_name
-      ,cis.advisor_name
-      ,cis.team
-      ,cis.lep_status
-      ,cis.iep_status
-      ,cis.term
-      ,cis.school_level
-      ,cis.gender
-      ,cis.ethnicity
-      ,cis.schoolid
-      ,il.lesson_id
-      ,il.passed_or_not_passed
-      ,'Math' AS [subject]
-      ,il.total_time_on_lesson_min_
-      ,dr.diagnostic_completion_date_most_recent_
-      ,dr.diagnostic_overall_scale_score_most_recent_
-      ,dr.diagnostic_overall_placement_most_recent_
-      ,dr.diagnostic_overall_relative_placement_most_recent_
-      ,dr.diagnostic_percentile_most_recent_
-      ,dr.annual_stretch_growth_measure
-      ,dr.annual_typical_growth_measure
-      ,dr.diagnostic_gain_note_negative_gains_zero_
-
-FROM gabby.powerschool.cohort_identifiers_scaffold_current_static cis
-LEFT JOIN iready_lessons il
-  ON cis.student_number = il.student_id
- AND cis.[date] = CAST(il.completion_date AS date)
- AND il.[subject] = 'Math'
-LEFT JOIN iready_diagnostic_recent dr
-  ON cis.student_number = dr.student_id
- AND dr.[subject] = 'Math' 
-
-WHERE cis.grade_level BETWEEN 0 AND 8
+WHERE cis.grade_level < 9
   AND cis.is_enrolled = 1
   AND cis.[date] <= GETDATE()
