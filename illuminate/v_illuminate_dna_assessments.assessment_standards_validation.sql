bc2a13d23612e3510141f3cd7c6ae51b7aababe3
Charlie Bini <5003326+cbini@users.noreply.github.com>
2021-10-22 14:06:22 -0500

    remove row hash

--- illuminate/v_illuminate_dna_assessments.assessment_standards_validation.sql
+++ illuminate/v_illuminate_dna_assessments.assessment_standards_validation.sql
@@ -3,7 +3,8 @@ GO
 
 CREATE OR ALTER VIEW illuminate_dna_assessments.assessment_standards_validation AS 
 
-SELECT CONVERT(VARCHAR(250),CONCAT(assessment_id, '_',standard_id)) AS row_hash
+SELECT assessment_id
+      ,standard_id
 FROM OPENQUERY(ILLUMINATE, '
   SELECT assessment_id
         ,standard_id
