72937caad0b019e0884d84ca31b9d6b33f3fcce9
Charlie Bini <5003326+cbini@users.noreply.github.com>
2021-11-18 14:06:16 -0600

    add person custom fields

--- adp/v_adp.workers_custom_field_group.sql
+++ adp/v_adp.workers_custom_field_group.sql
@@ -5,6 +5,7 @@ CREATE OR ALTER VIEW adp.workers_custom_field_group AS
 
 SELECT w.associate_oid
       ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'worker' AS parent_object
       ,'stringFields' AS custom_field_group
 
       ,cfg.itemID AS item_id
@@ -28,6 +29,7 @@ UNION ALL
 
 SELECT w.associate_oid
       ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'worker' AS parent_object
       ,'codeFields' AS custom_field_group
 
       ,cfg.itemID AS item_id
@@ -51,6 +53,7 @@ UNION ALL
 
 SELECT w.associate_oid
       ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'worker' AS parent_object
       ,'dateFields' AS custom_field_group
 
       ,cfg.itemID AS item_id
@@ -74,6 +77,7 @@ UNION ALL
 
 SELECT w.associate_oid
       ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'worker' AS parent_object
       ,'indicatorFields' AS custom_field_group
 
       ,cfg.itemID AS item_id
@@ -92,3 +96,138 @@ CROSS APPLY OPENJSON(w.custom_field_group, '$.indicatorFields')
  ) cfg
 WHERE JSON_QUERY(w.custom_field_group, '$.indicatorFields') <> '{}'
   AND cfg.indicatorValue IS NOT NULL
+
+UNION ALL
+
+SELECT w.associate_oid
+      ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'person' AS parent_object
+      ,'stringFields' AS custom_field_group
+
+      ,cfg.itemID AS item_id
+      ,cfg.stringValue AS string_value
+      ,NULL AS date_value
+      ,NULL AS bit_value
+      ,CASE WHEN ISNUMERIC(cfg.stringValue) = 1 THEN CONVERT(INT, cfg.stringValue) END AS numeric_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+FROM gabby.adp.workers w
+CROSS APPLY OPENJSON(w.person, '$.customFieldGroup.stringFields') 
+  WITH(
+    itemID NVARCHAR(128)
+   ,nameCode NVARCHAR(MAX) AS JSON
+   ,stringValue NVARCHAR(128)
+ ) cfg
+WHERE JSON_QUERY(w.person, '$.customFieldGroup.stringFields') <> '{}'
+  AND cfg.stringValue IS NOT NULL
+
+UNION ALL
+
+SELECT w.associate_oid
+      ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'person' AS parent_object
+      ,'codeFields' AS custom_field_group
+
+      ,cfg.itemID AS item_id
+      ,cfg.codeValue AS string_value
+      ,NULL AS date_value
+      ,NULL AS bit_value
+      ,CASE WHEN ISNUMERIC(cfg.codeValue) = 1 THEN CONVERT(INT, cfg.codeValue) END AS numeric_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+FROM gabby.adp.workers w
+CROSS APPLY OPENJSON(w.person, '$.customFieldGroup.codeFields')
+  WITH(
+    itemID NVARCHAR(128)
+   ,nameCode NVARCHAR(MAX) AS JSON
+   ,codeValue NVARCHAR(128)
+ ) cfg
+WHERE JSON_QUERY(w.person, '$.customFieldGroup.codeFields') <> '{}'
+  AND cfg.codeValue IS NOT NULL
+
+UNION ALL
+
+SELECT w.associate_oid
+      ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'person' AS parent_object
+      ,'dateFields' AS custom_field_group
+
+      ,cfg.itemID AS item_id
+      ,cfg.dateValue AS string_value
+      ,CONVERT(DATE, cfg.dateValue) AS date_value
+      ,NULL AS bit_value
+      ,NULL AS numeric_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+FROM gabby.adp.workers w
+CROSS APPLY OPENJSON(w.person, '$.customFieldGroup.dateFields') 
+  WITH(
+    itemID NVARCHAR(128)
+   ,nameCode NVARCHAR(MAX) AS JSON
+   ,dateValue NVARCHAR(128)
+ ) cfg
+WHERE JSON_QUERY(w.person, '$.customFieldGroup.dateFields') <> '{}'
+  AND cfg.dateValue IS NOT NULL
+
+UNION ALL
+
+SELECT w.associate_oid
+      ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'person' AS parent_object
+      ,'indicatorFields' AS custom_field_group
+
+      ,cfg.itemID AS item_id
+      ,cfg.indicatorValue AS string_value
+      ,NULL AS date_value
+      ,CONVERT(BIT, cfg.indicatorValue) AS bit_value
+      ,NULL AS numeric_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+FROM gabby.adp.workers w
+CROSS APPLY OPENJSON(w.person, '$.customFieldGroup.indicatorFields') 
+  WITH(
+    itemID NVARCHAR(128)
+   ,nameCode NVARCHAR(MAX) AS JSON
+   ,indicatorValue NVARCHAR(128)
+ ) cfg
+WHERE JSON_QUERY(w.person, '$.customFieldGroup.indicatorFields') <> '{}'
+  AND cfg.indicatorValue IS NOT NULL
+
+--UNION ALL
+
+--SELECT sub.associate_oid
+--      ,sub.worker_id
+--      ,'person' AS parent_object
+--      ,'multiCodeFields' AS custom_field_group
+--      ,sub.item_id
+--      ,cfg.codeValue AS string_value
+--      ,NULL AS date_value
+--      ,NULL AS bit_value
+--      ,CASE WHEN ISNUMERIC(cfg.codeValue) = 1 THEN CONVERT(INT, cfg.codeValue) END AS numeric_value
+--      ,sub.code_value
+--      ,sub.short_name
+--FROM
+--    (
+--     SELECT w.associate_oid
+--           ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+
+--           ,cfg.itemID AS item_id
+--           ,cfg.codes
+--           ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
+--           ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+--     FROM gabby.adp.workers w
+--     CROSS APPLY OPENJSON(w.person, '$.customFieldGroup.multiCodeFields') 
+--       WITH(
+--         itemID NVARCHAR(128)
+--        ,nameCode NVARCHAR(MAX) AS JSON
+--        ,codes NVARCHAR(MAX) AS JSON
+--      ) cfg
+--     WHERE JSON_QUERY(w.person, '$.customFieldGroup.multiCodeFields') <> '{}'
+--    ) sub
+--CROSS APPLY OPENJSON(sub.codes, '$') 
+--  WITH(
+--    codeValue NVARCHAR(128)
+--   ,longName NVARCHAR(MAX)
+--   ,shortName NVARCHAR(MAX)
+-- ) cfg
+--WHERE sub.codes <> '[]'
--- adp/v_adp.workers_custom_field_group_wide.sql
+++ adp/v_adp.workers_custom_field_group_wide.sql
@@ -5,16 +5,24 @@ CREATE OR ALTER VIEW adp.workers_custom_field_group_wide AS
 
 SELECT associate_oid
       ,worker_id
+      ,[Attended Relay]
+      ,[COVID 19 Vaccine Type]
+      ,[Date of last vaccine]
       ,[Employee Number]
-      ,[Miami - ACES Number]
+      ,[KIPP Alumni Status]
+      ,[Life Experience in Communities We Serve]
       ,[NJ Pension Number]
+      ,[Preferred Gender]
+      ,[Preferred Race/Ethnicity]
+      ,[Professional Experience in Communities We Serve]
+      ,[Teacher Prep Program]
       ,[WFMgr Accrual Profile]
+      ,[WFMgr Badge Number]
       ,[WFMgr EE Type]
       ,[WFMgr Home Hyperfind]
       ,[WFMgr LOA Return Date]
       ,[WFMgr Pay Rule]
       ,[WFMgr Trigger]
-      ,[WFMgr Badge Number]
       ,CASE 
         WHEN [WFMgr LOA] = 'true' THEN 1
         WHEN [WFMgr LOA] = 'false' THEN 0
@@ -29,15 +37,25 @@ FROM
     ) sub
 PIVOT(
   MAX(string_value)
-  FOR code_value IN ([Employee Number]
-                    ,[Miami - ACES Number]
-                    ,[NJ Pension Number]
-                    ,[WFMgr Accrual Profile]
-                    ,[WFMgr EE Type]
-                    ,[WFMgr Home Hyperfind]
-                    ,[WFMgr LOA]
-                    ,[WFMgr LOA Return Date]
-                    ,[WFMgr Pay Rule]
-                    ,[WFMgr Trigger]
-                    ,[WFMgr Badge Number])
+  FOR code_value IN (
+        [Attended Relay]
+       ,[COVID 19 Vaccine Type]
+       ,[Date of last vaccine]
+       ,[Employee Number]
+       ,[KIPP Alumni Status]
+       ,[Life Experience in Communities We Serve]
+       ,[NJ Pension Number]
+       ,[Preferred Gender]
+       ,[Preferred Race/Ethnicity]
+       ,[Professional Experience in Communities We Serve]
+       ,[Teacher Prep Program]
+       ,[WFMgr Accrual Profile]
+       ,[WFMgr Badge Number]
+       ,[WFMgr EE Type]
+       ,[WFMgr Home Hyperfind]
+       ,[WFMgr LOA Return Date]
+       ,[WFMgr LOA]
+       ,[WFMgr Pay Rule]
+       ,[WFMgr Trigger]
+      )
  ) p
