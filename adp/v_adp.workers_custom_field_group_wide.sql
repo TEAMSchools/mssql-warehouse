263ba0fa01b50d33299bf52d066ddbf059ad8bc8
Kevin Verhoff <kevin.verhoff@gmail.com>
2021-11-19 15:57:17 -0500

    adding new custom fields
    adding numeric value fields to workers_custom_field_group and workers_custom_field_group_wide
    
    Adjusting multiselect fields in workers_custom_field_group_wide

--- adp/v_adp.workers_custom_field_group_wide.sql
+++ adp/v_adp.workers_custom_field_group_wide.sql
@@ -1,32 +1,63 @@
 USE gabby
 GO
 
-CREATE OR ALTER VIEW adp.workers_custom_field_group_wide AS
+CREATE OR ALTER VIEW adp.workers_custom_field_group_wide AS (
 
-SELECT associate_oid
-      ,worker_id
-      ,[Attended Relay]
-      ,[COVID 19 Vaccine Type]
-      ,[Date of last vaccine]
-      ,[Employee Number]
-      ,[KIPP Alumni Status]
+WITH multi_pivot AS (
+SELECT worker_id
       ,[Life Experience in Communities We Serve]
-      ,[NJ Pension Number]
-      ,[Preferred Gender]
       ,[Preferred Race/Ethnicity]
       ,[Professional Experience in Communities We Serve]
       ,[Teacher Prep Program]
-      ,[WFMgr Accrual Profile]
-      ,[WFMgr Badge Number]
-      ,[WFMgr EE Type]
-      ,[WFMgr Home Hyperfind]
-      ,[WFMgr LOA Return Date]
-      ,[WFMgr Pay Rule]
-      ,[WFMgr Trigger]
+
+FROM (
+  
+  SELECT worker_id
+        ,code_value
+        ,gabby.dbo.GROUP_CONCAT(string_value) AS string_value
+  FROM gabby.adp.workers_custom_field_group
+  WHERE code_value IN ('Preferred Race/Ethnicity', 'Life Experience in Communities We Serve', 'Professional Experience in Communities We Serve', 'Teacher Prep Program')
+  GROUP BY worker_id, code_value
+       ) sub
+  PIVOT(MAX(string_value) FOR code_value IN ([Life Experience in Communities We Serve]
+                                            ,[Preferred Race/Ethnicity]
+                                            ,[Professional Experience in Communities We Serve]
+                                            ,[Teacher Prep Program]
+                                            )
+      ) p
+  )
+  
+
+SELECT p.associate_oid
+      ,p.worker_id
+      ,p.[Attended Relay]
+      ,p.[COVID 19 Vaccine Type]
+      ,p.[Date of last vaccine]
+      ,p.[Employee Number]
+      ,p.[KIPP Alumni Status]
+      ,p.[NJ Pension Number]
+      ,p.[Preferred Gender]
+      ,p.[WFMgr Accrual Profile]
+      ,p.[WFMgr Badge Number]
+      ,p.[WFMgr EE Type]
+      ,p.[WFMgr Home Hyperfind]
+      ,p.[WFMgr LOA Return Date]
+      ,p.[WFMgr LOA]
+      ,p.[WFMgr Pay Rule]
+      ,p.[WFMgr Trigger]
+      ,p.[Years Teaching - In NJ or FL]
+      ,p.[Years of Professional Experience before joining]
+      ,p.[Years Teaching - In any State]
       ,CASE 
-        WHEN [WFMgr LOA] = 'true' THEN 1
-        WHEN [WFMgr LOA] = 'false' THEN 0
+        WHEN p.[WFMgr LOA] = 'true' THEN 1
+        WHEN p.[WFMgr LOA] = 'false' THEN 0
        END AS [WFMgr LOA]
+
+      ,m.[Life Experience in Communities We Serve]
+      ,m.[Preferred Race/Ethnicity]
+      ,m.[Professional Experience in Communities We Serve]
+      ,m.[Teacher Prep Program]
+
 FROM
     (
      SELECT associate_oid
@@ -43,12 +74,8 @@ PIVOT(
        ,[Date of last vaccine]
        ,[Employee Number]
        ,[KIPP Alumni Status]
-       ,[Life Experience in Communities We Serve]
        ,[NJ Pension Number]
        ,[Preferred Gender]
-       ,[Preferred Race/Ethnicity]
-       ,[Professional Experience in Communities We Serve]
-       ,[Teacher Prep Program]
        ,[WFMgr Accrual Profile]
        ,[WFMgr Badge Number]
        ,[WFMgr EE Type]
@@ -57,5 +84,11 @@ PIVOT(
        ,[WFMgr LOA]
        ,[WFMgr Pay Rule]
        ,[WFMgr Trigger]
+       ,[Years Teaching - In NJ or FL]
+       ,[Years of Professional Experience before joining]
+       ,[Years Teaching - In any State]
       )
  ) p
+
+ LEFT JOIN multi_pivot m
+   ON p.worker_id = m.worker_id
