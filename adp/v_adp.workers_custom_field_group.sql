8adc6ffe83b599a43859f72a30a0811e6337708f
Kevin Verhoff <kevin.verhoff@gmail.com>
2021-12-01 10:18:07 -0500

    Implementing Feedback + correcting NULL shortnames
    Adding back deleted rows, adjusting short_names for NULLs

--- adp/v_adp.workers_custom_field_group.sql
+++ adp/v_adp.workers_custom_field_group.sql
@@ -38,7 +38,7 @@ SELECT w.associate_oid
       ,NULL AS bit_value
       ,CASE WHEN ISNUMERIC(cfg.codeValue) = 1 THEN CONVERT(INT, cfg.codeValue) END AS numeric_value
       ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
-      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS short_name
 FROM gabby.adp.workers w
 CROSS APPLY OPENJSON(w.custom_field_group, '$.codeFields') 
   WITH(
@@ -62,7 +62,7 @@ SELECT w.associate_oid
       ,NULL AS bit_value
       ,NULL AS numeric_value
       ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
-      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS short_name
 FROM gabby.adp.workers w
 CROSS APPLY OPENJSON(w.custom_field_group, '$.dateFields') 
   WITH(
@@ -97,7 +97,103 @@ CROSS APPLY OPENJSON(w.custom_field_group, '$.indicatorFields')
 WHERE JSON_QUERY(w.custom_field_group, '$.indicatorFields') <> '{}'
   AND cfg.indicatorValue IS NOT NULL
 
-  UNION ALL
+UNION ALL
+
+SELECT w.associate_oid
+      ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'person' AS parent_object
+      ,'stringFields' AS custom_field_group
+
+      ,cfg.itemID AS item_id
+      ,cfg.stringValue AS string_value
+      ,NULL AS date_value
+      ,NULL AS bit_value
+      ,CASE WHEN ISNUMERIC(cfg.stringValue) = 1 THEN CONVERT(INT, cfg.stringValue) END AS numeric_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+FROM gabby.adp.workers w
+CROSS APPLY OPENJSON(w.person, '$.customFieldGroup.stringFields') 
+  WITH(
+    itemID NVARCHAR(128)
+   ,nameCode NVARCHAR(MAX) AS JSON
+   ,stringValue NVARCHAR(128)
+ ) cfg
+WHERE JSON_QUERY(w.person, '$.customFieldGroup.stringFields') <> '{}'
+  AND cfg.stringValue IS NOT NULL
+
+UNION ALL
+
+SELECT w.associate_oid
+      ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'person' AS parent_object
+      ,'codeFields' AS custom_field_group
+
+      ,cfg.itemID AS item_id
+      ,cfg.codeValue AS string_value
+      ,NULL AS date_value
+      ,NULL AS bit_value
+      ,CASE WHEN ISNUMERIC(cfg.codeValue) = 1 THEN CONVERT(INT, cfg.codeValue) END AS numeric_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS short_name
+FROM gabby.adp.workers w
+CROSS APPLY OPENJSON(w.person, '$.customFieldGroup.codeFields')
+  WITH(
+    itemID NVARCHAR(128)
+   ,nameCode NVARCHAR(MAX) AS JSON
+   ,codeValue NVARCHAR(128)
+ ) cfg
+WHERE JSON_QUERY(w.person, '$.customFieldGroup.codeFields') <> '{}'
+  AND cfg.codeValue IS NOT NULL
+
+UNION ALL
+
+SELECT w.associate_oid
+      ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'person' AS parent_object
+      ,'dateFields' AS custom_field_group
+
+      ,cfg.itemID AS item_id
+      ,cfg.dateValue AS string_value
+      ,CONVERT(DATE, cfg.dateValue) AS date_value
+      ,NULL AS bit_value
+      ,NULL AS numeric_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+FROM gabby.adp.workers w
+CROSS APPLY OPENJSON(w.person, '$.customFieldGroup.dateFields') 
+  WITH(
+    itemID NVARCHAR(128)
+   ,nameCode NVARCHAR(MAX) AS JSON
+   ,dateValue NVARCHAR(128)
+ ) cfg
+WHERE JSON_QUERY(w.person, '$.customFieldGroup.dateFields') <> '{}'
+  AND cfg.dateValue IS NOT NULL
+
+UNION ALL
+
+SELECT w.associate_oid
+      ,CONVERT(NVARCHAR(16), JSON_VALUE(w.worker_id, '$.idValue')) AS worker_id
+      ,'person' AS parent_object
+      ,'indicatorFields' AS custom_field_group
+
+      ,cfg.itemID AS item_id
+      ,cfg.indicatorValue AS string_value
+      ,NULL AS date_value
+      ,CONVERT(BIT, cfg.indicatorValue) AS bit_value
+      ,NULL AS numeric_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
+      ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+FROM gabby.adp.workers w
+CROSS APPLY OPENJSON(w.person, '$.customFieldGroup.indicatorFields') 
+  WITH(
+    itemID NVARCHAR(128)
+   ,nameCode NVARCHAR(MAX) AS JSON
+   ,indicatorValue NVARCHAR(128)
+ ) cfg
+WHERE JSON_QUERY(w.person, '$.customFieldGroup.indicatorFields') <> '{}'
+  AND cfg.indicatorValue IS NOT NULL
+
+UNION ALL
 
 SELECT sub.associate_oid
       ,sub.worker_id
@@ -118,7 +214,7 @@ FROM
            ,cfg.itemID AS item_id
            ,cfg.codes
            ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS code_value
-           ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.shortName')) AS short_name
+           ,CONVERT(NVARCHAR(128), JSON_VALUE(cfg.nameCode, '$.codeValue')) AS short_name
      FROM gabby.adp.workers w
      CROSS APPLY OPENJSON(w.person, '$.customFieldGroup.multiCodeFields') 
        WITH(
