73779e25f53ef95f8b588dd14a84647c06044392
Charlie Bini <5003326+cbini@users.noreply.github.com>
2021-12-01 14:50:07 -0600

    remove union, group in cte, replace fields

--- adp/v_adp.workers_custom_field_group_wide.sql
+++ adp/v_adp.workers_custom_field_group_wide.sql
@@ -3,6 +3,15 @@ GO
 
 CREATE OR ALTER VIEW adp.workers_custom_field_group_wide AS
 
+WITH grouped_data AS (
+  SELECT associate_oid
+        ,worker_id
+        ,name_code_value
+        ,gabby.dbo.GROUP_CONCAT(item_value) AS item_value
+  FROM gabby.adp.workers_custom_field_group
+  GROUP BY associate_oid, worker_id, name_code_value
+ )
+
 SELECT p.associate_oid
       ,p.worker_id
       ,p.[Attended Relay]
@@ -17,45 +26,23 @@ SELECT p.associate_oid
       ,p.[WFMgr EE Type]
       ,p.[WFMgr Home Hyperfind]
       ,p.[WFMgr LOA Return Date]
-      ,p.[WFMgr LOA]
       ,p.[WFMgr Pay Rule]
       ,p.[WFMgr Trigger]
       ,p.[Years Teaching - In NJ or FL]
       ,p.[Years of Professional Experience before joining]
       ,p.[Years Teaching - In any State]
-      ,CASE 
-        WHEN p.[WFMgr LOA] = 'true' THEN 1
-        WHEN p.[WFMgr LOA] = 'false' THEN 0
-       END AS [WFMgr LOA]
-
       ,p.[Life Experience in Communities We Serve]
       ,p.[Preferred Race/Ethnicity]
       ,p.[Professional Experience in Communities We Serve]
       ,p.[Teacher Prep Program]
-
-FROM
-    (
-     SELECT associate_oid
-           ,worker_id
-           ,code_value
-           ,string_value
-     FROM gabby.adp.workers_custom_field_group
-     WHERE code_value NOT IN ('Preferred Race/Ethnicity', 'Life Experience in Communities We Serve', 'Professional Experience in Communities We Serve', 'Teacher Prep Program')
-
-     UNION ALL
-
-     SELECT associate_oid
-           ,worker_id
-           ,code_value
-           ,gabby.dbo.GROUP_CONCAT(string_value) AS string_value
-     FROM gabby.adp.workers_custom_field_group
-     WHERE code_value IN ('Preferred Race/Ethnicity', 'Life Experience in Communities We Serve', 'Professional Experience in Communities We Serve', 'Teacher Prep Program')
-     GROUP BY associate_oid, worker_id, code_value
-
-    ) sub
+      ,CASE
+        WHEN p.[WFMgr LOA] = 'true' THEN 1
+        WHEN p.[WFMgr LOA] = 'false' THEN 0
+       END AS [WFMgr LOA]
+FROM grouped_data
 PIVOT(
-  MAX(string_value)
-  FOR code_value IN (
+  MAX(item_value)
+  FOR name_code_value IN (
         [Attended Relay]
        ,[COVID 19 Vaccine Type]
        ,[Date of last vaccine]
