766b14b0ffe93ced3e7bd6dd243eb47351cc4828
Anthony Walters <awalters@kippteamandfamily.org>
2021-12-20 13:58:59 -0500

    Update v_tableau.student_info_audit.sql

--- tableau/v_tableau.student_info_audit.sql
+++ tableau/v_tableau.student_info_audit.sql
@@ -146,40 +146,27 @@ WHERE co.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
 UNION ALL
 
 SELECT co.[db_name]
-      ,schoolid
-      ,school_name
-      ,student_number 
-      ,region
-      ,lastfirst
-      ,grade_level
-      ,team
-      ,'Missing Race' AS element
-      ,CASE WHEN r.racecds IS NULL THEN 'Missing racecd' END AS detail
-      ,CASE WHEN r.racecds IS NULL THEN 1 ELSE 0 END AS flag
+      ,co.schoolid
+      ,co.school_name
+      ,co.student_number 
+      ,co.region
+      ,co.lastfirst
+      ,co.grade_level
+      ,co.team
+      ,'Missing Race/Ethnicity' AS element
+      ,CASE WHEN co.region = 'KMS' THEN co.ethnicity
+            WHEN co.region <> 'KMS' THEN r.racecds ELSE NULL END AS detail
+      ,CASE WHEN co.region = 'KMS' AND co.ethnicity IS NULL THEN 1
+            WHEN co.region = 'KMS' AND co.ethnicity = '' THEN 1
+            WHEN co.region <> 'KMS' AND r.racecds IS NULL AND s.fedethnicity IS NULL THEN 1 ELSE 0 END AS flag
 FROM gabby.powerschool.cohort_identifiers_static co
-LEFT JOIN race r
+LEFT JOIN gabby.powerschool.students s
+  ON co.student_number = s.student_number
+LEFT JOIN race r 
 ON co.studentid = r.studentid
 AND co.[db_name] = r.[db_name]
 WHERE academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
-  AND schoolid <> 999999
-  AND rn_year = 1
-
-UNION ALL
-
-SELECT co.[db_name]
-      ,schoolid
-      ,school_name
-      ,student_number 
-      ,region
-      ,lastfirst
-      ,grade_level
-      ,team
-      ,'Missing Ethnicity' AS element
-      ,CASE WHEN co.ethnicity IS NULL THEN 'Missing ethnicity' END AS detail
-      ,CASE WHEN co.ethnicity IS NULL THEN 1 ELSE 0 END AS flag
-FROM gabby.powerschool.cohort_identifiers_static co
-WHERE academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
-  AND schoolid <> 999999
+  AND co.schoolid <> 999999
   AND rn_year = 1
 
 UNION ALL
