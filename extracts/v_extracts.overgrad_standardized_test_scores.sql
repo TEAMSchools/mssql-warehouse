59cdad87a114611055d958958da7d2029baacc13
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-01-11 14:44:54 -0600

    Create v_extracts.overgrad_standardized_test_scores.sql

---
+++ extracts/v_extracts.overgrad_standardized_test_scores.sql
@@ -0,0 +1,28 @@
+USE gabby
+GO
+
+CREATE OR ALTER VIEW extracts.overgrad_standardized_test_scores AS
+
+SELECT stl.contact_c AS [Student ID]
+      ,stl.date_c AS [Test Date]
+      ,CONCAT(
+         CASE
+          WHEN stl.test_type = 'Advanced Placement' THEN 'AP'
+          WHEN stl.test_type = 'SAT' AND stl.score_type LIKE '%pre_2016%' THEN 'Old SAT'
+          WHEN stl.test_type = 'SAT' AND stl.score_type NOT LIKE '%essay%' THEN 'New SAT'
+          ELSE stl.test_type 
+         END
+        ,' ' + CASE 
+                WHEN stl.test_subject = 'Physics 1' THEN 'Physics 1: Algebra-Based'
+                WHEN stl.test_subject = 'Studio Art: 2-D Design Portfolio' THEN 'Studio Art: 2-D Design'
+                WHEN stl.test_subject = 'Studio Art: Drawing Portfolio' THEN 'Studio Art: Drawing'
+                WHEN stl.test_subject = 'United States History' THEN 'US History'
+                ELSE stl.test_subject
+               END
+       ) AS [Test]
+      ,stl.score AS [Score]
+
+      ,ei.hs_account_name AS [High School]
+FROM gabby.alumni.standardized_test_long stl
+JOIN gabby.alumni.enrollment_identifiers ei
+  ON stl.contact_c = ei.student_c
