78d1ce6a006da073c2e1c9916f6282a9780f4287
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-01-20 14:58:27 -0600

    fix subject_name :lipstick:

--- extracts/v_extracts.deanslist_iready_diagnostics.sql
+++ extracts/v_extracts.deanslist_iready_diagnostics.sql
@@ -8,9 +8,7 @@ SELECT di.student_id AS student_number
       ,di.diagnostic_percentile_most_recent_ AS percentile
       ,di.annual_typical_growth_measure
       ,di.annual_stretch_growth_measure
-      ,di.subject
+      ,di.[subject] AS subject_name
       ,di.diagnostic_gain_note_negative_gains_zero_
-
 FROM gabby.iready.diagnostic_and_instruction di
-
 WHERE di.diagnostic_completion_date_most_recent_ >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1)
