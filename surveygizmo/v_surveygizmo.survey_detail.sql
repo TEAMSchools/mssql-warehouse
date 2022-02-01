a57cd1be9283d9539ccde4ab04d0c4565dd64272
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-02-01 08:50:16 -0600

    Revert "reverting to regular joins"
    This reverts commit c5c0dec664bf006f05e0859fee882c58df2999e8.

--- surveygizmo/v_surveygizmo.survey_detail.sql
+++ surveygizmo/v_surveygizmo.survey_detail.sql
@@ -63,19 +63,19 @@ SELECT s.survey_id
 
       ,COALESCE(qo.option_title_english, srd.answer) AS answer
 FROM gabby.surveygizmo.survey_clean s
-JOIN gabby.surveygizmo.survey_question_clean_static sq
+INNER JOIN gabby.surveygizmo.survey_question_clean_static sq
   ON s.survey_id = sq.survey_id
  AND sq.base_type = 'Question'
  AND sq.is_identifier_question = 0
  AND sq.[type] IN ('RADIO', 'ESSAY', 'TEXTBOX')
-JOIN gabby.surveygizmo.survey_response_data srd
+INNER JOIN gabby.surveygizmo.survey_response_data srd
   ON sq.survey_id = srd.survey_id
  AND sq.survey_question_id = srd.question_id
-JOIN gabby.surveygizmo.survey_question_options_static qo
+LEFT JOIN gabby.surveygizmo.survey_question_options_static qo
   ON srd.survey_id = qo.survey_id
  AND srd.question_id = qo.question_id
  AND srd.answer_id = qo.option_id
-JOIN gabby.surveygizmo.survey_response_identifiers_static sri
+LEFT JOIN gabby.surveygizmo.survey_response_identifiers_static sri
   ON srd.survey_id = sri.survey_id
  AND srd.survey_response_id = sri.survey_response_id
  AND sri.[status] = 'Complete'
