1439dfe31bc926b721e2edf8d46346a2c213c058
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-02-08 15:53:19 -0600

    add hire date, fix date type

--- tableau/v_tableau.covid_vaccine_dashboard.sql
+++ tableau/v_tableau.covid_vaccine_dashboard.sql
@@ -11,12 +11,13 @@ SELECT sc.df_employee_number
       ,sc.primary_site
       ,sc.primary_job
       ,sc.manager_name
+      ,sc.original_hire_date
 
       ,vc.covid_19_vaccine_type
       ,vc.date_of_last_vaccine
       ,vc.covid_19_booster_1_type
       ,vc.covid_19_booster_1_date
-      ,MAX(vc._modified) OVER() AS refresh_timestamp
+      ,CAST(MAX(vc._modified) OVER() AS DATETIME2) AS refresh_timestamp
 FROM gabby.people.staff_crosswalk_static sc
 LEFT JOIN gabby.adp.vaccine_records vc
   ON sc.adp_associate_id = vc.associate_id
