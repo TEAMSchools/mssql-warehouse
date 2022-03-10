4942e56397203da4f50b15fb65fa5eab8ce0696f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-03-10 11:14:52 -0600

    :lipstick: only need app id on join

--- tableau/v_tableau.applicant_history.sql
+++ tableau/v_tableau.applicant_history.sql
@@ -4,11 +4,13 @@ GO
 CREATE OR ALTER VIEW tableau.applicant_history AS 
 
 SELECT c.candidate_id
-      ,ROW_NUMBER() OVER(
-			 PARTITION BY c.candidate_id
-			     ORDER BY COALESCE(app.application_state_lead_date,app.application_state_new_date)) AS row_most_recent
       ,c.candidate_first_name
       ,c.candidate_last_name
+      ,c.kf_race
+      ,c.kf_gender
+      ,c.kf_are_you_alumnus
+      ,c.kf_in_which_regions_alumnus
+
       ,app.job_city
       ,app.recruiters
       ,app.department_internal
@@ -18,7 +20,6 @@ SELECT c.candidate_id
       ,app.application_state_lead_date
       ,app.time_in_application_state_lead
       ,app.application_state_new_date
-      ,COALESCE(app.application_state_lead_date,app.application_state_new_date) AS enter_sr_date
       ,app.time_in_application_state_new
       ,app.application_status_last_change_date
       ,app.application_state_in_review_date
@@ -49,13 +50,12 @@ SELECT c.candidate_id
       ,app.application_status_interview_phone_screen_complete_date
       ,app.time_in_application_status_interview_phone_screen_requested
       ,app.application_status_interview_phone_screen_requested_date
-      ,c.kf_race
-      ,c.kf_gender
-      ,c.kf_are_you_alumnus
-      ,c.kf_in_which_regions_alumnus
-
+      ,COALESCE(app.application_state_lead_date, app.application_state_new_date) AS enter_sr_date
+      
+      ,ROW_NUMBER() OVER(
+         PARTITION BY c.candidate_id
+           ORDER BY COALESCE(app.application_state_lead_date, app.application_state_new_date)) AS row_most_recent
 FROM gabby.smartrecruiters.report_applications AS app
 JOIN gabby.smartrecruiters.report_applicants AS c
-  ON app.candidate_id = c.candidate_id
- AND app.application_id = c.application_id
-WHERE job_title != 'New Jersey - data migrated from Salesforce'
+  ON app.application_id = c.application_id
+WHERE app.job_title <> 'New Jersey - data migrated from Salesforce'
