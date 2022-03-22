330e3be8c4a08d4319d0ab65b8ed8b7063dae115
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-03-22 12:54:55 -0500

    Create q_qa.ar_individual_goals_audit.sql

---
+++ qa/q_qa.ar_individual_goals_audit.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM
+    (
+     SELECT vw.*
+
+           ,tbl.adjusted_goal AS adjusted_goal_tbl
+     FROM gabby.renaissance.ar_individualized_goals_long vw
+     FULL JOIN gabby.renaissance.ar_individualized_goals_long_static tbl
+       ON vw.student_number = tbl.student_number
+      AND vw.reporting_term = tbl.reporting_term
+    ) sub
+WHERE ISNULL(adjusted_goal, '') <> ISNULL(adjusted_goal_tbl, '')
