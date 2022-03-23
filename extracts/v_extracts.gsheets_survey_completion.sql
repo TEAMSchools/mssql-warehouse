deadb1fd99b1349d5fb5d01da599ed83949f1079
Cristina Baldor <ccbaldor@gmail.com>
2022-03-23 12:28:30 -0400

    date and formatting

--- extracts/v_extracts.gsheets_survey_completion.sql
+++ extracts/v_extracts.gsheets_survey_completion.sql
@@ -2,8 +2,7 @@ USE gabby
 GO
 CREATE OR ALTER VIEW extracts.gsheets_survey_completion AS
 
-SELECT 
-       academic_year
+SELECT academic_year
       ,reporting_term
       ,survey_taker_id
       ,survey_taker_name
@@ -13,4 +12,4 @@ SELECT
       ,survey_round_close
       ,survey_completion_date 
 FROM gabby.surveys.survey_tracking 
-WHERE GETDATE() BETWEEN survey_round_open AND survey_round_close
+WHERE CONVERT(DATE,GETDATE()) BETWEEN survey_round_open AND survey_round_close
