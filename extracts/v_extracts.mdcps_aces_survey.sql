bcadb1960c020fd1c8bd01c85f0b15147058fb9b
Cristina Baldor <ccbaldor@gmail.com>
2022-04-12 15:52:08 -0400

    Update v_extracts.mdcps_aces_survey.sql
    adding education info, switching to static table, deleting medical info no longer needed

--- extracts/v_extracts.mdcps_aces_survey.sql
+++ extracts/v_extracts.mdcps_aces_survey.sql
@@ -1,35 +1,42 @@
 USE gabby
 GO
 
-CREATE OR ALTER VIEW extracts.mdcps_aces_survey AS
+--CREATE OR ALTER VIEW extracts.mdcps_aces_survey AS
 
-SELECT sr.job_title 
-      ,sr.[location]
-      ,sr.position_status
-      ,sr.first_name
-      ,sr.last_name
-      ,sr.position_id
-      ,sr.business_unit
-      ,sr.employee_number
-      ,sr.original_hire_date
+SELECT s.primary_job
+      ,s.primary_site
+      ,s.[status]
+      ,s.first_name
+      ,s.last_name
+      ,s.position_id
+      ,s.legal_entity_name
+      ,s.df_employee_number
+      ,s.original_hire_date
+      ,s.flsa_status AS payclass
+      ,s.[address]
+      ,s.city
+      ,s.[state]
+      ,s.postal_code
+      ,s.annual_salary
+      ,s.userprincipalname
+      ,s.termination_date
+
       ,cf.[Miami - ACES Number] AS miami_aces
-      ,sr.flsa AS payclass
+
+      ,si.answer AS highest_education_level
+
       ,'2x Month' AS pay_frequency
-      ,NULL AS highest_education_level
-      ,NULL AS duty_days
+      ,'' AS duty_days
       ,'N/A' AS teacher_eval
-      ,sr.address_street
-      ,sr.address_city
-      ,sr.address_state
-      ,sr.address_zip
-      ,NULL AS MonthlyCarrierCost
-      ,NULL AS MedicalCoverage
       ,'N/A' AS Contribution504B
       ,'B' AS BasicLifePlan
-      ,sr.annual_salary
-      ,sr.position_effective_start_date
-      ,sr.termination_date
-FROM gabby.people.staff_roster sr
+
+FROM gabby.people.staff_crosswalk_static s
 LEFT JOIN gabby.adp.workers_custom_field_group_wide_static cf
-  ON sr.associate_id = cf.worker_id
-WHERE sr.business_unit = 'KIPP Miami'
+  ON s.adp_associate_id = cf.worker_id
+LEFT JOIN gabby.surveys.staff_information_survey_detail si
+  ON s.df_employee_number = si.employee_number
+WHERE s.legal_entity_name = 'KIPP Miami'
+  AND si.question_shortname = 'education_level'
+  AND si.rn_cur = 1
+ORDER BY s.[primary_site],s.last_name
