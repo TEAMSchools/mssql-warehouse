32f89814bdc028385fbcac55fd36828188f12545
Kevin Verhoff <kevin.verhoff@gmail.com>
2022-04-28 10:43:39 -0400

    Update v_people.years_experience.sql
    Two Changes:
    1) Adjusting the running sum from months to days, because the calculation was systemactically rounding down. Days will allow for more accuracy
    
    2) Adding in the survey demographic fields so that we can calculate total years teaching and years professional experience

--- people/v_people.years_experience.sql
+++ people/v_people.years_experience.sql
@@ -3,10 +3,10 @@ GO
 
 CREATE OR ALTER VIEW people.years_experience AS
 
-WITH months AS (
+WITH days_active AS (
   SELECT employee_number
-        ,ISNULL(active, 0) AS months_active
-        ,ISNULL(inactive, 0) AS months_inactive
+        ,ISNULL(active, 0) AS days_active
+        ,ISNULL(inactive, 0) AS days_inactive
   FROM 
       (
        SELECT employee_number
@@ -14,48 +14,46 @@ WITH months AS (
                WHEN position_status = 'Active' THEN 'active'
                ELSE 'inactive'
               END AS status_clean
-             ,DATEDIFF(MONTH, effective_start_date, effective_end_date) AS months
+             ,DATEDIFF(DAY, effective_start_date, effective_end_date) AS days
        FROM gabby.people.employment_history_static
        WHERE position_status NOT IN ('Terminated', 'Pre-Start')
          AND job_title <> 'Intern'
       ) sub
   PIVOT (
-    SUM(months)
+    SUM(days)
     FOR status_clean IN (active, inactive)
    ) p
  )
 
 ,years_teaching_at_kipp AS (
   SELECT was.employee_number
-        ,SUM(DATEDIFF(MONTH, was.effective_start_date, was.effective_end_date)) AS months_as_teacher
+        ,SUM(DATEDIFF(DAY, was.effective_start_date, was.effective_end_date)) AS days_as_teacher
   FROM gabby.people.employment_history_static was
   WHERE was.position_status NOT IN ('Terminated', 'Pre-Start')
     AND was.job_title IN ('Teacher', 'Learning Specialist', 'Learning Specialist Coordinator', 'Teacher in Residence', 'Teacher, ESL', 'Teacher ESL', 'Co-Teacher')
   GROUP BY was.employee_number
  )
 
-SELECT m.employee_number
-      ,ROUND(m.months_active / 12.0, 2) AS years_active_at_kipp
-      ,ROUND(m.months_inactive / 12.0, 2) AS years_inactive_at_kipp
-      ,ROUND(m.months_active / 12.0, 2) 
-         + ROUND(m.months_inactive / 12.0, 2) AS years_at_kipp_total
+SELECT d.employee_number
+      ,ROUND(d.days_active / 365.25, 2) AS years_active_at_kipp
+      ,ROUND(d.days_inactive / 365.25, 2) AS years_inactive_at_kipp
+      ,ROUND(d.days_active / 365.25, 2) 
+         + ROUND(d.days_inactive / 365.25, 2) AS years_at_kipp_total
 
-      ,ISNULL((y.months_as_teacher / 12), 0) AS years_teaching_at_kipp
+      ,ISNULL((y.days_as_teacher / 365.25), 0) AS years_teaching_at_kipp
 
-      --,ISNULL(dst.years_of_full_time_teaching, 0) AS years_teaching_prior_to_kipp
-      --,ISNULL(dst.years_full_time_experience, 0) AS years_experience_prior_to_kipp
-      ,0 AS years_teaching_prior_to_kipp
-      ,0 AS years_experience_prior_to_kipp
+      ,sdf.years_teaching_any_state AS years_teaching_prior_to_kipp
+      ,sdf.professional_experience_before_KIPP AS years_experience_prior_to_kipp
 
-      ,ISNULL((y.months_as_teacher / 12), 0) 
-         --+ ISNULL(dst.years_of_full_time_teaching, 0)
+      ,ISNULL((y.days_as_teacher / 365.25), 0) 
+         + ISNULL(sdf.years_teaching_any_state, 0)
          AS years_teaching_total
-      ,ROUND(m.months_active / 12.0, 2)
-         + ROUND(m.months_inactive / 12.0, 2)
-         --+ ISNULL(dst.years_full_time_experience, 0)
+      ,ROUND(d.days_active / 365.25, 2)
+         + ROUND(d.days_inactive / 365.25, 2)
+         + ISNULL(sdf.professional_experience_before_KIPP, 0)
          AS years_experience_total
-FROM months m
---LEFT JOIN gabby.recruiting.applicants dst
---  ON r.salesforce_id = dst.position_number
+FROM days_active d
 LEFT JOIN years_teaching_at_kipp y
-  ON m.employee_number = y.employee_number
+  ON d.employee_number = y.employee_number
+LEFT JOIN gabby.people.survey_demographics_fields sdf
+  ON d.employee_number = sdf.employee_number
