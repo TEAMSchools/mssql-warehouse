ff28641034eebaa333941890cd2b4d06cabefd1a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-04-29 13:40:45 -0500

    :lipstick:

--- extracts/v_extracts.mdcps_aces_survey.sql
+++ extracts/v_extracts.mdcps_aces_survey.sql
@@ -28,11 +28,8 @@ SELECT s.job_title
       ,'N/A' AS teacher_eval
       ,'N/A' AS Contribution504B
       ,'B' AS BasicLifePlan
-
 FROM gabby.people.staff_roster s
-JOIN gabby.adp.workers_custom_field_group_wide_static cf
+INNER JOIN gabby.adp.workers_custom_field_group_wide_static cf
   ON s.associate_id = cf.worker_id
 WHERE s.business_unit = 'KIPP Miami'
-  AND (s.termination_date >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(),07,01)
-   OR s.termination_date IS NULL)
-
+  AND (s.termination_date >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1) OR s.termination_date IS NULL)
