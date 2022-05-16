cdc63b3fa76706faafb83cde38af9834468dfa68
Cristina Baldor <ccbaldor@icloud.com>
2022-05-16 16:18:06 -0400

    Update v_recruiting.leading_indicators.sql

--- recruiting/v_recruiting.leading_indicators.sql
+++ recruiting/v_recruiting.leading_indicators.sql
@@ -3,13 +3,47 @@ GO
 
 --CREATE OR ALTER VIEW recruiting.leading_indicators AS
 
+WITH latest_update_date AS (
+SELECT application_id
+      ,MAX(latest_update) AS last_update_date
+FROM
+
+      (SELECT a.application_id 
+            ,a.application_state_new_date AS application_date
+            ,a.application_state_in_review_date AS review_date
+            ,a.application_state_interview_date AS interview_date
+            ,a.application_status_interview_phone_screen_requested_date AS phone_screen_requested
+            ,a.application_status_interview_phone_screen_complete_date AS phone_screen_complete
+            ,a.application_status_interview_demo_date AS final_interview_demo
+            ,a.application_state_offer_date AS offer_date
+            ,a.application_state_hired_date AS hired_date
+      FROM smartrecruiters.report_applications a) AS p
+
+UNPIVOT
+   (latest_update FOR date_val IN
+    (application_date
+    ,review_date
+    ,interview_date
+    ,phone_screen_requested
+    ,phone_screen_complete
+    ,final_interview_demo
+    ,offer_date
+    ,hired_date) ) AS unpvt
+GROUP BY application_id
+)
+
+
 SELECT a.application_id
       ,a.application_state_new_date AS application_date
+      ,a.application_state_in_review_date AS review_date
+      ,a.application_state_interview_date AS interview_date
       ,a.application_status_interview_phone_screen_requested_date AS phone_screen_requested
       ,a.application_status_interview_phone_screen_complete_date AS phone_screen_completed
       ,a.application_status_interview_demo_date AS final_interview_demo
       ,a.application_state_offer_date AS offer_made
       ,a.application_state_hired_date AS offer_accepted
+      ,a.application_state_rejected_date AS rejected_date
+      ,a.application_state_withdrawn_date AS withdrawn_date
       ,a.application_status
       ,a.department_internal
       ,a.job_city
@@ -34,19 +68,16 @@ SELECT a.application_id
              THEN 1
         ELSE 0
        END AS included_title
-      ,DATENAME(WW, a.application_state_new_date) AS application_date_week
-      ,DATENAME(WW, a.application_status_interview_phone_screen_requested_date) AS phone_screen_requested_week
-      ,DATENAME(WW, a.application_status_interview_phone_screen_complete_date) AS phone_screen_completed_week
-      ,DATENAME(WW, a.application_status_interview_demo_date) AS interview_demo_week
-      ,DATENAME(WW, a.application_state_offer_date) AS offer_made_week
-      ,DATENAME(WW, a.application_state_hired_date)AS offer_accepted_week
-
       ,p.candidate_id
       ,p.candidate_email
       ,p.candidate_first_name
       ,p.candidate_last_name
 
       ,DATENAME(WW, GETDATE()) AS current_week
+      ,d.last_update_date
+
 FROM smartrecruiters.report_applicants p
 INNER JOIN smartrecruiters.report_applications a
   ON p.application_id = a.application_id
+JOIN latest_update_date d
+  ON p.application_id = d.application_id
