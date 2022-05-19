bc938ad6491107464e6c81cd0a82d2c5d41ddf8f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-05-19 09:51:08 -0500

    refactor unpivot + :lipstick:

--- recruiting/v_recruiting.leading_indicators.sql
+++ recruiting/v_recruiting.leading_indicators.sql
@@ -3,34 +3,44 @@ GO
 
 CREATE OR ALTER VIEW recruiting.leading_indicators AS
 
-WITH latest_update_date AS (
+WITH app_unpivot AS (
   SELECT application_id
-        ,MAX(latest_update) AS last_update_date
+        ,status_type
+        ,date_val
   FROM
-    (SELECT a.application_id 
-        ,a.application_state_new_date AS application_date
-        ,a.application_state_in_review_date AS review_date
-        ,a.application_state_interview_date AS interview_date
-        ,a.application_status_interview_phone_screen_requested_date AS phone_screen_requested
-        ,a.application_status_interview_phone_screen_complete_date AS phone_screen_complete
-        ,a.application_status_interview_demo_date AS final_interview_demo
-        ,a.application_state_offer_date AS offer_date
-        ,a.application_state_hired_date AS hired_date
-     FROM smartrecruiters.report_applications a) AS p
-  
+      (
+       SELECT a.application_id 
+             ,a.application_state_new_date AS application_date
+             ,a.application_state_in_review_date AS review_date
+             ,a.application_state_interview_date AS interview_date
+             ,a.application_status_interview_phone_screen_requested_date AS phone_screen_requested
+             ,a.application_status_interview_phone_screen_complete_date AS phone_screen_complete
+             ,a.application_status_interview_demo_date AS final_interview_demo
+             ,a.application_state_offer_date AS offer_date
+             ,a.application_state_hired_date AS hired_date
+       FROM gabby.smartrecruiters.report_applications a
+      ) AS sub
   UNPIVOT(
-  latest_update 
-  FOR date_val IN (application_date
-                  ,review_date
-                  ,interview_date
-                  ,phone_screen_requested
-                  ,phone_screen_complete
-                  ,final_interview_demo
-                  ,offer_date
-                  ,hired_date) ) AS unpvt
-                  GROUP BY application_id
-  )
+    date_val 
+    FOR status_type IN (
+      application_date, 
+      review_date, 
+      interview_date, 
+      phone_screen_requested, 
+      phone_screen_complete, 
+      final_interview_demo, 
+      offer_date, 
+      hired_date
+    )
+  ) u
+ )
 
+,latest_update AS (
+  SELECT application_id
+        ,MAX(date_val) AS last_updated
+  FROM app_unpivot
+  GROUP BY application_id
+ )
 
 SELECT a.application_id
       ,a.application_state_new_date AS application_date
@@ -72,11 +82,11 @@ SELECT a.application_id
       ,p.candidate_first_name
       ,p.candidate_last_name
 
-      ,DATENAME(WW, GETDATE()) AS current_week
-      ,d.last_update_date
+      ,d.last_updated
 
-FROM smartrecruiters.report_applicants p
-INNER JOIN smartrecruiters.report_applications a
+      ,DATENAME(WW, GETDATE()) AS current_week
+FROM gabby.smartrecruiters.report_applicants p
+INNER JOIN gabby.smartrecruiters.report_applications a
   ON p.application_id = a.application_id
-JOIN latest_update_date d
+INNER JOIN latest_update d
   ON p.application_id = d.application_id
