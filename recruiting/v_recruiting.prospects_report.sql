6b86cca7df4af2985b75cf15113a579eb9591686
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-06-10 16:02:25 -0500

    fix TABLE > VIEW

--- recruiting/v_recruiting.prospects_report.sql
+++ recruiting/v_recruiting.prospects_report.sql
@@ -1,22 +1,22 @@
 USE gabby
 GO
 
-CREATE OR ALTER TABLE gabby.recruiting.prospects_report AS
+CREATE OR ALTER VIEW recruiting.prospects_report AS
 
-SELECT community_name
+SELECT candidate_id
+      ,candidate_email
       ,candidate_first_name
       ,candidate_last_name
-      ,candidate_email
+      ,candidate_source
+      ,candidate_tags_values
       ,current_employer
+      ,community_name
       ,community_application_status
       ,community_application_source
-      ,candidate_source
       ,community_application_state_added_date
       ,community_application_state_contacted_date
       ,community_application_state_not_interested_date
       ,community_application_state_not_selected_date
-      ,candidate_tags_values
       ,brand_org_field_value
-      ,candidate_id
-FROM smartrecruiters.report_prospects
-WHERE community_name LIKE '%Jami%'
+FROM gabby.smartrecruiters.report_prospects
+WHERE community_name LIKE 'New Jami%'
