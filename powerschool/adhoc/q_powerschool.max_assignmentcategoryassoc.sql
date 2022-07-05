56193d75477db081545885a88a1dad867fdcc29f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-07-05 14:19:00 -0500

    asdf

--- powerschool/adhoc/q_powerschool.max_assignmentcategoryassoc.sql
+++ powerschool/adhoc/q_powerschool.max_assignmentcategoryassoc.sql
@@ -3,14 +3,15 @@ GO
 
 SELECT ga.[db_name]
       ,MIN(ga.assignmentcategoryassocid) AS min_assignmentcategoryassocid
+      ,MAX(ga.assignmentcategoryassocid) + 1 AS next_min_assignmentcategoryassocid
 
       ,rt.alt_name
       ,rt.[start_date]
       ,rt.end_date
 FROM
     (
-     SELECT CONVERT(INT, asec.assignmentsectionid) AS assignmentsectionid
-           ,asec.duedate AS assign_date
+     SELECT asec.assignmentsectionid
+           ,asec.duedate
            ,asec.[db_name]
 
            ,aca.assignmentcategoryassocid
@@ -20,12 +21,9 @@ FROM
       AND asec.[db_name] = aca.[db_name]
      WHERE asec.duedate >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1)
     ) ga
-JOIN gabby.reporting.reporting_terms rt
-  ON ga.assign_date BETWEEN rt.[start_date] AND rt.end_date
+INNER JOIN gabby.reporting.reporting_terms rt
+  ON ga.duedate BETWEEN rt.[start_date] AND rt.end_date
  AND rt.identifier = 'RT'
  AND rt.schoolid = 0
-GROUP BY ga.[db_name]
-        ,rt.alt_name
-        ,rt.[start_date]
-        ,rt.end_date
-ORDER BY rt.alt_name, ga.[db_name]
+GROUP BY ga.[db_name], rt.alt_name, rt.[start_date], rt.end_date
+ORDER BY ga.[db_name], rt.alt_name
