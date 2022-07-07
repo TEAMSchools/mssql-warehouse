d211702b1886bc6c6fe3a89ddc04025adab4648f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-07-07 16:08:53 -0500

    move AY calc to main, remove order by

--- iready/v_iready.growth_metrics.sql
+++ iready/v_iready.growth_metrics.sql
@@ -5,9 +5,9 @@ CREATE OR ALTER VIEW iready.growth_metrics AS
 
 WITH baseline AS(
   SELECT dr.student_id AS student_number
-        ,CAST(dr.overall_scale_score AS FLOAT) AS baseline_scale
+        ,dr.academic_year
         ,dr.[percentile] AS baseline_percentile
-        ,LEFT(dr.academic_year, 4) AS academic_year
+        ,CAST(dr.overall_scale_score AS FLOAT) AS baseline_scale
         ,CASE 
           WHEN dr._file LIKE '%_ela%' THEN 'Reading'
           WHEN dr._file LIKE '%_math%' THEN 'Math'
@@ -19,9 +19,9 @@ WITH baseline AS(
 
 ,recent AS(
   SELECT dr.student_id AS student_number
-        ,CAST(dr.overall_scale_score AS FLOAT) AS recent_scale
+        ,dr.academic_year
         ,dr.[percentile] AS recent_percentile
-        ,LEFT(dr.academic_year, 4) AS academic_year
+        ,CAST(dr.overall_scale_score AS FLOAT) AS recent_scale
         ,CASE 
           WHEN dr._file LIKE '%_ela%' THEN 'Reading'
           WHEN dr._file LIKE '%_math%' THEN 'Math'
@@ -30,10 +30,10 @@ WITH baseline AS(
   FROM gabby.iready.diagnostic_results dr
   WHERE dr.diagnostic_used_to_establish_growth_measures_y_n_ = 'N'
     AND dr.most_recent_diagnostic_y_n_ = 'Y'
-)
+ )
 
 SELECT bl.student_number
-      ,bl.academic_year
+      ,LEFT(bl.academic_year, 4) AS academic_year
       ,bl.[subject]
       ,bl.baseline_scale
       ,bl.baseline_percentile
@@ -64,6 +64,4 @@ LEFT JOIN recent re
 LEFT JOIN gabby.iready.diagnostic_and_instruction di
   ON bl.student_number = di.student_id
  AND bl.[subject] = di.[subject]
- AND bl.academic_year = LEFT(di.academic_year, 4)
-
-ORDER BY bl.student_number, bl.academic_year, bl.subject
+ AND bl.academic_year = di.academic_year
