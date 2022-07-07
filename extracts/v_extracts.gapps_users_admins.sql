f09eeee6f325232233210529a32317aa326d399e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-07-07 13:17:11 -0500

    Update v_extracts.gapps_users_admins.sql

--- extracts/v_extracts.gapps_users_admins.sql
+++ extracts/v_extracts.gapps_users_admins.sql
@@ -31,6 +31,7 @@ SELECT df.google_email AS [user]
         WHEN df.primary_site_schoolid = 179902 THEN '/Students/KCNA/LSM'
         WHEN df.primary_site_schoolid = 179903 THEN '/Students/KCNA/WMS'
         WHEN df.primary_site_schoolid = 179904 THEN '/Students/KCNA/KCNHS'
+        WHEN df.primary_site_schoolid = 179905 THEN '/Students/KCNA/KSE'
         /* KMS */
         WHEN df.primary_site_schoolid = 30200801 THEN '/Students/Miami/Sunrise Academy'
         WHEN df.primary_site_schoolid = 30200802 THEN '/Students/Miami/Liberty Academy'
