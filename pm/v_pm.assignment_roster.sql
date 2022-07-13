9d91a0d91bb80c2ac9475fd90297130d8383a9bc
Cristina Baldor <ccbaldor@icloud.com>
2022-07-13 16:02:23 -0400

    Update v_pm.assignment_roster.sql

--- pm/v_pm.assignment_roster.sql
+++ pm/v_pm.assignment_roster.sql
@@ -1,7 +1,7 @@
 USE gabby
 GO
 
-CREATE OR ALTER VIEW pm.assignment_roster
+--CREATE OR ALTER VIEW pm.assignment_roster
 
 
 WITH elementary_grade AS (
@@ -28,34 +28,26 @@ SELECT
       ,s.legal_entity_name
       ,s.manager_df_employee_number
       /*default assignments based on title/location*/
-      ,CASE
-       WHEN s.primary_site NOT IN ('Room 9 - 60 Park Pl','Room 10 - 121 Market St','Room 11 - 1951 NW 7th Ave')
-       THEN 'Yes'
-       ELSE 'Yes - Manager survey only'
-       END AS survey_round_status
       ,CASE 
-        WHEN primary_site IN ('Room 9 - 60 Park Pl', 'Room 10 - 121 Market St', 'Room 11 - 1951 NW 7th Ave') THEN 'Regional Staff'
-        WHEN primary_job IN ('Teacher', 'Teacher in Residence', 'Learning Specialist', 'Learning Specialist Coordinator', 'Teacher, ESL', 'Teacher ESL') THEN 'Teacher'
-        WHEN primary_on_site_department = 'School Leadership' THEN 'School Leadership Team'
-        ELSE 'Non-teaching school based staff'
+       WHEN primary_site IN ('Room 9 - 60 Park Pl', 'Room 10 - 121 Market St', 'Room 11 - 1951 NW 7th Ave') THEN 'Regional Staff'
+       WHEN primary_job IN ('Teacher', 'Teacher in Residence', 'Learning Specialist', 'Learning Specialist Coordinator', 'Teacher, ESL', 'Teacher ESL') THEN 'Teacher'
+       WHEN primary_on_site_department = 'School Leadership' THEN 'School Leadership Team'
+       ELSE 'Non-teaching school based staff'
        END AS tntp_assignment
       ,CASE 
-        WHEN primary_job = 'Head of Schools' THEN 'Head of Schools'
-        WHEN primary_job = 'Assistant Superintendent' THEN 'Head of Schools'
-        WHEN primary_job IN ('Teacher', 'Teacher in Residence', 'Learning Specialist', 'Learning Specialist Coordinator', 'Teacher, ESL', 'Teacher ESL') THEN 'Teacher'
-        WHEN primary_job = 'Executive Director' THEN 'Executive Director'
-        WHEN primary_job IN ('Associate Director of School Operations') THEN 'ADSO'
-        WHEN primary_job IN ('Director Campus Operations', 'Director School Operations', 'Director of Campus Operations', 'Fellow School Operations Director') THEN 'DSO'
-        WHEN primary_job = 'Managing Director of Operations' THEN 'MDO'
-        WHEN primary_job = 'Managing Director of School Operations' THEN 'MDSO'
-        WHEN primary_job = 'School Leader' THEN 'School Leader'
-        WHEN primary_job IN ('Assistant School Leader', 'Assistant School Leader, SPED', 'School Leader in Residence') THEN 'AP'
-        ELSE 'Other'
+       WHEN primary_job = 'Head of Schools' THEN 'Head of Schools'
+       WHEN primary_job = 'Assistant Superintendent' THEN 'Head of Schools'
+       WHEN primary_job IN ('Teacher', 'Teacher in Residence', 'Learning Specialist', 'Learning Specialist Coordinator', 'Teacher, ESL', 'Teacher ESL') THEN 'Teacher'
+       WHEN primary_job = 'Executive Director' THEN 'Executive Director'
+       WHEN primary_job IN ('Associate Director of School Operations') THEN 'ADSO'
+       WHEN primary_job IN ('Director Campus Operations', 'Director School Operations', 'Director of Campus Operations', 'Fellow School Operations Director') THEN 'DSO'
+       WHEN primary_job = 'Managing Director of Operations' THEN 'MDO'
+       WHEN primary_job = 'Managing Director of School Operations' THEN 'MDSO'
+       WHEN primary_job = 'School Leader' THEN 'School Leader'
+       WHEN primary_job IN ('Assistant School Leader', 'Assistant School Leader, SPED', 'School Leader in Residence') THEN 'AP'
+       ELSE 'Other'
        END AS engagement_survey_assignment
-
 FROM people.staff_crosswalk_static s
-LEFT JOIN surveys.so_assignments a
-  ON s.df_employee_number = a.employee_number
 LEFT JOIN elementary_grade e
   ON s.df_employee_number = e.employee_number
 WHERE s.status = 'ACTIVE'
