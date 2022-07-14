0139b17148067fd0a937f6b488a0dfd48c0d5a2e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-07-14 13:56:58 -0500

    :lipstick: convert date

--- people/v_people.years_experience.sql
+++ people/v_people.years_experience.sql
@@ -14,7 +14,14 @@ WITH days_active AS (
                WHEN position_status = 'Active' THEN 'active'
                ELSE 'inactive'
               END AS status_clean
-             ,DATEDIFF(DAY, effective_start_date, CASE WHEN effective_end_date>GETDATE() THEN GETDATE() ELSE effective_end_date END) AS [days]
+             ,DATEDIFF(
+                DAY
+               ,effective_start_date
+               ,CASE
+                 WHEN effective_end_date > CONVERT(DATE, GETDATE()) THEN CONVERT(DATE, GETDATE()) 
+                 ELSE effective_end_date 
+                END
+              ) AS [days]
        FROM gabby.people.employment_history_static
        WHERE position_status NOT IN ('Terminated', 'Pre-Start')
          AND job_title <> 'Intern'
