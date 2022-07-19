63d636e44f245ac2077673052332832ab01403c9
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-07-19 14:34:34 -0500

    :lipstick:

--- tableau/v_tableau.compliance_school_register_summary.sql
+++ tableau/v_tableau.compliance_school_register_summary.sql
@@ -4,7 +4,7 @@ GO
 CREATE OR ALTER VIEW tableau.compliance_school_register_summary AS
 
 WITH att_mem AS (
-  SELECT studentid        
+  SELECT studentid
         ,yearid
         ,[db_name]
         ,SUM(attendancevalue) AS n_att
@@ -20,20 +20,16 @@ WITH att_mem AS (
 SELECT co.student_number
       ,co.lastfirst
       ,co.academic_year
-      ,co.region      
+      ,co.region
       ,co.reporting_schoolid
       ,co.grade_level
       ,co.entrydate
-      ,co.ethnicity      
+      ,co.ethnicity
       ,co.lunchstatus
       ,co.iep_status
       ,co.is_pathways
       ,co.track
       ,ISNULL(co.lep_status, 0) AS lep_status
-
-      ,nj.programtypecode
-
-      ,iep.nj_se_placement AS special_education_placement 
 
       ,d.days_total AS n_days_school
       ,d.days_remaining AS n_days_remaining
@@ -41,15 +37,11 @@ SELECT co.student_number
       ,sub.n_mem
       ,sub.n_att
       ,sub.n_mem_ytd
+
+      ,nj.programtypecode
+
+      ,iep.nj_se_placement AS special_education_placement 
 FROM gabby.powerschool.cohort_identifiers_static co
-LEFT JOIN gabby.powerschool.s_nj_stu_x nj
-  ON co.students_dcid = nj.studentsdcid
- AND co.[db_name] = nj.[db_name]
-LEFT JOIN gabby.easyiep.njsmart_powerschool_clean_static iep
-  ON co.student_number = iep.student_number
- AND co.academic_year = iep.academic_year
- AND co.[db_name] = iep.[db_name]
- AND iep.rn_stu_yr = 1
 INNER JOIN gabby.powerschool.calendar_rollup_static d
   ON co.schoolid = d.schoolid
  AND co.yearid = d.yearid
@@ -59,4 +51,12 @@ INNER JOIN att_mem sub
   ON co.studentid = sub.studentid
  AND co.yearid = sub.yearid
  AND co.[db_name] = sub.[db_name]
+LEFT JOIN gabby.powerschool.s_nj_stu_x nj
+  ON co.students_dcid = nj.studentsdcid
+ AND co.[db_name] = nj.[db_name]
+LEFT JOIN gabby.easyiep.njsmart_powerschool_clean_static iep
+  ON co.student_number = iep.student_number
+ AND co.academic_year = iep.academic_year
+ AND co.[db_name] = iep.[db_name]
+ AND iep.rn_stu_yr = 1
 WHERE co.rn_year = 1
