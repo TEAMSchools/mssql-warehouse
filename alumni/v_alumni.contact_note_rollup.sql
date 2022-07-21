d4b91d9a3d7a273a031e70c41024cbfcb3defb79
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-07-21 11:25:51 -0500

    remove fields, add to term CASE
    - comments/next steps
    - F/S depending on month

--- alumni/v_alumni.contact_note_rollup.sql
+++ alumni/v_alumni.contact_note_rollup.sql
@@ -1,7 +1,7 @@
 USE gabby
 GO
 
-CREATE OR ALTER VIEW alumni.contact_note_rollup AS
+--CREATE OR ALTER VIEW alumni.contact_note_rollup AS
 
 SELECT contact_id
       ,academic_year
@@ -78,14 +78,12 @@ SELECT contact_id
       ,ISDATE(CONVERT(VARCHAR(10) ,[DP_WorkforceF])) AS [DP_WorkforceF]
       ,ISDATE(CONVERT(VARCHAR(10) ,[DP_UnknownF])) AS [DP_UnknownF]
 
-
       ,ISDATE(CONVERT(VARCHAR(10) ,[BGP_4yearF])) AS [BGP_4yearF]
       ,ISDATE(CONVERT(VARCHAR(10) ,[BGP_2yearF])) AS [BGP_2yearF]
       ,ISDATE(CONVERT(VARCHAR(10) ,[BGP_CTEF])) AS [BGP_CTEF]
       ,ISDATE(CONVERT(VARCHAR(10) ,[BGP_MilitaryF])) AS [BGP_MilitaryF]
       ,ISDATE(CONVERT(VARCHAR(10) ,[BGP_WorkforceF])) AS [BGP_WorkforceF]
       ,ISDATE(CONVERT(VARCHAR(10) ,[BGP_UnknownF])) AS [BGP_UnknownF]
-
 
       ,ISDATE(CONVERT(VARCHAR(10), [HD_P])) AS [HD_P]
       ,ISDATE(CONVERT(VARCHAR(10), [HD_NR])) AS [HD_NR]
@@ -97,23 +95,15 @@ FROM
            ,sub.academic_year
            ,sub.contact_subject + sub.contact_term AS contact_type
            ,sub.contact_date
-		   ,sub.contact_comments
-		   ,sub.contact_next steps
+           --,sub.contact_comments
+           --,sub.contact_next_steps
      FROM
          (
           SELECT c.contact_c AS contact_id
-                ,gabby.utilities.DATE_TO_SY(c.date_c) AS academic_year
+                ,c.date_c AS contact_date
+                --,c.comments_c AS contact_comments
+                --,c.next_steps_c AS contact_next_steps
                 ,CASE
-                  WHEN c.subject_c IN ('Summer AAS', 'CCDM') THEN ''
-                  WHEN c.subject_c LIKE 'MC%' THEN ''
-                  WHEN c.subject_c LIKE '%HV' THEN ''
-                  WHEN c.subject_c LIKE 'SC[0-9]%' THEN ''
-                  WHEN c.subject_c LIKE 'Housing Deposit%' THEN ''
-                  WHEN c.subject_c LIKE 'Tuition Deposit%' THEN ''
-                  WHEN c.subject_c LIKE 'Q%' THEN 'Q' + SUBSTRING(c.subject_c, 2, 1)
-
-                 END AS contact_term
-                ,CASE 
                   WHEN c.subject_c LIKE 'SC[0-9]%' THEN 'SC'
                   WHEN c.subject_c LIKE 'Advising Session%' THEN 'AS'
                   WHEN c.subject_c = 'Summer AAS' THEN 'AS'
@@ -128,20 +118,33 @@ FROM
                   WHEN c.subject_c = 'Tuition Deposit Not Required' THEN 'TD_NR'
                   ELSE c.subject_c 
                  END AS contact_subject
-                ,c.date_c AS contact_date
+                ,CASE
+                  WHEN c.subject_c IN ('Summer AAS', 'CCDM') THEN ''
+                  WHEN c.subject_c LIKE 'Advising Session%' THEN ''
+                  WHEN c.subject_c LIKE 'MC%' THEN ''
+                  WHEN c.subject_c LIKE '%HV' THEN ''
+                  WHEN c.subject_c LIKE 'SC[0-9]%' THEN ''
+                  WHEN c.subject_c LIKE 'Housing Deposit%' THEN ''
+                  WHEN c.subject_c LIKE 'Tuition Deposit%' THEN ''
+                  WHEN c.subject_c LIKE 'Q%' THEN 'Q' + SUBSTRING(c.subject_c, 2, 1)
+                  /* catch-all for unspecified subjects */
+                  WHEN MONTH(c.date_c) >= 7 THEN 'F'
+                  WHEN MONTH(c.date_c) < 7 THEN 'S'
+                 END AS contact_term
+                ,gabby.utilities.DATE_TO_SY(c.date_c) AS academic_year
           FROM gabby.alumni.contact_note_c c
           WHERE c.is_deleted = 0
 
           UNION ALL
 
           SELECT contact_c
-                ,gabby.utilities.DATE_TO_SY(benchmark_date_c) AS academic_year
+                ,benchmark_date_c AS contact_date
+                ,'BM' AS contact_subject
                 ,CASE
                   WHEN MONTH(benchmark_date_c) >= 7 THEN 'F'
                   WHEN MONTH(benchmark_date_c) < 7 THEN 'S'
                  END AS contact_term
-                ,'BM' AS contact_subject
-                ,benchmark_date_c AS contact_date
+                ,gabby.utilities.DATE_TO_SY(benchmark_date_c) AS academic_year
           FROM gabby.alumni.college_persistence_c
           WHERE benchmark_status_c = 'Complete'
             AND benchmark_period_c <> 'Pre-College'
@@ -151,7 +154,7 @@ FROM
 PIVOT(
   MIN(contact_date)
   FOR contact_type IN ([AS1F],[AS2F],[AS3F],[AS4F],[AS5F],[AS6F],[AS7F],[AS8F],[AS9F],[AS10F],[AS11F],[AS12F],
-					   [AS13F],[AS14F],[AS15F],[AS16F],[AS17F],[AS18F],[AS19F],[AS20F],[AS21F],[AS22F],[AS23F],[AS24F]
+                       [AS13F],[AS14F],[AS15F],[AS16F],[AS17F],[AS18F],[AS19F],[AS20F],[AS21F],[AS22F],[AS23F],[AS24F]
                       ,[PSCF]
                       ,[BBBF]
                       ,[BMF]
@@ -160,4 +163,5 @@ PIVOT(
                       ,[SC],[CCDM],[HV]
                       ,[DP_4yearF],[DP_2yearF],[DP_CTEF],[DP_MilitaryF],[DP_WorkforceF],[DP_UnknownF]
                       ,[BGP_4yearF],[BGP_2yearF],[BGP_CTEF],[BGP_MilitaryF],[BGP_WorkforceF],[BGP_UnknownF]
-					  ,[HD_P],[HD_NR],[TD_P],[TD_NR])
+                      ,[HD_P],[HD_NR],[TD_P],[TD_NR])
+ ) p
