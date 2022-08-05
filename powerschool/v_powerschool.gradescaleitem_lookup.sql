7f40cbcfd9695d54c87b3606aec5fac639cbe546
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-05 13:36:31 -0500

    make decimal

--- powerschool/v_powerschool.gradescaleitem_lookup.sql
+++ powerschool/v_powerschool.gradescaleitem_lookup.sql
@@ -1,15 +1,15 @@
 CREATE OR ALTER VIEW powerschool.gradescaleitem_lookup AS
 
-SELECT CONVERT(INT, parent.id) AS gradescaleid
-      ,CONVERT(VARCHAR(125), parent.[name]) AS gradescale_name
+SELECT parent.id AS gradescaleid
+      ,parent.[name] AS gradescale_name
 
-      ,CONVERT(VARCHAR(125), items.[name]) AS letter_grade
+      ,items.[name] AS letter_grade
       ,items.grade_points
       ,items.cutoffpercentage AS min_cutoffpercentage
       ,LEAD(items.cutoffpercentage, 1, 1000) OVER(
          PARTITION BY parent.id 
-           ORDER BY items.cutoffpercentage) - 1 AS max_cutoffpercentage
+           ORDER BY items.cutoffpercentage) - 0.1 AS max_cutoffpercentage
 FROM powerschool.gradescaleitem parent
-JOIN powerschool.gradescaleitem items
+INNER JOIN powerschool.gradescaleitem items
   ON parent.id = items.gradescaleid
 WHERE parent.gradescaleid = -1
