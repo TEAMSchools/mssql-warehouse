064de904fd0c654171d8274cd2af28398c11ce9b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-24 14:48:53 -0500

    Update v_ops.income_form_data_clean.sql

--- ops/v_ops.income_form_data_clean.sql
+++ ops/v_ops.income_form_data_clean.sql
@@ -6,13 +6,13 @@ CREATE OR ALTER VIEW ops.income_form_data_clean AS
 SELECT student_number
       ,academic_year
       ,CASE 
-        WHEN [status] IS NULL OR [status] = 'No Application' THEN 'No Application'
+        WHEN [status] IS NULL OR [status] IN ('No Application', 'No') THEN 'No Application'
         WHEN [status] IN ('Application Received', 'Jotform') THEN 'Application Received'
         WHEN [status] = 'Direct Certification' THEN 'Direct Certification'
         ELSE LEFT([status], 1) + ' - ' + 'Income Form'
        END AS lunch_app_status
       ,CASE
-        WHEN [status] IN ('No Application', 'Application Received', 'Jotform') THEN NULL
+        WHEN [status] IN ('No Application', 'Application Received', 'Jotform', 'No') THEN NULL
         WHEN [status] = 'Direct Certification' THEN 'F'
         ELSE LEFT([status], 1)
        END AS lunch_status
