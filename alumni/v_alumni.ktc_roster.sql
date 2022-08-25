101db1d9b4c83564b7e665e511d043d7dc3664fb
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-25 14:24:48 -0500

    clean up logic

--- alumni/v_alumni.ktc_roster.sql
+++ alumni/v_alumni.ktc_roster.sql
@@ -136,20 +136,11 @@ FROM
            ,COALESCE(c.last_name + ', ' + c.first_name, co.lastfirst) COLLATE LATIN1_GENERAL_BIN AS lastfirst
 
            ,CASE
+             WHEN co.enroll_status = 0 THEN CONCAT(co.school_level, co.grade_level)
              WHEN c.kipp_hs_graduate_c = 1 THEN 'HSG'
-             WHEN co.school_level = 'HS' AND co.exitcode = 'G1' THEN 'HSG'
+             WHEN co.school_level = 'HS' AND co.exitcode = 'G1' THEN 'HSG' /* identify HS grads before SF enr update */
              WHEN c.kipp_ms_graduate_c = 1 AND c.kipp_hs_graduate_c = 0 AND rt.[name] = 'HS Student' THEN 'TAFHS'
              WHEN c.kipp_ms_graduate_c = 1 AND c.kipp_hs_graduate_c = 0 THEN 'TAF'
-             WHEN co.enroll_status = 0 THEN CONCAT('HS', co.grade_level)
-             WHEN rt.[name] = 'HS Student'
-              AND co.grade_level = 8
-              AND MONTH(co.exitdate) IN (6, 7)
-              AND (co.exitcode = 'G1' OR (co.exitcode LIKE 'T%' AND co.exitcode <> 'T2'))
-                  THEN 'TAFHS'
-             WHEN co.grade_level = 8
-              AND MONTH(co.exitdate) IN (6, 7)
-              AND (co.exitcode = 'G1' OR (co.exitcode LIKE 'T%' AND co.exitcode <> 'T2'))
-                  THEN 'TAF'
             END AS ktc_status
      FROM gabby.powerschool.cohort_identifiers_static co
      LEFT JOIN gabby.alumni.contact c
