8f5ba8566a6f9f5cbeb8a10e2d043eaec834648a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-16 14:00:41 -0500

    Update q_reset_table.sql

--- utilities/adhoc/q_reset_table.sql
+++ utilities/adhoc/q_reset_table.sql
@@ -3,11 +3,18 @@ DECLARE @db_name NVARCHAR(MAX) = DB_NAME()
        ,@view_name NVARCHAR(MAX) = ''
        ,@objname NVARCHAR(MAX)
        ,@table_name NVARCHAR(MAX)
-       ,@table_name_old NVARCHAR(MAX);
+       ,@table_name_old NVARCHAR(MAX)
+       ,@drop_sql NVARCHAR(MAX);
 
-SET @table_name = @view_name + '_static'
+SET @table_name = @view_name + '_static';
 SET @objname = CONCAT(@schema_name, '.', @table_name);
 SET @table_name_old = @table_name + '_OLD';
+SET @drop_sql = 'IF OBJECT_ID(N''' + @db_name + '.' + @schema_name + '.' + @table_name_old + ''') IS NOT NULL
+  BEGIN
+    DROP TABLE ' + @db_name + '.' + @schema_name + '.' + @table_name_old + ';
+  END';
+
+EXEC sp_sqlexec @drop_sql;
 
 EXEC sp_rename @objname=@objname, @newname=@table_name_old;
 
