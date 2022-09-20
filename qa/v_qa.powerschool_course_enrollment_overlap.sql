f6bb55923b4718c41c50ebafd1372e5035204aec
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-20 16:55:40 -0500

    Update v_qa.powerschool_course_enrollment_overlap.sql

--- qa/v_qa.powerschool_course_enrollment_overlap.sql
+++ qa/v_qa.powerschool_course_enrollment_overlap.sql
@@ -3,27 +3,35 @@ GO
 
 CREATE OR ALTER VIEW qa.powerschool_course_enrollment_overlap AS
 
-SELECT [db_name]
-      ,student_number
-      ,studentid
-      ,academic_year
-      ,schoolid
-      ,course_number
-      ,course_name
-      ,dateleft
-      ,dateleft_prev
-FROM
-    (
-     SELECT [db_name]
-           ,student_number
-           ,studentid
-           ,academic_year
-           ,schoolid
-           ,course_number
-           ,course_name
-           ,dateenrolled
-           ,dateleft
-           ,LAG(dateleft) OVER(PARTITION BY student_number, academic_year, course_number ORDER BY dateleft) AS dateleft_prev
-     FROM gabby.powerschool.course_enrollments_current_static
-    ) sub
-WHERE dateleft <= dateleft_prev
+WITH cc_lag AS (
+  SELECT studentid
+        ,studyear
+        ,schoolid
+        ,course_number
+        ,sectionid
+        ,dateenrolled
+        ,dateleft
+        ,[db_name]
+        ,LAG(dateleft) OVER(PARTITION BY studentid, studyear, course_number, [db_name] ORDER BY dateleft) AS dateleft_prev
+  FROM gabby.powerschool.cc
+)
+
+SELECT cc.studentid
+      ,s.student_number
+      ,CAST(RIGHT(cc.studyear, 2) AS INT) + 1990 AS academic_year
+      ,cc.schoolid
+      ,cc.course_number
+      ,sec.course_name
+      ,sec.sectionid
+      ,cc.dateenrolled
+      ,cc.dateleft
+      ,cc.dateleft_prev
+      ,cc.[db_name]
+FROM cc_lag cc
+INNER JOIN gabby.powerschool.students s
+  ON cc.studentid = s.id
+INNER JOIN gabby.powerschool.sections_identifiers sec
+  ON ABS(cc.sectionid) = sec.sectionid
+ AND cc.[db_name] = sec.[db_name]
+WHERE cc.dateleft <= cc.dateleft_prev
+  AND cc.dateenrolled >= DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR(), 7, 1)
