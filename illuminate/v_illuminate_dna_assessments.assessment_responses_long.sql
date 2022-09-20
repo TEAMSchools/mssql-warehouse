0d1ac35778dd810b105157b123f5a894f0afb5a3
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-20 16:55:13 -0500

    static year

--- illuminate/v_illuminate_dna_assessments.assessment_responses_long.sql
+++ illuminate/v_illuminate_dna_assessments.assessment_responses_long.sql
@@ -23,7 +23,7 @@ WITH asmts AS (
         ,ROW_NUMBER() OVER(
            PARTITION BY a.student_id, a.assessment_id 
              ORDER BY sa.student_assessment_id DESC) AS rn
-  FROM gabby.illuminate_dna_assessments.student_assessment_scaffold a
+  FROM gabby.illuminate_dna_assessments.student_assessment_scaffold_current_static a
   LEFT JOIN gabby.illuminate_dna_assessments.students_assessments sa
     ON a.student_id = sa.student_id
    AND a.assessment_id = sa.assessment_id
@@ -57,7 +57,7 @@ SELECT a.student_id
 FROM asmts a  
 LEFT JOIN gabby.illuminate_dna_assessments.agg_student_responses asr
   ON a.student_assessment_id = asr.student_assessment_id
- AND asr.points_possible > 0  
+ AND asr.points_possible > 0
 WHERE a.rn = 1
 
 UNION ALL
@@ -65,7 +65,7 @@ UNION ALL
 SELECT a.student_id
       ,a.assessment_id
       ,a.title
-      ,a.academic_year    
+      ,a.academic_year
       ,a.administered_at
       ,a.scope
       ,a.subject_area
@@ -80,22 +80,22 @@ SELECT a.student_id
       ,'S' AS response_type
 
       ,asrs.points
-      ,asrs.points_possible        
-      ,asrs.percent_correct             
+      ,asrs.points_possible
+      ,asrs.percent_correct
       ,asrs.standard_id
 
-      ,CAST(std.custom_code AS VARCHAR(125)) AS standard_code
-      ,CAST(std.description AS VARCHAR(2000)) AS standard_description
+      ,CAST(std.custom_code AS NVARCHAR(128)) AS standard_code
+      ,CAST(std.[description] AS NVARCHAR(2048)) AS standard_description
 
       ,dom.domain_description
 FROM asmts a  
-JOIN gabby.illuminate_dna_assessments.agg_student_responses_standard asrs
+INNER JOIN gabby.illuminate_dna_assessments.agg_student_responses_standard asrs
   ON a.student_assessment_id = asrs.student_assessment_id
  AND asrs.points_possible > 0  
-JOIN gabby.illuminate_dna_assessments.assessment_standards astd
+INNER JOIN gabby.illuminate_dna_assessments.assessment_standards astd
   ON asrs.assessment_id = astd.assessment_id
  AND asrs.standard_id = astd.standard_id
-JOIN gabby.illuminate_standards.standards std
+INNER JOIN gabby.illuminate_standards.standards std
   ON asrs.standard_id = std.standard_id   
 LEFT JOIN gabby.illuminate_standards.standards_domain_static dom
   ON asrs.standard_id = dom.standard_id
@@ -128,15 +128,15 @@ SELECT a.student_id
       ,asrg.reporting_group_id AS standard_id
 
       ,NULL AS standard_code
-      ,rg.label AS standard_description
+      ,rg.[label] AS standard_description
       ,NULL AS domain_description
 FROM asmts a  
-JOIN gabby.illuminate_dna_assessments.agg_student_responses_group asrg
+INNER JOIN gabby.illuminate_dna_assessments.agg_student_responses_group asrg
   ON a.student_assessment_id = asrg.student_assessment_id      
  AND asrg.points_possible > 0       
-JOIN gabby.illuminate_dna_assessments.assessments_reporting_groups arg
+INNER JOIN gabby.illuminate_dna_assessments.assessments_reporting_groups arg
   ON asrg.assessment_id = arg.assessment_id
  AND asrg.reporting_group_id = arg.reporting_group_id
-JOIN gabby.illuminate_dna_assessments.reporting_groups rg
+INNER JOIN gabby.illuminate_dna_assessments.reporting_groups rg
   ON asrg.reporting_group_id = rg.reporting_group_id
 WHERE a.rn = 1
