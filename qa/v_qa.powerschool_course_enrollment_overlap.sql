7a363972cd4fb726d6db78c503e0f3dfdafd15b2
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-29 13:45:14 -0500

    replace with qa view

--- qa/v_qa.powerschool_course_enrollment_overlap.sql
+++ qa/v_qa.powerschool_course_enrollment_overlap.sql
@@ -17,19 +17,29 @@ WITH cc_lag AS (
 )
 
 SELECT cc.studentid
-      ,s.student_number
-      ,CAST(RIGHT(cc.studyear, 2) AS INT) + 1990 AS academic_year
       ,cc.schoolid
       ,cc.course_number
-      ,sec.course_name
       ,cc.sectionid
+      ,cc.section_number
       ,cc.dateenrolled
       ,cc.dateleft
       ,cc.[db_name]
+      ,CAST(RIGHT(cc.studyear, 2) AS INT) + 1990 AS academic_year
+
+      ,s.student_number
+      ,s.lastfirst
+      ,s.grade_level
+      ,s.team
+
+      ,sec.course_name
 FROM gabby.powerschool.cc
 INNER JOIN gabby.powerschool.students s
   ON cc.studentid = s.id
 INNER JOIN gabby.powerschool.sections_identifiers sec
   ON ABS(cc.sectionid) = sec.sectionid
  AND cc.[db_name] = sec.[db_name]
-WHERE CONCAT(cc.studentid, cc.studyear, cc.course_number, cc.[db_name]) IN (SELECT CONCAT(studentid, studyear, course_number, [db_name]) COLLATE Latin1_General_BIN FROM cc_lag WHERE dateleft <= dateleft_prev)
+WHERE CONCAT(cc.studentid, cc.studyear, cc.course_number, cc.[db_name]) IN (
+        SELECT CONCAT(studentid, studyear, course_number, [db_name]) COLLATE Latin1_General_BIN 
+        FROM cc_lag 
+        WHERE dateleft <= dateleft_prev
+      )
