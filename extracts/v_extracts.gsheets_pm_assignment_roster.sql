895b55d5316e08fffb63b5c6caa3d91408b70f75
Cristina Baldor <ccbaldor@icloud.com>
2022-10-07 17:12:07 -0400

    Update v_extracts.gsheets_pm_assignment_roster.sql
    adding site to name, manager name, school-based, SOMs

--- extracts/v_extracts.gsheets_pm_assignment_roster.sql
+++ extracts/v_extracts.gsheets_pm_assignment_roster.sql
@@ -13,26 +13,32 @@ WITH elementary_grade AS (
 /*active staff info*/
 SELECT 
        s.df_employee_number
-      ,s.preferred_name
+      ,CONCAT(s.preferred_name,' - ',s.primary_site) AS preferred_name
       ,s.primary_job
       ,s.google_email
       ,s.userprincipalname AS user_email
       ,s.primary_site
       ,s.legal_entity_name
       ,s.manager_df_employee_number
-      /*default assignments based on title/location*/
+      ,s.manager_name
+      /*default TNTP assignments based on title/location*/
       ,CASE 
         WHEN s.primary_site IN ('Room 9 - 60 Park Pl', 'Room 10 - 121 Market St', 'Room 11 - 1951 NW 7th Ave') THEN 'Regional Staff'
         WHEN s.primary_job IN ('Teacher', 'Teacher in Residence', 'Learning Specialist', 'Learning Specialist Coordinator', 'Teacher, ESL', 'Teacher ESL') THEN 'Teacher'
         WHEN s.primary_on_site_department = 'School Leadership' THEN 'School Leadership Team'
         ELSE 'Non-teaching school based staff'
        END AS tntp_assignment
+      /*default school_based assignments based on title/location*/
+      ,CASE
+        WHEN s.legal_entity_name <> 'KIPP TEAM and Family Schools Inc.' AND s.primary_site NOT IN ('Room 9 - 60 Park Pl', 'Room 10 - 121 Market St', 'Room 11 - 1951 NW 7th Ave') THEN 'school-based'
+        END AS school_based
+      /*default tntp assignments based on title/location*/
       ,CASE 
         WHEN s.primary_job = 'Head of Schools' THEN 'Head of Schools'
         WHEN s.primary_job = 'Assistant Superintendent' THEN 'Head of Schools'
         WHEN s.primary_job IN ('Teacher', 'Teacher in Residence', 'Learning Specialist', 'Learning Specialist Coordinator', 'Teacher, ESL', 'Teacher ESL') THEN 'Teacher'
         WHEN s.primary_job = 'Executive Director' THEN 'Executive Director'
-        WHEN s.primary_job IN ('Associate Director of School Operations') THEN 'ADSO'
+        WHEN s.primary_job IN ('Associate Director of School Operations','School Operations Manager') THEN 'ADSO'
         WHEN s.primary_job IN ('Director Campus Operations', 'Director School Operations', 'Director of Campus Operations', 'Fellow School Operations Director') THEN 'DSO'
         WHEN s.primary_job = 'Managing Director of Operations' THEN 'MDO'
         WHEN s.primary_job = 'Managing Director of School Operations' THEN 'MDSO'
@@ -50,4 +56,4 @@ LEFT JOIN elementary_grade e
   ON s.df_employee_number = e.employee_number
 WHERE s.[status] = 'ACTIVE'
   AND s.primary_job <> 'Intern'
-  AND s.primary_job NOT LIKE '%Temp%'
+  AND s.primary_job NOT LIKE '%Temp%'    
