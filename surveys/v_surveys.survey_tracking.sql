6779884581f6944b8f05fc3963508d280efeb9a2
Cristina Baldor <ccbaldor@icloud.com>
2022-10-24 11:11:37 -0400

    Removing KTAF from Self & Others Manager Feedback

--- surveys/v_surveys.survey_tracking.sql
+++ surveys/v_surveys.survey_tracking.sql
@@ -1,7 +1,7 @@
 USE gabby
 GO
 
-CREATE OR ALTER VIEW surveys.survey_tracking AS
+--CREATE OR ALTER VIEW surveys.survey_tracking AS
 
 WITH surveys AS (
   SELECT c.survey_id
@@ -155,7 +155,7 @@ SELECT COALESCE(st.respondent_employee_number, c.respondent_employee_number) AS 
 FROM survey_term_staff_scaffold st
 INNER JOIN gabby.surveys.so_assignments_long sa
   ON st.respondent_employee_number = sa.survey_taker_id
- AND sa.survey_round_status IN ('Yes', 'Yes - Manager Survey Only')
+ AND sa.survey_round_status = 'Yes'
 LEFT JOIN clean_responses c
   ON sa.assignment_employee_id = c.subject_employee_number
  AND sa.survey_taker_id = c.respondent_employee_number
