4267bf2111f7a8c4b46238ffaf6faba0e3fb439b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-12-01 09:36:33 -0600

    :lipstick:

--- extracts/v_extracts.deanslist_mod_standards.sql
+++ extracts/v_extracts.deanslist_mod_standards.sql
@@ -4,36 +4,25 @@ GO
 CREATE OR ALTER VIEW extracts.deanslist_mod_standards AS
 
 WITH std_avg AS (
-  SELECT sub.local_student_id
-        ,sub.academic_year
-        ,sub.term_administered
-        ,sub.response_type
-        ,sub.subject_area
-        ,sub.standard_description
-        ,sub.performance_band_set_id
-        ,ROUND(AVG(percent_correct), 0) AS avg_percent_correct
-  FROM
-      (
-       SELECT asr.local_student_id
-             ,asr.academic_year
-             ,asr.term_administered
-             ,asr.response_type
-             ,asr.percent_correct
-             ,asr.subject_area
-             ,asr.performance_band_set_id
-             ,REPLACE(asr.standard_description, '"', '''') AS standard_description
-       FROM gabby.illuminate_dna_assessments.agg_student_responses_all_current asr
-       WHERE asr.response_type IN ('S', 'G')
-         AND asr.is_normed_scope = 1
-         AND asr.subject_area IN ('Text Study','Mathematics')
-      ) sub
-  GROUP BY sub.local_student_id
-          ,sub.academic_year
-          ,sub.term_administered
-          ,sub.response_type
-          ,sub.subject_area
-          ,sub.standard_description
-          ,sub.performance_band_set_id
+  SELECT asr.local_student_id
+        ,asr.academic_year
+        ,asr.term_administered
+        ,asr.response_type
+        ,asr.subject_area
+        ,asr.performance_band_set_id
+        ,REPLACE(asr.standard_description, '"', '''') AS standard_description
+        ,ROUND(AVG(asr.percent_correct), 0) AS avg_percent_correct
+  FROM gabby.illuminate_dna_assessments.agg_student_responses_all_current asr
+  WHERE asr.response_type IN ('S', 'G')
+    AND asr.is_normed_scope = 1
+    AND asr.subject_area IN ('Text Study','Mathematics')
+  GROUP BY asr.local_student_id
+          ,asr.academic_year
+          ,asr.term_administered
+          ,asr.response_type
+          ,asr.subject_area
+          ,asr.standard_description
+          ,asr.performance_band_set_id
  )
 
 SELECT sa.local_student_id AS student_number
@@ -43,6 +32,7 @@ SELECT sa.local_student_id AS student_number
       ,sa.avg_percent_correct AS percent_correct
       ,sa.subject_area
       ,sa.standard_description
+
       ,CASE
         WHEN pbl.label_number = 5 THEN 'Advanced Mastery'
         WHEN pbl.label_number = 4 THEN 'Mastery'
@@ -51,6 +41,6 @@ SELECT sa.local_student_id AS student_number
         WHEN pbl.label_number = 1 THEN 'Far Below Mastery'
        END AS standard_proficiency
 FROM std_avg sa
-JOIN gabby.illuminate_dna_assessments.performance_band_lookup_static pbl
+INNER JOIN gabby.illuminate_dna_assessments.performance_band_lookup_static pbl
   ON sa.performance_band_set_id = pbl.performance_band_set_id
  AND sa.avg_percent_correct BETWEEN pbl.minimum_value AND pbl.maximum_value
