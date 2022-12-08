4ae90e6acef4e0bceacf703480ac63048e09664a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-12-08 21:06:12 +0000

    update postcreate, init trunk

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -2,11 +2,11 @@
 
 # update apt packages
 sudo apt-get -qq -y update --no-install-recommends &&
-  sudo apt-get -qq -y install --no-install-recommends bash-completion &&
-  sudo apt-get -qq -y upgrade --no-install-recommends &&
-  sudo apt-get -qq autoremove -y &&
-  sudo apt-get -qq clean -y
+	sudo apt-get -qq -y install --no-install-recommends bash-completion &&
+	sudo apt-get -qq -y upgrade --no-install-recommends &&
+	sudo apt-get -qq autoremove -y &&
+	sudo apt-get -qq clean -y
 
 # install Trunk
 curl https://get.trunk.io -fsSL | bash -s -- -y
-trunk install --ci
+trunk init --yes-to-all --nocheck-sample
