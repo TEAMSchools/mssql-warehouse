ee811276a2516e8f313f08b72ee2e0f0ecbd0b94
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-12-08 20:55:14 +0000

    startup scripts

---
+++ .devcontainer/scripts/postStart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# update apt packages
+sudo apt-get -qq -y update --no-install-recommends &&
+  sudo apt-get -qq -y upgrade --no-install-recommends &&
+  sudo apt-get -qq autoremove -y &&
+  sudo apt-get -qq clean -y
+
+# update trunk
+trunk upgrade -y --no-progress
