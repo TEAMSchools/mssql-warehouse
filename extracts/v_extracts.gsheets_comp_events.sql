87fdac6f64544296196bd0e3d91548747d491a5c
Cristina Baldor <ccbaldor@icloud.com>
2022-12-12 16:54:19 -0500

    Update v_extracts.gsheets_comp_events.sql

--- extracts/v_extracts.gsheets_comp_events.sql
+++ extracts/v_extracts.gsheets_comp_events.sql
@@ -1,31 +1,51 @@
 USE gabby
 GO
 
-CREATE OR ALTER VIEW extracts.gsheets_comp_events
+--CREATE OR ALTER VIEW extracts.gsheets_comp_events
 
-WITH approvers AS (
-
-SELECT primary_site
-      ,userprincipalname AS sl_email
-      ,manager_userprincipalname AS hos_email
-FROM gabby.people.staff_crosswalk_static
-WHERE primary_job = 'School Leader'
-  AND status <> 'TERMINATED'
+WITH school_approvers AS (
+SELECT x.primary_site
+      ,x.userprincipalname AS sl_email
+      ,x.manager_userprincipalname AS hos_email
+      ,x.manager_df_employee_number
+      ,x.google_email AS sl_google
+FROM gabby.people.staff_crosswalk_static x
+WHERE x.primary_job = 'School Leader'
+  AND x.status <> 'TERMINATED'
 )
 
+,manager_approvers AS (
+SELECT df_employee_number
+      ,userprincipalname AS manager_email
+      ,google_email AS manager_google
+FROM gabby.people.staff_crosswalk_static
+)
 
-SELECT x.df_employee_number
-      ,x.preferred_name
+,hos_approvers AS (
+SELECT df_employee_number
+      ,userprincipalname AS hos_email
+      ,google_email AS hos_google
+FROM gabby.people.staff_crosswalk_static x
+JOIN school_approvers s
+  ON x.df_employee_number = s.manager_df_employee_number
+
+)
+
+SELECT x.payroll_company_code
+      ,x.legal_entity_name
+      ,CONCAT(x.preferred_name, ' - ', x.primary_site) AS preferred_name
+      ,x.file_number
       ,x.primary_site
       ,x.primary_job
-      ,x.legal_entity_name
-      ,x.google_email
-      ,x.userprincipalname
       
-      ,a.sl_email
-      ,a.hos_email
-
+      ,COALESCE(a.sl_google,m.manager_google) AS first_approver
+      ,COALESCE(h.hos_google, 'cbaldor@apps.teamschools.org') AS second_approver_username
+   
 FROM gabby.people.staff_crosswalk_static x
-LEFT JOIN approvers a
+LEFT JOIN school_approvers a
   ON x.primary_site = a.primary_site
+LEFT JOIN hos_approvers h
+  ON x.manager_df_employee_number = h.df_employee_number
+LEFT JOIN manager_approvers m
+  ON x.manager_df_employee_number = m.df_employee_number
 WHERE x.status <> 'TERMINATED'
