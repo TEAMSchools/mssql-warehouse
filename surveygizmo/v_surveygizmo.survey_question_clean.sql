040861bc020f206e919aae8400cab032c0c27ff6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-12-19 13:23:49 -0600

    fix data types

--- surveygizmo/v_surveygizmo.survey_question_clean.sql
+++ surveygizmo/v_surveygizmo.survey_question_clean.sql
@@ -5,15 +5,15 @@ CREATE OR ALTER VIEW surveygizmo.survey_question_clean AS
 
 SELECT id AS survey_question_id
       ,survey_id
-      ,base_type
-      ,[type]
+      ,CAST(base_type AS NVARCHAR(16)) AS base_type
+      ,CAST([type] AS NVARCHAR(32)) AS [type]
       ,comment
       ,has_showhide_deps
-      ,CASE WHEN shortname <> '' THEN shortname END AS shortname
+      ,CAST(CASE WHEN shortname <> '' THEN shortname END AS NVARCHAR(128)) AS shortname
       ,CASE 
-        WHEN [type] = 'ESSAY' THEN 'Y' 
-        WHEN [type] = 'TEXTBOX' THEN 'Y'
-        ELSE 'N' 
+        WHEN [type] = 'ESSAY' THEN N'Y' 
+        WHEN [type] = 'TEXTBOX' THEN N'Y'
+        ELSE N'N'
        END AS is_open_ended
       ,CASE 
         WHEN shortname IN (
@@ -24,17 +24,17 @@ SELECT id AS survey_question_id
         ELSE 0
        END AS is_identifier_question
 
-      ,JSON_VALUE(title, '$.English') AS title_english
-      ,JSON_VALUE(properties, '$.url') AS [url]
-      ,JSON_VALUE(properties, '$.orientation') AS orientation
-      ,JSON_QUERY(properties, '$.custom_css') AS custom_css
+      ,CAST(JSON_VALUE(title, '$.English') AS NVARCHAR(2048)) AS title_english
+      ,CAST(JSON_VALUE(properties, '$.url')  AS NVARCHAR(128)) AS [url]
+      ,CAST(JSON_VALUE(properties, '$.orientation')  AS NVARCHAR(16)) AS orientation
       ,CAST(JSON_VALUE(properties, '$.question_description_above') AS BIT) AS question_description_above
       ,CAST(JSON_VALUE(properties, '$."soft-required"') AS BIT) AS soft_required
       ,CAST(JSON_VALUE(properties, '$.disabled') AS BIT) AS [disabled]
       ,CAST(JSON_VALUE(properties, '$.hide_after_response') AS BIT) AS hide_after_response
       ,CAST(JSON_VALUE(properties, '$.break_after') AS BIT) AS break_after
-      ,CAST(gabby.utilities.STRIP_HTML(JSON_VALUE(title, '$.English')) AS VARCHAR(500)) AS title_clean
+      ,CAST(gabby.utilities.STRIP_HTML(JSON_VALUE(title, '$.English')) AS VARCHAR(2048)) AS title_clean
 
+      ,JSON_QUERY(properties, '$.custom_css') AS custom_css
       ,JSON_QUERY(properties, '$.messages') AS messages_json
       ,JSON_QUERY(properties, '$.show_rules') AS show_rules_json
       ,varname AS varname_json
