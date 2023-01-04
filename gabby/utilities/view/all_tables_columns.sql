715bb3159cb99dc90ad11dfcc51ddf3fe3c6c0d6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-04 16:28:05 -0600

    collate

--- gabby/utilities/view/all_tables_columns.sql
+++ gabby/utilities/view/all_tables_columns.sql
@@ -1,9 +1,18 @@
 CREATE OR ALTER VIEW
   utilities.all_tables_columns AS
 SELECT
-  DB_NAME() AS [db_name],
-  s.[name] AS [schema_name],
-  t.[name] AS table_name,
+  (
+    DB_NAME()
+    COLLATE LATIN1_GENERAL_BIN
+  ) AS [db_name],
+  (
+    s.[name]
+    COLLATE LATIN1_GENERAL_BIN
+  ) AS [schema_name],
+  (
+    t.[name]
+    COLLATE LATIN1_GENERAL_BIN
+  ) AS table_name,
   t.[type],
   -1 AS column_id,
   NULL AS column_name,
@@ -26,9 +35,15 @@ SELECT
   t.[name] AS table_name,
   t.[type],
   c.column_id,
-  c.[name] AS column_name,
+  (
+    c.[name]
+    COLLATE LATIN1_GENERAL_BIN
+  ) AS column_name,
   c.max_length AS column_max_length,
-  y.[name] AS column_type
+  (
+    y.[name]
+    COLLATE LATIN1_GENERAL_BIN
+  ) AS column_type
 FROM
   sys.schemas AS s
   INNER JOIN sys.objects AS t ON (
@@ -41,3 +56,67 @@ WHERE
   (
     s.[schema_id] BETWEEN 5 AND 16383
   )
+UNION ALL
+SELECT
+  [db_name],
+  [schema_name],
+  table_name,
+  [type],
+  column_id,
+  column_name,
+  column_max_length,
+  column_type
+FROM
+  kippmiami.utilities.all_tables_columns
+WHERE
+  column_id > -1
+  AND table_name NOT LIKE 'fivetran%'
+  AND [schema_name] != 'fivetran_log'
+UNION ALL
+SELECT
+  [db_name],
+  [schema_name],
+  table_name,
+  [type],
+  column_id,
+  column_name,
+  column_max_length,
+  column_type
+FROM
+  kippnewark.utilities.all_tables_columns
+WHERE
+  column_id > -1
+  AND table_name NOT LIKE 'fivetran%'
+  AND [schema_name] != 'fivetran_log'
+UNION ALL
+SELECT
+  [db_name],
+  [schema_name],
+  table_name,
+  [type],
+  column_id,
+  column_name,
+  column_max_length,
+  column_type
+FROM
+  kippcamden.utilities.all_tables_columns
+WHERE
+  column_id > -1
+  AND table_name NOT LIKE 'fivetran%'
+  AND [schema_name] != 'fivetran_log'
+UNION ALL
+SELECT
+  [db_name],
+  [schema_name],
+  table_name,
+  [type],
+  column_id,
+  column_name,
+  column_max_length,
+  column_type
+FROM
+  kipptaf.utilities.all_tables_columns
+WHERE
+  column_id > -1
+  AND table_name NOT LIKE 'fivetran%'
+  AND [schema_name] != 'fivetran_log'
--- gabby/utilities/view/generate_gabby_unions.sql
+++ gabby/utilities/view/generate_gabby_unions.sql
@@ -1,7 +1,75 @@
 CREATE OR ALTER VIEW
   utilities.generate_gabby_unions AS
 WITH
-  all_tables_columns_pivot AS (
+  atc AS (
+    SELECT
+      [db_name],
+      [schema_name],
+      table_name,
+      column_name,
+      column_type
+    FROM
+      utilities.all_tables_columns
+    WHERE
+      [db_name] IN (
+        'kippnewark',
+        'kippcamden',
+        'kippmiami'
+      )
+      AND column_id > -1
+      AND table_name NOT IN ('all_tables_columns')
+      AND table_name NOT LIKE 'fivetran%'
+      AND [schema_name] != 'fivetran_log'
+  ),
+  atc_pivot AS (
+    SELECT
+      [schema_name],
+      table_name,
+      column_name,
+      kippmiami AS kippmiami_column_type,
+      kippcamden AS kippcamden_column_type,
+      kippnewark AS kippnewark_column_type,
+      CONCAT(
+        kippnewark,
+        kippcamden,
+        kippmiami
+      ) AS column_type_concat,
+      CASE
+        WHEN (
+          (
+            CONCAT(kippcamden, kippnewark) != ''
+            AND CHARINDEX(
+              kippmiami,
+              CONCAT(kippcamden, kippnewark)
+            ) = 0
+          )
+          OR (
+            CONCAT(kippmiami, kippnewark) != ''
+            AND CHARINDEX(
+              kippcamden,
+              CONCAT(kippmiami, kippnewark)
+            ) = 0
+          )
+          OR (
+            CONCAT(kippmiami, kippcamden) != ''
+            AND CHARINDEX(
+              kippnewark,
+              CONCAT(kippmiami, kippcamden)
+            ) = 0
+          )
+        ) THEN 1
+        ELSE 0
+      END AS column_type_mismatch
+    FROM
+      atc PIVOT (
+        MAX(column_type) FOR [db_name] IN (
+          kippnewark,
+          kippcamden,
+          kippmiami
+        )
+      ) AS p
+  ),
+  cols AS (
     SELECT
       [schema_name],
       table_name,
@@ -15,7 +83,20 @@ WITH
         WHEN (
           column_type_mismatch = 1
           AND column_type_concat LIKE '%varchar%'
-        ) THEN 'CAST([' + column_name + '] AS NVARCHAR) AS [' + column_name + ']'
+        ) THEN CONCAT(
+          'CAST(',
+          '[' + column_name + '] ',
+          'AS NVARCHAR) ',
+          'COLLATE LATIN1_GENERAL_BIN ',
+          'AS [' + column_name + ']'
+        )
+        WHEN (
+          column_type_concat LIKE '%varchar%'
+        ) THEN CONCAT(
+          '[' + column_name + '] ',
+          'COLLATE LATIN1_GENERAL_BIN ',
+          'AS [' + column_name + ']'
+        )
         WHEN (
           column_type_mismatch = 1
           AND column_type_concat LIKE '%float%'
@@ -34,7 +115,20 @@ WITH
         WHEN (
           column_type_mismatch = 1
           AND column_type_concat LIKE '%varchar%'
-        ) THEN 'CAST([' + column_name + '] AS NVARCHAR) AS [' + column_name + ']'
+        ) THEN CONCAT(
+          'CAST(',
+          '[' + column_name + '] ',
+          'AS NVARCHAR) ',
+          'COLLATE LATIN1_GENERAL_BIN ',
+          'AS [' + column_name + ']'
+        )
+        WHEN (
+          column_type_concat LIKE '%varchar%'
+        ) THEN CONCAT(
+          '[' + column_name + '] ',
+          'COLLATE LATIN1_GENERAL_BIN ',
+          'AS [' + column_name + ']'
+        )
         WHEN (
           column_type_mismatch = 1
           AND column_type_concat LIKE '%float%'
@@ -53,7 +147,20 @@ WITH
         WHEN (
           column_type_mismatch = 1
           AND column_type_concat LIKE '%varchar%'
-        ) THEN 'CAST([' + column_name + '] AS NVARCHAR) AS [' + column_name + ']'
+        ) THEN CONCAT(
+          'CAST(',
+          '[' + column_name + '] ',
+          'AS NVARCHAR) ',
+          'COLLATE LATIN1_GENERAL_BIN ',
+          'AS [' + column_name + ']'
+        )
+        WHEN (
+          column_type_concat LIKE '%varchar%'
+        ) THEN CONCAT(
+          '[' + column_name + '] ',
+          'COLLATE LATIN1_GENERAL_BIN ',
+          'AS [' + column_name + ']'
+        )
         WHEN (
           column_type_mismatch = 1
           AND column_type_concat LIKE '%float%'
@@ -68,70 +175,7 @@ WITH
         ELSE '[' + column_name + ']'
       END AS kippcamden
     FROM
-      (
-        SELECT
-          [schema_name],
-          table_name,
-          column_name,
-          kippmiami AS kippmiami_column_type,
-          kippcamden AS kippcamden_column_type,
-          kippnewark AS kippnewark_column_type,
-          CONCAT(
-            kippnewark,
-            kippcamden,
-            kippmiami
-          ) AS column_type_concat,
-          CASE
-            WHEN (
-              (
-                CONCAT(kippcamden, kippnewark) != ''
-                AND CHARINDEX(
-                  kippmiami,
-                  CONCAT(kippcamden, kippnewark)
-                ) = 0
-              )
-              OR (
-                CONCAT(kippmiami, kippnewark) != ''
-                AND CHARINDEX(
-                  kippcamden,
-                  CONCAT(kippmiami, kippnewark)
-                ) = 0
-              )
-              OR (
-                CONCAT(kippmiami, kippcamden) != ''
-                AND CHARINDEX(
-                  kippnewark,
-                  CONCAT(kippmiami, kippcamden)
-                ) = 0
-              )
-            ) THEN 1
-            ELSE 0
-          END AS column_type_mismatch
-        FROM
-          (
-            SELECT
-              [db_name],
-              [schema_name],
-              table_name,
-              column_name,
-              column_type
-            FROM
-              gabby.utilities.all_tables_columns
-            WHERE
-              column_id > -1
-              AND [db_name] IN (
-                'kippnewark',
-                'kippcamden',
-                'kippmiami'
-              )
-          ) AS sub PIVOT (
-            MAX(column_type) FOR [db_name] IN (
-              [kippnewark],
-              [kippcamden],
-              [kippmiami]
-            )
-          ) AS p
-      ) AS sub
+      atc_pivot
   ),
   row_gen AS (
     SELECT
@@ -142,7 +186,7 @@ WITH
       (n BETWEEN 1 AND 2)
   )
 SELECT
-  TOP 100000 sub.[schema_name],
+  sub.[schema_name],
   sub.table_name,
   sub.column_type_mismatch,
   CASE
@@ -170,17 +214,17 @@ FROM
       table_name,
       CONCAT(
         'SELECT ''kippnewark'' AS [db_name],',
-        gabby.dbo.GROUP_CONCAT_D (kippnewark + ',', '') + ' ',
+        gabby.dbo.GROUP_CONCAT_D (kippnewark, ',') + ' ',
         'FROM kippnewark.' + [schema_name] + '.' + table_name
       ) AS kippnewark,
       CONCAT(
         'SELECT ''kippcamden'' AS [db_name],',
-        gabby.dbo.GROUP_CONCAT_D (kippcamden + ',', '') + ' ',
+        gabby.dbo.GROUP_CONCAT_D (kippcamden, ',') + ' ',
         'FROM kippcamden.' + [schema_name] + '.' + table_name
       ) AS kippcamden,
       CONCAT(
         'SELECT ''kippmiami'' AS [db_name],',
-        gabby.dbo.GROUP_CONCAT_D (kippmiami + ',', '') + ' ',
+        gabby.dbo.GROUP_CONCAT_D (kippmiami, ',') + ' ',
         'FROM kippmiami.' + [schema_name] + '.' + table_name
       ) AS kippmiami,
       MAX(column_type_mismatch) AS column_type_mismatch,
@@ -200,16 +244,9 @@ FROM
         END
       ) AS kippmiami_count
     FROM
-      all_tables_columns_pivot
-    WHERE
-      table_name NOT LIKE 'fivetran%'
-      AND [schema_name] != 'fivetran_log'
+      cols
     GROUP BY
       table_name,
       [schema_name]
   ) AS sub
   CROSS JOIN row_gen AS t
-ORDER BY
-  sub.[schema_name],
-  sub.table_name,
-  t.n
