9278fb58f8ddebd790c7d1c05848982b565a4d1a
Cristina Baldor <ccbaldor@icloud.com>
2023-01-10 12:58:00 -0500

    new approval loops

--- extracts/query/comp_event_output_init.sql
+++ extracts/query/comp_event_output_init.sql
@@ -1,6 +1,3 @@
-CREATE OR ALTER VIEW gsheets_comp_events
-
--- Roster of School Leaders and Heads of Schools/their managers for School instructional approval loop
 WITH school_leader_hos AS (
   SELECT x.primary_site
         ,x.userprincipalname AS first_approver_email
@@ -19,7 +16,9 @@ WITH school_leader_hos AS (
 -- Roster of MDSOs and MDOs/their managers for School operations approval loop
 
 mdso_mdo AS (
-SELECT   x.primary_site
+SELECT   x.df_employee_number
+        ,x.userprincipalname AS submitter
+        ,x.primary_site
         ,x.manager_userprincipalname AS first_approver_email
         ,m.manager_userprincipalname AS second_approver_email
         ,m.google_email AS first_approver_google
@@ -86,28 +85,31 @@ WHERE x.primary_site IN ('Room 9 - 60 Park Pl','Room 10 - 121 Market St','Room 1
 
 )
 
-SELECT x.df_employee_number
+SELECT 
+      CONCAT('init',x.df_employee_number) AS event_id
+      ,x.payroll_company_code
+      ,x.df_employee_number
       ,x.adp_associate_id
       ,x.file_number
-      ,x.payroll_company_code
-      ,x.legal_entity_name
-      ,x.primary_site
-      ,x.primary_on_site_department
-      ,x.primary_job
-      ,x.google_email
-      ,x.userprincipalname
-      ,CONCAT(x.preferred_name, ' - ', x.primary_site) AS preferred_name
-      
-      ,CASE
-       WHEN l.first_approver_google = 'rhill@apps.teamschools.org' THEN 'ssmall@apps.teamschools.org' ELSE l.first_approver_google END AS first_approver_google
-      ,CASE
-       WHEN l.second_approver_google = 'rhill@apps.teamschools.org' THEN 'ssmall@apps.teamschools.org' ELSE l.second_approver_google END AS second_approver_google
-      ,CASE
-       WHEN l.first_approver_email = 'rhill@kippteamandfamily.org' THEN 'ssmall@kippteamandfamily.org' ELSE l.first_approver_email END AS first_approver_email
-      ,CASE 
-       WHEN l.second_approver_email = 'rhill@kippteamandfamily.org' THEN 'ssmall@kippteamandfamily.org' ELSE l.second_approver_email END AS second_approver_email
-   
+      ,'' AS stipend_type
+      ,'' AS pay_code
+      ,'' AS amount
+      ,'' AS payment_date
+      ,'' AS description
+      ,'Pending' AS first_approval
+      ,'Pending' AS second_approval
+
+      ,l.first_approver_email
+      ,l.second_approver_email
+      ,l.first_approver_google
+      ,l.second_approver_google
+      ,o.submitter AS submitter --note this is pre-filled as the DSO/DCO, THRIVE has two
+      ,'' AS edited_by
+      ,'' AS edited_at
+
 FROM gabby.people.staff_crosswalk_static x
 LEFT JOIN approval_loops l
   ON (x.df_employee_number = l.df_employee_number)
+LEFT JOIN mdso_mdo o
+  ON (x.primary_site = o.primary_site)
 WHERE x.status <> 'TERMINATED'
