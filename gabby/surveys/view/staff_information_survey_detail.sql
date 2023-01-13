9f6bd735630559389fc2b4b8e0ec406614e431ab
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-13 09:44:33 -0600

    fmt

--- gabby/surveys/view/staff_information_survey_detail.sql
+++ gabby/surveys/view/staff_information_survey_detail.sql
@@ -1,41 +1,43 @@
 CREATE OR ALTER VIEW
   surveys.staff_information_survey_detail AS
-WITH 
+WITH
   responses AS (
-				SELECT 
-							sri.survey_id,
-							sri.subject_df_employee_number AS employee_number,
-							sri.survey_response_id,
-							sri.campaign_academic_year,
-							sri.campaign_reporting_term,
-							sri.campaign_name,
-							sri.date_started,
-							sri.date_submitted,
-
-							sd.question_id,
-							sd.options,
-							sd.question,
-							sd.answer,
-							ROW_NUMBER() OVER (
-							PARTITION BY
-									sri.respondent_df_employee_number,
-									sri.survey_id,
-									sd.question_id
-							ORDER BY
-									sri.date_submitted DESC,
-									sri.survey_response_id DESC
-					) AS rn_cur
-			FROM surveygizmo.survey_response_identifiers_static AS sri
-									INNER JOIN surveygizmo.survey_response_data AS sd ON (
-											sri.survey_id = sd.survey_id
-											AND sri.survey_response_id = sd.survey_response_id
-											AND (sd.answer IS NOT NULL OR sd.question_id IN (5,8,21,30))
-											AND sri.survey_id = 6330385
-											AND sri.[status] = 'Complete'
-											AND sri.rn_respondent_subject = 1
-									)
-		)
-
+    SELECT
+      sri.survey_id,
+      sri.subject_df_employee_number AS employee_number,
+      sri.survey_response_id,
+      sri.campaign_academic_year,
+      sri.campaign_reporting_term,
+      sri.campaign_name,
+      sri.date_started,
+      sri.date_submitted,
+      sd.question_id,
+      sd.options,
+      sd.question,
+      sd.answer,
+      ROW_NUMBER() OVER (
+        PARTITION BY
+          sri.respondent_df_employee_number,
+          sri.survey_id,
+          sd.question_id
+        ORDER BY
+          sri.date_submitted DESC,
+          sri.survey_response_id DESC
+      ) AS rn_cur
+    FROM
+      surveygizmo.survey_response_identifiers_static AS sri
+      INNER JOIN surveygizmo.survey_response_data AS sd ON (
+        sri.survey_id = sd.survey_id
+        AND sri.survey_response_id = sd.survey_response_id
+        AND (
+          sd.answer IS NOT NULL
+          OR sd.question_id IN (5, 8, 21, 30)
+        )
+        AND sri.survey_id = 6330385
+        AND sri.[status] = 'Complete'
+        AND sri.rn_respondent_subject = 1
+      )
+  )
 SELECT
   sub.employee_number,
   sub.survey_id,
@@ -64,8 +66,7 @@ SELECT
       sub.date_submitted DESC,
       sub.survey_response_id DESC
   ) AS rn_campaign,
-		sub.rn_cur
-
+  sub.rn_cur
 FROM
   (
     SELECT
@@ -77,10 +78,11 @@ FROM
       r.campaign_name,
       r.date_started,
       r.date_submitted,
-						qo.question_id AS q_id,
       r.question_id,
       r.question,
+      r.rn_cur,
       sq.shortname,
+      qo.question_id AS q_id,
       COALESCE(
         CASE
           WHEN qo.question_id = 5 THEN 'race_ethnicity_'
@@ -99,8 +101,7 @@ FROM
           r.question_id
         ORDER BY
           qo.option_value
-      ) AS rn_multiselect,
-						r.rn_cur
+      ) AS rn_multiselect
     FROM
       responses AS r
       INNER JOIN surveygizmo.survey_question_clean_static AS sq ON (
