dd7d47554f1bd3cdf1ad1d43839c4098be5b8c15
Cristina Baldor <ccbaldor@icloud.com>
2023-01-18 20:18:27 -0500

    diagnostic completion view

---
+++ gabby/surveys/query/scd_family_roster.sql
@@ -0,0 +1,13 @@
+SELECT 
+     CONCAT(c.student_web_id,'@teamstudents.org') AS student_email
+    ,c.cohort
+    ,c.lastfirst
+    ,c.grade_level
+    ,c.region
+    ,c.reporting_school_name
+    ,c.academic_year
+    
+FROM gabby.powerschool.cohort_identifiers_static c
+WHERE c.enroll_status = 0
+AND c.rn_year = 1
+AND c.academic_year = 2022
--- gabby/surveys/view/scd_survey_completion.sql
+++ gabby/surveys/view/scd_survey_completion.sql
@@ -1,10 +1,28 @@
---CREATE OR ALTER VIEW scd_survey_completion AS
+CREATE OR ALTER VIEW scd_survey_completion AS
 
 WITH student_responses AS (
-SELECT email_address 
-      ,[timestamp]
+SELECT email_address
       ,gabby.utilities.DATE_TO_SY([timestamp]) AS survey_academic_year
-FROM surveys.scds_responses
+FROM gabby.surveys.scds_responses
+GROUP BY email_address,timestamp
+)
+
+,family_responses AS (
+SELECT student_emaiL
+      ,gabby.utilities.DATE_TO_SY(date_submitted) AS survey_academic_year 
+FROM 
+	(
+      SELECT question_shortname
+      	,answer
+      	,date_submitted
+	  FROM gabby.surveygizmo.survey_detail
+      WHERE survey_id = 6829997
+      ) AS sub 
+      PIVOT (
+      MAX(answer) FOR question_shortname IN (
+      [student_email]
+      )
+    ) AS p
 )
 
 SELECT 
@@ -16,17 +34,15 @@ SELECT
     ,c.reporting_school_name
     ,c.academic_year
     
-    ,CASE
-     WHEN c.grade_level BETWEEN 0 AND 3 THEN NULL
-     WHEN c.grade_level BETWEEN 4 AND 12 AND s.[timestamp] IS NOT NULL THEN 1 
-     ELSE 0 END AS student_completion
-    ,NULL AS family_completion
+    ,CASE WHEN s.survey_academic_year IS NOT NULL THEN 1 ELSE 0 END AS student_completion
+    ,CASE WHEN f.survey_academic_year IS NOT NULL THEN 1 ELSE 0 END AS family_completion
     
-FROM powerschool.cohort_identifiers_static c
+FROM gabby.powerschool.cohort_identifiers_static c
 LEFT JOIN student_responses s
-  ON CONCAT(c.student_web_id,'@teamstudents.org') = s.email_address
+  ON (CONCAT(c.student_web_id,'@teamstudents.org') = s.email_address)
  AND c.academic_year = s.survey_academic_year
+LEFT JOIN family_responses f
+  ON (CONCAT(c.student_web_id,'@teamstudents.org') = f.student_email)
 WHERE c.enroll_status = 0
 AND c.rn_year = 1
 AND c.academic_year >= 2021
-
