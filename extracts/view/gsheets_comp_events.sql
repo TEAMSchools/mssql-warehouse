037bb7b837a00063c16f143c6bb5f1884a192743
Cristina Baldor <ccbaldor@icloud.com>
2023-02-28 19:03:57 -0500

    Update gsheets_comp_events.sql

--- extracts/view/gsheets_comp_events.sql
+++ extracts/view/gsheets_comp_events.sql
@@ -1,113 +1,62 @@
-CREATE OR ALTER VIEW gsheets_comp_events
+--CREATE OR ALTER VIEW gsheets_comp_events
 
--- Roster of School Leaders and Heads of Schools/their managers for School instructional approval loop
-WITH school_leader_hos AS (
-  SELECT x.primary_site
-        ,x.userprincipalname AS first_approver_email
-        ,x.manager_userprincipalname AS second_approver_email
-        ,x.google_email AS first_approver_google
-        
-        ,m.google_email AS second_approver_google
-        
-  FROM gabby.people.staff_crosswalk_static x
-  LEFT JOIN gabby.people.staff_crosswalk_static m
-    ON (x.manager_df_employee_number = m.df_employee_number)
-  WHERE x.primary_job = 'School Leader'
-    AND x.status <> 'TERMINATED'
-),
-
--- Roster of MDSOs and MDOs/their managers for School operations approval loop
-
-mdso_mdo AS (
-SELECT   x.primary_site
-        ,x.manager_userprincipalname AS first_approver_email
-        ,m.manager_userprincipalname AS second_approver_email
-        ,m.google_email AS first_approver_google
-        ,gm.google_email AS second_approver_google
-        
-  FROM gabby.people.staff_crosswalk_static x
-  LEFT JOIN gabby.people.staff_crosswalk_static m
-    ON (x.manager_df_employee_number = m.df_employee_number)
-LEFT JOIN gabby.people.staff_crosswalk_static gm
-    ON (m.manager_df_employee_number = gm.df_employee_number)
-  WHERE x.primary_job IN ('Director School Operations', 'Director Campus Operations')
-    AND x.status <> 'TERMINATED'
-
+WITH approval_loops AS (
+SELECT 
+    primary_site
+   ,legal_entity_name
+   ,[School Leader] AS school_leader
+   ,COALESCE([Director School Operations],[Director Campus Operations]) AS dso_dco 
+   ,[Managing Director of Operations] AS mdo
+   ,[Managing Director of Growth] AS mdg
+   FROM
+      ( SELECT  primary_site
+               ,legal_entity_name
+               ,preferred_name
+               ,primary_job
+         FROM gabby.people.staff_crosswalk_static x
+         WHERE status <> 'TERMINATED') AS sub
+         PIVOT (
+         MAX(preferred_name) FOR primary_job IN (
+         [School Leader]
+        ,[Director School Operations]
+        ,[Director Campus Operations]
+        ,[Managing Director of Operations]
+        ,[Managing Director of Growth]
+        )
+         ) AS p
 )
 
-,approval_loops AS (
-SELECT   x.df_employee_number
-        ,s.first_approver_email
-        ,s.second_approver_email
-        ,s.first_approver_google
-        ,s.second_approver_google
-        ,'Instructional' AS approval_loop
-
-  FROM gabby.people.staff_crosswalk_static x
-  JOIN school_leader_hos s
-    ON (x.primary_site = s.primary_site)
-  WHERE x.primary_on_site_department <> 'Operations'
-    AND x.status <> 'TERMINATED'
-
-UNION ALL
-
-SELECT   x.df_employee_number
-        ,s.first_approver_email
-        ,s.second_approver_email
-        ,s.first_approver_google
-        ,s.second_approver_google
-        ,'Operations' AS approval_loop
-
-  FROM gabby.people.staff_crosswalk_static x
-  JOIN mdso_mdo s
-    ON (x.primary_site = s.primary_site)
-  WHERE x.primary_on_site_department = 'Operations'
-    AND x.primary_site NOT IN ('Room 9 - 60 Park Pl','Room 10 - 121 Market St','Room 11 - 1951 NW 7th Ave')
-    AND x.status <> 'TERMINATED'
-
-UNION ALL
-
--- Manager and Manager of Manager for non-school-based staff
-
-SELECT x.df_employee_number
-      ,m.userprincipalname AS first_approver_email
-      ,gm.userprincipalname AS second_approver_email
-      ,m.google_email AS first_approver_google
-      ,gm.google_email AS second_approver_google
-      ,'Non-School Based' AS approval_loop
-
-FROM gabby.people.staff_crosswalk_static x
-LEFT JOIN gabby.people.staff_crosswalk_static m
-  ON (x.manager_df_employee_number = m.df_employee_number)
-LEFT JOIN gabby.people.staff_crosswalk_static gm
-  ON (m.manager_df_employee_number = gm.df_employee_number)
-WHERE x.primary_site IN ('Room 9 - 60 Park Pl','Room 10 - 121 Market St','Room 11 - 1951 NW 7th Ave')
-  AND x.status <> 'TERMINATED'
-
-)
-
-SELECT x.df_employee_number
-      ,x.adp_associate_id
-      ,x.file_number
-      ,x.payroll_company_code
-      ,x.legal_entity_name
-      ,x.primary_site
-      ,x.primary_on_site_department
-      ,x.primary_job
-      ,x.google_email
-      ,x.userprincipalname
-      ,CONCAT(x.preferred_name, ' - ', x.primary_site) AS preferred_name
-      
-      ,CASE
-       WHEN l.first_approver_google = 'rhill@apps.teamschools.org' THEN 'ssmall@apps.teamschools.org' ELSE l.first_approver_google END AS first_approver_google
-      ,CASE
-       WHEN l.second_approver_google = 'rhill@apps.teamschools.org' THEN 'ssmall@apps.teamschools.org' ELSE l.second_approver_google END AS second_approver_google
-      ,CASE
-       WHEN l.first_approver_email = 'rhill@kippteamandfamily.org' THEN 'ssmall@kippteamandfamily.org' ELSE l.first_approver_email END AS first_approver_email
-      ,CASE 
-       WHEN l.second_approver_email = 'rhill@kippteamandfamily.org' THEN 'ssmall@kippteamandfamily.org' ELSE l.second_approver_email END AS second_approver_email
+SELECT l.primary_site
+   ,l.legal_entity_name
+   ,l.school_leader
+   ,l.dso_dco 
+   ,l.mdo
+   ,l.mdg 
    
-FROM gabby.people.staff_crosswalk_static x
-LEFT JOIN approval_loops l
-  ON (x.df_employee_number = l.df_employee_number)
-WHERE x.status <> 'TERMINATED'
+   ,b.preferred_name AS hos_ed
+   
+   ,c.preferred_name AS ed
+   
+   ,e.preferred_name AS mdso
+   
+   ,f.preferred_name AS mdo
+   
+FROM approval_loops l
+--School Leaders
+LEFT JOIN gabby.people.staff_crosswalk_static a
+ON l.school_leader = a.preferred_name
+--School Leader Managers (HsOS)
+LEFT JOIN gabby.people.staff_crosswalk_static b
+ON a.manager_name = b.preferred_name 
+--HOS Managers (Executive Directors)
+LEFT JOIN gabby.people.staff_crosswalk_static c
+ON b.manager_name = c.preferred_name
+--DSO/DCO
+LEFT JOIN gabby.people.staff_crosswalk_static d
+ON l.dso_dco = d.preferred_name
+--DSO/DCO Managers (MDSOs)
+LEFT JOIN gabby.people.staff_crosswalk_static e
+ON d.manager_name = e.preferred_name
+--MDSO Managers (MDOs)
+LEFT JOIN gabby.people.staff_crosswalk_static f
+ON e.manager_name = f.preferred_name
