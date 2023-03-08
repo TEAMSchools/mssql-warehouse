80368d3d81f5dde7586b785bb3dc88c11dbcafa7
Cristina Baldor <ccbaldor@icloud.com>
2023-03-08 15:35:52 -0500

    Update kipp_forward_survey.sql

--- gabby/tableau/view/kipp_forward_survey.sql
+++ gabby/tableau/view/kipp_forward_survey.sql
@@ -208,7 +208,7 @@ SELECT
   s.linkedin_link,
   s.debt_binary,
   s.debt_amount,
-  s.annual_income AS annual_income,
+  s.annual_income,
   /*weighted satisfaction scores based on relative importance of each*/
   s.cur_1 * p.imp_1 AS level_pay_quality,
   s.cur_2 * p.imp_2 AS stable_pay_quality,
