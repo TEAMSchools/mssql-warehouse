1ba5a965141ad9dea6410a835d9e9da1309c4c53
Anthony Walters <anthony@u.northwestern.edu>
2023-03-10 15:21:02 -0500

    Queries for gsheet board dash

---
+++ gabby/extracts/query/board_dash_queries/act_superscore_pivot.sql
@@ -0,0 +1,42 @@
+SELECT
+    contact_c,
+    [act_english_c] AS max_english,
+    [act_math_c] AS max_math,
+    [act_reading_c] AS max_reading,
+    [act_science_c] AS max_science,
+    [act_composite_c] AS max_composite,
+    ROUND(
+        (
+            [act_english_c] + [act_math_c] + [act_reading_c] + [act_science_c]
+        ) / 4,
+        0
+    ) AS act_superscore
+FROM
+    (
+        SELECT
+            st.contact_c,
+            st.score_type,
+            MAX(st.score) AS max_score
+        FROM
+            gabby.alumni.standardized_test_long AS st
+        WHERE
+            st.test_type = 'ACT'
+            AND st.score_type IN (
+                'act_english_c',
+                'act_math_c',
+                'act_reading_c',
+                'act_science_c',
+                'act_composite_c'
+            )
+        GROUP BY
+            st.contact_c,
+            st.score_type
+    ) AS sub PIVOT (
+        MAX(max_score) FOR score_type IN (
+            [act_english_c],
+            [act_math_c],
+            [act_reading_c],
+            [act_science_c],
+            [act_composite_c]
+        )
+    ) AS p
