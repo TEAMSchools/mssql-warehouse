ceb49dd89e4861929a2e0126497f00482a295ba7
Kevin Verhoff <kevin.verhoff@gmail.com>
2023-03-10 14:19:19 -0500

    Update kipp_foundation_staff_roster_submission.sql
    take 3

--- gabby/people/procedure/kipp_foundation_staff_roster_submission.sql
+++ gabby/people/procedure/kipp_foundation_staff_roster_submission.sql
@@ -1,17 +1,17 @@
 WITH job_loc_mins AS (
-  SELECT employee_number,
-         [location],
-         job_title,
-         MIN(eh.position_effective_start_date) OVER(PARTITION BY eh.employee_number, eh.[location], job_title) AS job_loc_start
-  FROM people.employment_history_static eh
+  SELECT eh.employee_number,
+         eh.[location],
+         eh.job_title,
+         MIN(eh.position_effective_start_date) OVER(PARTITION BY eh.employee_number, eh.[location], eh.job_title) AS job_loc_start
+  FROM people.employment_history_static AS eh
   )
 
 SELECT eh.employee_number,
-        NULL AS State_staff_ID,
+        NULL AS state_staff_id,
        cw.preferred_first_name,
        cw.preferred_last_name,
        cw.userprincipalname,
-       birth_date AS dob,
+       cw.birth_date AS dob,
        cw.primary_race_ethnicity_reporting,
        cw.primary_race_ethnicity_reporting,
        cw.gender,
@@ -25,25 +25,30 @@ SELECT eh.employee_number,
        cw.primary_on_site_department,
        cw.legal_entity_name,
        CASE 
-         WHEN cw.status = 'Leave' THEN 'DO NOT SURVEY'
-         WHEN cw.primary_job = 'Security' THEN 'DO NOT SURVEY'
-         ELSE NULL
+         WHEN (cw.status = 'Leave') THEN 'DO NOT SURVEY'
+         WHEN (cw.primary_job = 'Security') THEN 'DO NOT SURVEY'
         END AS survey_override
-FROM people.employment_history_static eh
-JOIN people.staff_crosswalk_static cw
+FROM people.employment_history_static AS eh
+INNER JOIN people.staff_crosswalk_static cw
   ON (eh.employee_number = cw.df_employee_number
-  AND CONVERT(DATE, '2023-03-01') BETWEEN eh.position_effective_start_date AND COALESCE(eh.position_effective_end_date, DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR()+1,6,30)) --update as needed
+  AND (CONVERT(DATE, '2023-03-01') BETWEEN 
+           eh.position_effective_start_date 
+       AND COALESCE(eh.position_effective_end_date, 
+                    DATEFROMPARTS(gabby.utilities.GLOBAL_ACADEMIC_YEAR()+1,6,30)
+                    )
+       )
   AND eh.primary_position = 'Yes')
-JOIN job_loc_mins jlm
+INNER JOIN job_loc_mins AS jlm
   ON (jlm.employee_number = eh.employee_number
   AND jlm.job_title = eh.job_title
   AND jlm.[location] = eh.[location])
-WHERE cw.termination_date > CONVERT(DATE, '2022-03-01') OR termination_date IS NULL --update as needed
+WHERE (cw.termination_date > CONVERT(DATE, '2022-03-01') 
+   OR cw.termination_date IS NULL)
 GROUP BY eh.employee_number,
          cw.preferred_first_name,
          cw.preferred_last_name,
          cw.userprincipalname,
-         birth_date
+         cw.birth_date,
          cw.primary_race_ethnicity_reporting,
          cw.primary_race_ethnicity_reporting,
          cw.gender,
