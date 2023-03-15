d5de978bd64496d7fb1242329af453bb585e8880
Cristina Baldor <ccbaldor@icloud.com>
2023-03-15 16:20:13 -0400

    Update gsheets_comp_events.sql
    removing KTAF, adding regional MDOs

--- gabby/extracts/view/gsheets_comp_events.sql
+++ gabby/extracts/view/gsheets_comp_events.sql
@@ -4,7 +4,15 @@ WITH
   approval_pivot AS (
     SELECT
       primary_site,
+      campus_name,
       legal_entity_name,
+      CASE
+        WHEN legal_entity_name IN (
+          'TEAM Academy Charter School',
+          'KIPP Cooper Norcross Academy'
+        ) THEN 'KIPP NJ'
+        ELSE legal_entity_name
+      END AS region,
       [School Leader] AS school_leader,
       COALESCE(
         [Director School Operations],
@@ -13,12 +21,14 @@ WITH
     FROM
       (
         SELECT
-          primary_site,
-          legal_entity_name,
-          primary_job,
-          df_employee_number
+          x.primary_site,
+          x.legal_entity_name,
+          x.primary_job,
+          x.df_employee_number,
+          c.campus_name
         FROM
-          people.staff_crosswalk_static
+          people.staff_crosswalk_static x
+          LEFT JOIN people.campus_crosswalk c ON x.primary_site = c.site_name
         WHERE
           status != 'TERMINATED'
           AND primary_site NOT IN (
@@ -34,12 +44,47 @@ WITH
         )
       ) AS p
   ),
+  ops_pivot AS (
+    SELECT
+      CASE
+        WHEN region IN (
+          'TEAM Academy Charter School',
+          'KIPP Cooper Norcross Academy'
+        ) THEN 'KIPP NJ'
+        ELSE region
+      END AS region,
+      COALESCE(
+        [Managing Director of Operations],
+        [Managing Director of Growth]
+      ) AS mdo
+    FROM
+      (
+        SELECT
+          x.primary_job,
+          x.df_employee_number,
+          s.region
+        FROM
+          people.staff_crosswalk_static AS x
+          LEFT JOIN people.school_crosswalk AS s ON (
+            x.primary_site = s.site_name_clean
+          )
+        WHERE
+          status != 'TERMINATED'
+      ) AS sub PIVOT (
+        MAX(df_employee_number) FOR primary_job IN (
+          [Managing Director of Operations],
+          [Managing Director of Growth]
+        )
+      ) AS p
+  ),
   school_approval_loops AS (
     SELECT
       l.primary_site,
       l.legal_entity_name,
       l.school_leader,
       l.dso_dco,
+      COALESCE(l.campus_name, l.primary_site) AS site_campus,
+      o.region,
       a.userprincipalname AS sl_email,
       a.google_email AS sl_google,
       b.df_employee_number AS hos_ed,
@@ -57,12 +102,13 @@ WITH
       e.userprincipalname AS mdso_email,
       e.google_email AS mdso_google,
       f.df_employee_number AS coo,
-      f.preferred_name AS coo_name,
-      f.userprincipalname AS coo_email,
-      f.google_email AS coo_google
+      f.preferred_name AS mdo_name,
+      f.userprincipalname AS mdo_email,
+      f.google_email AS mdo_google
     FROM
       approval_pivot AS l
       /*School Leaders*/
+      LEFT JOIN ops_pivot AS o ON (l.region = o.region)
       LEFT JOIN people.staff_crosswalk_static AS a ON (
         l.school_leader = a.df_employee_number
       )
@@ -80,10 +126,10 @@ WITH
       LEFT JOIN people.staff_crosswalk_static AS e ON (
         d.manager_df_employee_number = e.df_employee_number
       )
-      /*MDSO Managers (COOs)*/
-      LEFT JOIN people.staff_crosswalk_static AS f ON (
-        e.manager_df_employee_number = f.df_employee_number
-      )
+      /*MDOs*/
+      LEFT JOIN people.staff_crosswalk_static AS f ON (o.mdo = f.df_employee_number)
+    WHERE
+      o.mdo IS NOT NULL
   )
 SELECT
   x.df_employee_number,
@@ -97,8 +143,8 @@ SELECT
   x.userprincipalname,
   x.google_email,
   x.status,
-  s.region,
-  COALESCE(c.campus_name, x.primary_site) AS site_campus,
+  l.site_campus,
+  l.dso_email AS notify,
   CASE
     WHEN x.primary_job IN ('School Leader', 'DSO') THEN l.hos_ed_email
     WHEN x.primary_on_site_department != 'Operations' THEN l.sl_email
@@ -112,18 +158,20 @@ SELECT
   CASE
     WHEN x.primary_job IN ('School Leader', 'DSO') THEN l.ed_email
     WHEN x.primary_on_site_department != 'Operations' THEN l.hos_ed_email
-    WHEN x.primary_on_site_department = 'Operations' THEN l.coo_email
+    WHEN x.primary_on_site_department = 'Operations' THEN l.mdo_email
   END AS second_approver_email,
   CASE
     WHEN x.primary_job IN ('School Leader', 'DSO') THEN l.ed_google
     WHEN x.primary_on_site_department != 'Operations' THEN l.hos_ed_google
-    WHEN x.primary_on_site_department = 'Operations' THEN l.coo_google
-  END AS second_approver_google,
-  l.dso_email AS notify
+    WHEN x.primary_on_site_department = 'Operations' THEN l.mdo_google
+  END AS second_approver_google
 FROM
   people.staff_crosswalk_static AS x
   LEFT JOIN school_approval_loops AS l ON (x.primary_site = l.primary_site)
-  LEFT JOIN people.school_crosswalk AS s ON (x.primary_site = s.site_name)
-  LEFT JOIN people.campus_crosswalk AS c ON (x.primary_site = c.site_name)
 WHERE
   x.status != 'TERMINATED'
+  AND x.primary_site NOT IN (
+    'Room 11 - 1951 NW 7th Ave',
+    'Room 10 - 121 Market St',
+    'Room 9 - 60 Park Pl'
+  )
