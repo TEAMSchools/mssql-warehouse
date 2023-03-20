faad8f45750a879954693a5865379d21f1ca6053
Anthony Walters <anthony@u.northwestern.edu>
2023-03-20 17:56:15 -0400

    trunk fix

--- gabby/extracts/view/gsheets_collab_roster.sql
+++ gabby/extracts/view/gsheets_collab_roster.sql
@@ -1,37 +1,44 @@
-CREATE
-OR ALTER VIEW extracts.gsheets_collab_roster AS 
+CREATE OR ALTER VIEW
+  extracts.gsheets_collab_roster AS
 SELECT
-    c.id AS [Salesforce ID],
-    c.school_specific_id_c AS [SIS ID],
-    u.name AS [College Counselor],
-    c.first_name AS [First Name],
-    c.last_name AS [Last Name],
-    c.currently_enrolled_school_c AS [Current Enrolled High School],
-    c.mobile_phone AS [Student Cell Phone],
-    CASE
-        WHEN co.enroll_status = 0 THEN COALESCE(c.email, co.student_web_id + '@teamstudents.org')
-        ELSE c.email
-    END AS [Personal Email Address],
-    sc.contact_1_name AS [Primary Parent Name],
-    sc.contact_1_phone_mobile AS [Primary Parent Cell Phone],
-    sc.contact_1_phone_primary AS [Primary Phone],
-    sc.contact_1_email_current AS [Primary Parent Email],
-    co.street + ' ' + co.city + ', ' + co.[state] + ' ' + co.zip AS [Mailing Address],
-    CASE
-        WHEN c.most_recent_iep_date_c IS NOT NULL THEN 1
-    END AS [IEP],
-    co.c_504_status AS [504 Plan],
-    c.df_has_fafsa_c AS [FAFSA Complete],
-    CASE
-        WHEN c.latest_state_financial_aid_app_date_c IS NOT NULL THEN 'Yes'
-        ELSE 'No'
-    END AS [HESAA Complete],
-    c.efc_from_fafsa_c AS [EFC Actual]
+  c.id AS [Salesforce ID],
+  c.school_specific_id_c AS [SIS ID],
+  u.name AS [College Counselor],
+  c.first_name AS [First Name],
+  c.last_name AS [Last Name],
+  c.currently_enrolled_school_c AS [Current Enrolled High School],
+  c.mobile_phone AS [Student Cell Phone],
+  CASE
+    WHEN co.enroll_status = 0 THEN COALESCE(
+      c.email,
+      co.student_web_id + '@teamstudents.org'
+    )
+    ELSE c.email
+  END AS [Personal Email Address],
+  sc.contact_1_name AS [Primary Parent Name],
+  sc.contact_1_phone_mobile AS [Primary Parent Cell Phone],
+  sc.contact_1_phone_primary AS [Primary Phone],
+  sc.contact_1_email_current AS [Primary Parent Email],
+  co.street + ' ' + co.city + ', ' + co.[state] + ' ' + co.zip AS [Mailing Address],
+  CASE
+    WHEN c.most_recent_iep_date_c IS NOT NULL THEN 1
+  END AS [IEP],
+  co.c_504_status AS [504 Plan],
+  c.df_has_fafsa_c AS [FAFSA Complete],
+  CASE
+    WHEN c.latest_state_financial_aid_app_date_c IS NOT NULL THEN 'Yes'
+    ELSE 'No'
+  END AS [HESAA Complete],
+  c.efc_from_fafsa_c AS [EFC Actual]
 FROM
-    gabby.alumni.contact AS c
-    LEFT JOIN gabby.alumni.[user] AS u ON c.owner_id = u.id
-    LEFT JOIN gabby.powerschool.student_contacts_wide_static AS sc ON CAST(sc.student_number AS nvarchar) = CAST(c.school_specific_id_c AS nvarchar)
-    LEFT JOIN gabby.powerschool.cohort_identifiers_static AS co ON CAST(co.student_number AS nvarchar) = CAST(c.school_specific_id_c AS nvarchar)
-    AND co.rn_undergrad = 1
+  gabby.alumni.contact AS c
+  LEFT JOIN gabby.alumni.[user] AS u ON c.owner_id = u.id
+  LEFT JOIN gabby.powerschool.student_contacts_wide_static AS sc ON CAST(sc.student_number AS NVARCHAR) = CAST(
+    c.school_specific_id_c AS NVARCHAR
+  )
+  LEFT JOIN gabby.powerschool.cohort_identifiers_static AS co ON CAST(co.student_number AS NVARCHAR) = CAST(
+    c.school_specific_id_c AS NVARCHAR
+  )
+  AND co.rn_undergrad = 1
 WHERE
-    c.kipp_hs_class_c = gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1
+  c.kipp_hs_class_c = gabby.utilities.GLOBAL_ACADEMIC_YEAR () + 1
--- gabby/extracts/view/gsheets_collab_transition.sql
+++ gabby/extracts/view/gsheets_collab_transition.sql
@@ -1,40 +1,41 @@
-CREATE
-OR ALTER VIEW extracts.gsheets_collab_transition AS 
+CREATE OR ALTER VIEW
+  extracts.gsheets_collab_transition AS
 SELECT
-    sub.sf_contact_id,
-    sub.last_name,
-    sub.first_name,
-    sub.application_id,
-    sub.application_name,
-    sub.matriculation_decision,
-    sub.[CCDM Complete]
+  sub.sf_contact_id,
+  sub.last_name,
+  sub.first_name,
+  sub.application_id,
+  sub.application_name,
+  sub.matriculation_decision,
+  sub.[CCDM Complete]
 FROM
-    (
-        SELECT
-            kt.sf_contact_id,
-            kt.last_name,
-            kt.first_name,
-            ap.application_id,
-            ap.application_name,
-            ap.matriculation_decision,
-            CASE
-                WHEN cn.subject_c IS NULL THEN 'No'
-                ELSE 'Yes'
-            END AS 'CCDM Complete',
-            COUNT(*) OVER(
-                PARTITION BY ap.sf_contact_id
-                ORDER BY
-                    ap.application_id ASC
-            ) AS row_count
-        FROM
-            gabby.alumni.ktc_roster AS kt
-            LEFT JOIN gabby.alumni.application_identifiers AS ap ON kt.sf_contact_id = ap.sf_contact_id
-            LEFT JOIN gabby.alumni.contact_note_c AS cn ON kt.sf_contact_id = cn.contact_c
-            AND cn.subject_c = 'CCDM'
-            AND cn.is_deleted = 0
-        WHERE
-            kt.ktc_cohort = gabby.utilities.GLOBAL_ACADEMIC_YEAR() + 1
-            AND ap.matriculation_decision = 'Matriculated (Intent to Enroll)'
-    ) AS sub
+  (
+    SELECT
+      kt.sf_contact_id,
+      kt.last_name,
+      kt.first_name,
+      ap.application_id,
+      ap.application_name,
+      ap.matriculation_decision,
+      CASE
+        WHEN cn.subject_c IS NULL THEN 'No'
+        ELSE 'Yes'
+      END AS 'CCDM Complete',
+      COUNT(*) OVER (
+        PARTITION BY
+          ap.sf_contact_id
+        ORDER BY
+          ap.application_id ASC
+      ) AS row_count
+    FROM
+      gabby.alumni.ktc_roster AS kt
+      LEFT JOIN gabby.alumni.application_identifiers AS ap ON kt.sf_contact_id = ap.sf_contact_id
+      LEFT JOIN gabby.alumni.contact_note_c AS cn ON kt.sf_contact_id = cn.contact_c
+      AND cn.subject_c = 'CCDM'
+      AND cn.is_deleted = 0
+    WHERE
+      kt.ktc_cohort = gabby.utilities.GLOBAL_ACADEMIC_YEAR () + 1
+      AND ap.matriculation_decision = 'Matriculated (Intent to Enroll)'
+  ) AS sub
 WHERE
-    sub.row_count = 1
+  sub.row_count = 1
