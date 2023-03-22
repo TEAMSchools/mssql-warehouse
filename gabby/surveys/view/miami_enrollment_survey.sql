5f30d21f0e42986eadc0ee739ad779cc0b58f4f5
Cristina Baldor <ccbaldor@icloud.com>
2023-03-22 15:43:17 -0400

    Update miami_enrollment_survey.sql

--- gabby/surveys/view/miami_enrollment_survey.sql
+++ gabby/surveys/view/miami_enrollment_survey.sql
@@ -42,25 +42,48 @@ WITH survey_pivot AS (
         ) p
 )
 SELECT
-    region,
-    miami_plan,
-    miami_not_returning,
-    miami_first,
-    miami_middle,
-    miami_last,
-    miami_phone,
-    miami_email,
-    miami_address,
-    miami_bus,
-    miami_bus_stop,
-    miami_transport_consent,
-    miami_shirt,
-    student_number,
-    survey_id,
-    survey_response_id,
-    date_submitted
+    s.region,
+    s.miami_plan,
+    s.miami_not_returning,
+    s.miami_first,
+    s.miami_middle,
+    s.miami_last,
+    s.miami_phone,
+    s.miami_email,
+    s.miami_address,
+    s.miami_bus,
+    s.miami_bus_stop,
+    s.miami_transport_consent,
+    s.miami_shirt,
+    s.student_number,
+    p.student_number,
+    s.survey_id,
+    s.survey_response_id,
+    s.date_submitted,
+    
+    p.first_name AS db_first,
+    p.middle_name AS db_middle,
+    p.last_name AS db_last,
+    p.street AS db_street,
+    p.city AS db_city,
+    p.zip AS db_zip,
+    
+    p.cohort,
+    p.grade_level,
+    p.school_name,
+    p.school_level,
+    p.iep_status,
+    p.gender,
+    p.ethnicity
+    
+    
 FROM
-    survey_pivot
+    survey_pivot s
 /*JOIN to student data for school, cohort, IEP, gender, race, other*/
+LEFT JOIN powerschool.cohort_identifiers_static p
+ON s.student_number = p.student_number
 WHERE
-    region = 'KMS'
+    s.region = 'KMS'
+ AND p.enroll_status = 0
+ AND p.rn_year = 1
+ AND p.academic_year = gabby.utilities.GLOBAL_ACADEMIC_YEAR()
