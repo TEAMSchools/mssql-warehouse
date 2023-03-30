fbabb0ead1293a4f265d6d0c3b2a482851f01f99
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-30 10:58:11 -0500

    dbt cleanup

--- dbt/gabby/models/illuminate/dna_assessments/illuminate_dna_assessments__performance_bands.sql
+++ dbt/gabby/models/illuminate/dna_assessments/illuminate_dna_assessments__performance_bands.sql
@@ -1,11 +1,11 @@
-{{
+{# https://docs.getdbt.com/reference/resource-configs/mssql-configs#indices #}
+{{-
     config(
         materialized="table",
         schema="illuminate_dna_assessments",
         alias="performance_bands",
-        {# https://docs.getdbt.com/reference/resource-configs/mssql-configs#indices #}
     )
-}}
+-}}
 
 select *
 from {{ source("illuminate_dna_assessments", "performance_bands") }}
