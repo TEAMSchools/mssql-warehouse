a171ead3ed77d54b0958a8482ffd5e0e31ec99a4
Cristina Baldor <ccbaldor@icloud.com>
2023-04-11 16:16:37 -0400

    adding manager contact info for app

--- gabby/extracts/view/gsheets_pm_assignment_roster.sql
+++ gabby/extracts/view/gsheets_pm_assignment_roster.sql
@@ -32,6 +32,8 @@ SELECT
   s.manager_name,
   x.region,
   COALESCE(c.campus_name, s.primary_site) AS site_campus,
+  m.userprincipalname AS manager_email,
+  m.google_email AS manager_google,
   /*default TNTP assignments based on title/location*/
   CASE
     WHEN s.primary_site IN (
@@ -102,7 +104,10 @@ SELECT
     ) THEN 'school-based'
   END AS school_based
 FROM
-  people.staff_crosswalk_static AS s
+  people.staff_crosswalk_static AS m
+  LEFT JOIN people.staff_crosswalk_static AS s ON (
+    m.df_employee_number = s.manager_df_employee_number
+  )
   LEFT JOIN elementary_grade AS e ON (
     s.df_employee_number = e.employee_number
   )
@@ -112,3 +117,5 @@ WHERE
   s.[status] != 'TERMINATED'
   AND s.primary_job != 'Intern'
   AND s.primary_job NOT LIKE '%Temp%'
+ORDER BY
+  s.df_employee_number
